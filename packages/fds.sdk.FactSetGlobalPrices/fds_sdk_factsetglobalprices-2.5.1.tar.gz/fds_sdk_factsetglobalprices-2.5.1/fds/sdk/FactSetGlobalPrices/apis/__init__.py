7355b6ad731464839b8298b759c44bc4cfdb9be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:15:50 +0000

    Add package fds.sdk.FactSetGlobalPrices/2.5.1/fds_sdk_factsetglobalprices-2.5.1.tar.gz

---
+++ packages/fds.sdk.FactSetGlobalPrices/fds_sdk_factsetglobalprices-2.5.1.tar.gz/fds_sdk_factsetglobalprices-2.5.1/fds/sdk/FactSetGlobalPrices/apis/__init__.py
@@ -0,0 +1,23 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.batch_processing_api import BatchProcessingApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from fds.sdk.FactSetGlobalPrices.api.batch_processing_api import BatchProcessingApi
+from fds.sdk.FactSetGlobalPrices.api.corporate_actions_api import CorporateActionsApi
+from fds.sdk.FactSetGlobalPrices.api.corporate_actions_for_calendar_api import CorporateActionsForCalendarApi
+from fds.sdk.FactSetGlobalPrices.api.market_value_api import MarketValueApi
+from fds.sdk.FactSetGlobalPrices.api.prices_api import PricesApi
+from fds.sdk.FactSetGlobalPrices.api.returns_api import ReturnsApi
+from fds.sdk.FactSetGlobalPrices.api.shares_outstanding_api import SharesOutstandingApi
---
+++ packages/fds.sdk.FactSetGlobalPrices/fds_sdk_factsetglobalprices-2.5.1.tar.gz/fds_sdk_factsetglobalprices-2.5.1/fds/sdk/FactSetGlobalPrices/models/__init__.py
@@ -0,0 +1,76 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from fds.sdk.FactSetGlobalPrices.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from fds.sdk.FactSetGlobalPrices.model.adjust import Adjust
+from fds.sdk.FactSetGlobalPrices.model.annualized_dividend_response import AnnualizedDividendResponse
+from fds.sdk.FactSetGlobalPrices.model.annualized_dividends_object import AnnualizedDividendsObject
+from fds.sdk.FactSetGlobalPrices.model.annualized_dividends_request import AnnualizedDividendsRequest
+from fds.sdk.FactSetGlobalPrices.model.batch import Batch
+from fds.sdk.FactSetGlobalPrices.model.batch_error_object import BatchErrorObject
+from fds.sdk.FactSetGlobalPrices.model.batch_error_object_links import BatchErrorObjectLinks
+from fds.sdk.FactSetGlobalPrices.model.batch_error_response import BatchErrorResponse
+from fds.sdk.FactSetGlobalPrices.model.batch_result import BatchResult
+from fds.sdk.FactSetGlobalPrices.model.batch_result_response import BatchResultResponse
+from fds.sdk.FactSetGlobalPrices.model.batch_status import BatchStatus
+from fds.sdk.FactSetGlobalPrices.model.batch_status_response import BatchStatusResponse
+from fds.sdk.FactSetGlobalPrices.model.calendar import Calendar
+from fds.sdk.FactSetGlobalPrices.model.cancelled_dividend import CancelledDividend
+from fds.sdk.FactSetGlobalPrices.model.corporate_action import CorporateAction
+from fds.sdk.FactSetGlobalPrices.model.corporate_action_calendar import CorporateActionCalendar
+from fds.sdk.FactSetGlobalPrices.model.corporate_actions_request import CorporateActionsRequest
+from fds.sdk.FactSetGlobalPrices.model.corporate_actions_response import CorporateActionsResponse
+from fds.sdk.FactSetGlobalPrices.model.corporate_actions_response_calendar import CorporateActionsResponseCalendar
+from fds.sdk.FactSetGlobalPrices.model.dividend_adjust import DividendAdjust
+from fds.sdk.FactSetGlobalPrices.model.dividend_calendar import DividendCalendar
+from fds.sdk.FactSetGlobalPrices.model.dividends_response import DividendsResponse
+from fds.sdk.FactSetGlobalPrices.model.error_object import ErrorObject
+from fds.sdk.FactSetGlobalPrices.model.error_object_response import ErrorObjectResponse
+from fds.sdk.FactSetGlobalPrices.model.error_response import ErrorResponse
+from fds.sdk.FactSetGlobalPrices.model.error_response_calendar import ErrorResponseCalendar
+from fds.sdk.FactSetGlobalPrices.model.error_response_sub_errors import ErrorResponseSubErrors
+from fds.sdk.FactSetGlobalPrices.model.event_category import EventCategory
+from fds.sdk.FactSetGlobalPrices.model.event_count import EventCount
+from fds.sdk.FactSetGlobalPrices.model.event_count_response import EventCountResponse
+from fds.sdk.FactSetGlobalPrices.model.exchange import Exchange
+from fds.sdk.FactSetGlobalPrices.model.exchanges_response import ExchangesResponse
+from fds.sdk.FactSetGlobalPrices.model.fields_corporate_actions import FieldsCorporateActions
+from fds.sdk.FactSetGlobalPrices.model.frequency import Frequency
+from fds.sdk.FactSetGlobalPrices.model.global_prices_request import GlobalPricesRequest
+from fds.sdk.FactSetGlobalPrices.model.global_prices_response import GlobalPricesResponse
+from fds.sdk.FactSetGlobalPrices.model.ids_batch_max2000 import IdsBatchMax2000
+from fds.sdk.FactSetGlobalPrices.model.ids_max50 import IdsMax50
+from fds.sdk.FactSetGlobalPrices.model.market_value_request import MarketValueRequest
+from fds.sdk.FactSetGlobalPrices.model.market_value_request_body import MarketValueRequestBody
+from fds.sdk.FactSetGlobalPrices.model.market_value_response import MarketValueResponse
+from fds.sdk.FactSetGlobalPrices.model.market_value_response_object import MarketValueResponseObject
+from fds.sdk.FactSetGlobalPrices.model.meta import Meta
+from fds.sdk.FactSetGlobalPrices.model.pagination import Pagination
+from fds.sdk.FactSetGlobalPrices.model.precision import Precision
+from fds.sdk.FactSetGlobalPrices.model.price import Price
+from fds.sdk.FactSetGlobalPrices.model.prices_fields import PricesFields
+from fds.sdk.FactSetGlobalPrices.model.returns import Returns
+from fds.sdk.FactSetGlobalPrices.model.returns_request import ReturnsRequest
+from fds.sdk.FactSetGlobalPrices.model.returns_response import ReturnsResponse
+from fds.sdk.FactSetGlobalPrices.model.rights_issue import RightsIssue
+from fds.sdk.FactSetGlobalPrices.model.rights_issues_response import RightsIssuesResponse
+from fds.sdk.FactSetGlobalPrices.model.shares_outstanding_request import SharesOutstandingRequest
+from fds.sdk.FactSetGlobalPrices.model.shares_outstanding_request_body import SharesOutstandingRequestBody
+from fds.sdk.FactSetGlobalPrices.model.shares_outstanding_response import SharesOutstandingResponse
+from fds.sdk.FactSetGlobalPrices.model.shares_outstanding_response_object import SharesOutstandingResponseObject
+from fds.sdk.FactSetGlobalPrices.model.so_error_object import SoErrorObject
+from fds.sdk.FactSetGlobalPrices.model.so_error_object_links import SoErrorObjectLinks
+from fds.sdk.FactSetGlobalPrices.model.so_error_response import SoErrorResponse
+from fds.sdk.FactSetGlobalPrices.model.spin_off import SpinOff
+from fds.sdk.FactSetGlobalPrices.model.spin_offs_response import SpinOffsResponse
+from fds.sdk.FactSetGlobalPrices.model.split_calendar import SplitCalendar
+from fds.sdk.FactSetGlobalPrices.model.splits_response_calendar import SplitsResponseCalendar
+from fds.sdk.FactSetGlobalPrices.model.stock_distribution import StockDistribution
+from fds.sdk.FactSetGlobalPrices.model.stock_distributions_response_calendar import StockDistributionsResponseCalendar
