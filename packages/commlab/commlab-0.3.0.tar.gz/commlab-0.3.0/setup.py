ab2b5a8fa1e3502c83db1196b1b7efbbb014984a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:08:59 +0000

    Add package commlab/0.3.0/commlab-0.3.0.tar.gz

---
+++ packages/commlab/commlab-0.3.0.tar.gz/commlab-0.3.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='commlab',
+    version='0.3.0',
+    description='Communication Lab utilities for signal processing and noise addition',
+    author='Padmapriya C J',
+    author_email='padmapriyacj06@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.20.0',
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+
+long_description= description,
+long_description_content_type="text/markdown",
+)
