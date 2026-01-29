205a53b094ab10dd72488b2b45e4e1af75c978d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:12:58 +0000

    Add package driveup/0.9.7/driveup-0.9.7.tar.gz

---
+++ packages/driveup/driveup-0.9.7.tar.gz/driveup-0.9.7/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    README_DESCRIPTION = fh.read()
+
+setup(
+    name='driveup',
+    version='0.9.7',
+    author='Raúl M.R.',
+    author_email="raul.martin4bc@gmail.com",
+    url="https://github.com/raul-martin-dev/Driveup",
+    license="MIT",
+    description='Python package for uploading files and folders to Google Drive.',
+    long_description=README_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    
+    packages=find_packages(include=["Driveup", "Driveup.*"]),
+    
+    install_requires=[
+        'google-api-python-client>=2.0.0',
+        'pandas>=1.0.0',
+        # 'google-auth-httplib2>=0.1.0',
+        'google-auth-oauthlib>=0.8.0',
+    ],
+
+    extras_require={
+        'airflow': [
+            'apache-airflow>=2.4.0,<3.0.0', 
+        ]
+    },
+
+    entry_points={
+        "apache_airflow_provider": [
+            "provider_info=driveup.providers.airflow.get_provider_info:get_provider_info"
+        ]
+    },
+
+    python_requires='>3.7',
+)
