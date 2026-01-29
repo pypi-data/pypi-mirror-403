456de3b50f6b58b82391c72e0b9ccd83ca28b173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:06:46 +0000

    Add package lazyssh/1.6.0/lazyssh-1.6.0-py3-none-any.whl

---
+++ packages/lazyssh/lazyssh-1.6.0-py3-none-any.whl/lazyssh/plugins/__init__.py
@@ -0,0 +1,8 @@
+"""LazySSH Plugins Package
+
+This package contains built-in and user-defined plugins for LazySSH.
+Plugins can be Python scripts (.py) or shell scripts (.sh) that execute
+commands on remote SSH connections.
+"""
+
+__version__ = "1.0.0"
