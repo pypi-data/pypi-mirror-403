c2f99ad19ed87e93e88d40d1dc7365618f516ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:12:58 +0000

    Add package a4agro-vegetation-index-v2/1.0.0/a4agro_vegetation_index_v2-1.0.0-py3-none-any.whl

---
+++ packages/a4agro-vegetation-index-v2/a4agro_vegetation_index_v2-1.0.0-py3-none-any.whl/a4agro_vegetation_index_calculation/__init__.py
@@ -0,0 +1,9 @@
+from .colorsmaps import (
+    create_colormap,
+    create_cgi_colormap,
+)
+from .index_calculation import IndexCalculation
+from .plot_generation import (
+    generate_figure_save_and_show,
+    generate_figure_save_and_show_5_normalized,
+)
