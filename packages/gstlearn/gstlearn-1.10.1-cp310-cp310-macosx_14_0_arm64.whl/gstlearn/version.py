1f4cbdd79b217124b747d2be2df281f56ed32aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:12:21 +0000

    Add package gstlearn/1.10.1/gstlearn-1.10.1-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/gstlearn/gstlearn-1.10.1-cp310-cp310-macosx_14_0_arm64.whl/gstlearn/version.py
@@ -0,0 +1,13 @@
+################################################################################
+#                                                                              #
+#                         gstlearn Python package                              #
+#                                                                              #
+# Copyright (c) (2023) MINES Paris / ARMINES                                   #
+# Authors: gstlearn Team                                                       #
+# Website: https://gstlearn.org                                                #
+# License: BSD 3-clause                                                        #
+#                                                                              #
+################################################################################
+
+__version__ = "1.10.1"
+__author__ = "Team gstlearn"
