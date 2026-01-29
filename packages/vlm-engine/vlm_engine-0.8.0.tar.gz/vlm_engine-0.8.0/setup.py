1cf09aef60759194b25588d280bc2975f9679232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:13:17 +0000

    Add package vlm-engine/0.8.0/vlm_engine-0.8.0.tar.gz

---
+++ packages/vlm-engine/vlm_engine-0.8.0.tar.gz/vlm_engine-0.8.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vlm_engine",
+    version="0.8.0",
+    description="Advanced Vision-Language Model Engine for content tagging",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="HAVEN Network",
+    author_email="officialhavennetwork@gmail.com",
+    url="https://github.com/Haven-hvn/haven-vlm-engine-package",
+    packages=find_packages(),
+    install_requires=[
+        "pydantic",
+        "numpy",
+        "torch",
+        "torchvision",
+        "aiohttp",
+        "pyyaml",
+        "opencv-python",
+        "decord",
+        "requests",
+        "multiplexer-llm==0.2.3"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
+)
