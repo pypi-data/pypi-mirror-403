67b49d9b62e297638f93efd12672a619d80b3b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:15:39 +0000

    Add package qhash-python/0.1.16/qhash_python-0.1.16-py3-none-any.whl

---
+++ packages/qhash-python/qhash_python-0.1.16-py3-none-any.whl/qhash/clients/call.py
@@ -0,0 +1,34 @@
+from typing import Union
+from qhash.clients.protos.talk_api_pb2 import (
+    CreateBulkPhoneCallRequest,
+    CreateBulkPhoneCallResponse,
+    CreatePhoneCallRequest,
+    CreatePhoneCallResponse,
+)
+from qhash.connections import ConnectionConfig, UserAuthInfo, ClientAuthInfo
+
+
+def create_phone_call(
+    client_cfg: ConnectionConfig,
+    request: CreatePhoneCallRequest,
+    auth: Union[UserAuthInfo, ClientAuthInfo, None] = None,
+) -> CreatePhoneCallResponse:
+    if auth is None:
+        auth = client_cfg.auth
+    return client_cfg.conversation_client.CreatePhoneCall(
+        request,
+        metadata=auth,
+    )
+
+
+def create_bulk_phone_call(
+    client_cfg: ConnectionConfig,
+    request: CreateBulkPhoneCallRequest,
+    auth: Union[UserAuthInfo, ClientAuthInfo, None] = None,
+) -> CreateBulkPhoneCallResponse:
+    if auth is None:
+        auth = client_cfg.auth
+    return client_cfg.conversation_client.CreateBulkPhoneCall(
+        request,
+        metadata=auth,
+    )
---
+++ packages/qhash-python/qhash_python-0.1.16-py3-none-any.whl/qhash/configs/__init__.py
@@ -0,0 +1,37 @@
+"""
+Copyright (c) 2024. Qhash
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Author: Bapan <sbapan41@gmail.com>
+
+This module provides functions for managing projects through the ProjectService.
+"""
+
+ASSISTANT_API = "workflow-01.qhash.ai"
+WEB_API = "api.qhash.ai"
+ENDPOINT_API = "endpoint-01.qhash.ai"
+
+LOCAL_ASSISTANT_API = "localhost:9007"
+LOCAL_WEB_API = "localhost:9001"
+LOCAL_ENDPOINT_API = "localhost:9005"
+
+GRPC_ENDPOINT_URL = "endpoint-01.qhash.ai"
+GRPC_ASSISTANT_URL = "workflow-01.qhash.ai"
+GRPC_GATEWAY_URL = "gateway-01.qhash.ai"
