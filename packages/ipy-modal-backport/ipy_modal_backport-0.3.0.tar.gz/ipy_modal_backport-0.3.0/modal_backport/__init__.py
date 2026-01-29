6e8155d1c7927155c0fa7f1925af7ea01b746825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:06:40 +0000

    Add package ipy-modal-backport/0.3.0/ipy_modal_backport-0.3.0.tar.gz

---
+++ packages/ipy-modal-backport/ipy_modal_backport-0.3.0.tar.gz/ipy_modal_backport-0.3.0/modal_backport/__init__.py
@@ -0,0 +1,34 @@
+"""
+ipy-modal-phase-backport
+
+A project to backport new modal features into interactions.py v5.
+:copyright: (c) 2026-present AstreaTSS
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.3.0"
+
+from .components import *
+from .context import *
+from .enums import *
+from .modal import *
+
+__all__ = (
+    "BaseSelectMenu",
+    "ChannelSelectMenu",
+    "ComponentType",
+    "DefaultableSelectMenu",
+    "FileUploadComponent",
+    "InputText",
+    "LabelComponent",
+    "MentionableSelectMenu",
+    "Modal",
+    "ModalContext",
+    "ParagraphText",
+    "RoleSelectMenu",
+    "ShortText",
+    "StringSelectMenu",
+    "UpdatedModalContextMixin",
+    "UserSelectMenu",
+    "__version__",
+)
---
+++ packages/ipy-modal-backport/ipy_modal_backport-0.3.0.tar.gz/ipy_modal_backport-0.3.0/setup.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from setuptools import find_packages
+from setuptools import setup
+
+requirements = []
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="ipy-modal-backport",
+    description="A project to backport new modal features into interactions.py v5.",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="AstreaTSS",
+    url="https://github.com/Astrea-Stellarium-Labs/ipy-modal-backport",
+    version="0.3.0",
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
