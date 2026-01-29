3010564bc92ee26deff4555dd4ab85225e4c5f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:12:27 +0000

    Add package caicurator/0.6.0/caicurator-0.6.0.tar.gz

---
+++ packages/caicurator/caicurator-0.6.0.tar.gz/caicurator-0.6.0/src/caicurator/constants.py
@@ -0,0 +1,24 @@
+import configparser
+import pathlib
+from importlib.metadata import metadata
+from importlib.resources import files
+
+import platformdirs
+
+if not __package__:
+    raise ImportError("caicurator must be run as a module")
+
+PACKAGE_NAME = metadata(__package__)["Name"]
+DESCRIPTION = metadata(PACKAGE_NAME)["Summary"]
+VERSION = metadata(PACKAGE_NAME)["Version"]
+EPILOG = "🔗 Homepage: https://github.com/eeriemyxi/caicurator"
+
+CONFIG_DIR = pathlib.Path(
+    platformdirs.user_config_dir(PACKAGE_NAME, ensure_exists=True)
+)
+
+CONFIG_FILE_PATH = CONFIG_DIR / "config.ini"
+CONFIG = configparser.ConfigParser()
+CONFIG.read(CONFIG_FILE_PATH)
+
+TEMPLATE_PATH = files(f"{PACKAGE_NAME}.assets") / "template.pt"
