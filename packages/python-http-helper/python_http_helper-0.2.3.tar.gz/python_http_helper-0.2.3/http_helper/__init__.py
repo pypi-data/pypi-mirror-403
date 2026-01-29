1c57123aa4685fcc37bbc65c71ba5e8cc5e31127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:18:41 +0000

    Add package python-http-helper/0.2.3/python_http_helper-0.2.3.tar.gz

---
+++ packages/python-http-helper/python_http_helper-0.2.3.tar.gz/python_http_helper-0.2.3/http_helper/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+# ---------------------------------------------------------------------------------------------------------
+# ProjectName:  http-helper
+# FileName:     __init__.py
+# Description:  http帮助包
+# Author:       ASUS
+# CreateDate:   2025/11/24
+# Copyright ©2011-2025. Hunan xxxxxxx Company limited. All rights reserved.
+# ---------------------------------------------------------------------------------------------------------
+"""
