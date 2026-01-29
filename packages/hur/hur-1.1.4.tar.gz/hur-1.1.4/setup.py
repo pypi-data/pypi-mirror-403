acb8d17645095522558768647f9a3d2b5c1da747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:11:54 +0000

    Add package hur/1.1.4/hur-1.1.4.tar.gz

---
+++ packages/hur/hur-1.1.4.tar.gz/hur-1.1.4/setup.py
@@ -0,0 +1,40 @@
+"""
+This algorithm proposes a new paradigm in language model architecture, aiming to revolutionize natural language processing through direct weights adjustment and automatic parameter configuration.
+The Hur-Model is a development module that implements this concept, enabling the training, retraining, tuning, and inference of language models in a groundbreaking way.
+It reduces dependence on backpropagation through an initial weight adjustment using a HurNet network and accelerates model parameterization through optimization calculations and self-configuration.
+This code was architected, developed, and programmed by Ben-Hur Varriano for Sapiens Technology®️.
+Any use, modification, disclosure, or public commentary without prior authorization from Sapiens Technology®️ will be subject to legal action by our legal team.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from setuptools import setup, find_packages
+package_name = 'hur'
+version = '1.1.4'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    install_requires=[
+        'hurnet-torch',
+        'sapiens-tokenizer',
+        'sapiens-embedding',
+        'sapiens-attention',
+        'sapiens-infinite-context-window',
+        'sapiens-generalization',
+        'scn',
+        'torch',
+        'requests',
+        'ijson',
+        'tqdm'
+    ],
+    url='https://github.com/sapiens-technology/Hur-Model',
+    license='Proprietary Software'
+)
+"""
+This algorithm proposes a new paradigm in language model architecture, aiming to revolutionize natural language processing through direct weights adjustment and automatic parameter configuration.
+The Hur-Model is a development module that implements this concept, enabling the training, retraining, tuning, and inference of language models in a groundbreaking way.
+It reduces dependence on backpropagation through an initial weight adjustment using a HurNet network and accelerates model parameterization through optimization calculations and self-configuration.
+This code was architected, developed, and programmed by Ben-Hur Varriano for Sapiens Technology®️.
+Any use, modification, disclosure, or public commentary without prior authorization from Sapiens Technology®️ will be subject to legal action by our legal team.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
