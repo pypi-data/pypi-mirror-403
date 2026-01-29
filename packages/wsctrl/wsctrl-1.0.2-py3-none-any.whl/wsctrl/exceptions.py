b786b01bb94cc4d9f78264bdb6248b8179d706a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:15:08 +0000

    Add package wsctrl/1.0.2/wsctrl-1.0.2-py3-none-any.whl

---
+++ packages/wsctrl/wsctrl-1.0.2-py3-none-any.whl/wsctrl/exceptions.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025 CTHINGS.CO
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""`sink_ctrl` module exceptions."""
+
+
+class SinkCtrlException(Exception):
+    ...
+
+
+class SinkCtrlNoComms(SinkCtrlException):
+    ...
