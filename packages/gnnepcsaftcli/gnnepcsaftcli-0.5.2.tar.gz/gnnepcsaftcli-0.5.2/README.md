c4e1f8d38061e2a7d5116e867f7e2e68da4b5a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:13:27 +0000

    Add package gnnepcsaftcli/0.5.2/gnnepcsaftcli-0.5.2.tar.gz

---
+++ packages/gnnepcsaftcli/gnnepcsaftcli-0.5.2.tar.gz/gnnepcsaftcli-0.5.2/README.md
@@ -0,0 +1,45 @@
+# GNNPCSAFT CLI
+
+The GNNPCSAFT CLI is an implementation of [our project](https://github.com/wildsonbbl/gnnepcsaft/) that focuses on using Graph Neural Networks ([GNN](https://en.wikipedia.org/wiki/Graph_neural_network)) to estimate the pure-component parameters of the Equation of State [PC-SAFT](https://en.wikipedia.org/wiki/PC-SAFT). We developed this CLI so the scientific community can access the model's results easily.
+
+You can usually find an app for the project at [SourceForge](https://sourceforge.net/projects/gnnepcsaft/).
+
+The CLI can installed with [pipx](https://github.com/pypa/pipx):
+
+```bash
+pipx install gnnepcsaftcli
+```
+
+## CLI
+
+GNNPCSAFT CLI tool
+
+**Usage**:
+
+```console
+gnnepcsaftcli [OPTIONS] COMMAND [ARGS]...
+```
+
+**Options**:
+
+- `--install-completion`: Install completion for the current shell.
+- `--show-completion`: Show completion for the current shell, to copy it or customize the installation.
+- `--help`: Show this message and exit.
+
+**Commands**:
+
+- `pred`: Predict PCSAFT parameters from SMILES...
+
+## `pred`
+
+Predict PCSAFT parameters from SMILES with a GNNPCSAFT model
+
+**Usage**:
+
+```console
+pred [OPTIONS]
+```
+
+**Options**:
+
+- `--help`: Show this message and exit.
---
+++ packages/gnnepcsaftcli/gnnepcsaftcli-0.5.2.tar.gz/gnnepcsaftcli-0.5.2/gnnepcsaftcli/main.py
@@ -0,0 +1,59 @@
+"""GNNPCsAFT CLI tool"""
+
+from pathlib import Path
+
+import typer
+from gnnepcsaft_mcp_server.utils import predict_pcsaft_parameters
+from rich import print as richprint
+from rich.prompt import Prompt
+
+app = typer.Typer()
+
+file_dir = Path(__file__)
+
+
+@app.callback()
+def callback():
+    """
+    GNNPCsAFT CLI tool
+    """
+
+
+@app.command()
+def pred():
+    """
+    Predict PCSAFT parameters from SMILES with a GNNPCSAFT model
+    """
+
+    while True:
+        try:
+            smiles = Prompt.ask(
+                "\nEnter a valid[green] SMILES [/green]or [yellow]/bye[/yellow] to exit",
+            )
+            if smiles == "/bye":
+                richprint("\n[green] :rocket:  Bye! :rocket: [/green]")
+                break
+            richprint(
+                f"\n:star2::test_tube::thermometer: "
+                f"[yellow]Predicting parameters for SMILES:[/yellow] "
+                f"[green]{smiles}[/green] "
+                f":star2::test_tube::thermometer:\n"
+            )
+        except KeyboardInterrupt:
+            richprint("\n[green] :rocket:  Bye! :rocket: [/green]")
+            break
+
+        try:
+            para = [round(p, 5) for p in predict_pcsaft_parameters(smiles)]
+            msigmae = para[:3]
+            assoc = para[3:5]
+            na = para[6]
+            nb = para[7]
+            richprint("[ m sigma e ]: " + str(msigmae))
+            richprint("[ k_ab e_ab ]: " + str(assoc))
+            richprint(f"[  na, nb   ]: [  {na}, {nb}  ]")
+        except (RuntimeError, ValueError, TypeError):
+            richprint(
+                "[red]Error[/red]: [yellow]SMILES[/yellow] not valid, "
+                "please enter a valid [yellow]SMILES[/yellow]"
+            )
