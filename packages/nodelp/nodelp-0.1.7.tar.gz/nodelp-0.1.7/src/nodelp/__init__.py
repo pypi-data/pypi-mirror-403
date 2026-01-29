3c3280cd0698b3b4cc4a14af29c6a3412c1da2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:10:14 +0000

    Add package nodelp/0.1.7/nodelp-0.1.7.tar.gz

---
+++ packages/nodelp/nodelp-0.1.7.tar.gz/nodelp-0.1.7/src/nodelp/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Date          : 2025-08-28
+# Author        : Lancelot PINCET
+# GitHub        : https://github.com/LancelotPincet
+# Library       : nodeLP
+
+"""
+A library that generates various kinds of point clouds and coordinates.
+"""
+
+
+
+# %% Source import
+sources = {
+
+}
+
+
+
+# %% Hidden imports
+if False :
+
+
+
+
+# %% Lazy imports
+from corelp import getmodule
+__getattr__, __all__ = getmodule(sources)
