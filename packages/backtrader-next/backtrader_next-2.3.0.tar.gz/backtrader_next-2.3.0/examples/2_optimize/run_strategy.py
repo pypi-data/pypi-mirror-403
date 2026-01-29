01a62150517c35a35ce6b26e25fd38ffd884b2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:06:51 +0000

    Add package backtrader-next/2.3.0/backtrader_next-2.3.0.tar.gz

---
+++ packages/backtrader-next/backtrader_next-2.3.0.tar.gz/backtrader_next-2.3.0/examples/2_optimize/run_strategy.py
@@ -0,0 +1,31 @@
+import time
+import pandas as pd
+import backtrader_next as bt
+from backtrader_next.feeds import PandasData
+from strategy import SimpleSizer, SmaCross
+
+
+if __name__ == '__main__':
+
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(1_000_000.0)
+    cerebro.broker.set_shortcash(False)
+    cerebro.broker.setcommission(commission=0, margin=False)
+    cerebro.addsizer(SimpleSizer, percents=90)
+
+    df = pd.read_csv(f"MSFT_1d.csv.zip", sep=";")
+    df['Datetime'] = pd.to_datetime(df['Date'].astype(str) , format='%Y-%m-%d')
+    df.set_index('Datetime', inplace=True)
+
+    data = PandasData(dataframe=df, timeframe=bt.TimeFrame.Days, compression=1)
+    cerebro.adddata(data, name='MSFT')
+
+    cerebro.addstrategy(SmaCross, MA1=35, MA2=65) # use optimized parameters
+
+    cerebro.run()
+
+    print(cerebro.statistics)
+
+    cerebro.plot(filename="smacross.html")
+    cerebro.show_report(filename="smacross_stats.html")
+
