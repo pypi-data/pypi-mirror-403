15e04c52b5e7023dd6f7daa8b4ee883877297aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:09:59 +0000

    Add package GameBoxUtils/0.2.2/gameboxutils-0.2.2-py3-none-any.whl

---
+++ packages/GameBoxUtils/gameboxutils-0.2.2-py3-none-any.whl/GameBoxUtils/__init__.py
@@ -0,0 +1,20 @@
+"""
+GameBoxUtils is a set of helfut code for python
+"""
+
+
+__version__ = "0.2.1"
+__author__ = "Sam Fertig"
+
+#____imports____
+from ._vectors import Vector2D, Vector3D
+from ._inputs import Keys
+
+
+
+__all__ = [
+    "Vector2D",
+    "Vector3D",
+    "Keys"
+]
+
