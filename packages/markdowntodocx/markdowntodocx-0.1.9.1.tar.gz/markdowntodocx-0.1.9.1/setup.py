610df5dba0f7959256d5a2c3bfaf5533891a3192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:15:08 +0000

    Add package markdowntodocx/0.1.9.1/markdowntodocx-0.1.9.1.tar.gz

---
+++ packages/markdowntodocx/markdowntodocx-0.1.9.1.tar.gz/markdowntodocx-0.1.9.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# Read requirements from requirements.txt
+with open(HERE / "requirements.txt", "r", encoding="utf-8") as f:
+    requirements = [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+setup(
+    name='markdowntodocx',
+    packages=find_packages(include=['markdowntodocx']),
+    version='0.1.9.1',
+    url="https://github.com/fbarre96/MarkDownToDocxStyle",
+    description='Convert markdown inside Docx to docx styles',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Fabien Barre',
+    license='MIT',
+    install_requires=requirements,
+)
