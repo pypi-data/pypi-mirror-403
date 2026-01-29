876a50fbf8c546234c98ff77a7f8297373d744e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:14:29 +0000

    Add package qdk-chemistry/1.0.0/qdk_chemistry-1.0.0-cp311-cp311-macosx_26_0_arm64.whl

---
+++ packages/qdk-chemistry/qdk_chemistry-1.0.0-cp311-cp311-macosx_26_0_arm64.whl/qdk_chemistry/algorithms/time_evolution/builder/__init__.py
@@ -0,0 +1,12 @@
+"""QDK/Chemistry time evolution constructor module."""
+
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from .base import TimeEvolutionBuilderFactory
+
+__all__ = [
+    "TimeEvolutionBuilderFactory",
+]
