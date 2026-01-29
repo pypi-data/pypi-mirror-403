995ee5b35614f1794ee7c81d7ace44d288f5a401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:29 +0000

    Add package opticlp/0.1.6/opticlp-0.1.6.tar.gz

---
+++ packages/opticlp/opticlp-0.1.6.tar.gz/opticlp-0.1.6/src/opticlp/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Date          : 2025-08-28
+# Author        : Lancelot PINCET
+# GitHub        : https://github.com/LancelotPincet
+# Library       : opticLP
+
+"""
+A library that calculates simple calculations for optical alignments.
+"""
+
+
+
+# %% Source code
+sources = {
+
+}
+
+
+
+# %% Lazy imports
+from corelp import getmodule
+__getattr__, __all__ = getmodule(sources)
