d6db5d4657f256dcf72e44f537ca81e722da2e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:09:09 +0000

    Add package ez-a-sync/0.33.13/ez_a_sync-0.33.13-cp310-cp310-manylinux1_i686.manylinux_2_28_i686.manylinux_2_5_i686.whl

---
+++ packages/ez-a-sync/ez_a_sync-0.33.13-cp310-cp310-manylinux1_i686.manylinux_2_28_i686.manylinux_2_5_i686.whl/a_sync/asyncio/gather.pyx
@@ -0,0 +1,218 @@
+# cython: boundscheck=False
+"""
+This module provides an enhanced version of :func:`asyncio.gather`.
+"""
+from itertools import filterfalse
+from typing import Any, Awaitable, Callable, Mapping, overload
+
+from a_sync._typing import K, T, V
+from a_sync.asyncio cimport as_completed_mapping, cigather
+
+try:
+    from tqdm.asyncio import tqdm_asyncio
+except ImportError as e:
+
+    class tqdm_asyncio:  # type: ignore [no-redef]
+        @staticmethod
+        async def gather(*args, **kwargs):
+            raise ImportError(
+                "You must have tqdm installed in order to use this feature"
+            )
+
+
+Excluder = Callable[[T], bool]
+
+
+@overload
+async def gather(
+    awaitables: Mapping[K, Awaitable[V]],
+    bint return_exceptions = False,
+    exclude_if: Excluder[V] | None = None,
+    tqdm: bool = False,
+    **tqdm_kwargs: Any,
+) -> dict[K, V]:
+    """
+    Concurrently awaits a k:v mapping of awaitables and returns the results.
+
+    Args:
+        awaitables (Mapping[K, Awaitable[V]]): A mapping of keys to awaitable objects.
+        return_exceptions (bool, optional): If True, exceptions are returned as results instead of raising them. Defaults to False.
+        exclude_if (Optional[Excluder[V]], optional): A callable that takes a result and returns True if the result should be excluded from the final output. Defaults to None. Note: This is only applied when the input is not a mapping.
+        tqdm (bool, optional): If True, enables progress reporting using tqdm. Defaults to False.
+        **tqdm_kwargs: Additional keyword arguments for tqdm if progress reporting is enabled.
+
+    Examples:
+        Awaiting a mapping of awaitables:
+
+        >>> mapping = {'key1': thing1(), 'key2': thing2()}
+        >>> results = await gather(mapping)
+        >>> results
+        {'key1': 'result', 'key2': 123}
+
+    See Also:
+        :func:`asyncio.gather`
+    """
+
+
+@overload
+async def gather(
+    *awaitables: Awaitable[T],
+    bint return_exceptions = False,
+    exclude_if: Excluder[T] | None = None,
+    tqdm: bool = False,
+    **tqdm_kwargs: Any,
+) -> list[T]:
+    """
+    Concurrently awaits a series of awaitable objects and returns the results.
+
+    Args:
+        *awaitables (Awaitable[T]): The awaitables to await concurrently.
+        return_exceptions (bool, optional): If True, exceptions are returned as results instead of raising them. Defaults to False.
+        exclude_if (Optional[Excluder[T]], optional): A callable that takes a result and returns True if the result should be excluded from the final output. Defaults to None.
+        tqdm (bool, optional): If True, enables progress reporting using tqdm. Defaults to False.
+        **tqdm_kwargs: Additional keyword arguments for tqdm if progress reporting is enabled.
+
+    Examples:
+        Awaiting individual awaitables:
+
+        >>> results = await gather(thing1(), thing2())
+        >>> results
+        ['result', 123]
+
+    See Also:
+        :func:`asyncio.gather`
+    """
+
+
+async def gather(
+    *awaitables: Awaitable[T] | Mapping[K, Awaitable[V]],
+    bint return_exceptions = False,
+    exclude_if: Excluder[T] | None = None,
+    tqdm: bool = False,
+    **tqdm_kwargs: Any,
+) -> list[T] | dict[K, V]:
+    """
+    Concurrently awaits a list of awaitable objects or a k:v mapping of awaitables, and returns the results.
+
+    This function extends Python's :func:`asyncio.gather`, providing additional features for handling either individual
+    awaitable objects or a single mapping of awaitables.
+
+    Differences from :func:`asyncio.gather`:
+    - Uses type hints for use with static type checkers.
+    - Supports gathering either individual awaitables or a k:v mapping of awaitables.
+    - Provides progress reporting using tqdm if 'tqdm' is set to True.
+    - Allows exclusion of results based on a condition using the 'exclude_if' parameter. Note: This is only applied when the input is not a mapping.
+
+    Args:
+        *awaitables (Awaitable[T] | Mapping[K, Awaitable[V]]): The awaitables to await concurrently. It can be a list of individual awaitables or a single mapping of awaitables.
+        return_exceptions (bool, optional): If True, exceptions are returned as results instead of raising them. Defaults to False.
+        exclude_if (Optional[Excluder[T]], optional): A callable that takes a result and returns True if the result should be excluded from the final output. Defaults to None. Note: This is only applied when the input is not a mapping.
+        tqdm (bool, optional): If True, enables progress reporting using tqdm. Defaults to False.
+        **tqdm_kwargs: Additional keyword arguments for tqdm if progress reporting is enabled.
+
+    Examples:
+        Awaiting individual awaitables:
+
+        >>> results = await gather(thing1(), thing2())
+        >>> results
+        ['result', 123]
+
+        Awaiting a mapping of awaitables:
+
+        >>> mapping = {'key1': thing1(), 'key2': thing2()}
+        >>> results = await gather(mapping)
+        >>> results
+        {'key1': 'result', 'key2': 123}
+
+    See Also:
+        :func:`asyncio.gather`
+    """
+    if is_mapping := _is_mapping(awaitables):
+        results = await gather_mapping(
+            awaitables[0],
+            return_exceptions=return_exceptions,
+            exclude_if=exclude_if,
+            tqdm=tqdm,
+            **tqdm_kwargs,
+        )
+    elif tqdm:
+        if return_exceptions:
+            awaitables = map(_exc_wrap, awaitables)
+        results = await tqdm_asyncio.gather(*awaitables, **tqdm_kwargs)
+    else:
+        results = await cigather(awaitables, return_exceptions=return_exceptions)
+
+    if exclude_if and not is_mapping:
+        return list(filterfalse(exclude_if, results))
+
+    return results
+
+
+async def gather_mapping(
+    mapping: Mapping[K, Awaitable[V]],
+    bint return_exceptions = False,
+    exclude_if: Excluder[V] | None = None,
+    tqdm: bool = False,
+    **tqdm_kwargs: Any,
+) -> dict[K, V]:
+    """
+    Concurrently awaits a mapping of awaitable objects and returns a dictionary of results.
+
+    This function is designed to await a mapping of awaitable objects, where each key-value pair represents a unique awaitable. It enables concurrent execution and gathers results into a dictionary.
+
+    Args:
+        mapping (Mapping[K, Awaitable[V]]): A dictionary-like object where keys are of type K and values are awaitable objects of type V.
+        return_exceptions (bool, optional): If True, exceptions are returned as results instead of raising them. Defaults to False.
+        exclude_if (Optional[Excluder[V]], optional): A callable that takes a result and returns True if the result should be excluded from the final output. Defaults to None. Note: This is not applied when the input is a mapping.
+        tqdm (bool, optional): If True, enables progress reporting using tqdm. Defaults to False.
+        **tqdm_kwargs: Additional keyword arguments for tqdm if progress reporting is enabled.
+
+    Example:
+        The 'results' dictionary will contain the awaited results, where keys match the keys in the 'mapping' and values contain the results of the corresponding awaitables.
+
+        >>> mapping = {'task1': async_function1(), 'task2': async_function2(), 'task3': async_function3()}
+        >>> results = await gather_mapping(mapping)
+        >>> results
+        {'task1': "result", 'task2': 123, 'task3': None}
+
+    See Also:
+        :func:`asyncio.gather`
+    """
+    results = {
+        k: v
+        async for k, v in as_completed_mapping(
+            mapping=mapping,
+            timeout=0,
+            return_exceptions=return_exceptions,
+            aiter=True,
+            tqdm=tqdm,
+            tqdm_kwargs=tqdm_kwargs,
+        )
+        if exclude_if is None or not exclude_if(v)
+    }
+    # return data in same order as input mapping
+    return {k: results[k] for k in mapping}
+
+
+def cgather(*coros_or_futures: Awaitable[T], bint return_exceptions = False) -> Awaitable[list[T]]:
+    """`asyncio.gather` implemented in c"""
+    return cigather(coros_or_futures, return_exceptions=return_exceptions)
+
+
+cdef inline bint _is_mapping(tuple awaitables):
+    return len(awaitables) == 1 and isinstance(awaitables[0], Mapping)
+
+
+async def _exc_wrap(awaitable: Awaitable[T]) -> T | Exception:
+    """Wraps an awaitable to catch exceptions and return them instead of raising.
+
+    Args:
+        awaitable: The awaitable to wrap.
+
+    Returns:
+        The result of the awaitable or the exception if one is raised.
+    """
+    try:
+        return await awaitable
+    except Exception as e:
+        return e
---
+++ packages/ez-a-sync/ez_a_sync-0.33.13-cp310-cp310-manylinux1_i686.manylinux_2_28_i686.manylinux_2_5_i686.whl/a_sync/primitives/locks/event.pyx
@@ -0,0 +1,189 @@
+# cython: boundscheck=False
+"""
+This module provides an enhanced version of asyncio.Event with additional debug logging to help detect deadlocks.
+"""
+
+import sys
+from asyncio import AbstractEventLoop, Future, get_event_loop, sleep
+
+from libc.stdint cimport uint16_t
+from libc.stdlib cimport free, malloc
+from libc.string cimport strcpy
+from libc.time cimport time
+
+from weakref import ref
+
+from cpython.unicode cimport PyUnicode_CompareWithASCIIString
+
+from a_sync._typing import *
+
+from a_sync.primitives._debug cimport _DebugDaemonMixin, _LoopBoundMixin
+
+
+cdef extern from "time.h":
+    ctypedef long time_t
+
+
+async def _return_true():
+    return True
+
+
+cdef bint _loop_kwarg_deprecated = sys.version_info >= (3, 10)
+
+cdef class CythonEvent(_DebugDaemonMixin):
+    """
+    An asyncio.Event with additional debug logging to help detect deadlocks.
+
+    This event class extends asyncio.Event by adding debug logging capabilities. It logs
+    detailed information about the event state and waiters, which can be useful for
+    diagnosing and debugging potential deadlocks.
+    """
+    def __cinit__(self):
+        self._waiters = []
+        
+    def __init__(
+        self,
+        name: str = "",
+        debug_daemon_interval: int = 300,
+        *,
+        loop: AbstractEventLoop | None = None,
+    ):
+        """
+        Initializes the Event.
+
+        Args:
+            name: An optional name for the event, used in debug logs.
+            debug_daemon_interval: The interval in seconds for the debug daemon to log information.
+            loop: The event loop to use.
+        """
+        if _loop_kwarg_deprecated:
+            _LoopBoundMixin.__init__(self)
+        else:
+            _LoopBoundMixin.__init__(self, loop=loop)
+
+        # backwards compatability
+        if hasattr(self, "_loop"):
+            self._loop = self._loop or get_event_loop()
+        if debug_daemon_interval > 65535:
+            raise ValueError(f"'debug_daemon_interval' is stored as a uint16 and must be less than 65535")
+        self._debug_daemon_interval = debug_daemon_interval
+        # we need a constant to coerce to char*
+        cdef bytes encoded_name = name.encode("utf-8")
+        cdef Py_ssize_t length = len(encoded_name)
+
+        # Allocate memory for the char* and add 1 for the null character
+        self.__name = <char*>malloc(length + 1)
+        """An optional name for the counter, used in debug logs."""
+
+        if self.__name == NULL:
+            raise MemoryError("Failed to allocate memory for __name.")
+        # Copy the bytes data into the char*
+        strcpy(self.__name, encoded_name)
+
+    def __dealloc__(self):
+        # Free the memory allocated for __name
+        if self.__name is not NULL:
+            free(self.__name)
+
+    def __repr__(self) -> str:
+        cdef str label = (
+            "name={}".format(self.__name.decode("utf-8"))
+            if self.__name
+            else "object"
+        )
+        
+        cdef str status = "set" if self._value else "unset"
+        
+        if len_waiters := len(self._waiters):
+            status += ", waiters:{}".format(len_waiters)
+                
+        return "<{}.{} {} at {} [{}]>".format(
+            self.__class__.__module__, 
+            self.__class__.__name__, 
+            label,
+            hex(id(self)),
+            status,
+        )
+
+    cpdef bint is_set(self):
+        """Return True if and only if the internal flag is true."""
+        return self._value
+
+    cpdef void set(self):
+        """Set the internal flag to true. All coroutines waiting for it to
+        become true are awakened. Coroutine that call wait() once the flag is
+        true will not block at all.
+        """
+        cdef object fut
+
+        if not self._value:
+            self._value = True
+
+            for fut in self._waiters:
+                if _is_not_done(fut):
+                    fut.set_result(True)
+
+    cpdef void clear(self):
+        """Reset the internal flag to false. Subsequently, coroutines calling
+        wait() will block until set() is called to set the internal flag
+        to true again."""
+        self._value = False
+
+    cpdef object wait(self):
+        """Block until the internal flag is true.
+
+        If the internal flag is true on entry, return True
+        immediately.  Otherwise, block until another coroutine calls
+        set() to set the flag to true, then return True.
+
+        Returns:
+            True when the event is set.
+        """
+        return self.c_wait()
+    
+    cdef object c_wait(self):
+        if self._value:
+            return _return_true()
+
+        self._c_ensure_debug_daemon((),{})
+
+        cdef object fut = Future(loop=self._c_get_loop())
+        self._waiters.append(fut)
+        return self.__wait(fut)
+
+    @property
+    def _name(self) -> str:
+        return self.__name.decode("utf-8")
+    
+    async def __wait(self, fut: Future) -> Literal[True]:
+        try:
+            await fut
+            return True
+        finally:
+            self._waiters.remove(fut)
+
+    async def _debug_daemon(self) -> None:
+        """
+        Periodically logs debug information about the event state and waiters.
+
+        This code will only run if `self.logger.isEnabledFor(logging.DEBUG)` is True. You do not need to include any level checks in your custom implementations.
+        """
+        cdef time_t start, now 
+        cdef object weakself = ref(self)
+        cdef unsigned int loops = 0
+        cdef uint16_t interval = self._debug_daemon_interval
+
+        start = time(NULL)
+        while (self := weakself()) and not self._value:
+            if loops:
+                now = time(NULL)
+                self.get_logger().debug(
+                    "Waiting for %s for %sm", self, round((now - start) / 60, 2)
+                )
+            del self  # no need to hold a reference here
+            await sleep(interval)
+            loops += 1                
+
+
+cdef inline bint _is_not_done(fut: Future):
+    return PyUnicode_CompareWithASCIIString(fut._state, b"PENDING") == 0
