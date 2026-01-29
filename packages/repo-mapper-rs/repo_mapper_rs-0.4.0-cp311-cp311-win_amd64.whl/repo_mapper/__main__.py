ceb4c5997b953f884745b0b089183fed938b08af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:07:25 +0000

    Add package repo-mapper-rs/0.4.0/repo_mapper_rs-0.4.0-cp311-cp311-win_amd64.whl

---
+++ packages/repo-mapper-rs/repo_mapper_rs-0.4.0-cp311-cp311-win_amd64.whl/repo_mapper/__main__.py
@@ -0,0 +1,78 @@
+"""repo-map-desc: Main CLI entry point
+
+This file is the main python entry point for the tool.
+"""
+
+import argparse
+import os
+import sys
+import repo_mapper_py
+
+if __name__ == "__main__":
+
+    def str_to_list(inp_str: str) -> list[str]:
+        return inp_str.split(",")
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--repo-root",
+        type=os.path.abspath,
+        required=True,
+        help="Path to the root of the repo to generate the map for.",
+    )
+    parser.add_argument(
+        "--readme-path",
+        type=os.path.abspath,
+        default="./README.md",
+        help="Path to the readme file to add the map to.",
+    )
+    parser.add_argument(
+        "--gitignore-path",
+        type=os.path.abspath,
+        default="./.gitignore",
+        help="Path to the .gitignore.",
+    )
+    parser.add_argument(
+        "--allowed-exts",
+        default=["py", "md", "toml", "lock", "yaml", "ipynb"],
+        type=str_to_list,
+        help="A comma separated string of extensions to remove. E.g. 'py,rs,toml'. Defaults to: 'py,md,toml,lock,yaml,ipynb'",
+    )
+    parser.add_argument(
+        "--ignore-dirs",
+        default=[".git", ".venv", "build", "dist"],
+        type=str_to_list,
+        help="A comma separated string of directories to ignore. E.g. '.venv,target'.",
+    )
+    parser.add_argument(
+        "--output-mode",
+        choices=["shell", "readme"],
+        default="readme",
+        help="Write to readme file or print to shell",
+    )
+    parser.add_argument(
+        "--ignore-hidden",
+        action="store_true",
+        help="Flag to ignore hidden files. E.g. those that start with a '.' like '.env'.",
+    )
+    parser.add_argument(
+        "--dirs-only",
+        action="store_true",
+        help="Flag to only map directories instead of files",
+    )
+    args = parser.parse_args()
+    sys.exit(
+        int(
+            repo_mapper_py.py_main(
+                repo_root=args.repo_root,
+                readme_path=args.readme_path,
+                gitignore_path=args.gitignore_path,
+                allowed_exts=args.allowed_exts,
+                ignore_dirs=args.ignore_dirs,
+                ignore_hidden=args.ignore_hidden,
+                dirs_only=args.dirs_only,
+                output_mode=args.output_mode,
+            )
+        )
+    )
