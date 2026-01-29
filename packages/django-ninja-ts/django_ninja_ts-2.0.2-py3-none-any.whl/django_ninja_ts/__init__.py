586b94824c3799ebd17d79c16fecab300d632847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:13:49 +0000

    Add package django-ninja-ts/2.0.2/django_ninja_ts-2.0.2-py3-none-any.whl

---
+++ packages/django-ninja-ts/django_ninja_ts-2.0.2-py3-none-any.whl/django_ninja_ts/__init__.py
@@ -0,0 +1,5 @@
+"""Django Ninja TS - Auto-generate TypeScript clients for Django Ninja."""
+
+__version__ = "2.0.2"
+
+default_app_config = "django_ninja_ts.apps.NinjaTsConfig"
---
+++ packages/django-ninja-ts/django_ninja_ts-2.0.2-py3-none-any.whl/django_ninja_ts/management/__init__.py
@@ -0,0 +1,1 @@
+"""Django management commands for django_ninja_ts."""
