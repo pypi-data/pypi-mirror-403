e242d176b5e31dea9fb33d7feeb1b67561a92e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:06:48 +0000

    Add package pipescript/0.0.18/pipescript-0.0.18.tar.gz

---
+++ packages/pipescript/pipescript-0.0.18.tar.gz/pipescript-0.0.18/docs/HISTORY.rst
@@ -0,0 +1,37 @@
+History
+=======
+
+0.0.18 (2026-01-24)
+-------------------
+* Improved completions integration;
+
+0.0.14 (2026-01-19)
+-------------------
+* Bump pyccolo to include fstring improvement;
+
+0.0.11 (2026-01-18)
+-------------------
+* Implement custom dynamic macros;
+* Support numbered arguments;
+* Eliminate ipyflow dependency;
+* Add console entrypoint;
+
+0.0.10 (2026-01-09)
+-------------------
+*  Bump dependencies with range-based augmentation detection bugfix;
+
+0.0.9 (2026-01-08)
+------------------
+* Improve optional chaining call behavior and enable optional chaining for subscripts;
+
+0.0.8 (2026-01-07)
+------------------
+* Bugfixes, performance improvements, and additional macros;
+
+0.0.6 (2026-01-04)
+------------------
+* Initial full release;
+
+0.0.1 (2025-12-30)
+------------------
+* Initial placeholder release;
---
+++ packages/pipescript/pipescript-0.0.18.tar.gz/pipescript-0.0.18/pipescript/__init__.py
@@ -0,0 +1,103 @@
+"""
+pipescript: powerful pipeline syntax for IPython and Jupyter.
+Just run `%load_ext pipescript` to begin using pipe operators, placeholders, and more.
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import TYPE_CHECKING, cast
+
+import pyccolo as pyc
+
+import pipescript.api
+from pipescript.api import *  # noqa: F403
+from pipescript.extension import (
+    clear_tracer_stacks,
+    identify_dynamic_macros,
+    load_builtin_dynamic_macros,
+)
+from pipescript.extension import load_ipython_extension as load_ipython_extension_base
+from pipescript.extension import (
+    unload_ipython_extension as unload_ipython_extension_base,
+)
+from pipescript.patches.completion_patch import patch_completer, unpatch_completer
+from pipescript.tracers.macro_tracer import MacroTracer
+from pipescript.tracers.optional_chaining_tracer import OptionalChainingTracer
+from pipescript.tracers.pipeline_tracer import PipelineTracer
+
+from . import _version  # noqa: E402
+
+__version__ = _version.get_versions()["version"]
+
+if TYPE_CHECKING:
+    from IPython.core.interactiveshell import InteractiveShell
+
+
+def load_ipython_extension_ipyflow(shell: InteractiveShell) -> None:
+    from ipyflow.shell.interactiveshell import IPyflowInteractiveShell
+
+    assert isinstance(shell, IPyflowInteractiveShell)
+    shell.run_line_magic(
+        "flow", f"register {PipelineTracer.__module__}.{PipelineTracer.__name__}"
+    )
+    shell.run_line_magic(
+        "flow",
+        f"register {MacroTracer.__module__}.{MacroTracer.__name__}",
+    )
+    shell.run_line_magic(
+        "flow",
+        f"register {OptionalChainingTracer.__module__}.{OptionalChainingTracer.__name__}",
+    )
+    shell.events.register("post_run_cell", clear_tracer_stacks)
+    shell.events.register("post_run_cell", identify_dynamic_macros)
+    tracers = [
+        cast(pyc.BaseTracer, cls).instance()
+        for cls in [PipelineTracer, MacroTracer, OptionalChainingTracer]
+    ]
+    patch_completer(shell.Completer, tracers=tracers)
+    load_builtin_dynamic_macros(shell)
+
+
+def unload_ipython_extension_ipyflow(shell: InteractiveShell) -> None:
+    unpatch_completer(shell.Completer)
+    shell.events.unregister("post_run_cell", identify_dynamic_macros)
+    shell.events.unregister("post_run_cell", clear_tracer_stacks)
+    shell.run_line_magic(
+        "flow",
+        f"deregister {OptionalChainingTracer.__module__}.{OptionalChainingTracer.__name__}",
+    )
+    shell.run_line_magic(
+        "flow",
+        f"deregister {MacroTracer.__module__}.{MacroTracer.__name__}",
+    )
+    shell.run_line_magic(
+        "flow", f"deregister {PipelineTracer.__module__}.{PipelineTracer.__name__}"
+    )
+
+
+def load_ipython_extension(shell: InteractiveShell) -> None:
+    IPyflowInteractiveShell = getattr(
+        sys.modules.get("ipyflow.shell.interactiveshell"),
+        "IPyflowInteractiveShell",
+        type(None),
+    )
+    if isinstance(shell, IPyflowInteractiveShell):
+        load_ipython_extension_ipyflow(shell)
+    else:
+        load_ipython_extension_base(shell)
+
+
+def unload_ipython_extension(shell: InteractiveShell) -> None:
+    IPyflowInteractiveShell = getattr(
+        sys.modules.get("ipyflow.shell.interactiveshell"),
+        "IPyflowInteractiveShell",
+        type(None),
+    )
+    if isinstance(shell, IPyflowInteractiveShell):
+        unload_ipython_extension_ipyflow(shell)
+    else:
+        unload_ipython_extension_base(shell)
+
+
+__all__ = list(pipescript.api.__all__)
