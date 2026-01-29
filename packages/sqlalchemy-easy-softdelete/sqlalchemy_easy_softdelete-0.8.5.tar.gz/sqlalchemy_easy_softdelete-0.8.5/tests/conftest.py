f691c416267752cd0ac0d18e079b3356efbaafaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:06:19 +0000

    Add package sqlalchemy-easy-softdelete/0.8.5/sqlalchemy_easy_softdelete-0.8.5.tar.gz

---
+++ packages/sqlalchemy-easy-softdelete/sqlalchemy_easy_softdelete-0.8.5.tar.gz/sqlalchemy_easy_softdelete-0.8.5/tests/conftest.py
@@ -0,0 +1,65 @@
+import os
+
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Connection, Engine
+from sqlalchemy.orm import Session, sessionmaker
+
+from sqlalchemy_easy_softdelete.handler.rewriter import SoftDeleteQueryRewriter
+from tests.model import TestModelBase
+from tests.seed_data import generate_table_with_inheritance_obj
+from tests.seed_data.parent_child_childchild import generate_parent_child_object_hierarchy
+
+env_connection_string = os.environ.get("TEST_CONNECTION_STRING", None)
+
+
+@pytest.fixture
+def sqla2_warnings() -> Engine:
+    # Enable SQLAlchemy 2.0 Warnings mode to help with 2.0 support
+    os.environ["SQLALCHEMY_WARN_20"] = "1"
+
+
+@pytest.fixture
+def db_engine(sqla2_warnings) -> Engine:
+    test_db_url = env_connection_string or "sqlite://"
+    print(f"connection_string={test_db_url}")
+    return create_engine(test_db_url, future=True)
+
+
+@pytest.fixture
+def db_connection(db_engine) -> Connection:
+    connection = db_engine.connect()
+
+    # start a transaction
+    transaction = connection.begin()
+
+    try:
+        yield connection
+    finally:
+        transaction.rollback()
+    connection.close()
+
+
+@pytest.fixture
+def db_session(db_connection) -> Session:
+    TestModelBase.metadata.create_all(db_connection)
+    return sessionmaker(autocommit=False, autoflush=False, bind=db_connection)()
+
+
+@pytest.fixture
+def seeded_session(db_session) -> Session:
+    generate_parent_child_object_hierarchy(db_session, 1000)
+    generate_parent_child_object_hierarchy(db_session, 1001)
+    generate_parent_child_object_hierarchy(db_session, 1002, parent_deleted=True)
+
+    generate_table_with_inheritance_obj(db_session, 1000, deleted=False)
+    generate_table_with_inheritance_obj(db_session, 1001, deleted=False)
+    generate_table_with_inheritance_obj(db_session, 1002, deleted=True)
+    return db_session
+
+
+@pytest.fixture
+def rewriter() -> SoftDeleteQueryRewriter:
+    from sqlalchemy_easy_softdelete.handler.sqlalchemy_easy_softdelete import global_rewriter
+
+    return global_rewriter
