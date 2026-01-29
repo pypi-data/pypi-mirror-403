2246a6bdb4b7ae53142fcc953dd9273194213801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:11:15 +0000

    Add package idc-index/0.11.6/idc_index-0.11.6-py3-none-any.whl

---
+++ packages/idc-index/idc_index-0.11.6-py3-none-any.whl/idc_index/__init__.py
@@ -0,0 +1,15 @@
+"""Copyright (c) 2024 Imaging Data Commons. All rights reserved.
+
+idc-index: Package to query and download data from an index of ImagingDataCommons
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
+
+from .index import IDCClient, IDCClientInsufficientDiskSpaceError
+
+_ = IDCClient
+_ = IDCClientInsufficientDiskSpaceError
