dbddbc7a4b5ae25c9f4b9536d30c40c1c5725463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:12:43 +0000

    Add package pychoco/0.2.5/pychoco-0.2.5-cp38-cp38-manylinux_2_34_x86_64.whl

---
+++ packages/pychoco/pychoco-0.2.5-cp38-cp38-manylinux_2_34_x86_64.whl/tests/int_constraints/test_lex_chain_less_eq.py
@@ -0,0 +1,24 @@
+import unittest
+
+from pychoco.model import Model
+
+
+class TestLexChainLessEq(unittest.TestCase):
+
+    def testLexChainLessEq1(self):
+        m = Model()
+        ar1 = m.intvars(3, 0, 5)
+        ar2 = m.intvars(3, -1, 4)
+        c = m.lex_chain_less_eq([ar1, ar2])
+        c.post()
+        while m.get_solver().solve():
+            self.assertTrue(c.is_satisfied())
+
+    def testLexChainLessEq2(self):
+        m = Model()
+        ar1 = m.intvars(3, 0, 5)
+        ar2 = m.intvars(3, -1, 4)
+        c = m.lex_chain_less_eq(ar1, ar2)
+        c.post()
+        while m.get_solver().solve():
+            self.assertTrue(c.is_satisfied())
