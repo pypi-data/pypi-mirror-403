eb00df9102b327dfec7f061ea45cc4fb77cf666d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:12:53 +0000

    Add package core-module-excel-uploader-app/2.19.0/core_module_excel_uploader_app-2.19.0.tar.gz

---
+++ packages/core-module-excel-uploader-app/core_module_excel_uploader_app-2.19.0.tar.gz/core_module_excel_uploader_app-2.19.0/core_module_excel_uploader_app/views/forms.py
@@ -0,0 +1,10 @@
+""" Excel Uploader forms
+"""
+
+from django import forms
+
+
+class ExcelUploaderForm(forms.Form):
+    """Excel Uploader Form"""
+
+    file = forms.FileField(label="Select Excel File")
