fe6ea4af9388c8178256640bc59ebaf8a842dd13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:14:15 +0000

    Add package types-python-dateutil/2.9.0.20260124/types_python_dateutil-2.9.0.20260124.tar.gz

---
+++ packages/types-python-dateutil/types_python_dateutil-2.9.0.20260124.tar.gz/types_python_dateutil-2.9.0.20260124/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'dateutil-stubs': ['__init__.pyi', '_common.pyi', '_version.pyi', 'easter.pyi', 'parser/__init__.pyi', 'parser/_parser.pyi', 'parser/isoparser.pyi', 'relativedelta.pyi', 'rrule.pyi', 'tz/__init__.pyi', 'tz/_common.pyi', 'tz/tz.pyi', 'tz/win.pyi', 'tzwin.pyi', 'utils.pyi', 'zoneinfo/__init__.pyi', 'zoneinfo/rebuild.pyi', 'METADATA.toml', 'py.typed']})
