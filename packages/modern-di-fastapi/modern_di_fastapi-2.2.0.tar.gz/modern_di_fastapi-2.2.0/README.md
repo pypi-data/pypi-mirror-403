9992b0e3e940fbe5c0f11a0b5a5b1472c1d40c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:06:48 +0000

    Add package modern-di-fastapi/2.2.0/modern_di_fastapi-2.2.0.tar.gz

---
+++ packages/modern-di-fastapi/modern_di_fastapi-2.2.0.tar.gz/modern_di_fastapi-2.2.0/README.md
@@ -0,0 +1,6 @@
+"Modern-DI-FastAPI"
+==
+
+Integration of [Modern-DI](https://github.com/modern-python/modern-di) to FastAPI
+
+📚 [Documentation](https://modern-di.readthedocs.io)
---
+++ packages/modern-di-fastapi/modern_di_fastapi-2.2.0.tar.gz/modern_di_fastapi-2.2.0/modern_di_fastapi/__init__.py
@@ -0,0 +1,9 @@
+from modern_di_fastapi.main import FromDI, build_di_container, fetch_di_container, setup_di
+
+
+__all__ = [
+    "FromDI",
+    "build_di_container",
+    "fetch_di_container",
+    "setup_di",
+]
