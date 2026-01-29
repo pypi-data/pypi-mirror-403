126c50aeddd22bae148f8e5557584dffe2d30c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:02 +0000

    Add package solas-ai/0.6.0/solas_ai-0.6.0-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/solas-ai/solas_ai-0.6.0-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/solas_disparity/plots/_plot_multilevel.pyi
@@ -0,0 +1,3 @@
+from ._adverse_impact_ratio import plot_adverse_impact_ratio as plot_adverse_impact_ratio
+from solas_disparity import const as const
+from solas_disparity.types import Disparity as Disparity, DisparityCalculation as DisparityCalculation
