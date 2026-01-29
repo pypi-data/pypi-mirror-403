a48847aaedbef3ce3bb945140ebf83e6cc894eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:07:35 +0000

    Add package backtrader-next/2.3.1/backtrader_next-2.3.1-py3-none-any.whl

---
+++ packages/backtrader-next/backtrader_next-2.3.1-py3-none-any.whl/backtrader_next/nplot/utils.py
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2015-2023 Sergey Malinin
+# GPL 3.0 license <http://www.gnu.org/licenses/>
+#
+
+import sys
+from contextlib import contextmanager
+from tempfile import NamedTemporaryFile, gettempdir
+from datetime import datetime
+
+@contextmanager
+def tmpfilename():
+    with NamedTemporaryFile(suffix='.html') as f:
+        if sys.platform.startswith('win'):
+            f.close()
+        yield f.name
+
+def gen_timestamp():
+    now = datetime.now()
+    return now.strftime('%Y_%m_%d_%H_%M_%S_%f')
+
