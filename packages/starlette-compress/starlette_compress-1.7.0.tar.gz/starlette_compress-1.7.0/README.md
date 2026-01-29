a9ef81812b0344f3c02f3d3674e233a7c1796c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:09:32 +0000

    Add package starlette-compress/1.7.0/starlette_compress-1.7.0.tar.gz

---
+++ packages/starlette-compress/starlette_compress-1.7.0.tar.gz/starlette_compress-1.7.0/README.md
@@ -0,0 +1,93 @@
+# starlette-compress
+
+[![PyPI - Python Version](https://shields.monicz.dev/pypi/pyversions/starlette-compress)](https://pypi.org/project/starlette-compress)
+
+**starlette-compress** is a fast and simple middleware for compressing responses in [Starlette](https://www.starlette.io). It supports more compression algorithms than Starlette's built-in GZipMiddleware, and has more sensible defaults.
+
+- Python 3.9+ support
+- Python 3.14 `compression.zstd` support
+- Compatible with `asyncio` and `trio` backends
+- ZStd, Brotli, and GZip compression
+- Sensible default configuration
+- [Zero-Clause BSD](https://choosealicense.com/licenses/0bsd/) — public domain dedication
+- [Semantic Versioning](https://semver.org) compliance
+
+## Installation
+
+```sh
+pip install starlette-compress
+```
+
+## Basic Usage
+
+### Starlette
+
+```py
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette_compress import CompressMiddleware
+
+middleware = [
+    Middleware(CompressMiddleware)
+]
+
+app = Starlette(routes=..., middleware=middleware)
+```
+
+### FastAPI
+
+You can use starlette-compress with [FastAPI](https://fastapi.tiangolo.com) too:
+
+```py
+from fastapi import FastAPI
+from starlette_compress import CompressMiddleware
+
+app = FastAPI()
+app.add_middleware(CompressMiddleware)
+```
+
+## Advanced Usage
+
+### Changing Minimum Response Size
+
+Control the minimum size of the response to compress. By default, responses must be at least 500 bytes to be compressed.
+
+```py
+# Starlette
+middleware = [
+    Middleware(CompressMiddleware, minimum_size=1000)
+]
+
+# FastAPI
+app.add_middleware(CompressMiddleware, minimum_size=1000)
+```
+
+### Tuning Compression Levels
+
+Adjust the compression levels for each algorithm. Higher levels mean smaller files but slower compression. Default level is 4 for all algorithms.
+
+```py
+# Starlette
+middleware = [
+    Middleware(CompressMiddleware, zstd_level=6, brotli_quality=6, gzip_level=6)
+]
+
+# FastAPI
+app.add_middleware(CompressMiddleware, zstd_level=6, brotli_quality=6, gzip_level=6)
+```
+
+### Supporting Custom Content-Types
+
+Manage the supported content-types. Unknown response types are not compressed. [Check here](https://github.com/Zaczero/pkgs/blob/main/starlette-compress/starlette_compress/_utils.py) for the default configuration.
+
+```py
+from starlette_compress import add_compress_type, remove_compress_type
+
+add_compress_type("application/my-custom-type")
+remove_compress_type("application/json")
+```
+
+### Dropping Accept-Encoding Header
+
+By default, starlette-compress leaves the request `Accept-Encoding` header intact. Set
+`remove_accept_encoding=True` to remove it before calling downstream middleware/application.
---
+++ packages/starlette-compress/starlette_compress-1.7.0.tar.gz/starlette_compress-1.7.0/tests/test___init__.py
@@ -0,0 +1,304 @@
+import random
+import sys
+from typing import Callable
+
+import pytest
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.requests import Request
+from starlette.responses import (
+    PlainTextResponse,
+    Response,
+    StreamingResponse,
+)
+from starlette.routing import Route
+from starlette.testclient import TestClient
+from starlette.types import ASGIApp
+
+from starlette_compress import (
+    CompressMiddleware,
+    add_compress_type,
+    remove_compress_type,
+)
+from starlette_compress._utils import parse_accept_encoding
+
+TestClientFactory = Callable[[ASGIApp], TestClient]
+
+
+def test_compress_responses(test_client_factory: TestClientFactory):
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse('x' * 4000)
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[Middleware(CompressMiddleware)],
+    )
+
+    client = test_client_factory(app)
+
+    for encoding in ('gzip', 'br', 'zstd'):
+        response = client.get('/', headers={'accept-encoding': encoding})
+        assert response.status_code == 200
+
+        try:
+            assert response.text == 'x' * 4000
+        except AssertionError:
+            # TODO: remove after new zstd support in httpx
+            if encoding != 'zstd' or sys.version_info < (3, 14):
+                raise
+            from compression import zstd
+
+            assert zstd.decompress(response.content) == b'x' * 4000
+
+        assert response.headers['Content-Encoding'] == encoding
+        assert int(response.headers['Content-Length']) < 4000
+        assert response.headers['Vary'] == 'Accept-Encoding'
+
+
+def test_compress_not_in_accept_encoding(test_client_factory: TestClientFactory):
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse('x' * 4000)
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[Middleware(CompressMiddleware)],
+    )
+
+    client = test_client_factory(app)
+    response = client.get('/', headers={'accept-encoding': 'identity'})
+    assert response.status_code == 200
+    assert response.text == 'x' * 4000
+    assert 'Content-Encoding' not in response.headers
+    assert int(response.headers['Content-Length']) == 4000
+    assert response.headers['Vary'] == 'Accept-Encoding'
+
+
+def test_compress_ignored_for_small_responses(test_client_factory: TestClientFactory):
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse('OK')
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[Middleware(CompressMiddleware)],
+    )
+
+    client = test_client_factory(app)
+
+    for encoding in ('gzip', 'br', 'zstd'):
+        response = client.get('/', headers={'accept-encoding': encoding})
+        assert response.status_code == 200
+        assert response.text == 'OK'
+        assert 'Content-Encoding' not in response.headers
+        assert int(response.headers['Content-Length']) == 2
+        assert 'Vary' not in response.headers
+
+
+@pytest.mark.parametrize(
+    'chunk_size',
+    [
+        1,
+        128 * 1024,  # 128KB
+    ],
+)
+def test_compress_streaming_response(
+    test_client_factory: TestClientFactory, chunk_size: int
+):
+    random.seed(42)
+    chunk_count = 70
+
+    def homepage(request: Request) -> StreamingResponse:
+        async def generator(count: int):
+            for _ in range(count):
+                # enough entropy is required for successful chunks
+                yield random.getrandbits(8 * chunk_size).to_bytes(chunk_size, 'big')
+
+        streaming = generator(chunk_count)
+        return StreamingResponse(streaming, media_type='text/plain')
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[Middleware(CompressMiddleware)],
+    )
+
+    client = test_client_factory(app)
+
+    for encoding in ('gzip', 'br', 'zstd'):
+        response = client.get('/', headers={'accept-encoding': encoding})
+        assert response.status_code == 200
+
+        try:
+            assert len(response.content) == chunk_count * chunk_size
+        except AssertionError:
+            # TODO: remove after new zstd support in httpx
+            if encoding != 'zstd' or sys.version_info < (3, 14):
+                raise
+            from compression import zstd
+
+            assert len(zstd.decompress(response.content)) == chunk_count * chunk_size
+
+        assert response.headers['Content-Encoding'] == encoding
+        assert 'Content-Length' not in response.headers
+        assert response.headers['Vary'] == 'Accept-Encoding'
+
+
+def test_compress_ignored_for_responses_with_encoding_set(
+    test_client_factory: TestClientFactory,
+):
+    def homepage(request: Request) -> StreamingResponse:
+        async def generator(content: bytes, count: int):
+            for _ in range(count):
+                yield content
+
+        streaming = generator(content=b'x' * 400, count=10)
+        return StreamingResponse(streaming, headers={'Content-Encoding': 'test'})
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[Middleware(CompressMiddleware)],
+    )
+
+    client = test_client_factory(app)
+
+    for encoding in ('gzip', 'br', 'zstd'):
+        response = client.get('/', headers={'accept-encoding': f'{encoding}, test'})
+        assert response.status_code == 200
+        assert response.text == 'x' * 4000
+        assert response.headers['Content-Encoding'] == 'test'
+        assert 'Content-Length' not in response.headers
+        assert 'Vary' not in response.headers
+
+
+def test_compress_not_ignored_for_identity_content_encoding(
+    test_client_factory: TestClientFactory,
+):
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse(
+            'x' * 4000, headers={'Content-Encoding': 'identity, identity'}
+        )
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[Middleware(CompressMiddleware)],
+    )
+
+    client = test_client_factory(app)
+    response = client.get('/', headers={'accept-encoding': 'gzip'})
+    assert response.status_code == 200
+    assert response.text == 'x' * 4000
+    assert response.headers['Content-Encoding'] == 'gzip'
+    assert int(response.headers['Content-Length']) < 4000
+    assert response.headers['Vary'] == 'Accept-Encoding'
+
+
+@pytest.mark.parametrize(
+    'remove_accept_encoding',
+    [True, False],
+)
+def test_accept_encoding_removed_from_scope(
+    test_client_factory: TestClientFactory,
+    remove_accept_encoding: bool,
+):
+    def homepage(request: Request) -> PlainTextResponse:
+        accept_encoding = request.headers.get('accept-encoding', '<missing>')
+        return PlainTextResponse(headers={'x-request-accept-encoding': accept_encoding})
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[
+            Middleware(
+                CompressMiddleware, remove_accept_encoding=remove_accept_encoding
+            )
+        ],
+    )
+
+    client = test_client_factory(app)
+    response = client.get('/', headers={'accept-encoding': 'gzip'})
+    assert response.status_code == 200
+    assert response.headers['x-request-accept-encoding'] == (
+        '<missing>' if remove_accept_encoding else 'gzip'
+    )
+
+
+def test_compress_ignored_for_missing_accept_encoding(
+    test_client_factory: TestClientFactory,
+):
+    def homepage(request: Request) -> PlainTextResponse:
+        return PlainTextResponse('x' * 4000)
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[Middleware(CompressMiddleware)],
+    )
+
+    client = test_client_factory(app)
+    response = client.get('/', headers={'accept-encoding': ''})
+    assert response.status_code == 200
+    assert response.text == 'x' * 4000
+    assert 'Content-Encoding' not in response.headers
+    assert int(response.headers['Content-Length']) == 4000
+    assert response.headers['Vary'] == 'Accept-Encoding'
+
+
+def test_compress_ignored_for_missing_content_type(
+    test_client_factory: TestClientFactory,
+):
+    def homepage(request: Request) -> Response:
+        return Response('x' * 4000)
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[Middleware(CompressMiddleware)],
+    )
+
+    client = test_client_factory(app)
+
+    for encoding in ('gzip', 'br', 'zstd'):
+        response = client.get('/', headers={'accept-encoding': encoding})
+        assert response.status_code == 200
+        assert response.text == 'x' * 4000
+        assert 'Content-Encoding' not in response.headers
+        assert int(response.headers['Content-Length']) == 4000
+        assert 'Vary' not in response.headers
+
+
+def test_compress_registered_content_type(test_client_factory: TestClientFactory):
+    def homepage(request: Request) -> Response:
+        return Response('x' * 4000, media_type='test/test')
+
+    app = Starlette(
+        routes=[Route('/', endpoint=homepage)],
+        middleware=[Middleware(CompressMiddleware)],
+    )
+
+    client = test_client_factory(app)
+
+    for encoding in ('gzip', 'br', 'zstd'):
+        response = client.get('/', headers={'accept-encoding': encoding})
+        assert response.status_code == 200
+        assert 'Content-Encoding' not in response.headers
+        assert int(response.headers['Content-Length']) == 4000
+        assert 'Vary' not in response.headers
+
+    add_compress_type('test/test')
+
+    for encoding in ('gzip', 'br', 'zstd'):
+        response = client.get('/', headers={'accept-encoding': encoding})
+        assert response.status_code == 200
+        assert response.headers['Content-Encoding'] == encoding
+        assert int(response.headers['Content-Length']) < 4000
+        assert response.headers['Vary'] == 'Accept-Encoding'
+
+    remove_compress_type('test/test')
+
+    for encoding in ('gzip', 'br', 'zstd'):
+        response = client.get('/', headers={'accept-encoding': encoding})
+        assert response.status_code == 200
+        assert 'Content-Encoding' not in response.headers
+        assert int(response.headers['Content-Length']) == 4000
+        assert 'Vary' not in response.headers
+
+
+def test_parse_accept_encoding():
+    assert parse_accept_encoding('') == frozenset()
+    assert parse_accept_encoding('gzip, deflate') == {'gzip', 'deflate'}
+    assert parse_accept_encoding('br;q=1.0,gzip;q=0.8, *;q=0.1') == {'br', 'gzip'}
