7490708583d3f809f914f1936f0c284b11121ef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:08:59 +0000

    Add package area-jot/0.0.1/area_jot-0.0.1-py3-none-any.whl

---
+++ packages/area-jot/area_jot-0.0.1-py3-none-any.whl/area/area.py
@@ -0,0 +1,13 @@
+import math
+
+def aroftriangle(base, height):
+    return 0.5 * base * height
+
+def arofrectangle(length, width):
+    return length * width
+
+def arofcircle(radius):
+    return math.pi * radius * radius
+
+def arofcube(side):
+    return 6 * side * side
