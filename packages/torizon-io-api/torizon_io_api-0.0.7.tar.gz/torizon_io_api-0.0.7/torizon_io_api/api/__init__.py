cb39963e2581c0bffe962c844ca578d271351d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:15:32 +0000

    Add package torizon-io-api/0.0.7/torizon_io_api-0.0.7.tar.gz

---
+++ packages/torizon-io-api/torizon_io_api-0.0.7.tar.gz/torizon_io_api-0.0.7/torizon_io_api/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from torizon_io_api.api.device_metrics_api import DeviceMetricsApi
+from torizon_io_api.api.devices_api import DevicesApi
+from torizon_io_api.api.fleets_api import FleetsApi
+from torizon_io_api.api.packages_api import PackagesApi
+from torizon_io_api.api.remote_access_api import RemoteAccessApi
+from torizon_io_api.api.updates_api import UpdatesApi
+
