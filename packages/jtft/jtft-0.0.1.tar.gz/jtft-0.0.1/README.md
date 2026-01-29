9c3f01c4e89a1500342c880cc5e155ccd7b75eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:06:27 +0000

    Add package jtft/0.0.1/jtft-0.0.1.tar.gz

---
+++ packages/jtft/jtft-0.0.1.tar.gz/jtft-0.0.1/README.md
@@ -0,0 +1,2 @@
+# jtft
+Jsonic Text File Tree. for deploy it PyPI. I was need it.
---
+++ packages/jtft/jtft-0.0.1.tar.gz/jtft-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="jtft",
+    version="0.0.1",
+    py_modules=["jtft"],
+    install_requires=["chardet"],
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+)
