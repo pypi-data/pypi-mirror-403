2cc4cbd7950b17baafbfa533019a364c09b7c0ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:17:48 +0000

    Add package tactrandom/1.0.0/tactrandom-1.0.0-py3-none-any.whl

---
+++ packages/tactrandom/tactrandom-1.0.0-py3-none-any.whl/trandom/__init__.py
@@ -0,0 +1,7 @@
+from trandom.factory import Factory
+from trandom.generator import Generator
+from trandom.proxy import TRandom
+
+VERSION = "1.0.0"
+
+__all__ = ("Factory", "Generator", "TRandom")
---
+++ packages/tactrandom/tactrandom-1.0.0-py3-none-any.whl/trandom/providers/currency/nl_NL/__init__.py
@@ -0,0 +1,8 @@
+from trandom.providers.currency import Provider as CurrencyProvider
+
+
+class Provider(CurrencyProvider):
+    price_formats = ["#,##", "%#,##", "%##,##", "%.###,##", "%#.###,##"]
+
+    def pricetag(self) -> str:
+        return "\N{EURO SIGN}" + self.numerify(self.random_element(self.price_formats))
