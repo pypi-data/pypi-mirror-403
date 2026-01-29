2367b61c769f4ca268bf5a6f2b74edd581d80ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:07:00 +0000

    Add package spacekernel/0.1.0.dev8/spacekernel-0.1.0.dev8-cp314-cp314-macosx_15_0_arm64.whl

---
+++ packages/spacekernel/spacekernel-0.1.0.dev8-cp314-cp314-macosx_15_0_arm64.whl/spacekernel/datamodel/structured.pyi
@@ -0,0 +1,15 @@
+#  -*- coding: utf-8 -*-
+"""
+Author: Rafael R. L. Benevides
+"""
+
+from numpy import dtype
+
+
+SurfCoord_dtype: dtype = ...
+
+LLA_dtype: dtype = ...
+
+ENU_dtype: dtype = ...
+
+AER_dtype: dtype = ...
---
+++ packages/spacekernel/spacekernel-0.1.0.dev8-cp314-cp314-macosx_15_0_arm64.whl/spacekernel/propagators/analytical/sgp4/__init__.py
@@ -0,0 +1,7 @@
+#  -*- coding: utf-8 -*-
+"""
+Author: Rafael R. L. Benevides
+"""
+
+from .tle import TLE, ELSET
+from .sgp4imp import propagate_tle, SGP4
