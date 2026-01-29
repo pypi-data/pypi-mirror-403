425fcf96696c63f57d638d1bd7955df123408aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:07:55 +0000

    Add package pyconfix/0.10.3/pyconfix-0.10.3.tar.gz

---
+++ packages/pyconfix/pyconfix-0.10.3.tar.gz/pyconfix-0.10.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="pyconfix",
+    version="0.10.3",
+    description="A simple feature managment tool library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Nemesis",
+    author_email="nemesiswasalientoo@proton.me",
+    url="https://github.com/NemesisWasAlienToo/pyconfix",
+    py_modules=["pyconfix", "main"],
+    python_requires=">=3.7",
+    license="MIT",
+    install_requires=[
+        "windows-curses;platform_system=='Windows'"
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyconfix = main:main',
+        ],
+    },
+)
