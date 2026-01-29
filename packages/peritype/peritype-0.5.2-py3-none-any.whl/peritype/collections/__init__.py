3b31ba4c0fef59b32f7ce34480451f9090483853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:07:25 +0000

    Add package peritype/0.5.2/peritype-0.5.2-py3-none-any.whl

---
+++ packages/peritype/peritype-0.5.2-py3-none-any.whl/peritype/collections/__init__.py
@@ -0,0 +1,3 @@
+from peritype.collections.bag import TypeBag as TypeBag
+from peritype.collections.map import TypeMap as TypeMap, TypeSetMap as TypeSetMap
+from peritype.collections.tree import TypeSuperTree as TypeSuperTree
