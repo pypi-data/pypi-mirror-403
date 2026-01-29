1d0a447bbd0d5212b3c10ca1a1118f19507647f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:14:07 +0000

    Add package SigProfilerAssignment/1.1.2/sigprofilerassignment-1.1.2.tar.gz

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-1.1.2.tar.gz/sigprofilerassignment-1.1.2/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '1.1.2'
+version = '1.1.2'
+Update = 'v1.1.2: Fixed to_csv() calls for Python 3.12 compatibility'
+
+    
