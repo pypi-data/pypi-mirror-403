24113148d228c5a50cb59f93fe2b7eb8e62babb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:10:51 +0000

    Add package NEMO/7.3.3/nemo-7.3.3-py3-none-any.whl

---
+++ packages/NEMO/nemo-7.3.3-py3-none-any.whl/NEMO/views/get_projects.py
@@ -0,0 +1,63 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.db.models import Q
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_GET
+
+from NEMO.decorators import staff_member_or_tool_superuser_or_tool_staff_required
+from NEMO.models import Tool, User
+
+
+def projects_for_consumable_permissions(user):
+    return user.is_active and (user.is_any_part_of_staff or user.has_perm("NEMO.add_consumablewithdraw"))
+
+
+@staff_member_or_tool_superuser_or_tool_staff_required
+@require_GET
+def get_projects_for_training(request):
+    return get_projects(request)
+
+
+@user_passes_test(projects_for_consumable_permissions)
+@require_GET
+def get_projects_for_consumables(request):
+    # Only return project for which consumable withdrawals are allowed
+    return get_projects(request, Q(allow_consumable_withdrawals=True))
+
+
+def get_projects(request, project_filter=Q()):
+    """Gets a list of all active projects for a specific user. This is only accessible by staff members."""
+    user = get_object_or_404(User, id=request.GET.get("user_id", None))
+    projects = user.active_projects().filter(project_filter)
+    source_template = request.GET.get("source_template")
+    if source_template == "training":
+        entry_number = int(request.GET["entry_number"])
+        return render(request, "training/get_projects.html", {"projects": projects, "entry_number": entry_number})
+    return JsonResponse(dict(projects=list(projects.values("id", "name"))))
+
+
+@login_required
+@require_GET
+def get_projects_for_tool_control(request):
+    if not request.user.is_any_part_of_staff and not request.user.is_tool_staff:
+        return redirect("landing")
+    customer_id = request.GET.get("user_id")
+    tool_id = get_object_or_404(Tool, id=request.GET.get("tool_id")).id
+    user = get_object_or_404(User, id=customer_id)
+    return render(
+        request,
+        "tool_control/get_projects.html",
+        {"active_projects": user.active_projects(), "customer_id": customer_id, "tool_id": tool_id},
+    )
+
+
+@login_required
+@require_GET
+def get_projects_for_self(request):
+    """Gets a list of all active projects for the current user."""
+    tool_id = get_object_or_404(Tool, id=request.GET.get("tool_id")).id
+    return render(
+        request,
+        "tool_control/get_projects.html",
+        {"active_projects": request.user.active_projects(), "customer_id": request.user.id, "tool_id": tool_id},
+    )
