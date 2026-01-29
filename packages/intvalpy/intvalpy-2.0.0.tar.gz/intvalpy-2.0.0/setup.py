8a636ab75e453fcbc2d1e6ae6133bf7a1353f204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:11:49 +0000

    Add package intvalpy/2.0.0/intvalpy-2.0.0.tar.gz

---
+++ packages/intvalpy/intvalpy-2.0.0.tar.gz/intvalpy-2.0.0/setup.py
@@ -0,0 +1,52 @@
+import io
+import os
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+
+os.environ['CVXOPT_BUILD_GLPK'] = '1'
+
+readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+with io.open(readme_file, mode='r', encoding='utf-8') as f:
+    README = f.read()
+
+INSTALL_REQUIRES = [
+    'matplotlib',
+    'numpy',
+    'cvxopt',
+    'mpmath',
+    'pandas',
+    'cython'
+]
+
+setup(
+    name='intvalpy',
+    version='2.0.0',
+    description='IntvalPy - a Python interval computation library',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    keywords=[
+        'interval',
+        'machine learning',
+        'data fitting',
+        'inequality visualization',
+        'optimal solutions',
+        'math'
+    ],
+    author='Андросов Артем Станиславович, Шарый Сергей Петрович',
+    author_email='artem.androsov@gmail.com, shary@ict.nsc.ru',
+    url='https://github.com/AndrosovAS/intvalpy',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    ext_modules=cythonize(
+        Extension(
+            'intvalpy.kernel.interval_arithmetics',
+            ['intvalpy/kernel/interval_arithmetics.pyx']
+        ),
+        compiler_directives={'language_level': "3"}
+    ),
+    include_dirs=[np.get_include()],
+    zip_safe=False,
+)
