6f4a4dee3c630e60282426d266b66f104e034cd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:07:55 +0000

    Add package argclass/1.6.0/argclass-1.6.0.tar.gz

---
+++ packages/argclass/argclass-1.6.0.tar.gz/argclass-1.6.0/argclass/__main__.py
@@ -0,0 +1,41 @@
+import argparse
+import sys
+from pathlib import Path
+
+import argclass
+
+
+class GreetCommand(argclass.Parser):
+    user: str = argclass.Argument("user", help="User to greet")
+
+    def __call__(self) -> int:
+        print(f"Hello, {self.user}!")
+        return 0
+
+
+class Parser(argclass.Parser):
+    verbose: bool = False
+    secret_key: str = argclass.Secret(help="Secret API key")
+    greet = GreetCommand()
+
+    def __call__(self) -> int:
+        self.print_help()
+        return 0
+
+
+def main() -> None:
+    parser = Parser(
+        prog=f"{Path(sys.executable).name} -m argclass",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=(
+            "This code produces this help:\n\n```"
+            f"python\n{open(__file__).read().strip()}\n```"
+        ),
+    )
+    parser.parse_args()
+    parser.sanitize_env()
+    exit(parser())
+
+
+if __name__ == "__main__":
+    main()
