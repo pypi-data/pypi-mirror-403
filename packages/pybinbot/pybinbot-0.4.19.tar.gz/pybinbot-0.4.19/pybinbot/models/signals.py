e073c3eae14cfaed9ad8949898747d0f67aa19d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:18:36 +0000

    Add package pybinbot/0.4.19/pybinbot-0.4.19.tar.gz

---
+++ packages/pybinbot/pybinbot-0.4.19.tar.gz/pybinbot-0.4.19/pybinbot/models/signals.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict, Field, field_validator
+
+
+class HABollinguerSpread(BaseModel):
+    """Pydantic model for the Bollinguer spread."""
+
+    bb_high: float
+    bb_mid: float
+    bb_low: float
+
+
+class SignalsConsumer(BaseModel):
+    """Pydantic model for the signals consumer."""
+
+    type: str = Field(default="signal")
+    date: str = Field(
+        default_factory=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    )
+    spread: Optional[float] = Field(default=0)
+    current_price: Optional[float] = Field(default=0)
+    msg: str
+    symbol: str
+    algo: str
+    bot_strategy: str = Field(default="long")
+    bb_spreads: Optional[HABollinguerSpread]
+    autotrade: bool = Field(default=True, description="If it is in testing mode, False")
+
+    model_config = ConfigDict(
+        extra="allow",
+        use_enum_values=True,
+    )
+
+    @field_validator("spread", "current_price")
+    @classmethod
+    def name_must_contain_space(cls, v):
+        if v is None:
+            return 0
+        elif isinstance(v, str):
+            return float(v)
+        elif isinstance(v, float):
+            return v
+        else:
+            raise ValueError("must be a float or 0")
+
+
+class SingleCandle(BaseModel):
+    """
+    Pydantic model for a single candle.
+    """
+
+    symbol: str
+    open_time: int = Field()
+    close_time: int
+    open_price: float
+    close_price: float
+    high_price: float
+    low_price: float
+    volume: float
+
+    @field_validator("open_time", "close_time")
+    @classmethod
+    def validate_time(cls, v):
+        if v is None:
+            return 0
+        elif isinstance(v, str):
+            return int(v)
+        elif isinstance(v, int):
+            return v
+        else:
+            raise ValueError("must be a int or 0")
+
+    @field_validator("open_price", "close_price", "high_price", "low_price", "volume")
+    @classmethod
+    def validate_price(cls, v):
+        if v is None:
+            return 0
+        elif isinstance(v, str):
+            return float(v)
+        elif isinstance(v, float):
+            return v
+        else:
+            raise ValueError("must be a float or 0")
