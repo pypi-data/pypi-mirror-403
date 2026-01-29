bd5ee792eb093a2a0a111acf42309d36cddf59ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:10:26 +0000

    Add package argus-automatix/0.5.0/argus_automatix-0.5.0.tar.gz

---
+++ packages/argus-automatix/argus_automatix-0.5.0.tar.gz/argus_automatix-0.5.0/src/automatix/weights/__init__.py
@@ -0,0 +1,8 @@
+"""Guard-based weight functions for automata.
+
+Provides predicate classes and weight function implementations.
+"""
+
+import automatix.weights.guard_weights as guard_weights
+
+__all__ = ["guard_weights"]
