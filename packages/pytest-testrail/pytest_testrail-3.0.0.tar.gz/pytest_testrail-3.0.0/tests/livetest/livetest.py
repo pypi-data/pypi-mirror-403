6408803b5c5c3619de14dc0abc884daf81207af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:09:54 +0000

    Add package pytest-testrail/3.0.0/pytest_testrail-3.0.0.tar.gz

---
+++ packages/pytest-testrail/pytest_testrail-3.0.0.tar.gz/pytest_testrail-3.0.0/tests/livetest/livetest.py
@@ -0,0 +1,32 @@
+# -*- coding: UTF-8 -*-
+import time
+
+import pytest
+
+from pytest_testrail.plugin import pytestrail, testrail
+
+
+@testrail('C344', 'C366')
+def test_func1():
+    time.sleep(0.5)
+
+
+@testrail('C345')
+def test_func2():
+    time.sleep(1.6)
+    pytest.fail()
+
+
+@testrail('C99999')
+def test_func3():
+    time.sleep(0.5)
+
+
+@pytestrail.case('C1788')
+def test_func4():
+    pytest.skip()
+
+
+@pytestrail.case('C1789')
+def test_func5():
+    time.sleep(0.5)
