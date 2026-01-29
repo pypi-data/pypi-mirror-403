da392b249d40d965aa9840467424a1c61adff82e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:06:53 +0000

    Add package scikit23/0.1.0/scikit23-0.1.0.tar.gz

---
+++ packages/scikit23/scikit23-0.1.0.tar.gz/scikit23-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="scikit23",          # PyPI name (can have hyphen)
+    version="0.1.0",
+    author="Johndoe",
+    description="scikit replica",
+    packages=["scikit23"],
+    python_requires=">=3.7",
+)
