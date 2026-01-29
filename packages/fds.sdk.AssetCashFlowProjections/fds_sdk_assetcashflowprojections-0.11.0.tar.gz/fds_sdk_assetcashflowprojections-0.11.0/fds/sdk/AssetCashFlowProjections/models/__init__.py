46bb56fb881b7b5b622075d11690956668e914c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:15:50 +0000

    Add package fds.sdk.AssetCashFlowProjections/0.11.0/fds_sdk_assetcashflowprojections-0.11.0.tar.gz

---
+++ packages/fds.sdk.AssetCashFlowProjections/fds_sdk_assetcashflowprojections-0.11.0.tar.gz/fds_sdk_assetcashflowprojections-0.11.0/fds/sdk/AssetCashFlowProjections/models/__init__.py
@@ -0,0 +1,30 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from fds.sdk.AssetCashFlowProjections.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from fds.sdk.AssetCashFlowProjections.model.batch_job_response import BatchJobResponse
+from fds.sdk.AssetCashFlowProjections.model.batch_job_response_root import BatchJobResponseRoot
+from fds.sdk.AssetCashFlowProjections.model.client_error_response import ClientErrorResponse
+from fds.sdk.AssetCashFlowProjections.model.client_scenarios_upload import ClientScenariosUpload
+from fds.sdk.AssetCashFlowProjections.model.client_scenarios_upload_parameters import ClientScenariosUploadParameters
+from fds.sdk.AssetCashFlowProjections.model.client_upload_response import ClientUploadResponse
+from fds.sdk.AssetCashFlowProjections.model.client_upload_response_parameters import ClientUploadResponseParameters
+from fds.sdk.AssetCashFlowProjections.model.create_batch_job import CreateBatchJob
+from fds.sdk.AssetCashFlowProjections.model.create_batch_job_root import CreateBatchJobRoot
+from fds.sdk.AssetCashFlowProjections.model.create_new_document import CreateNewDocument
+from fds.sdk.AssetCashFlowProjections.model.create_new_document_root import CreateNewDocumentRoot
+from fds.sdk.AssetCashFlowProjections.model.document_response import DocumentResponse
+from fds.sdk.AssetCashFlowProjections.model.document_response_root import DocumentResponseRoot
+from fds.sdk.AssetCashFlowProjections.model.error_source import ErrorSource
+from fds.sdk.AssetCashFlowProjections.model.errors import Errors
+from fds.sdk.AssetCashFlowProjections.model.update_new_document import UpdateNewDocument
+from fds.sdk.AssetCashFlowProjections.model.update_new_document_root import UpdateNewDocumentRoot
+from fds.sdk.AssetCashFlowProjections.model.upload_status_response import UploadStatusResponse
+from fds.sdk.AssetCashFlowProjections.model.upload_status_response_parameters import UploadStatusResponseParameters
