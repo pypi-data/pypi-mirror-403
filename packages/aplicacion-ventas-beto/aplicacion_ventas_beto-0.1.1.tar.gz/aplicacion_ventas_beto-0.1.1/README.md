313380d6703819e1660440bee3df45c1987406aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:11:54 +0000

    Add package aplicacion-ventas-beto/0.1.1/aplicacion_ventas_beto-0.1.1.tar.gz

---
+++ packages/aplicacion-ventas-beto/aplicacion_ventas_beto-0.1.1.tar.gz/aplicacion_ventas_beto-0.1.1/README.md
@@ -0,0 +1,8 @@
+# Aplicacion de ventas
+
+Este paquete proporciona funcionalidades para gestionar ventas, incluyendo, cálculo de precios, impuestos y descuentos
+
+## Instalacion
+
+Puedes instalar el paquete usando:
+
---
+++ packages/aplicacion-ventas-beto/aplicacion_ventas_beto-0.1.1.tar.gz/aplicacion_ventas_beto-0.1.1/aplicacion_ventas/gestor_ventas.py
@@ -0,0 +1,17 @@
+from .descuentos import Descuentos
+from .impuestos import Impuestos
+from .precios import Precios
+
+
+class GestorVentas:
+    def __init__(self, precio_base, impuesto_porcentaje, descuento_porcentaje):
+        self.precio_base = precio_base
+        self.impuesto = Impuestos(impuesto_porcentaje)
+        self.descuento = Descuentos(descuento_porcentaje)
+
+    def calcular_precio_final(self):
+        impuesto_aplicado = self.impuesto.aplicar_impuesto(self.precio_base)
+        descuento_aplicado = self.descuento.aplicar_descuentos(self.precio_base)
+        precio_final = Precios.calcular_precio_final(self.precio_base, impuesto_aplicado, descuento_aplicado)
+
+        return round(precio_final, 2)
