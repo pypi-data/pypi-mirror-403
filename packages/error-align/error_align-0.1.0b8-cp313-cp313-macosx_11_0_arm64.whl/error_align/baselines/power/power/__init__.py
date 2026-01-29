505e0e50f2c46dbc39d52444aed8888ee366ace7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:16:29 +0000

    Add package error-align/0.1.0b8/error_align-0.1.0b8-cp313-cp313-macosx_11_0_arm64.whl

---
+++ packages/error-align/error_align-0.1.0b8-cp313-cp313-macosx_11_0_arm64.whl/error_align/baselines/power/power/__init__.py
@@ -0,0 +1,3 @@
+# from align_labeler import AlignLabeler
+from error_align.baselines.power.power.levenshtein import ExpandedAlignment, Levenshtein
+from error_align.baselines.power.power.phonemes import Phonemes
