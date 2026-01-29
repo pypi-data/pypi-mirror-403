4f705a872f2f0ea71ccec2f8f3211ba2e19f6c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:12 +0000

    Add package pvoutput-ocf/0.1.37/pvoutput_ocf-0.1.37.tar.gz

---
+++ packages/pvoutput-ocf/pvoutput_ocf-0.1.37.tar.gz/pvoutput_ocf-0.1.37/pvoutput/exceptions.py
@@ -0,0 +1,32 @@
+"""Custom Exeception classes"""
+
+import requests
+
+
+class BadStatusCode(Exception):
+    """Bad status code excepction"""
+
+    def __init__(self, response: requests.Response, message: str = ""):
+        """Init"""
+        self.response = response
+        super(BadStatusCode, self).__init__(message)
+
+    def __str__(self) -> str:
+        """String method"""
+        string = super(BadStatusCode, self).__str__()
+        string += "Status code: {}; ".format(self.response.status_code)
+        string += "Response content: {}; ".format(self.response.content)
+        string += "Response headers: {}; ".format(self.response.headers)
+        return string
+
+
+class NoStatusFound(BadStatusCode):
+    """Exeception for when no status code is found"""
+
+    pass
+
+
+class RateLimitExceeded(BadStatusCode):
+    """Class for rate limit is exceeded"""
+
+    pass
