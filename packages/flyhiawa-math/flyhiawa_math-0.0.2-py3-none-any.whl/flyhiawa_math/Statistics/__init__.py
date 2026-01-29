45d1518bf0ee66e9e4f79da450c5e487e9cd8811
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:06:49 +0000

    Add package flyhiawa-math/0.0.2/flyhiawa_math-0.0.2-py3-none-any.whl

---
+++ packages/flyhiawa-math/flyhiawa_math-0.0.2-py3-none-any.whl/flyhiawa_math/Statistics/__init__.py
@@ -0,0 +1,2 @@
+# __init__.py
+from .means import harmonic_mean, arithmetic_mean
---
+++ packages/flyhiawa-math/flyhiawa_math-0.0.2-py3-none-any.whl/flyhiawa_math/__init__.py
@@ -0,0 +1,3 @@
+# __init__.py
+from .Arithmetic import plus_list, fatorial, termial, inverse
+from .Physical import mass_energy, sensible_heat
