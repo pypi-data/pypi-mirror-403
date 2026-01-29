574686f3fcaab4195be5b495863d6c5388cbc3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:12:36 +0000

    Add package area-lib-mehak/0.1.2/area_lib_mehak-0.1.2.tar.gz

---
+++ packages/area-lib-mehak/area_lib_mehak-0.1.2.tar.gz/area_lib_mehak-0.1.2/README.md
@@ -0,0 +1,11 @@
+# Area Library
+
+Calculate area of basic shapes.
+
+## Usage
+```python
+from area import square, triangle, circle
+
+square(4)
+triangle(5, 6)
+circle(3)
---
+++ packages/area-lib-mehak/area_lib_mehak-0.1.2.tar.gz/area_lib_mehak-0.1.2/area_mehak/shapes.py
@@ -0,0 +1,10 @@
+import math
+
+def square(side):
+    return side * side
+
+def triangle(base, height):
+    return 0.5 * base * height
+
+def circle(radius):
+    return math.pi * radius * radius
