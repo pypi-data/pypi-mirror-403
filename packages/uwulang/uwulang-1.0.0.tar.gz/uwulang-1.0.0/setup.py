1be2cc7a0536a3a58fced247a8a899afcc7525f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:11:25 +0000

    Add package uwulang/1.0.0/uwulang-1.0.0.tar.gz

---
+++ packages/uwulang/uwulang-1.0.0.tar.gz/uwulang-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+name='uwulang',
+version='1.0.0',
+description='UwU',
+long_description='''UwU Lang.
+Sample:
+Hello, world!
+hewwo, wowwd!
+l - w
+k - w
+r - w
+Enjoy it!''',
+long_description_content_type='text/plain',
+install_requires=[],
+packages=find_packages(),
+author='kanderusss'
+)
