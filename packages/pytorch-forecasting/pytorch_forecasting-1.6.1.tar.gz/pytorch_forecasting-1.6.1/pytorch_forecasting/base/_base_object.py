5ecf7ab6438fe3381f035f04de44d40712542aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:12:38 +0000

    Add package pytorch-forecasting/1.6.1/pytorch_forecasting-1.6.1.tar.gz

---
+++ packages/pytorch-forecasting/pytorch_forecasting-1.6.1.tar.gz/pytorch_forecasting-1.6.1/pytorch_forecasting/base/_base_object.py
@@ -0,0 +1,9 @@
+"""Base object class for pytorch-forecasting metrics."""
+
+from skbase.base import BaseObject as _SkbaseBaseObject
+
+__all__ = ["_BaseObject"]
+
+
+class _BaseObject(_SkbaseBaseObject):
+    pass
---
+++ packages/pytorch-forecasting/pytorch_forecasting-1.6.1.tar.gz/pytorch_forecasting-1.6.1/pytorch_forecasting/utils/_dependencies/_dependencies.py
@@ -0,0 +1,32 @@
+"""Utilities for managing dependencies.
+
+Copied from sktime/skbase.
+"""
+
+from skbase.utils.dependencies import _check_soft_dependencies
+
+__all__ = ["_check_soft_dependencies", "_check_matplotlib"]
+
+
+def _check_matplotlib(ref="This feature", raise_error=True):
+    """Check if matplotlib is installed.
+
+    Parameters
+    ----------
+    ref : str, optional (default="This feature")
+        reference to the feature that requires matplotlib, used in error message
+    raise_error : bool, optional (default=True)
+        whether to raise an error if matplotlib is not installed
+
+    Returns
+    -------
+    bool : whether matplotlib is installed
+    """
+    matplotlib_present = _check_soft_dependencies("matplotlib", severity="none")
+    if raise_error and not matplotlib_present:
+        raise ImportError(
+            f"{ref} requires matplotlib."
+            " Please install matplotlib with `pip install matplotlib`."
+        )
+
+    return matplotlib_present
