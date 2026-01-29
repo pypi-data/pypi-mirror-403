e7efe43c894061a07b300fe5804b25468ef82104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:14:26 +0000

    Add package specauto/1.0.0/specauto-1.0.0-py3-none-any.whl

---
+++ packages/specauto/specauto-1.0.0-py3-none-any.whl/specauto/output/__init__.py
@@ -0,0 +1,4 @@
+"""Output subpackage init."""
+from .tables import format_autospec_results, format_test_summary, format_critical_values
+
+__all__ = ["format_autospec_results", "format_test_summary", "format_critical_values"]
