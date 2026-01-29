ce3382b58e52d753703e7545a062f82e14faea37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:31 +0000

    Add package pura-utils/0.1.0/pura_utils-0.1.0-py3-none-any.whl

---
+++ packages/pura-utils/pura_utils-0.1.0-py3-none-any.whl/pura/clipboard.py
@@ -0,0 +1,21 @@
+import subprocess
+
+
+def clipcopy(data: str) -> None:
+    """
+    Sends 'data' to 'clipcopy'; saving to your clipboard
+    """
+    proc = subprocess.run(["clipcopy"], input=data.encode())
+    proc.check_returncode()
+
+
+def clippaste() -> str:
+    """
+    Paste text from your clipboard
+    """
+    proc = subprocess.run(["clippaste"], capture_output=True)
+    try:
+        proc.check_returncode()
+    except subprocess.CalledProcessError:
+        return ""
+    return proc.stdout.decode("utf-8")
