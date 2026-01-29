4fe26fcc7c4efbd68bfed6fc7fc427d269c32579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:11:52 +0000

    Add package hur/1.1.3/hur-1.1.3.tar.gz

---
+++ packages/hur/hur-1.1.3.tar.gz/hur-1.1.3/hur/__init__.py
@@ -0,0 +1,17 @@
+"""
+This algorithm proposes a new paradigm in language model architecture, aiming to revolutionize natural language processing through direct weights adjustment and automatic parameter configuration.
+The Hur-Model is a development module that implements this concept, enabling the training, retraining, tuning, and inference of language models in a groundbreaking way.
+It reduces dependence on backpropagation through an initial weight adjustment using a HurNet network and accelerates model parameterization through optimization calculations and self-configuration.
+This code was architected, developed, and programmed by Ben-Hur Varriano for Sapiens Technology®️.
+Any use, modification, disclosure, or public commentary without prior authorization from Sapiens Technology®️ will be subject to legal action by our legal team.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from .hur import *
+"""
+This algorithm proposes a new paradigm in language model architecture, aiming to revolutionize natural language processing through direct weights adjustment and automatic parameter configuration.
+The Hur-Model is a development module that implements this concept, enabling the training, retraining, tuning, and inference of language models in a groundbreaking way.
+It reduces dependence on backpropagation through an initial weight adjustment using a HurNet network and accelerates model parameterization through optimization calculations and self-configuration.
+This code was architected, developed, and programmed by Ben-Hur Varriano for Sapiens Technology®️.
+Any use, modification, disclosure, or public commentary without prior authorization from Sapiens Technology®️ will be subject to legal action by our legal team.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
