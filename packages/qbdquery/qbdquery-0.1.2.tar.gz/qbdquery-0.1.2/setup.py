4ea22ddb93c81ccb9ec35670bf74725f4ff86112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:10:35 +0000

    Add package qbdquery/0.1.2/qbdquery-0.1.2.tar.gz

---
+++ packages/qbdquery/qbdquery-0.1.2.tar.gz/qbdquery-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qbdquery",
+    version="0.1.2",
+    packages=find_packages(exclude=["tests*", "venv*"]),
+    install_requires=[
+        "pywin32>=306",
+    ],
+    python_requires=">=3.7",
+)
