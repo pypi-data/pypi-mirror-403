1a71b4e6124d95dcc974b14393e70476d6b0777b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:18:02 +0000

    Add package superagentx/1.0.5.1/superagentx-1.0.5.1-py3-none-any.whl

---
+++ packages/superagentx/superagentx-1.0.5.1-py3-none-any.whl/superagentx/llm/client.py
@@ -0,0 +1,79 @@
+from abc import ABCMeta, abstractmethod
+
+
+from typing import Callable
+
+
+class Client(metaclass=ABCMeta):
+
+    def __init__(
+            self,
+            model: str | None = None,
+            embed_model: str | None = None,
+            **kwargs
+    ):
+        self._model = model
+        self._embed_model = embed_model
+
+    @abstractmethod
+    def chat_completion(
+            self,
+            *args,
+            **kwargs
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def achat_completion(
+            self,
+            *args,
+            **kwargs
+    ):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_tool_json(
+            self,
+            func: Callable
+    ) -> dict:
+        raise NotImplementedError
+
+    @abstractmethod
+    def embed(
+            self,
+            text: str,
+            **kwargs
+    ):
+        """
+        Get the embedding for the given text using Client.
+
+        Args:
+            text (str): The text to embed.
+
+        Returns:
+            list: The embedding vector.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    async def aembed(
+            self,
+            text: str,
+            **kwargs
+    ):
+        """
+        Get the embedding for the given text using Client.
+
+        Args:
+            text (str): The text to embed.
+
+        Returns:
+            list: The embedding vector.
+        """
+        raise NotImplementedError
+
+    def count_tokens(self, **kwargs):
+        raise NotImplementedError
+
+    async def account_tokens(self, **kwargs):
+        raise NotImplementedError
---
+++ packages/superagentx/superagentx-1.0.5.1-py3-none-any.whl/superagentx/utils/helper.py
@@ -0,0 +1,52 @@
+from typing import Any
+import re
+import asyncio
+
+from typing import Callable, Awaitable, Any, Optional, Union
+
+StatusCallback = Callable[..., Awaitable[Any]]
+
+
+async def sync_to_async(func, *args, **kwargs) -> Any:
+    return await asyncio.to_thread(func, *args, **kwargs)
+
+
+async def _maybe_await(coro):
+    if asyncio.iscoroutine(coro):
+        await coro
+
+
+async def iter_to_aiter(iterable):
+    for item in iterable:
+        yield item
+
+
+async def get_fstring_variables(s: str):
+    # This regular expression looks for variables in curly braces
+    return re.findall(r'\{(.*?)}', s)
+
+
+async def ptype_to_json_scheme(ptype: str) -> str:
+    match ptype:
+        case 'int':
+            return "integer"
+        case 'str':
+            return "string"
+        case 'bool':
+            return "boolean"
+        case 'list':
+            return "array"
+        case 'dict' | _:
+            return "object"
+
+
+async def rm_trailing_spaces(data):
+    """Recursively remove trailing whitespace from all string values in a JSON-like structure."""
+    if isinstance(data, dict):
+        return {k: await rm_trailing_spaces(v) for k, v in data.items()}
+    elif isinstance(data, list):
+        return [await rm_trailing_spaces(v) for v in data]
+    elif isinstance(data, str):
+        return data.rstrip()  # Remove trailing whitespace
+    else:
+        return data
