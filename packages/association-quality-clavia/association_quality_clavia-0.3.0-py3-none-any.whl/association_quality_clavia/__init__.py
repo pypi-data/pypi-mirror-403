6b4c8a51ae0b2cb44f7d56c594cb01c35f58e939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:11:49 +0000

    Add package association-quality-clavia/0.3.0/association_quality_clavia-0.3.0-py3-none-any.whl

---
+++ packages/association-quality-clavia/association_quality_clavia-0.3.0-py3-none-any.whl/association_quality_clavia/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.3.0'
+
+from .association_quality import ANN_ID_ABSENT, UPD_ID_LOOSE, AssociationQuality, BinClass
+
+
+__all__ = ['AssociationQuality', 'BinClass', 'UPD_ID_LOOSE', 'ANN_ID_ABSENT']
