f70f3fce0945d13721e834efa605c86cfc5245df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:09:55 +0000

    Add package debugsnacks/0.1.4/debugsnacks-0.1.4-py3-none-any.whl

---
+++ packages/debugsnacks/debugsnacks-0.1.4-py3-none-any.whl/debugsnacks/chaos.py
@@ -0,0 +1,7 @@
+def chaos(level=1):
+    if level == 0:
+        print("Chaos disabled. Boring.")
+    elif level == 1:
+        print("⚠️ Minor chaos introduced. Keys slightly judgmental.")
+    else:
+        print("🔥 MAXIMUM CHAOS. MAY THE ODDS BE EVER IN YOUR FAVOR.")
