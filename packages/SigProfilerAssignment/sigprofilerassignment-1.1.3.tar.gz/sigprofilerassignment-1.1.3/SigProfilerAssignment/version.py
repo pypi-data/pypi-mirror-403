45fb38f20d57921e68fbe5c517b350012491e794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:14:29 +0000

    Add package SigProfilerAssignment/1.1.3/sigprofilerassignment-1.1.3.tar.gz

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-1.1.3.tar.gz/sigprofilerassignment-1.1.3/SigProfilerAssignment/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '1.1.3'
+version = '1.1.3'
+Update = 'v1.1.3: Fixed set_index() calls for Python 3.12 StringArray compatibility'
+
+    
