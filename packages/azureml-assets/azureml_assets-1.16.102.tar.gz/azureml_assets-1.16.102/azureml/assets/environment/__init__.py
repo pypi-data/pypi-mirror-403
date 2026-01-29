bc8856f49da063b93af5aa65e226c258ca92f033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:12:57 +0000

    Add package azureml-assets/1.16.102/azureml_assets-1.16.102.tar.gz

---
+++ packages/azureml-assets/azureml_assets-1.16.102.tar.gz/azureml_assets-1.16.102/azureml/assets/environment/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from .build import build_images
+from .pin_versions import transform_file
+from .pin_image_versions import pin_images
+from .pin_package_versions import pin_packages
