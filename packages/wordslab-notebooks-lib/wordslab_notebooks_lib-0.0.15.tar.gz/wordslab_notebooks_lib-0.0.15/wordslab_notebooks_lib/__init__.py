03fa03bc8f210015503260a27a92a60a845dbc21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:12:28 +0000

    Add package wordslab-notebooks-lib/0.0.15/wordslab_notebooks_lib-0.0.15.tar.gz

---
+++ packages/wordslab-notebooks-lib/wordslab_notebooks_lib-0.0.15.tar.gz/wordslab_notebooks_lib-0.0.15/wordslab_notebooks_lib/__init__.py
@@ -0,0 +1,11 @@
+from .core import Wordslab
+from .notebook import WordslabNotebook
+
+__version__ = "0.0.15"
+__all__ = ["Wordslab", "WordslabNotebook"]
+
+def _jupyter_labextension_paths():
+    return [{
+        "src": "labextension",
+        "dest": "wordslab-notebooks-lib"
+    }]
