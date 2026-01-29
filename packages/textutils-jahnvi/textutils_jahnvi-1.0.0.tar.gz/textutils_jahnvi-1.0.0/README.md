96b8c97dc455c3973f1694592eb14dce11a0d66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:10:14 +0000

    Add package textutils-jahnvi/1.0.0/textutils_jahnvi-1.0.0.tar.gz

---
+++ packages/textutils-jahnvi/textutils_jahnvi-1.0.0.tar.gz/textutils_jahnvi-1.0.0/README.md
@@ -0,0 +1,10 @@
+A simple Python text utility library.
+
+# Features
+* Word count
+* Character count
+* Upper & lower case formatting
+* Palindrome checker
+
+# Installation
+pip install textutils-jahnvi
---
+++ packages/textutils-jahnvi/textutils_jahnvi-1.0.0.tar.gz/textutils_jahnvi-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textutils-jahnvi",
+    version="1.0.0",
+    description="Text utility library for counting, formatting and checking text",
+    author="Jahnvi Goyal",
+    packages=find_packages(),
+)
