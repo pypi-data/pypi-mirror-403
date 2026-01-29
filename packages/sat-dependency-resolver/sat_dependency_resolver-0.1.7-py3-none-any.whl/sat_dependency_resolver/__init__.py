ad14f877fb13def3ad66e4c170814c592fe09858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:13:26 +0000

    Add package sat-dependency-resolver/0.1.7/sat_dependency_resolver-0.1.7-py3-none-any.whl

---
+++ packages/sat-dependency-resolver/sat_dependency_resolver-0.1.7-py3-none-any.whl/sat_dependency_resolver/__init__.py
@@ -0,0 +1,5 @@
+from .encoder import SATEncoder, Package
+from .resolver import DependencyResolver, ResolutionResult
+
+__version__ = "0.1.7"
+__all__ = ["SATEncoder", "Package", "DependencyResolver", "ResolutionResult"]
