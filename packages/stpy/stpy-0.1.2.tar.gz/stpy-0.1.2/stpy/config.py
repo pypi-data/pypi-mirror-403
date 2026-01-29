4190c6a512c0b480bce611605d38a51203678294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:06:49 +0000

    Add package stpy/0.1.2/stpy-0.1.2.tar.gz

---
+++ packages/stpy/stpy-0.1.2.tar.gz/stpy-0.1.2/stpy/config.py
@@ -0,0 +1,11 @@
+"""
+This contains some configurations.
+
+"""
+
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent
+sys.path.append(str(PROJECT_ROOT))
+# print(PROJECT_ROOT)
