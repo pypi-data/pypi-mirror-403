547eee45220ae85b80f1e29b14fde4a3cdbe08f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:19:10 +0000

    Add package ls-dlp/0.0.4/ls_dlp-0.0.4-py3-none-any.whl

---
+++ packages/ls-dlp/ls_dlp-0.0.4-py3-none-any.whl/ls_dlp/headers.py
@@ -0,0 +1,7 @@
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/70.0.3538.102 Safari/537.36 Edge/18.1958",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:132.0) Gecko/20100101 Firefox/132.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+]
