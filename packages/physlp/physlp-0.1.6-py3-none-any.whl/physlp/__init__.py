c4e3ac7b87edb62593d5ed6d2ec15b15b8bea4fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:29 +0000

    Add package physlp/0.1.6/physlp-0.1.6-py3-none-any.whl

---
+++ packages/physlp/physlp-0.1.6-py3-none-any.whl/physlp/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Date          : 2025-08-28
+# Author        : Lancelot PINCET
+# GitHub        : https://github.com/LancelotPincet
+# Library       : physLP
+
+"""
+A library providing tools for basic physics.
+"""
+
+
+
+# %% Source code
+sources = {
+
+}
+
+
+
+# %% Lazy imports
+from corelp import getmodule
+__getattr__, __all__ = getmodule(sources)
