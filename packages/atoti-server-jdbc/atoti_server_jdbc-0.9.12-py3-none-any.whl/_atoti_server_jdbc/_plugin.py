949be78f3b99252fb6100d1a4ad67ce24896c058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:12:17 +0000

    Add package atoti-server-jdbc/0.9.12/atoti_server_jdbc-0.9.12-py3-none-any.whl

---
+++ packages/atoti-server-jdbc/atoti_server_jdbc-0.9.12-py3-none-any.whl/_atoti_server_jdbc/_plugin.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from dataclasses import replace
+from typing import TYPE_CHECKING, final
+
+from _atoti_core import Plugin
+from typing_extensions import override
+
+from ._resources_directory import RESOURCES_DIRECTORY
+
+if TYPE_CHECKING:
+    import atoti as tt  # pylint: disable=nested-import,undeclared-dependency
+
+
+@final
+class SqlPlugin(Plugin):
+    @override
+    def session_config_hook(
+        self,
+        session_config: tt.SessionConfig,
+    ) -> tt.SessionConfig:
+        return replace(
+            session_config,
+            extra_jars=[
+                *session_config.extra_jars,
+                RESOURCES_DIRECTORY / "plugin.jar",
+            ],
+        )
+
+    @property
+    @override
+    def _key(self) -> str | None:
+        return "jdbc"
