71a1dd6622e238b99f110f3f5af9a48c5c351c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:19:07 +0000

    Add package pyJHTDB/20260124.2/pyjhtdb-20260124.2.tar.gz

---
+++ packages/pyJHTDB/pyjhtdb-20260124.2.tar.gz/pyjhtdb-20260124.2/pyJHTDB/__init__.py
@@ -0,0 +1,7 @@
+raise ImportError(
+    "\n\n"
+    "'pyJHTDB' is DEPRECATED and no longer maintained.\n"
+    "Please use 'giverny' instead:\n\n"
+    "    pip install giverny\n\n"
+    "The Johns Hopkins Turbulence 'giverny' library can be found at: https://github.com/sciserver/giverny\n"
+)
