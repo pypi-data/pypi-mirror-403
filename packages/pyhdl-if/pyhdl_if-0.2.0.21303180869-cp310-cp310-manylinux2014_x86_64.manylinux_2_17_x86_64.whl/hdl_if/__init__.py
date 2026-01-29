bbf34490b20c628a9163970b04048df98de04bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:11:10 +0000

    Add package pyhdl-if/0.2.0.21303180869/pyhdl_if-0.2.0.21303180869-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/pyhdl-if/pyhdl_if-0.2.0.21303180869-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/hdl_if/__init__.py
@@ -0,0 +1,91 @@
+
+import ctypes
+import os
+import platform
+
+from .hdl_services import HdlServices
+from .backend import Backend
+from .impl.decl_rgy import DeclRgy, MethodDecoratorBase, ClsDecoratorBase
+from .impl.typeinfo import TypeInfo
+from .decorators import *
+from .hdl_obj_rgy import *
+
+# class s_vpi_vlog_info(ctypes.Structure):
+#     _fields_ = [
+#         ('argc', ctypes.c_int32),
+#         ('argv', ctypes.POINTER(ctypes.c_char_p)),
+#         ('product', ctypes.c_char_p),
+#         ('version', ctypes.c_char_p)
+#     ]
+
+
+# print("hdl_if.init", flush=True)
+
+# def init():
+#     print("::init", flush=True)
+
+# try:
+#     exe = ctypes.cdll.LoadLibrary(None)
+
+#     exe.vpi_get_vlog_info.restype = ctypes.c_int
+#     exe.vpi_get_vlog_info.argtypes = [
+#         ctypes.POINTER(s_vpi_vlog_info)
+#     ]
+
+#     info = s_vpi_vlog_info()
+#     exe.vpi_get_vlog_info(ctypes.pointer(info))
+
+#     print("argc: %d" % info.argc)
+
+# except Exception as e:
+#     print("Exception: %s" % str(e))
+
+def vpi_init():
+    import hdl_if.vpi as vpi
+    print("::vpi_init")
+    vpi.vpi_init()
+
+# def dpi_init(scope):
+#     import hdl_if.dpi as dpi
+#     print("::dpi_init")
+
+#     dpi.dpi_init(scope)
+
+def get_entry():
+    hdl_pi_if_dir = os.path.dirname(os.path.abspath(__file__))
+    libpref = "lib"
+    dllext = ".so"
+    if platform.system() == "Windows":
+        libpref = ""
+        dllext = ".dll"
+    elif platform.system() == "Darwin":
+        libpref = "lib"
+        dllext = ".dylib"
+
+    ext = os.path.join(hdl_pi_if_dir, "%spyhdl_if%s" % (libpref, dllext))
+    if os.path.isfile(ext):
+        return ext
+    else:
+        raise Exception("Library path %s doesn't exist" % ext)
+
+def root():
+    pkg_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(pkg_dir)
+
+def share():
+    pkg_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(pkg_dir, "share")
+
+def libs(kind="dpi"):
+    import sys
+    import sysconfig
+
+    lib_filename = get_entry()
+
+    ret = [ lib_filename ]
+
+    return ret
+
+@property
+def backend() -> Backend:
+    return Backend.inst()
