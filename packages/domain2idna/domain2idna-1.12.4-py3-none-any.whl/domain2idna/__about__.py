cce2c9802b5ed41476879a7b890bb46c462fbb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:18:42 +0000

    Add package domain2idna/1.12.4/domain2idna-1.12.4-py3-none-any.whl

---
+++ packages/domain2idna/domain2idna-1.12.4-py3-none-any.whl/domain2idna/__about__.py
@@ -0,0 +1,44 @@
+"""
+domain2idna - The tool to convert a domain or a file with a list
+of domain to the famous IDNA format.
+
+Author:
+    Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+
+Contributors:
+    Let's contribute to domains2idna!!
+
+Project link:
+    https://github.com/PyFunceble/domain2idna
+
+Project documentation:
+    http://domain2idna.readthedocs.io
+
+License:
+::
+
+    MIT License
+
+    Copyright (c) 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026 Nissar Chababy
+    Copyright (c) 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026 PyFunceble Contributors
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+
+__version__ = "1.12.4"
