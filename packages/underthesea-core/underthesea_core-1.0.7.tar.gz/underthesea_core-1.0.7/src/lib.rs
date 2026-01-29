72e6efabb939bc4c8e12f27ea877d9bf976b7ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:14:19 +0000

    Add package underthesea-core/1.0.7/underthesea_core-1.0.7.tar.gz

---
+++ packages/underthesea-core/underthesea_core-1.0.7.tar.gz/underthesea_core-1.0.7/src/lib.rs
@@ -0,0 +1,39 @@
+extern crate regex;
+extern crate pyo3;
+
+use pyo3::prelude::*;
+use pyo3::types::PyModule;
+use std::collections::HashSet;
+
+pub mod featurizers;
+
+#[pyclass]
+pub struct CRFFeaturizer {
+    pub object: featurizers::CRFFeaturizer
+}
+
+
+#[pymethods]
+impl CRFFeaturizer {
+    #[new]
+    pub fn new(feature_configs: Vec<String>, dictionary: HashSet<String>) -> PyResult<Self> {
+        Ok(CRFFeaturizer {
+            object: featurizers::CRFFeaturizer::new(feature_configs, dictionary)
+        })
+    }
+
+    pub fn process(self_: PyRef<Self>, sentences: Vec<Vec<Vec<String>>>) -> PyResult<Vec<Vec<Vec<String>>>> {
+        let output = self_.object.process(sentences);
+        Ok(output)
+    }
+
+    // pub fn process(self_: PyRef<Self>, sentences: Vec<Vec<Vec<String>>>) -> PyResult<String> {
+    //     Ok("5".to_string())
+    // }
+}
+
+#[pymodule]
+fn underthesea_core(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
+    m.add_class::<CRFFeaturizer>()?;
+    Ok(())
+}
