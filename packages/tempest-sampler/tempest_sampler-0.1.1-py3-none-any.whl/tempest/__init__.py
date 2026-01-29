11787fc0a8c8ce10776b9c3951e9c0c5ef1c1a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:14:58 +0000

    Add package tempest-sampler/0.1.1/tempest_sampler-0.1.1-py3-none-any.whl

---
+++ packages/tempest-sampler/tempest_sampler-0.1.1-py3-none-any.whl/tempest/__init__.py
@@ -0,0 +1,22 @@
+__bibtex__ = """
+@article{karamanis2025persistent,
+  title={Persistent Sampling: Enhancing the Efficiency of Sequential Monte Carlo},
+  author={Karamanis, Minas and Seljak, Uro{\v{s}}},
+  journal={Statistics and Computing},
+  volume={35},
+  number={5},
+  pages={1--22},
+  year={2025},
+  publisher={Springer}
+}
+"""
+__url__ = "https://tempest-sampler.readthedocs.io"
+__author__ = "Minas Karamanis"
+__email__ = "minaskar@gmail.com"
+__license__ = "MIT"
+__description__ = "A Python implementation of Persistent Sampling for accelerated Bayesian Computation"
+__version__ = "0.1.1"
+
+from .sampler import Sampler
+
+__all__ = ["Sampler"]
