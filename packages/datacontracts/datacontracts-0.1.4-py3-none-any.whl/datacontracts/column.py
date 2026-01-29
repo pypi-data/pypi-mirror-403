f28865f744da26b22d837879cd9f4bbc1860afe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:10:02 +0000

    Add package datacontracts/0.1.4/datacontracts-0.1.4-py3-none-any.whl

---
+++ packages/datacontracts/datacontracts-0.1.4-py3-none-any.whl/datacontracts/column.py
@@ -0,0 +1,20 @@
+class Column:
+    def __init__(
+        self,
+        dtype,
+        min=None,
+        max=None,
+        allowed=None,
+        lt=None,
+        gt=None,
+        between=None,
+        nullable=False,
+    ):
+        self.dtype = dtype
+        self.min = min
+        self.max = max
+        self.allowed = allowed
+        self.lt = lt
+        self.gt = gt
+        self.between = between
+        self.nullable = nullable
