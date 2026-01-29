8f1f24f48c992da06cf6d6cbc7e258ffc16c2a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:18:36 +0000

    Add package area-dudi/0.0.1/area_dudi-0.0.1.tar.gz

---
+++ packages/area-dudi/area_dudi-0.0.1.tar.gz/area_dudi-0.0.1/Library/Area_calculations.py
@@ -0,0 +1,11 @@
+def square(side):
+    return side * side
+
+def triangle(base, height):
+    return 0.5 * base * height
+
+def circle(radius):
+    return 3.14 * radius * radius
+
+def rectangle(l, b):
+    return l*b
