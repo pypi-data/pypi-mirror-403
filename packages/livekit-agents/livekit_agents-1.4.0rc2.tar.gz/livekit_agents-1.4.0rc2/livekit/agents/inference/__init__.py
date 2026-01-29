8fe0cd37aab1d15277a171b9e3cc1cdf218e5ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:10:16 +0000

    Add package livekit-agents/1.4.0rc2/livekit_agents-1.4.0rc2.tar.gz

---
+++ packages/livekit-agents/livekit_agents-1.4.0rc2.tar.gz/livekit_agents-1.4.0rc2/livekit/agents/inference/__init__.py
@@ -0,0 +1,21 @@
+from .interruption import (
+    AdaptiveInterruptionDetector,
+    InterruptionDetectionError,
+    InterruptionEvent,
+)
+from .llm import LLM, LLMModels, LLMStream
+from .stt import STT, STTModels
+from .tts import TTS, TTSModels
+
+__all__ = [
+    "STT",
+    "TTS",
+    "LLM",
+    "LLMStream",
+    "STTModels",
+    "TTSModels",
+    "LLMModels",
+    "AdaptiveInterruptionDetector",
+    "InterruptionEvent",
+    "InterruptionDetectionError",
+]
