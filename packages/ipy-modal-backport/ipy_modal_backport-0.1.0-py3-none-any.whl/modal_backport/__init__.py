425b67bd6ea2b429dcdb297fbb1dfd8b6188e829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:06:43 +0000

    Add package ipy-modal-backport/0.1.0/ipy_modal_backport-0.1.0-py3-none-any.whl

---
+++ packages/ipy-modal-backport/ipy_modal_backport-0.1.0-py3-none-any.whl/modal_backport/__init__.py
@@ -0,0 +1,31 @@
+"""
+ipy-modal-phase-backport
+
+A project to backport new modal features into interactions.py v5.
+:copyright: (c) 2026-present AstreaTSS
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.1.0"
+
+from .components import *
+from .context import *
+from .enums import *
+from .modal import *
+
+__all__ = (
+    "BaseSelectMenu",
+    "ChannelSelectMenu",
+    "ComponentType",
+    "DefaultableSelectMenu",
+    "FileUploadComponent",
+    "LabelComponent",
+    "MentionableSelectMenu",
+    "Modal",
+    "ModalContext",
+    "RoleSelectMenu",
+    "StringSelectMenu",
+    "UpdatedModalContextMixin",
+    "UserSelectMenu",
+    "__version__",
+)
