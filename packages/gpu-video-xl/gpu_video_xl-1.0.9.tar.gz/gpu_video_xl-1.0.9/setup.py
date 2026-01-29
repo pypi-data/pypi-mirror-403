b785ccc42c481d6faedd61b96e5943ba4bd6d540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:19:08 +0000

    Add package gpu-video-xl/1.0.9/gpu_video_xl-1.0.9.tar.gz

---
+++ packages/gpu-video-xl/gpu_video_xl-1.0.9.tar.gz/gpu_video_xl-1.0.9/setup.py
@@ -0,0 +1,27 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+    name='gpu_video_xl',
+    version='1.0.9',
+    license="MIT",  # Especifica la licencia directamente
+    include_package_data=True,  # ← Añade esta línea
+    author='IA (Sistema de Intres)',
+    author_email='system.ai.of.interest@gmail.com',
+    description='Esta librería fue creada por IA Sistema de Intres',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://www.youtube.com/@IA.Sistema.de.Interes',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+        'tqdm',
+        'Pillow',
+        'IPython'
+    ],
+)
+
