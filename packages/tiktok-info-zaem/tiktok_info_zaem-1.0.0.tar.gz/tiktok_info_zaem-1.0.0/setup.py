e38c820d7e5560f39a0fb2000eb99fbe0a181c99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:13:46 +0000

    Add package tiktok-info-zaem/1.0.0/tiktok_info_zaem-1.0.0.tar.gz

---
+++ packages/tiktok-info-zaem/tiktok_info_zaem-1.0.0.tar.gz/tiktok_info_zaem-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="tiktok-info-zaem",
+    version="1.0.0",
+    description="TikTok user info library",
+    author="ZAEM",
+    packages=["tiktok_info"],
+    install_requires=["requests"],
+)
