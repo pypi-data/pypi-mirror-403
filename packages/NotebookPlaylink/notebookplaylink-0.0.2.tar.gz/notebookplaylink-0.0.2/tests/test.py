5251e320d18c312ef29632836587d1e585c5c98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:10:33 +0000

    Add package NotebookPlaylink/0.0.2/notebookplaylink-0.0.2.tar.gz

---
+++ packages/NotebookPlaylink/notebookplaylink-0.0.2.tar.gz/notebookplaylink-0.0.2/tests/test.py
@@ -0,0 +1,10 @@
+from NotebookPlaylink.logger import logger
+
+from NotebookPlaylink.custom_exception import InvalidURLException
+
+logger.info("This is a test log messasge from test.py")
+
+try:
+    raise InvalidURLException("The provided URL is invalid")
+except Exception as e:
+    logger.error(f"Caught an exception: {e}")
