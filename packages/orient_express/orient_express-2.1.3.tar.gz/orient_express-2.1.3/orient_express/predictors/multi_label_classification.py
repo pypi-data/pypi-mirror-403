1101396e6e386898cc7dcb34fbb97dedce96d155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:11:51 +0000

    Add package orient_express/2.1.3/orient_express-2.1.3.tar.gz

---
+++ packages/orient_express/orient_express-2.1.3.tar.gz/orient_express-2.1.3/orient_express/predictors/multi_label_classification.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+
+import cv2
+import numpy as np
+from PIL import Image
+
+from .predictor import OnnxSessionWrapper, ImagePredictor
+
+
+@dataclass
+class MultiLabelClassificationPrediction:
+    classes: list[str]
+    class_scores: dict[str, float]
+
+    def to_dict(self):
+        return {
+            "classes": self.classes,
+            "class_scores": self.class_scores,
+        }
+
+
+class OnnxMultiLabelClassifier(OnnxSessionWrapper):
+    def __call__(self, pil_images: list[Image.Image]):
+        images_array = self.collate_images(pil_images)
+
+        input_dict = {self.input_names[0]: images_array}
+
+        scores = self.session.run(None, input_dict)[0]
+        return scores
+
+
+class MultiLabelClassificationPredictor(ImagePredictor):
+    model_type = "multi-label-classification-onnx"
+    backend_model = OnnxMultiLabelClassifier
+
+    def predict(
+        self, images: list[Image.Image], confidence: float
+    ) -> list[MultiLabelClassificationPrediction]:
+        if not images:
+            return []
+        raw_outputs = self.model(images)
+        outputs = []
+        for class_scores in raw_outputs:
+            classes = []
+            # self.classes is 1-indexed
+            for idx, score in enumerate(class_scores):
+                if score > confidence:
+                    classes.append(self.classes.get(idx + 1, "Unknown"))
+            outputs.append(
+                MultiLabelClassificationPrediction(
+                    classes=classes,
+                    class_scores={
+                        # self.classes is 1-indexed
+                        clss: float(class_scores[class_idx - 1])
+                        for class_idx, clss in self.classes.items()
+                    },
+                )
+            )
+        return outputs
+
+    def get_annotated_image(
+        self, image: Image.Image, predictions: MultiLabelClassificationPrediction
+    ):
+        return None
