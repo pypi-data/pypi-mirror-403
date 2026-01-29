f40d5d6718e91f7d3d078ecfcee8ee0cf9e03403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:08:27 +0000

    Add package vuer/0.0.79rc1/vuer-0.0.79rc1-py3-none-any.whl

---
+++ packages/vuer/vuer-0.0.79rc1-py3-none-any.whl/vuer/__init__.py
@@ -0,0 +1,26 @@
+# Conditional import for the pyScript.
+try:
+    from vuer.server import Vuer, VuerSession
+except ImportError:
+    print("""
+    By default, vuer does not include the aiohttp and aiohttp-cors packages
+    to enable installation in PyScript environments. For most usecase, you
+    should install with the [all] option to include all dependencies.
+
+        pip install 'vuer[all]'
+
+    Use `'` if using zsh since `[` is a special character.
+
+    Also, we require numpy>=1.21 for numpy.typing.NDArray.
+    """)
+
+from vuer.client import VuerClient
+
+
+def entrypoint():
+    """CLI entrypoint for vuer command."""
+    app = Vuer()
+    app.run()
+
+
+__all__ = ["Vuer", "VuerSession", "VuerClient", "entrypoint"]
