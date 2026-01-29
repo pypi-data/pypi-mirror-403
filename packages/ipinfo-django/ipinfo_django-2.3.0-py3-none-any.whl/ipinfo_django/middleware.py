164c825863935400b768893c958649b8164e13de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:11:55 +0000

    Add package ipinfo-django/2.3.0/ipinfo_django-2.3.0-py3-none-any.whl

---
+++ packages/ipinfo-django/ipinfo_django-2.3.0-py3-none-any.whl/ipinfo_django/middleware.py
@@ -0,0 +1,212 @@
+import logging
+import traceback
+
+import ipinfo
+from django.conf import settings
+
+from ipinfo_django.helpers import is_bot
+from ipinfo_django.ip_selector.default import DefaultIPSelector
+
+LOGGER = logging.getLogger(__name__)
+
+
+class IPinfoMiddleware:
+    def __init__(self, get_response=None):
+        """
+        Initializes class while gettings user settings and creating the cache.
+        """
+        self.get_response = get_response
+        self.filter = getattr(settings, "IPINFO_FILTER", self.is_bot)
+
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ip_selector = getattr(settings, "IPINFO_IP_SELECTOR", DefaultIPSelector())
+        self.ipinfo = ipinfo.getHandler(ipinfo_token, **ipinfo_settings)
+
+    def __call__(self, request):
+        """Middleware hook that acts on and modifies request object."""
+        try:
+            if self.filter and self.filter(request):
+                request.ipinfo = None
+            else:
+                request.ipinfo = self.ipinfo.getDetails(
+                    self.ip_selector.get_ip(request)
+                )
+        except Exception:
+            request.ipinfo = None
+            LOGGER.error(traceback.format_exc())
+
+        response = self.get_response(request)
+        return response
+
+    def is_bot(self, request):
+        return is_bot(request)
+
+
+class IPinfoAsyncMiddleware:
+    sync_capable = False
+    async_capable = True
+
+    def __init__(self, get_response):
+        """Initialize class, get settings, and create the cache."""
+        self.get_response = get_response
+
+        self.filter = getattr(settings, "IPINFO_FILTER", self.is_bot)
+
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ip_selector = getattr(settings, "IPINFO_IP_SELECTOR", DefaultIPSelector())
+        self.ipinfo = ipinfo.getHandlerAsync(ipinfo_token, **ipinfo_settings)
+
+    def __call__(self, request):
+        return self.__acall__(request)
+
+    async def __acall__(self, request):
+        """Middleware hook that acts on and modifies request object."""
+        try:
+            if self.filter and self.filter(request):
+                request.ipinfo = None
+            else:
+                request.ipinfo = await self.ipinfo.getDetails(
+                    self.ip_selector.get_ip(request)
+                )
+        except Exception:
+            request.ipinfo = None
+            LOGGER.error(traceback.format_exc())
+
+        response = await self.get_response(request)
+        return response
+
+    def is_bot(self, request):
+        return is_bot(request)
+
+
+class IPinfoLiteMiddleware(IPinfoMiddleware):
+    def __init__(self, get_response):
+        super().__init__(get_response=get_response)
+
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ipinfo = ipinfo.getHandlerLite(ipinfo_token, **ipinfo_settings)
+
+
+class IPinfoAsyncLiteMiddleware(IPinfoAsyncMiddleware):
+    sync_capable = False
+    async_capable = True
+
+    def __init__(self, get_response):
+        super().__init__(get_response=get_response)
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ipinfo = ipinfo.getHandlerAsyncLite(ipinfo_token, **ipinfo_settings)
+
+
+class IPinfoCoreMiddleware(IPinfoMiddleware):
+    def __init__(self, get_response):
+        super().__init__(get_response=get_response)
+
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ipinfo = ipinfo.getHandlerCore(ipinfo_token, **ipinfo_settings)
+
+
+class IPinfoAsyncCoreMiddleware(IPinfoAsyncMiddleware):
+    sync_capable = False
+    async_capable = True
+
+    def __init__(self, get_response):
+        super().__init__(get_response=get_response)
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ipinfo = ipinfo.getHandlerAsyncCore(ipinfo_token, **ipinfo_settings)
+
+
+class IPinfoPlusMiddleware(IPinfoMiddleware):
+    def __init__(self, get_response):
+        super().__init__(get_response=get_response)
+
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ipinfo = ipinfo.getHandlerPlus(ipinfo_token, **ipinfo_settings)
+
+
+class IPinfoAsyncPlusMiddleware(IPinfoAsyncMiddleware):
+    sync_capable = False
+    async_capable = True
+
+    def __init__(self, get_response):
+        super().__init__(get_response=get_response)
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ipinfo = ipinfo.getHandlerAsyncPlus(ipinfo_token, **ipinfo_settings)
+
+
+class IPinfoResproxyMiddleware:
+    def __init__(self, get_response=None):
+        """
+        Initializes class while gettings user settings and creating the cache.
+        """
+        self.get_response = get_response
+        self.filter = getattr(settings, "IPINFO_FILTER", self.is_bot)
+
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ip_selector = getattr(settings, "IPINFO_IP_SELECTOR", DefaultIPSelector())
+        self.ipinfo = ipinfo.getHandler(ipinfo_token, **ipinfo_settings)
+
+    def __call__(self, request):
+        """Middleware hook that acts on and modifies request object."""
+        try:
+            if self.filter and self.filter(request):
+                request.ipinfo_resproxy = None
+            else:
+                request.ipinfo_resproxy = self.ipinfo.getResproxy(
+                    self.ip_selector.get_ip(request)
+                )
+        except Exception:
+            request.ipinfo_resproxy = None
+            LOGGER.error(traceback.format_exc())
+
+        response = self.get_response(request)
+        return response
+
+    def is_bot(self, request):
+        return is_bot(request)
+
+
+class IPinfoAsyncResproxyMiddleware:
+    sync_capable = False
+    async_capable = True
+
+    def __init__(self, get_response):
+        """Initialize class, get settings, and create the cache."""
+        self.get_response = get_response
+
+        self.filter = getattr(settings, "IPINFO_FILTER", self.is_bot)
+
+        ipinfo_token = getattr(settings, "IPINFO_TOKEN", None)
+        ipinfo_settings = getattr(settings, "IPINFO_SETTINGS", {})
+        self.ip_selector = getattr(settings, "IPINFO_IP_SELECTOR", DefaultIPSelector())
+        self.ipinfo = ipinfo.getHandlerAsync(ipinfo_token, **ipinfo_settings)
+
+    def __call__(self, request):
+        return self.__acall__(request)
+
+    async def __acall__(self, request):
+        """Middleware hook that acts on and modifies request object."""
+        try:
+            if self.filter and self.filter(request):
+                request.ipinfo_resproxy = None
+            else:
+                request.ipinfo_resproxy = await self.ipinfo.getResproxy(
+                    self.ip_selector.get_ip(request)
+                )
+        except Exception:
+            request.ipinfo_resproxy = None
+            LOGGER.error(traceback.format_exc())
+
+        response = await self.get_response(request)
+        return response
+
+    def is_bot(self, request):
+        return is_bot(request)
