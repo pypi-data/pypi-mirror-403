8ed7f5aa189095b22b1764e5b56a32148f836693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:12:29 +0000

    Add package tcurve/0.1.2/tcurve-0.1.2-py3-none-any.whl

---
+++ packages/tcurve/tcurve-0.1.2-py3-none-any.whl/tcurve/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+'''
+Created by Seria at 18/03/20125 11:00 PM
+Email: zzqsummerai@yeah.net
+
+                    _ooOoo_
+                  o888888888o
+                 o88`_ . _`88o
+                 (|  0   0  |)
+                 O \   。   / O
+              _____/`-----‘\_____
+            .’   \||  _ _  ||/   `.
+            |  _ |||   |   ||| _  |
+            |  |  \\       //  |  |
+            |  |    \-----/    |  |
+             \ .\ ___/- -\___ /. /
+         ,--- /   ___\<|>/___   \ ---,
+         | |:    \    \ /    /    :| |
+         `\--\_    -. ___ .-    _/--/‘
+   ===========  \__  NOBUG  __/  ===========
+
+'''
+# -*- coding:utf-8 -*-
+from .dash import Dash, RAW, PERCENT, INVIZ, IMAGE, CUSTOM
