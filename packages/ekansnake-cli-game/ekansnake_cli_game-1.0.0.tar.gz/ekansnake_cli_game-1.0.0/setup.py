65e37efb661607edc8a20e6632c2260d7c623385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:08:01 +0000

    Add package ekansnake-cli-game/1.0.0/ekansnake_cli_game-1.0.0.tar.gz

---
+++ packages/ekansnake-cli-game/ekansnake_cli_game-1.0.0.tar.gz/ekansnake_cli_game-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ekansnake-cli-game",  # MUST be unique on PyPI
+    version="1.0.0",
+    author="Abhishek Jha",
+    description="A CLI Snake Game with obstacles and high scores",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/AbhishekJha3511/cli-snake-game",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "windows-curses; platform_system=='Windows'",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ekansnake=snake_game.__init__:main_menu",
+        ],
+    },
+)
