12483d0ed4c740e319d8a03e90b123912ebc5361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:17 +0000

    Add package crunch-convert/0.4.1/crunch_convert-0.4.1.tar.gz

---
+++ packages/crunch-convert/crunch_convert-0.4.1.tar.gz/crunch_convert-0.4.1/crunch_convert/__init__.py
@@ -0,0 +1,10 @@
+"""
+crunch-convert
+~~~~~~~~~~~~~~
+The crunch module to convert a notebook into multiple components!
+"""
+
+from crunch_convert import notebook as notebook
+from crunch_convert import requirements_txt as requirements_txt
+from crunch_convert.__version__ import __version__
+from crunch_convert._model import RequirementLanguage as RequirementLanguage
---
+++ packages/crunch-convert/crunch_convert-0.4.1.tar.gz/crunch_convert-0.4.1/crunch_convert/_model.py
@@ -0,0 +1,27 @@
+from enum import Enum
+from typing import Any, cast
+
+
+class RequirementLanguage(Enum):
+    PYTHON = "PYTHON", "requirements.txt", "requirements.original.txt"
+    R = "R", "requirements.r.txt", "requirements.r.original.txt"
+
+    # https://stackoverflow.com/questions/12680080/python-enums-with-attributes/19300424#19300424
+    def __new__(
+        cls,
+        *args: Any,
+        **kwds: Any,
+    ) -> "RequirementLanguage":
+        obj = object.__new__(cls)
+        obj._value_ = cast(str, args[0])
+        return obj
+
+    def __init__(
+        self,
+        value: str,
+        txt_file_name: str,
+        original_txt_file_name: str,
+    ):
+        value  # type: ignore
+        self.txt_file_name = txt_file_name
+        self.original_txt_file_name = original_txt_file_name
