d6aef7a580c96a0d1675a3ceb6c8a3feecd9270c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:11:39 +0000

    Add package unitelabs-sila/0.6.2/unitelabs_sila-0.6.2-py3-none-any.whl

---
+++ packages/unitelabs-sila/unitelabs_sila-0.6.2-py3-none-any.whl/sila/framework/command/command_execution.py
@@ -0,0 +1,17 @@
+import typing_extensions as typing
+
+if typing.TYPE_CHECKING:
+    from .observable_command import ObservableCommand
+
+
+class CommandExecution(typing.Protocol):
+    """Tracks the progress of an observable command execution."""
+
+    command_execution_uuid: str
+    """Uniquely identifies the Command execution."""
+
+    command: "ObservableCommand"
+    """The observable command being executed."""
+
+    def cancel(self) -> None:
+        """Cancel the ongoing command execution."""
---
+++ packages/unitelabs-sila/unitelabs_sila-0.6.2-py3-none-any.whl/sila/framework/common/__init__.py
@@ -0,0 +1,17 @@
+from .context import Context
+from .context_proxy import ContextProxy
+from .execution import Execution
+from .feature import Feature
+from .handler import Handler
+from .server import Server
+from .stream import Stream
+
+__all__ = [
+    "Context",
+    "ContextProxy",
+    "Execution",
+    "Feature",
+    "Handler",
+    "Server",
+    "Stream",
+]
