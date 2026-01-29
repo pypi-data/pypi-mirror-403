1127f8e0bb60e99948fc29bc40e40a7d769b4bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 14:16:16 +0000

    Add package DocLM/1.0.0/doclm-1.0.0.tar.gz

---
+++ packages/DocLM/doclm-1.0.0.tar.gz/doclm-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="DocLM",
+    version="1.0.0",
+    author="Louati Mahdi",
+    author_email="louatimahdi390@gmail.com",
+    description="An intelligent document analysis library for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your-username/DocLM",  # Replace with your GitHub repo URL
+    packages=find_packages(),
+    install_requires=[
+        "PyMuPDF",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
