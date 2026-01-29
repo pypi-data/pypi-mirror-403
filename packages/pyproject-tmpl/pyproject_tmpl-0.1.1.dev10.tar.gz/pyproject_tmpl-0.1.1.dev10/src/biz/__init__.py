e4a198e54f47fbfd0bb7daf7de7928cc3bf746c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 18:12:33 +0000

    Add package pyproject-tmpl/0.1.1.dev10/pyproject_tmpl-0.1.1.dev10.tar.gz

---
+++ packages/pyproject-tmpl/pyproject_tmpl-0.1.1.dev10.tar.gz/pyproject_tmpl-0.1.1.dev10/src/biz/__init__.py
@@ -0,0 +1,9 @@
+from ipa.decorator import deprecated
+from my_sdk import add as my_sdk_add
+
+
+@deprecated("use my_sdk.add instead")
+def demo_biz_add(a: int, b: int) -> int:
+    c = my_sdk_add(a, b)
+    print(f"add({a}, {b}) = {c}")
+    return c
