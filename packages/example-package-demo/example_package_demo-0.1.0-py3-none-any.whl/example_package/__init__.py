e6f15eb768fb1dcae5a4cc367eed16d39f45dd79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:14:23 +0000

    Add package example-package-demo/0.1.0/example_package_demo-0.1.0-py3-none-any.whl

---
+++ packages/example-package-demo/example_package_demo-0.1.0-py3-none-any.whl/example_package/__init__.py
@@ -0,0 +1,11 @@
+"""一个简单的 Python 包示例"""
+
+__version__ = "0.1.0"
+
+def hello(name: str) -> str:
+    """返回问候语"""
+    return f"Hello, {name}!"
+
+def add(a: int, b: int) -> int:
+    """两数相加"""
+    return a + b
