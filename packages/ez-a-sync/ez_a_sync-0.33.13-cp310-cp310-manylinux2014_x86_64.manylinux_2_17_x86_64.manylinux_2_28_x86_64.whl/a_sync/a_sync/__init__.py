cd274f5df608b162467c7d475e3ba6b37b2af416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:09:04 +0000

    Add package ez-a-sync/0.33.13/ez_a_sync-0.33.13-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/ez-a-sync/ez_a_sync-0.33.13-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/a_sync/a_sync/__init__.py
@@ -0,0 +1,52 @@
+"""
+This module enables developers to write both synchronous and asynchronous code without having to write redundant code.
+
+The two main objects you should use are
+    - a decorator `@a_sync()`
+    - a base class `ASyncGenericBase` which can be used to create classes that can be utilized in both synchronous and asynchronous contexts.
+
+The rest of the objects are exposed for type checking only, you should not make use of them otherwise.
+"""
+
+# TODO: double check on these before adding them to docs
+# - two decorators @:class:`property` and @:class:`cached_property` for the creation of dual-function properties and cached properties, respectively.
+
+from a_sync.a_sync.base import ASyncGenericBase
+from a_sync.a_sync.decorator import a_sync
+from a_sync.a_sync.function import (ASyncFunction, ASyncFunctionAsyncDefault,
+                                    ASyncFunctionSyncDefault)
+# NOTE: We purposely import this without including in __all__. Do not remove.
+from a_sync.a_sync.modifiers.semaphores import apply_semaphore
+from a_sync.a_sync.property import ASyncCachedPropertyDescriptor
+from a_sync.a_sync.property import ASyncCachedPropertyDescriptor as cached_property
+from a_sync.a_sync.property import (ASyncCachedPropertyDescriptorAsyncDefault,
+                                    ASyncCachedPropertyDescriptorSyncDefault)
+from a_sync.a_sync.property import ASyncPropertyDescriptor
+from a_sync.a_sync.property import ASyncPropertyDescriptor as property
+from a_sync.a_sync.property import (ASyncPropertyDescriptorAsyncDefault,
+                                    ASyncPropertyDescriptorSyncDefault, HiddenMethod,
+                                    HiddenMethodDescriptor)
+from a_sync.a_sync.singleton import ASyncGenericSingleton
+
+__all__ = [
+    # entrypoints
+    "a_sync",
+    "ASyncGenericBase",
+    "ASyncGenericSingleton",
+    # maybe entrypoints (?)
+    # TODO: double check how I intended for these to be used
+    "property",
+    "cached_property",
+    # classes exposed for type hinting only
+    "ASyncFunction",
+    "ASyncFunctionSyncDefault",
+    "ASyncFunctionAsyncDefault",
+    "ASyncPropertyDescriptor",
+    "ASyncPropertyDescriptorSyncDefault",
+    "ASyncPropertyDescriptorAsyncDefault",
+    "ASyncCachedPropertyDescriptor",
+    "ASyncCachedPropertyDescriptorSyncDefault",
+    "ASyncCachedPropertyDescriptorAsyncDefault",
+    "HiddenMethod",
+    "HiddenMethodDescriptor",
+]
