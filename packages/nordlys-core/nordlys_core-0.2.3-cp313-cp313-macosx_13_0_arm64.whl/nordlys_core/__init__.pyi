913fe15bcced8e0fd30181b99aa63d26dd78442a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:08:17 +0000

    Add package nordlys-core/0.2.3/nordlys_core-0.2.3-cp313-cp313-macosx_13_0_arm64.whl

---
+++ packages/nordlys-core/nordlys_core-0.2.3-cp313-cp313-macosx_13_0_arm64.whl/nordlys_core/__init__.pyi
@@ -0,0 +1,29 @@
+"""Type stubs for the nordlys_core package."""
+
+from ._core import (
+    Nordlys32,
+    Nordlys64,
+    NordlysCheckpoint,
+    RouteResult32,
+    RouteResult64,
+    TrainingMetrics,
+    EmbeddingConfig,
+    ClusteringConfig,
+    RoutingConfig,
+    ModelFeatures,
+    load_checkpoint,
+)
+
+__all__ = [
+    "Nordlys32",
+    "Nordlys64",
+    "NordlysCheckpoint",
+    "RouteResult32",
+    "RouteResult64",
+    "TrainingMetrics",
+    "EmbeddingConfig",
+    "ClusteringConfig",
+    "RoutingConfig",
+    "ModelFeatures",
+    "load_checkpoint",
+]
