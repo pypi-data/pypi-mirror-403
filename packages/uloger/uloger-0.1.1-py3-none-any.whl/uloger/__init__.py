394a101d38e20febccfe05d7643a496b27039f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:11:52 +0000

    Add package uloger/0.1.1/uloger-0.1.1-py3-none-any.whl

---
+++ packages/uloger/uloger-0.1.1-py3-none-any.whl/uloger/__init__.py
@@ -0,0 +1,24 @@
+# 导入标准logging模块，方便用户直接从uloger导入使用
+import logging
+
+# 从uloger.py导入logger，使得可以直接通过from uloger import logger导入
+from .uloger import __version__
+from .uloger import logger
+from .uloger import log_helper
+from .uloger import ULoger
+from .uloger import log_info, log_error, log_debug, log_warning, log_exception, log_critical
+
+# 导出所有公共接口
+__all__ = [
+    '__version__',  # 版本信息
+    'logging',  # 添加logging模块到导出列表
+    'logger',
+    'log_helper',
+    'ULoger',
+    'log_info',
+    'log_error',
+    'log_debug',
+    'log_warning',
+    'log_exception',
+    'log_critical'
+]
