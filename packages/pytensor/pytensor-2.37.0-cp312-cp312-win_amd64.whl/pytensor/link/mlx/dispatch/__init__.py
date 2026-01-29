3a81ebdedff8dac84608070d67533877b5b484b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:14:16 +0000

    Add package pytensor/2.37.0/pytensor-2.37.0-cp312-cp312-win_amd64.whl

---
+++ packages/pytensor/pytensor-2.37.0-cp312-cp312-win_amd64.whl/pytensor/link/mlx/dispatch/__init__.py
@@ -0,0 +1,17 @@
+# isort: off
+from pytensor.link.mlx.dispatch.basic import mlx_funcify, mlx_typify
+
+import pytensor.link.mlx.dispatch.math
+import pytensor.link.mlx.dispatch.basic
+import pytensor.link.mlx.dispatch.elemwise
+import pytensor.link.mlx.dispatch.shape
+import pytensor.link.mlx.dispatch.subtensor
+import pytensor.link.mlx.dispatch.core
+import pytensor.link.mlx.dispatch.signal
+import pytensor.link.mlx.dispatch.signal.conv
+import pytensor.link.mlx.dispatch.blockwise
+import pytensor.link.mlx.dispatch.extra_ops
+import pytensor.link.mlx.dispatch.sort
+import pytensor.link.mlx.dispatch.slinalg
+import pytensor.link.mlx.dispatch.nlinalg
+# isort: on
