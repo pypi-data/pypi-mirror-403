e0205bc34f406b517863485123e1d055f1583d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:12:21 +0000

    Add package paperless-asn-qr-codes/0.4.1.dev31/paperless_asn_qr_codes-0.4.1.dev31.tar.gz

---
+++ packages/paperless-asn-qr-codes/paperless_asn_qr_codes-0.4.1.dev31.tar.gz/paperless_asn_qr_codes-0.4.1.dev31/README.md
@@ -0,0 +1,103 @@
+# paperless-asn-qr-codes
+
+`paperless-asn-qr-codes` is a command line utility for generating ASN labels
+for paperless with both a human-readable representation and a QR code
+for machine consumption. The labels are Avery 4731 labels by default.
+
+## Installation
+
+```console
+pip install paperless-asn-qr-codes
+```
+
+In case you wanna use unrelease features, we also publish the main branch as development version to [PyPi](https://pypi.org/project/paperless-asn-qr-codes/#history).
+
+## Usage
+
+Run with uvx: 
+```console
+uvx paperless-asn-qr-codes
+```
+Run with uvx with a specific version, e.g. for pre-releases:
+```console
+uvx --from paperless-asn-qr-codes==<version>
+```
+E.g. for dev15:
+```console
+uvx --from paperless-asn-qr-codes==0.4.1dev15
+```
+Command Line Arguments:
+```
+usage: paperless-asn-qr-codes [-h] [--format {averyL4731,avery5160,avery5161,avery5163,avery5167,avery5371,herma10003}]
+                              [--digits DIGITS] [--border] [--row-wise] [--num-labels NUM_LABELS] [--pages PAGES]
+                              [--start-position START_POSITION]
+                              start_asn output_file
+
+CLI Tool for generating paperless ASN labels with QR codes
+
+positional arguments:
+  start_asn             The value of the first ASN
+  output_file           The output file to write to (default: labels.pdf)
+
+options:
+  -h, --help            show this help message and exit
+  --format, -f {avery5160,avery5161,avery5163,avery5167,avery5371,averyL4731,averyL4732,herma10003,herma4201,herma4346}
+  --digits, -d DIGITS   Number of digits in the ASN (default: 7, produces 'ASN0000001')
+  --border, -b          Display borders around labels, useful for debugging the printer alignment
+  --row-wise, -r        Increment the ASNs row-wise, go from left to right
+  --num-labels, -n NUM_LABELS
+                        Number of labels to be printed on the sheet
+  --pages, -p PAGES     Number of pages to be printed, ignored if NUM_LABELS is set (default: 1)
+  --start-position, -s START_POSITION
+                        Define the starting position on the sheet, eighter as ROW:COLUMN or COUNT, both starting from 1 (default: 1:1 or 1)
+```
+
+### Mandatory arguments
+
+- `<start_asn>`: The value of the first ASN to generate
+
+### Optional arguments
+
+- `<output_file>`: The name of the output file to write to (default: labels.pdf)
+
+---
+
+- `-h`, `--help`: Shows the help message
+- `-f`, `--format`: Selects the format of the output sheet (see [Supported Sheets](#supported-sheets))
+- `-d`, `--digits`: Specifies the number of digits in the ASN (e.g. for the default number 7, the ASN will look like 'ASN0000001')
+- `-b`, `--border`: Generates the borders around the labels to help debug alignment issues (see [Tips & Tricks](#tips--tricks))
+- `-r`, `--row-wise`: Increments the labels from left to right instead of top to bottom
+- `-n`, `--num-labels`: Number of labels to be printed on the sheet
+- `-p`, `--pages`: Number of pages to be generated, ignored if -n is present.
+- `-s`, `--start-position`: Positon of first label to be printed, either defined as ROW:COLUMN or NUMBER. Starting from 1 eg. to use the whole sheet it would be 1:1 or 1. Useful if you have a partly used sheet from using `-n`.
+
+## Supported Sheets
+Some different sheet types are supported with the `-f`/`--format` argument, however, not all are tested.
+
+The default is Avery L4731.
+
+Currently tested and known working are:
+- **Avery L4731 (189 Labels on DIN A4, the default)**
+- Avery L4732 (80 Labels on DIN A4)
+- Avery 3657 (40 Labels on DIN A4)
+- Avery L7162 (manual cutting needed, 4 ASNs per Label)
+- Herma 10003 (80 Labels on DIN A4, formerly Herma 4345)
+- Herma 4201 (64 Labels on DIN A4, [Disclaimer: Not perfect ;)](https://github.com/entropia/paperless-asn-qr-codes/pull/36))
+- Herma 4346 (48 Labels on DIN A4)
+- Herma 4333 (189 Labes on DIN A4)
+
+If you don't have label sheets at hand, you can also use:
+- `genericA4large` (70 Labels on plain DIN A4, manual cutting needed)
+
+## Tips & Tricks
+
+In case your printer has alignment issues, you can generate a PDF with borders around the labels by using the
+`-b`/`--border` option.
+
+## Attribution
+This script is based upon a public domain label generation class from @timrprobocom [https://gist.github.com/timrprobocom/3946aca8ab75df8267bbf892a427a1b7](https://gist.github.com/timrprobocom/3946aca8ab75df8267bbf892a427a1b7)
+
+## License
+
+`paperless-asn-qr-codes` is distributed under the terms of the
+[GPL-3.0](https://spdx.org/licenses/GPL-3.0.html) license.
---
+++ packages/paperless-asn-qr-codes/paperless_asn_qr_codes-0.4.1.dev31.tar.gz/paperless_asn_qr_codes-0.4.1.dev31/paperless_asn_qr_codes/avery_labels.py
@@ -0,0 +1,295 @@
+# pylint: disable=invalid-name,too-many-instance-attributes
+"""This module is used to generate label PDFs for Avery labels and other label types."""
+
+from dataclasses import dataclass, KW_ONLY
+from collections.abc import Iterator
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import LETTER, A4
+from reportlab.lib.units import mm, inch
+
+
+# Usage:
+#   label = AveryLabels.AveryLabel(5160)
+#   label.open( "labels5160.pdf" )
+#   label.render( RenderAddress, 30 )
+#   label.close()
+#
+# 'render' can either pass a callable, which receives the canvas object
+# (with X,Y=0,0 at the lower right) or a string "form" name of a form
+# previously created with canv.beginForm().
+
+
+@dataclass
+class LabelInfo:
+    """Class for modeling label info"""
+
+    _: KW_ONLY
+    labels_horizontal: int
+    labels_vertical: int
+    label_size: tuple[float, float]
+    gutter_size: tuple[float, float]
+    margin: tuple[float, float]
+    pagesize: tuple[float, float]
+    textsize: float = 2 * mm
+
+
+labelInfo: dict[str, LabelInfo] = {
+    "averyL4731": LabelInfo(
+        labels_horizontal=7,
+        labels_vertical=27,
+        label_size=(25.4 * mm, 10 * mm),
+        gutter_size=(2.5 * mm, 0),
+        margin=(9 * mm, 13.5 * mm),
+        pagesize=A4,
+    ),
+    "averyL4732": LabelInfo(
+        labels_horizontal=5,
+        labels_vertical=16,
+        label_size=(35.6 * mm, 16.9 * mm),
+        gutter_size=(2.5 * mm, 0),
+        margin=(11 * mm, 13.5 * mm),
+        pagesize=A4,
+    ),
+    "averyL7658": LabelInfo(
+        labels_horizontal=7,
+        labels_vertical=27,
+        label_size=(25.4 * mm, 10 * mm),
+        gutter_size=(2.5 * mm, 0.07 * mm),
+        margin=(9 * mm, 12.5 * mm),
+        pagesize=A4,
+    ),
+    # 2.6 x 1 address labels
+    "avery5160": LabelInfo(
+        labels_horizontal=3,
+        labels_vertical=10,
+        label_size=(187, 72),
+        gutter_size=(11, 0),
+        margin=(14, 36),
+        pagesize=LETTER,
+    ),
+    "avery5161": LabelInfo(
+        labels_horizontal=2,
+        labels_vertical=10,
+        label_size=(288, 72),
+        gutter_size=(0, 0),
+        margin=(18, 36),
+        pagesize=LETTER,
+    ),
+    # 4 x 2 address labels
+    "avery5163": LabelInfo(
+        labels_horizontal=2,
+        labels_vertical=5,
+        label_size=(288, 144),
+        gutter_size=(0, 0),
+        margin=(18, 36),
+        pagesize=LETTER,
+    ),
+    # 1.75 x 0.5 return address labels
+    "avery5167": LabelInfo(
+        labels_horizontal=4,
+        labels_vertical=20,
+        label_size=(1.75 * inch, 0.5 * inch),
+        gutter_size=(0.3 * inch, 0),
+        margin=(0.3 * inch, 0.5 * inch),
+        pagesize=LETTER,
+    ),
+    # 3.5 x 2 business cards
+    "avery5371": LabelInfo(
+        labels_horizontal=2,
+        labels_vertical=5,
+        label_size=(252, 144),
+        gutter_size=(0, 0),
+        margin=(54, 36),
+        pagesize=LETTER,
+    ),
+    "averyL7162": LabelInfo(
+        labels_horizontal=4,
+        labels_vertical=16,
+        label_size=(49.5 * mm, 16.9 * mm),
+        gutter_size=(2.5 * mm, 0),
+        margin=(11 * mm, 13.5 * mm),
+        pagesize=A4,
+    ),
+    # Herma 4201, 64 removable labels
+    "herma4201": LabelInfo(
+        labels_horizontal=4,
+        labels_vertical=16,
+        label_size=(45.7 * mm, 16.9 * mm),
+        gutter_size=(2.5 * mm, 0),
+        margin=(8 * mm, 13 * mm),
+        pagesize=A4,
+    ),
+    # HERMA No. 10003 labels (former article No. 4345)
+    "herma10003": LabelInfo(
+        labels_horizontal=5,
+        labels_vertical=16,
+        label_size=(35.56 * mm, 16.93 * mm),
+        gutter_size=(2.54 * mm, 0),
+        margin=(11.02 * mm, 13.06 * mm),
+        pagesize=A4,
+    ),
+    "herma4336": LabelInfo(
+        labels_horizontal=5,
+        labels_vertical=16,
+        label_size=(35.6 * mm, 16.9 * mm),
+        gutter_size=(2.5 * mm, 0),
+        margin=(11 * mm, 12.5 * mm),
+        pagesize=A4,
+    ),
+    "herma4346": LabelInfo(
+        labels_horizontal=4,
+        labels_vertical=12,
+        label_size=(45.72 * mm, 21.167 * mm),
+        gutter_size=(2.54 * mm, 0),
+        margin=(9.75 * mm, 21.5 * mm),
+        pagesize=A4,
+    ),
+    # AVERY 3657 (48.5mm x 25.4mm)
+    "avery3657": LabelInfo(
+        labels_horizontal=4,
+        labels_vertical=10,
+        label_size=(48.5 * mm, 25.4 * mm),
+        gutter_size=(0, 0),
+        margin=(8 * mm, 21.75 * mm),
+        pagesize=A4,
+    ),
+    "herma4333": LabelInfo(
+        labels_horizontal=7,
+        labels_vertical=27,
+        label_size=(25.4 * mm, 10.0 * mm),
+        gutter_size=(2.54 * mm, 0),
+        margin=(8.48 * mm, 13.5 * mm),
+        pagesize=A4,
+    ),
+    # generic uncut A4 label paper
+    # (slightly larger for bad scanners & printers)
+    "genericA4large": LabelInfo(
+        labels_horizontal=5,
+        labels_vertical=14,
+        label_size=(40 * mm, 20 * mm),
+        gutter_size=(0 * mm, 0),
+        margin=(5 * mm, 4 * mm),
+        pagesize=A4,
+        textsize=3 * mm,
+    ),
+}
+
+RETURN_ADDRESS = 5167
+BUSINESS_CARDS = 5371
+
+
+class AveryLabel:
+    """class for creating the pdfs"""
+
+    def __init__(self, label, debug, topDown=True, start_pos=None, **kwargs):
+        data = labelInfo[label]
+        self.across = data.labels_horizontal
+        self.down = data.labels_vertical
+        self.size = data.label_size
+        self.textsize = data.textsize
+        self.labelsep = (
+            self.size[0] + data.gutter_size[0],
+            self.size[1] + data.gutter_size[1],
+        )
+        self.margins = data.margin
+        self.topDown = topDown
+        self.debug = debug
+        self.pagesize = data.pagesize
+        self.canvas = None
+
+        # Calculate start offset
+        if isinstance(start_pos, tuple):
+            rows, columns = start_pos
+            # Minimum Value 1 for row/column
+            rows = max(rows, 1)
+            columns = max(columns, 1)
+            if self.topDown:
+                offset = (columns - 1) * self.down + rows - 1
+            else:
+                offset = (rows - 1) * self.across + columns - 1
+        elif start_pos:
+            offset = start_pos - 1
+        else:
+            offset = 0
+        # Limit start position to number of labels - 1
+        self.position = min(offset, self.across * self.down - 1)
+
+        self.__dict__.update(kwargs)
+
+    def open(self, filename):
+        """handles canvas and reportlab page"""
+        self.canvas = canvas.Canvas(filename, pagesize=self.pagesize)
+        if self.debug:
+            self.canvas.setPageCompression(0)
+        self.canvas.setLineJoin(1)
+        self.canvas.setLineCap(1)
+
+    def topLeft(self, x=None, y=None):
+        """returns the top left corner of the label"""
+        if x is None:
+            x = self.position
+        if y is None:
+            if self.topDown:
+                x, y = divmod(x, self.down)
+            else:
+                y, x = divmod(x, self.across)
+
+        return (
+            self.margins[0] + x * self.labelsep[0],
+            self.pagesize[1] - self.margins[1] - (y + 1) * self.labelsep[1],
+        )
+
+    def advance(self):
+        """advances the position to the next label"""
+        self.position += 1
+        if self.position == self.across * self.down:
+            self.canvas.showPage()
+            self.position = 0
+
+    def close(self):
+        """closes the canvas and finishes the sheet"""
+        if self.position:
+            self.canvas.showPage()
+        self.canvas.save()
+        self.canvas = None
+
+    # To render, you can either create a template and tell me
+    # "go draw N of these templates" or provide a callback.
+    # Callback receives canvas, width, height.
+    #
+    # Or, pass a callable and an iterator.  We'll do one label
+    # per iteration of the iterator.
+
+    def render(self, thing, count, *args):
+        """renders all the labels on the sheet via callbacks"""
+        assert callable(thing) or isinstance(thing, str)
+        if isinstance(count, Iterator):
+            return self.render_iterator(thing, count)
+
+        canv = self.canvas
+        for _ in range(count):
+            canv.saveState()
+            canv.translate(*self.topLeft())
+            if self.debug:
+                canv.setLineWidth(0.25)
+                canv.rect(0, 0, self.size[0], self.size[1])
+            if callable(thing):
+                thing(canv, self.size[0], self.size[1], self.textsize, *args)
+            elif isinstance(thing, str):
+                canv.doForm(thing)
+            canv.restoreState()
+            self.advance()
+        return None
+
+    def render_iterator(self, func, iterator):
+        """iterator interface"""
+        canv = self.canvas
+        for chunk in iterator:
+            canv.saveState()
+            canv.translate(*self.topLeft())
+            if self.debug:
+                canv.setLineWidth(0.25)
+                canv.rect(0, 0, self.size[0], self.size[1])
+            func(canv, self.size[0], self.size[1], self.textsize, chunk)
+            canv.restoreState()
+            self.advance()
