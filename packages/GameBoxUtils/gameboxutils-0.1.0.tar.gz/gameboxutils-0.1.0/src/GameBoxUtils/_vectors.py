d3ed81c66bfb0004eb528939d21d4b0a06b2c7cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:09:41 +0000

    Add package GameBoxUtils/0.1.0/gameboxutils-0.1.0.tar.gz

---
+++ packages/GameBoxUtils/gameboxutils-0.1.0.tar.gz/gameboxutils-0.1.0/src/GameBoxUtils/_vectors.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        if isinstance(other, Vector2D):
+            return Vector2D(self.x + other.x, self.y + other.y)
+        elif isinstance(other, (int, float)):
+            return Vector2D(self.x + other, self.y + other)
+
+    def __sub__(self, other):
+        if isinstance(other, Vector2D):
+            return Vector2D(self.x - other.x, self.y - other.y)
+        elif isinstance(other, (int, float)):
+            return Vector2D(self.x - other, self.y - other)
+
+    def __mul__(self, other):
+        if isinstance(other, Vector2D):
+            return Vector2D(self.x * other.x, self.y * other.y)
+        elif isinstance(other, (int, float)):
+            return Vector2D(self.x * other, self.y * other)
+
+    def __truediv__(self, other):
+        if isinstance(other, Vector2D):
+            return Vector2D(self.x / other.x, self.y / other.y)
+        elif isinstance(other, (int, float)):
+            return Vector2D(self.x / other, self.y / other)
+
+    def __repr__(self):
+        return f"Vector2D({self.x}, {self.y})"
+
+class Vector3D:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def __add__(self, other):
+        if isinstance(other, Vector3D):
+            return Vector3D(self.x + other.x, self.y + other.y, self.z + other.z)
+        elif isinstance(other, (int, float)):
+            return Vector3D(self.x + other, self.y + other, self.z + other)
+
+    def __sub__(self, other):
+        if isinstance(other, Vector3D):
+            return Vector3D(self.x - other.x, self.y - other.y, self.z - other.z)
+        elif isinstance(other, (int, float)):
+            return Vector3D(self.x - other, self.y - other, self.z - other)
+
+    def __mul__(self, other):
+        if isinstance(other, Vector3D):
+            return Vector3D(self.x * other.x, self.y * other.y, self.z * other.z)
+        elif isinstance(other, (int, float)):
+            return Vector3D(self.x * other, self.y * other, self.z * other)
+
+    def __truediv__(self, other):
+        if isinstance(other, Vector3D):
+            return Vector3D(self.x / other.x, self.y / other.y, self.z / other.z)
+        elif isinstance(other, (int, float)):
+            return Vector3D(self.x / other, self.y / other, self.z / other)
+
+    def __repr__(self):
+        return f"Vector3D({self.x}, {self.y}, {self.z})"
