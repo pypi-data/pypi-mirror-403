86f624c96dac1de0f15f99f38f19636b851cfb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 17:13:32 +0000

    Add package rootlp/0.1.12/rootlp-0.1.12-py3-none-any.whl

---
+++ packages/rootlp/rootlp-0.1.12-py3-none-any.whl/rootlp/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Date          : 2026-01-10
+# Author        : Lancelot PINCET
+# GitHub        : https://github.com/LancelotPincet
+# Library       : rootLP
+
+"""
+A library that gathers root functions for custom script execution.
+"""
+
+
+
+# %% Source import
+sources = {
+'Section': 'rootlp.modules.Section_LP.Section',
+'main': 'rootlp.modules.main_LP.main',
+'menu': 'rootlp.modules.menu_LP.menu',
+'mo': 'rootlp.modules.mo_LP.mo',
+'print': 'rootlp.modules.print_LP.print',
+'project_server': 'rootlp.modules.project_server_LP.project_server',
+'user_inputs': 'rootlp.modules.user_inputs_LP.user_inputs'
+}
+
+
+
+# %% Lazy imports
+from corelp import getmodule
+__getattr__, __all__ = getmodule(sources)
