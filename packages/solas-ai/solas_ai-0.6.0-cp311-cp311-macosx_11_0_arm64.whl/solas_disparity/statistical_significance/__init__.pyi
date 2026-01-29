aa64b75f8236c983f51a5a35002d3c65816439ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 15:13:03 +0000

    Add package solas-ai/0.6.0/solas_ai-0.6.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/solas-ai/solas_ai-0.6.0-cp311-cp311-macosx_11_0_arm64.whl/solas_disparity/statistical_significance/__init__.pyi
@@ -0,0 +1,7 @@
+from ._bootstrapping import bootstrapping as bootstrapping
+from ._chi_squared_test import chi_squared_test as chi_squared_test
+from ._fishers_exact import fishers_exact as fishers_exact
+from ._fishers_or_chi_squared import fishers_or_chi_squared as fishers_or_chi_squared
+from ._stacked_regression import stacked_regression as stacked_regression
+
+__all__ = ['bootstrapping', 'chi_squared_test', 'fishers_exact', 'fishers_or_chi_squared', 'stacked_regression']
