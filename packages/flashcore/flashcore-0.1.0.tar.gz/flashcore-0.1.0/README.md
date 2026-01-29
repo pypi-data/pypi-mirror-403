cb917f8ab18e725600032dc73bf35195064af0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 21:06:30 +0000

    Add package flashcore/0.1.0/flashcore-0.1.0.tar.gz

---
+++ packages/flashcore/flashcore-0.1.0.tar.gz/flashcore-0.1.0/README.md
@@ -0,0 +1,3 @@
+# flashcore
+
+Minimal PyO3 bindings extracted from Nautilus Trader for core components.
---
+++ packages/flashcore/flashcore-0.1.0.tar.gz/flashcore-0.1.0/crates/core/src/consts.rs
@@ -0,0 +1,25 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2026 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
+//! Core constants.
+
+/// The NautilusTrader string constant.
+pub static NAUTILUS_TRADER: &str = "NautilusTrader";
+
+/// The NautilusTrader common User-Agent string including the current version at compile time.
+pub static NAUTILUS_USER_AGENT: &str = "flashcore";
+
+/// Prefix for log messages outside the main logging subsystem.
+pub static NAUTILUS_PREFIX: &str = "[NAUTILUS]";
