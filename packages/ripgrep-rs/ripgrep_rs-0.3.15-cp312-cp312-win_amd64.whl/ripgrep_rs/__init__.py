b1dc87311996f1fbe0cee01125a4687a9d85fbb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:12:32 +0000

    Add package ripgrep-rs/0.3.15/ripgrep_rs-0.3.15-cp312-cp312-win_amd64.whl

---
+++ packages/ripgrep-rs/ripgrep_rs-0.3.15-cp312-cp312-win_amd64.whl/ripgrep_rs/__init__.py
@@ -0,0 +1,17 @@
+from .ripgrep_rs import (
+    FileInfo,
+    PySortMode,
+    PySortModeKind,
+    files,
+    files_with_info,
+    search,
+)
+
+__all__ = [
+    "FileInfo",
+    "PySortMode",
+    "PySortModeKind",
+    "files",
+    "files_with_info",
+    "search",
+]
