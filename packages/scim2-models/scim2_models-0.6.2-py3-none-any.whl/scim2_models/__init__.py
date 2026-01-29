5bd0919e964fd05fec413127f1637aaf2820880a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 23:09:47 +0000

    Add package scim2-models/0.6.2/scim2_models-0.6.2-py3-none-any.whl

---
+++ packages/scim2-models/scim2_models-0.6.2-py3-none-any.whl/scim2_models/__init__.py
@@ -0,0 +1,141 @@
+from .annotations import CaseExact
+from .annotations import Mutability
+from .annotations import Required
+from .annotations import Returned
+from .annotations import Uniqueness
+from .attributes import ComplexAttribute
+from .attributes import MultiValuedComplexAttribute
+from .base import BaseModel
+from .context import Context
+from .exceptions import InvalidFilterException
+from .exceptions import InvalidPathException
+from .exceptions import InvalidSyntaxException
+from .exceptions import InvalidValueException
+from .exceptions import InvalidVersionException
+from .exceptions import MutabilityException
+from .exceptions import NoTargetException
+from .exceptions import PathNotFoundException
+from .exceptions import SCIMException
+from .exceptions import SensitiveException
+from .exceptions import TooManyException
+from .exceptions import UniquenessException
+from .messages.bulk import BulkOperation
+from .messages.bulk import BulkRequest
+from .messages.bulk import BulkResponse
+from .messages.error import Error
+from .messages.list_response import ListResponse
+from .messages.message import Message
+from .messages.patch_op import PatchOp
+from .messages.patch_op import PatchOperation
+from .messages.search_request import SearchRequest
+from .path import URN
+from .path import Path
+from .reference import URI
+from .reference import External
+from .reference import ExternalReference
+from .reference import Reference
+from .reference import URIReference
+from .resources.enterprise_user import EnterpriseUser
+from .resources.enterprise_user import Manager
+from .resources.group import Group
+from .resources.group import GroupMember
+from .resources.resource import AnyExtension
+from .resources.resource import AnyResource
+from .resources.resource import Extension
+from .resources.resource import Meta
+from .resources.resource import Resource
+from .resources.resource_type import ResourceType
+from .resources.resource_type import SchemaExtension
+from .resources.schema import Attribute
+from .resources.schema import Schema
+from .resources.service_provider_config import AuthenticationScheme
+from .resources.service_provider_config import Bulk
+from .resources.service_provider_config import ChangePassword
+from .resources.service_provider_config import ETag
+from .resources.service_provider_config import Filter
+from .resources.service_provider_config import Patch
+from .resources.service_provider_config import ServiceProviderConfig
+from .resources.service_provider_config import Sort
+from .resources.user import Address
+from .resources.user import Email
+from .resources.user import Entitlement
+from .resources.user import GroupMembership
+from .resources.user import Im
+from .resources.user import Name
+from .resources.user import PhoneNumber
+from .resources.user import Photo
+from .resources.user import Role
+from .resources.user import User
+from .resources.user import X509Certificate
+
+__all__ = [
+    "Address",
+    "AnyResource",
+    "AnyExtension",
+    "Attribute",
+    "AuthenticationScheme",
+    "BaseModel",
+    "Bulk",
+    "BulkOperation",
+    "BulkRequest",
+    "BulkResponse",
+    "CaseExact",
+    "ChangePassword",
+    "ComplexAttribute",
+    "Context",
+    "ETag",
+    "Email",
+    "EnterpriseUser",
+    "Entitlement",
+    "Error",
+    "External",
+    "ExternalReference",
+    "Extension",
+    "Filter",
+    "Group",
+    "GroupMember",
+    "GroupMembership",
+    "Im",
+    "InvalidFilterException",
+    "InvalidPathException",
+    "InvalidSyntaxException",
+    "InvalidValueException",
+    "InvalidVersionException",
+    "ListResponse",
+    "Manager",
+    "Message",
+    "Meta",
+    "Mutability",
+    "MutabilityException",
+    "MultiValuedComplexAttribute",
+    "Name",
+    "NoTargetException",
+    "Path",
+    "PathNotFoundException",
+    "Patch",
+    "PatchOp",
+    "PatchOperation",
+    "PhoneNumber",
+    "Photo",
+    "Reference",
+    "Required",
+    "Resource",
+    "ResourceType",
+    "Returned",
+    "Role",
+    "SCIMException",
+    "Schema",
+    "SchemaExtension",
+    "SearchRequest",
+    "SensitiveException",
+    "ServiceProviderConfig",
+    "Sort",
+    "TooManyException",
+    "Uniqueness",
+    "UniquenessException",
+    "URI",
+    "URIReference",
+    "URN",
+    "User",
+    "X509Certificate",
+]
