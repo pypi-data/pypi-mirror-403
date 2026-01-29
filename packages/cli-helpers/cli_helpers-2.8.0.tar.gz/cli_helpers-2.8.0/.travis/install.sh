0572fa1347805c9e0e947cbdbef20be48ee89437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 19:17:54 +0000

    Add package cli-helpers/2.8.0/cli_helpers-2.8.0.tar.gz

---
+++ packages/cli-helpers/cli_helpers-2.8.0.tar.gz/cli_helpers-2.8.0/.travis/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ex
+
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+    sw_vers
+
+    git clone --depth 1 https://github.com/pyenv/pyenv ~/.pyenv
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
+
+    case "${TOXENV}" in
+        py36)
+            pyenv install 3.6.1
+            pyenv global 3.6.1
+            ;;
+    esac
+    pyenv rehash
+fi
+
+pip install virtualenv
+python -m virtualenv ~/.venv
+source ~/.venv/bin/activate
+pip install -r requirements-dev.txt -U --upgrade-strategy only-if-needed
