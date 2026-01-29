501d64906b9deac1e6e255a8db1bd56cae5cf2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 22:11:52 +0000

    Add package netHighTech-STT/1.0.5/nethightech_stt-1.0.5.tar.gz

---
+++ packages/netHighTech-STT/nethightech_stt-1.0.5.tar.gz/nethightech_stt-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='netHighTech_STT', # Naya package naam
+    version='1.0.5', # Naya version number
+    author='Zubair Khan',
+    author_email='zubairdev00@gmail.com',
+    description='Speech recognition Jarvis package',
+    packages=find_packages(),
+    include_package_data=True, # Assets shamil karne ke liye
+    install_requires=[
+        'selenium',
+        'webdriver-manager',
+    ],
+)
