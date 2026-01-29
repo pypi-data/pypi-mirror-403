e230a1e97a041a0ab6b9cd0f41bc827997105596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 16:12:25 +0000

    Add package atoti-server-spring-boot-admin/0.9.12/atoti_server_spring_boot_admin-0.9.12-py3-none-any.whl

---
+++ packages/atoti-server-spring-boot-admin/atoti_server_spring_boot_admin-0.9.12-py3-none-any.whl/atoti_spring_boot_admin/__init__.py
@@ -0,0 +1,7 @@
+"""Plugin enabling `Spring Boot Admin <https://docs.spring-boot-admin.com/>`__.
+
+This exposes a web interface to monitor resource usage, manage loggers, interact with JMX beans, and more.
+
+Spring Boot Admin can be accessed at ``f"{session.url}/spring-boot-admin"``.
+Only users with the :guilabel:`ROLE_ADMIN` role can access it.
+"""
---
+++ packages/atoti-server-spring-boot-admin/atoti_server_spring_boot_admin-0.9.12-py3-none-any.whl/atoti_spring_boot_admin/_plugin.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from dataclasses import replace
+from typing import TYPE_CHECKING, final
+
+from _atoti_core import Plugin
+from typing_extensions import override
+
+from ._resources_directory import RESOURCES_DIRECTORY
+
+if TYPE_CHECKING:
+    import atoti as tt  # pylint: disable=nested-import,undeclared-dependency
+
+_PLUGIN_KEY = "spring-boot-admin"
+
+
+@final
+class SpringBootAdminPlugin(Plugin):
+    @override
+    def session_config_hook(
+        self,
+        session_config: tt.SessionConfig,
+    ) -> tt.SessionConfig:
+        return replace(
+            session_config,
+            extra_jars=[
+                *session_config.extra_jars,
+                RESOURCES_DIRECTORY / "plugin.jar",
+            ],
+        )
+
+    @property
+    @override
+    def _key(self) -> str | None:
+        return _PLUGIN_KEY
