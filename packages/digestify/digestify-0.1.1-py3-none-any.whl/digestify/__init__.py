cd178709f2b23b4091673acab12dc4491e7d3619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-29 20:07:26 +0000

    Add package digestify/0.1.1/digestify-0.1.1-py3-none-any.whl

---
+++ packages/digestify/digestify-0.1.1-py3-none-any.whl/digestify/__init__.py
@@ -0,0 +1,4 @@
+from digestify.client import Digestify, DigestifySettings
+from digestify.models import Digest, Story, Topic
+
+__all__ = ["Digestify", "DigestifySettings", "Story", "Digest", "Topic"]
