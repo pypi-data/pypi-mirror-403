b9fea8b25d2a6f749858706616fb688da0aec8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:14 +0000

    Add package valanga/0.1.13/valanga-0.1.13-py3-none-any.whl

---
+++ packages/valanga/valanga-0.1.13-py3-none-any.whl/valanga/policy.py
@@ -0,0 +1,45 @@
+"""
+Policy-related classes and protocols for branch selection in game trees.
+"""
+
+from dataclasses import dataclass
+from typing import Mapping, Protocol, TypeVar
+
+from valanga.evaluations import StateEvaluation
+from valanga.game import BranchKey, BranchName, Seed, State
+
+
+@dataclass(frozen=True, slots=True)
+class BranchPolicy:
+    """
+    Represents a probability distribution over branches.
+    """
+
+    probs: Mapping[BranchKey, float]  # should sum to ~1.0
+
+
+@dataclass(frozen=True, slots=True)
+class Recommendation:
+    """A recommendation for a specific branch in a tree node."""
+
+    recommended_name: BranchName
+    evaluation: StateEvaluation | None = None
+    policy: BranchPolicy | None = None
+    branch_evals: Mapping[BranchName, StateEvaluation] | None = None
+
+
+StateT_contra = TypeVar("StateT_contra", bound=State, contravariant=True)
+
+
+class BranchSelector(Protocol[StateT_contra]):
+    """Protocol for a branch selector."""
+
+    def recommend(self, state: StateT_contra, seed: Seed) -> Recommendation:
+        """Given a state and a seed, recommends a branch to take.
+        Args:
+            state (State): The current state of the game.
+            seed (Seed): A seed for any randomness involved in the selection.
+        Returns:
+            Recommendation: The recommended branch to take.
+        """
+        ...
