812abe777c00fd5a8363cc78c6c8692fa295dfad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:10 +0000

    Add package viam-sdk/0.67.0/viam_sdk-0.67.0-py3-none-manylinux2014_aarch64.whl

---
+++ packages/viam-sdk/viam_sdk-0.67.0-py3-none-manylinux2014_aarch64.whl/viam/components/arm/__init__.py
@@ -0,0 +1,18 @@
+from viam.components import KinematicsReturn
+from viam.proto.common import KinematicsFileFormat, Pose
+from viam.proto.component.arm import JointPositions
+from viam.resource.registry import Registry, ResourceRegistration
+
+from .arm import Arm
+from .client import ArmClient
+from .service import ArmRPCService
+
+__all__ = [
+    "Arm",
+    "JointPositions",
+    "KinematicsFileFormat",
+    "KinematicsReturn",
+    "Pose",
+]
+
+Registry.register_api(ResourceRegistration(Arm, ArmRPCService, lambda name, channel: ArmClient(name, channel)))
