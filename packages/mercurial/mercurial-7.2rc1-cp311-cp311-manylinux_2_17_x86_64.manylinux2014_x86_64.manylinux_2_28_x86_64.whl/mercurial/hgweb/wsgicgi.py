73f119282571c9e4660dbf828910b58c2d3b1fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:33:11 +0000

    Add package mercurial/7.2rc1/mercurial-7.2rc1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/mercurial/mercurial-7.2rc1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/mercurial/hgweb/wsgicgi.py
@@ -0,0 +1,15 @@
+# hgweb/wsgicgi.py - external entry point for hgweb's wsgi
+#
+# This software may be used and distributed according to the terms of the
+# GNU General Public License version 2 or any later version.
+
+from __future__ import annotations
+
+from .wsgicgi_inner import *
+
+from .. import (
+    initialization,
+)
+
+
+initialization.init()
