3b29f3dbbb37b97a1438b438533b0ecbe8be0c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:24:02 +0000

    Add package metkitlib/1.16.0.9/metkitlib-1.16.0.9-cp313-cp313-manylinux_2_28_x86_64.whl

---
+++ packages/metkitlib/metkitlib-1.16.0.9-cp313-cp313-manylinux_2_28_x86_64.whl/metkitlib/include/metkit/mars/TypeMixed.h
@@ -0,0 +1,47 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @file   TypeMixed.h
+/// @author Baudouin Raoult
+/// @author Tiago Quintino
+/// @date   April 2016
+
+#ifndef metkit_TypeMixed_H
+#define metkit_TypeMixed_H
+
+#include "metkit/mars/Type.h"
+
+namespace metkit {
+namespace mars {
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class TypeMixed : public Type {
+
+public:  // methods
+
+    TypeMixed(const std::string& name, const eckit::Value& settings);
+
+    ~TypeMixed() noexcept override;
+
+private:  // methods
+
+    void print(std::ostream& out) const override;
+    bool expand(std::string& value, const MarsRequest& request) const override;
+
+    std::vector<std::pair<std::unique_ptr<Context>, Type*>> types_;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}  // namespace mars
+}  // namespace metkit
+
+#endif
