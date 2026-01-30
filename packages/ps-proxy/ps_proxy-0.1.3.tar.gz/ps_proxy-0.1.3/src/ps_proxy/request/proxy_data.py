08aa35c4f22eaac641165a3ff99258416486add8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:49 +0000

    Add package ps-proxy/0.1.3/ps_proxy-0.1.3.tar.gz

---
+++ packages/ps-proxy/ps_proxy-0.1.3.tar.gz/ps_proxy-0.1.3/src/ps_proxy/request/proxy_data.py
@@ -0,0 +1,6 @@
+proxy_data = {
+    "pp1":"https://proxylist.geonode.com/api/proxy-list?limit=15&page=1&sort_by=lastChecked&sort_type=desc",
+    "pp2":"https://proxydb.net/",
+    "pp3":"https://free-proxy-list.net/en/",
+    "pp4" :"https://www.freeproxy.world/"
+}
