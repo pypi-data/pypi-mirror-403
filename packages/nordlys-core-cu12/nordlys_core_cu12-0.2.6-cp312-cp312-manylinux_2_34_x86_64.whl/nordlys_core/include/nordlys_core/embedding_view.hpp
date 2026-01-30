0a967363d955b97772bc0d80d564acd07d6dd162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:35:52 +0000

    Add package nordlys-core-cu12/0.2.6/nordlys_core_cu12-0.2.6-cp312-cp312-manylinux_2_34_x86_64.whl

---
+++ packages/nordlys-core-cu12/nordlys_core_cu12-0.2.6-cp312-cp312-manylinux_2_34_x86_64.whl/nordlys_core/include/nordlys_core/embedding_view.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "device.hpp"
+
+struct EmbeddingView {
+  const float* data;
+  size_t dim;
+  Device device;
+};
+
+struct EmbeddingBatchView {
+  const float* data;
+  size_t count;
+  size_t dim;
+  Device device;
+};
