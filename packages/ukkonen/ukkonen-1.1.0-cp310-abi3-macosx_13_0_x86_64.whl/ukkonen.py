f4af5fe76a24a562565f55d1404882b99055dc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:35:26 +0000

    Add package ukkonen/1.1.0/ukkonen-1.1.0-cp310-abi3-macosx_13_0_x86_64.whl

---
+++ packages/ukkonen/ukkonen-1.1.0-cp310-abi3-macosx_13_0_x86_64.whl/ukkonen.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import _ukkonen
+
+_lib = _ukkonen.lib
+
+
+def distance(s1: str, s2: str, k: int) -> int:
+    return _lib.edit_distance_k(s1, len(s1), s2, len(s2), k)
