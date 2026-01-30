5b96c65883595cb2ec504faf78fb046521cb9eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:35:55 +0000

    Add package dash-mantine-components/2.5.0/dash_mantine_components-2.5.0-py3-none-any.whl

---
+++ packages/dash-mantine-components/dash_mantine_components-2.5.0-py3-none-any.whl/dash_mantine_components/_imports_.py
@@ -0,0 +1,395 @@
+from .AreaChart import AreaChart
+from .BarChart import BarChart
+from .BubbleChart import BubbleChart
+from .CompositeChart import CompositeChart
+from .DonutChart import DonutChart
+from .LineChart import LineChart
+from .PieChart import PieChart
+from .RadarChart import RadarChart
+from .ScatterChart import ScatterChart
+from .Sparkline import Sparkline
+from .AreaChart import AreaChart
+from .BarChart import BarChart
+from .BubbleChart import BubbleChart
+from .CompositeChart import CompositeChart
+from .DonutChart import DonutChart
+from .LineChart import LineChart
+from .PieChart import PieChart
+from .RadarChart import RadarChart
+from .ScatterChart import ScatterChart
+from .Sparkline import Sparkline
+from .Accordion import Accordion
+from .AccordionControl import AccordionControl
+from .AccordionItem import AccordionItem
+from .AccordionPanel import AccordionPanel
+from .AppShell import AppShell
+from .AppShellAside import AppShellAside
+from .AppShellFooter import AppShellFooter
+from .AppShellHeader import AppShellHeader
+from .AppShellMain import AppShellMain
+from .AppShellNavbar import AppShellNavbar
+from .AppShellSection import AppShellSection
+from .Avatar import Avatar
+from .AvatarGroup import AvatarGroup
+from .ActionIcon import ActionIcon
+from .ActionIconGroup import ActionIconGroup
+from .Button import Button
+from .ButtonGroup import ButtonGroup
+from .CopyButton import CopyButton
+from .CustomCopyButton import CustomCopyButton
+from .UnstyledButton import UnstyledButton
+from .Card import Card
+from .CardSection import CardSection
+from .Checkbox import Checkbox
+from .CheckboxCard import CheckboxCard
+from .CheckboxGroup import CheckboxGroup
+from .CheckboxIndicator import CheckboxIndicator
+from .Chip import Chip
+from .ChipGroup import ChipGroup
+from .ChipGroupContext import ChipGroupContext
+from .ColorInput import ColorInput
+from .ColorPicker import ColorPicker
+from .Autocomplete import Autocomplete
+from .MultiSelect import MultiSelect
+from .Select import Select
+from .TagsInput import TagsInput
+from .Drawer import Drawer
+from .DrawerStack import DrawerStack
+from .ManagedDrawer import ManagedDrawer
+from .Grid import Grid
+from .GridCol import GridCol
+from .HoverCard import HoverCard
+from .HoverCardDropdown import HoverCardDropdown
+from .HoverCardTarget import HoverCardTarget
+from .BackgroundImage import BackgroundImage
+from .Image import Image
+from .InputWrapper import InputWrapper
+from .JsonInput import JsonInput
+from .NumberInput import NumberInput
+from .PasswordInput import PasswordInput
+from .PinInput import PinInput
+from .TextInput import TextInput
+from .Textarea import Textarea
+from .List import List
+from .ListItem import ListItem
+from .Menu import Menu
+from .MenuDivider import MenuDivider
+from .MenuDropdown import MenuDropdown
+from .MenuItem import MenuItem
+from .MenuLabel import MenuLabel
+from .MenuTarget import MenuTarget
+from .SubMenu import SubMenu
+from .SubMenuDropdown import SubMenuDropdown
+from .SubMenuItem import SubMenuItem
+from .SubMenuTarget import SubMenuTarget
+from .ManagedModal import ManagedModal
+from .Modal import Modal
+from .ModalStack import ModalStack
+from .Popover import Popover
+from .PopoverDropdown import PopoverDropdown
+from .PopoverTarget import PopoverTarget
+from .Progress import Progress
+from .ProgressLabel import ProgressLabel
+from .ProgressRoot import ProgressRoot
+from .ProgressSection import ProgressSection
+from .Radio import Radio
+from .RadioCard import RadioCard
+from .RadioGroup import RadioGroup
+from .RadioGroupContext import RadioGroupContext
+from .RadioIndicator import RadioIndicator
+from .ScrollArea import ScrollArea
+from .ScrollAreaAutosize import ScrollAreaAutosize
+from .RangeSlider import RangeSlider
+from .Slider import Slider
+from .Stepper import Stepper
+from .StepperCompleted import StepperCompleted
+from .StepperStep import StepperStep
+from .Table import Table
+from .TableCaption import TableCaption
+from .TableScrollContainer import TableScrollContainer
+from .TableTbody import TableTbody
+from .TableTd import TableTd
+from .TableTfoot import TableTfoot
+from .TableTh import TableTh
+from .TableThead import TableThead
+from .TableTr import TableTr
+from .Tabs import Tabs
+from .TabsList import TabsList
+from .TabsPanel import TabsPanel
+from .TabsTab import TabsTab
+from .Timeline import Timeline
+from .TimelineItem import TimelineItem
+from .FloatingTooltip import FloatingTooltip
+from .Tooltip import Tooltip
+from .Affix import Affix
+from .Alert import Alert
+from .Anchor import Anchor
+from .AspectRatio import AspectRatio
+from .Badge import Badge
+from .Blockquote import Blockquote
+from .Box import Box
+from .Breadcrumbs import Breadcrumbs
+from .Burger import Burger
+from .Center import Center
+from .Code import Code
+from .Collapse import Collapse
+from .Container import Container
+from .Divider import Divider
+from .Fieldset import Fieldset
+from .Flex import Flex
+from .Group import Group
+from .Highlight import Highlight
+from .Indicator import Indicator
+from .Kbd import Kbd
+from .Loader import Loader
+from .LoadingOverlay import LoadingOverlay
+from .Mark import Mark
+from .NavLink import NavLink
+from .NumberFormatter import NumberFormatter
+from .Overlay import Overlay
+from .Pagination import Pagination
+from .Paper import Paper
+from .Rating import Rating
+from .RingProgress import RingProgress
+from .SegmentedControl import SegmentedControl
+from .SemiCircleProgress import SemiCircleProgress
+from .SimpleGrid import SimpleGrid
+from .Skeleton import Skeleton
+from .Space import Space
+from .Spoiler import Spoiler
+from .Stack import Stack
+from .Switch import Switch
+from .TableOfContents import TableOfContents
+from .Text import Text
+from .ThemeIcon import ThemeIcon
+from .Title import Title
+from .Tree import Tree
+from .VisuallyHidden import VisuallyHidden
+from .DateInput import DateInput
+from .DatePicker import DatePicker
+from .DatePickerInput import DatePickerInput
+from .DateTimePicker import DateTimePicker
+from .DatesProvider import DatesProvider
+from .MiniCalendar import MiniCalendar
+from .MonthPickerInput import MonthPickerInput
+from .TimeGrid import TimeGrid
+from .TimeInput import TimeInput
+from .TimePicker import TimePicker
+from .YearPickerInput import YearPickerInput
+from .Carousel import Carousel
+from .CarouselSlide import CarouselSlide
+from .CodeHighlight import CodeHighlight
+from .CodeHighlightTabs import CodeHighlightTabs
+from .InlineCodeHighlight import InlineCodeHighlight
+from .CodeHighlight import CodeHighlight
+from .CodeHighlightTabs import CodeHighlightTabs
+from .InlineCodeHighlight import InlineCodeHighlight
+from .Notification import Notification
+from .NotificationContainer import NotificationContainer
+from .NotificationProvider import NotificationProvider
+from .NavigationProgress import NavigationProgress
+from .NavigationProgressProvider import NavigationProgressProvider
+from .RichTextEditor import RichTextEditor
+from .RichTextEditor import RichTextEditor
+from .DirectionProvider import DirectionProvider
+from .MantineProvider import MantineProvider
+from .TypographyStylesProvider import TypographyStylesProvider
+
+__all__ = [
+    "AreaChart",
+    "BarChart",
+    "BubbleChart",
+    "CompositeChart",
+    "DonutChart",
+    "LineChart",
+    "PieChart",
+    "RadarChart",
+    "ScatterChart",
+    "Sparkline",
+    "AreaChart",
+    "BarChart",
+    "BubbleChart",
+    "CompositeChart",
+    "DonutChart",
+    "LineChart",
+    "PieChart",
+    "RadarChart",
+    "ScatterChart",
+    "Sparkline",
+    "Accordion",
+    "AccordionControl",
+    "AccordionItem",
+    "AccordionPanel",
+    "AppShell",
+    "AppShellAside",
+    "AppShellFooter",
+    "AppShellHeader",
+    "AppShellMain",
+    "AppShellNavbar",
+    "AppShellSection",
+    "Avatar",
+    "AvatarGroup",
+    "ActionIcon",
+    "ActionIconGroup",
+    "Button",
+    "ButtonGroup",
+    "CopyButton",
+    "CustomCopyButton",
+    "UnstyledButton",
+    "Card",
+    "CardSection",
+    "Checkbox",
+    "CheckboxCard",
+    "CheckboxGroup",
+    "CheckboxIndicator",
+    "Chip",
+    "ChipGroup",
+    "ChipGroupContext",
+    "ColorInput",
+    "ColorPicker",
+    "Autocomplete",
+    "MultiSelect",
+    "Select",
+    "TagsInput",
+    "Drawer",
+    "DrawerStack",
+    "ManagedDrawer",
+    "Grid",
+    "GridCol",
+    "HoverCard",
+    "HoverCardDropdown",
+    "HoverCardTarget",
+    "BackgroundImage",
+    "Image",
+    "InputWrapper",
+    "JsonInput",
+    "NumberInput",
+    "PasswordInput",
+    "PinInput",
+    "TextInput",
+    "Textarea",
+    "List",
+    "ListItem",
+    "Menu",
+    "MenuDivider",
+    "MenuDropdown",
+    "MenuItem",
+    "MenuLabel",
+    "MenuTarget",
+    "SubMenu",
+    "SubMenuDropdown",
+    "SubMenuItem",
+    "SubMenuTarget",
+    "ManagedModal",
+    "Modal",
+    "ModalStack",
+    "Popover",
+    "PopoverDropdown",
+    "PopoverTarget",
+    "Progress",
+    "ProgressLabel",
+    "ProgressRoot",
+    "ProgressSection",
+    "Radio",
+    "RadioCard",
+    "RadioGroup",
+    "RadioGroupContext",
+    "RadioIndicator",
+    "ScrollArea",
+    "ScrollAreaAutosize",
+    "RangeSlider",
+    "Slider",
+    "Stepper",
+    "StepperCompleted",
+    "StepperStep",
+    "Table",
+    "TableCaption",
+    "TableScrollContainer",
+    "TableTbody",
+    "TableTd",
+    "TableTfoot",
+    "TableTh",
+    "TableThead",
+    "TableTr",
+    "Tabs",
+    "TabsList",
+    "TabsPanel",
+    "TabsTab",
+    "Timeline",
+    "TimelineItem",
+    "FloatingTooltip",
+    "Tooltip",
+    "Affix",
+    "Alert",
+    "Anchor",
+    "AspectRatio",
+    "Badge",
+    "Blockquote",
+    "Box",
+    "Breadcrumbs",
+    "Burger",
+    "Center",
+    "Code",
+    "Collapse",
+    "Container",
+    "Divider",
+    "Fieldset",
+    "Flex",
+    "Group",
+    "Highlight",
+    "Indicator",
+    "Kbd",
+    "Loader",
+    "LoadingOverlay",
+    "Mark",
+    "NavLink",
+    "NumberFormatter",
+    "Overlay",
+    "Pagination",
+    "Paper",
+    "Rating",
+    "RingProgress",
+    "SegmentedControl",
+    "SemiCircleProgress",
+    "SimpleGrid",
+    "Skeleton",
+    "Space",
+    "Spoiler",
+    "Stack",
+    "Switch",
+    "TableOfContents",
+    "Text",
+    "ThemeIcon",
+    "Title",
+    "Tree",
+    "VisuallyHidden",
+    "DateInput",
+    "DatePicker",
+    "DatePickerInput",
+    "DateTimePicker",
+    "DatesProvider",
+    "MiniCalendar",
+    "MonthPickerInput",
+    "TimeGrid",
+    "TimeInput",
+    "TimePicker",
+    "YearPickerInput",
+    "Carousel",
+    "CarouselSlide",
+    "CodeHighlight",
+    "CodeHighlightTabs",
+    "InlineCodeHighlight",
+    "CodeHighlight",
+    "CodeHighlightTabs",
+    "InlineCodeHighlight",
+    "Notification",
+    "NotificationContainer",
+    "NotificationProvider",
+    "NavigationProgress",
+    "NavigationProgressProvider",
+    "RichTextEditor",
+    "RichTextEditor",
+    "DirectionProvider",
+    "MantineProvider",
+    "TypographyStylesProvider"
+]
