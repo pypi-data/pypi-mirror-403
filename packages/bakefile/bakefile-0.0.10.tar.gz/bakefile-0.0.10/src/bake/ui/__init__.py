eb6d9f39e1e0590ecf25bf8d6e9d2eef1fa3065a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:29 +0000

    Add package bakefile/0.0.10/bakefile-0.0.10.tar.gz

---
+++ packages/bakefile/bakefile-0.0.10.tar.gz/bakefile-0.0.10/src/bake/ui/__init__.py
@@ -0,0 +1,11 @@
+from bake.ui import console, params
+from bake.ui.logger.setup import setup_logging
+from bake.ui.run import run, run_uv
+
+__all__ = [
+    "console",
+    "params",
+    "run",
+    "run_uv",
+    "setup_logging",
+]
