71009d19ac550d49b97caffb33421d0034af3b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:15:26 +0000

    Add package aldict/1.1.2/aldict-1.1.2-py3-none-any.whl

---
+++ packages/aldict/aldict-1.1.2-py3-none-any.whl/aldict/__init__.py
@@ -0,0 +1,6 @@
+from .alias_dict import AliasDict as AliasDict
+from .exception import AliasError as AliasError
+from .exception import AliasValueError as AliasValueError
+
+__version__ = "1.1.1"
+__all__ = ["AliasDict", "AliasError", "AliasValueError"]
