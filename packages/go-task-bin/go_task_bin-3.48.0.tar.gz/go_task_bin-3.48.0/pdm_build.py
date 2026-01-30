c8954f2df27be288da84e798b58932c896af62fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:33:39 +0000

    Add package go-task-bin/3.48.0/go_task_bin-3.48.0.tar.gz

---
+++ packages/go-task-bin/go_task_bin-3.48.0.tar.gz/go_task_bin-3.48.0/pdm_build.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+
+import os
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
+
+if TYPE_CHECKING:
+    from pdm.backend.hooks import Context
+
+NAME = "task"
+pwd = Path(__file__).parent
+with pwd.joinpath("pyproject.toml").open("rb") as f:
+    pyproject = tomllib.load(f)
+VERSION: str = pyproject["project"]["version"]
+
+
+def is_windows():
+    if "GOOS" in os.environ:
+        return os.environ["GOOS"] == "windows"
+    return sys.platform == "win32"
+
+
+def build(output: str) -> None:
+    go = shutil.which("go")
+    if go is None:
+        msg = "golang is required and 'go' should be in $PATH"
+        raise RuntimeError(msg)
+
+    os.environ.setdefault("CGO_ENABLED", "0")
+
+    args = [
+        go,
+        "build",
+        "-o",
+        output,
+        "-trimpath",
+        "-ldflags",
+        f"-s -w -X github.com/go-task/task/v3/internal/version.version={VERSION} -X github.com/go-task/task/v3/internal/version.commit=pypi",
+        "./cmd/task",
+    ]
+
+    submodule = Path(__file__).parent.joinpath(NAME)
+    subprocess.run(args, check=True, cwd=submodule)
+    Path(output).chmod(0o777)
+
+
+def pdm_build_hook_enabled(context: Context):
+    return context.target != "sdist"
+
+
+def pdm_build_initialize(context: Context) -> None:
+    setting = {"--python-tag": "py3", "--py-limited-api": "none"}
+    context.builder.config_settings = {**setting, **context.builder.config_settings}
+
+    context.ensure_build_dir()
+    output_path = Path(context.build_dir, "bin", NAME)
+    if is_windows():
+        output_path = output_path.with_suffix(".exe")
+    build(str(output_path))
+
+
+def pdm_build_finalize(context: Context, artifact: Path) -> None:
+    if Path(context.build_dir).exists():
+        shutil.rmtree(context.build_dir)
---
+++ packages/go-task-bin/go_task_bin-3.48.0.tar.gz/go_task_bin-3.48.0/task/README.md
@@ -0,0 +1,32 @@
+<div align="center">
+  <a href="https://taskfile.dev">
+    <img src="website/src/public/img/logo.svg" width="200px" height="200px" />
+  </a>
+
+  <h1>Task</h1>
+
+  <p>
+    Task is a task runner / build tool that aims to be simpler and easier to use than, for example, <a href="https://www.gnu.org/software/make/">GNU Make<a>.
+  </p>
+
+  <p>
+    <a href="https://taskfile.dev/docs/installation">Installation</a> | <a href="https://taskfile.dev/docs/getting-started">Getting Started</a> | <a href="https://taskfile.dev/docs/guide">Docs</a> | <a href="https://twitter.com/taskfiledev">Twitter</a> | <a href="https://bsky.app/profile/taskfile.dev">Bluesky</a> | <a href="https://fosstodon.org/@task">Mastodon</a> | <a href="https://discord.gg/6TY36E39UK">Discord</a>
+  </p>
+
+  <h1>Gold Sponsors</h1>
+
+  <table>
+    <tr>
+      <td align="center" valign="middle">
+        <a target="_blank" href="https://devowl.io">
+          <img src="website/src/public/img/devowl.io.svg" height="100px" width="200px" title="devowl.io" />
+        </a>
+      </td>
+      <td align="center" valign="middle">
+        <a target="_blank" href="https://magic.dev/">
+          <img src="website/src/public/img/magic.png" height="100px" width="200px" title="Magic" />
+        </a>
+      </td>
+    </tr>
+  </table>
+</div>
