a0219812ae9b38f35bc8c3471023b9cfe66db5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:35:26 +0000

    Add package svg-text2path/0.4.2/svg_text2path-0.4.2.tar.gz

---
+++ packages/svg-text2path/svg_text2path-0.4.2.tar.gz/svg_text2path-0.4.2/svg_text2path/svg/elements.py
@@ -0,0 +1,7 @@
+"""SVG element detection and manipulation utilities.
+
+This module will contain utilities for detecting and manipulating
+specific SVG element types (text, tspan, textPath, etc.).
+"""
+
+# TODO: Implement element detection utilities
