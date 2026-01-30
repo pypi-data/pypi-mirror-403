812c5e427c75a8b54c07b9b2c3b217b5218e705d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:35 +0000

    Add package sawyer-cz/0.1.0/sawyer_cz-0.1.0-py3-none-any.whl

---
+++ packages/sawyer-cz/sawyer_cz-0.1.0-py3-none-any.whl/sawyer_cz.py
@@ -0,0 +1,151 @@
+from commitizen.cz.conventional_commits.conventional_commits import ConventionalCommitsCz
+from commitizen.cz.conventional_commits.conventional_commits import ConventionalCommitsAnswers
+from commitizen.cz.utils import multiple_line_breaker, required_validator
+from commitizen.question import CzQuestion
+
+__all__ = ["SawyerCZ"]
+
+
+def _parse_scope(text: str) -> str:
+    return "-".join(text.strip().split())
+
+
+def _parse_subject(text: str) -> str:
+    return required_validator(text.strip(".").strip(), msg="Subject is required.")
+
+class SawyerCZ(ConventionalCommitsCz):
+    name = "sawyer-cz"
+
+    def questions(self) -> list[CzQuestion]:
+        return [
+            {
+                "type": "list",
+                "name": "prefix",
+                "message": "Select the type of change you are committing",
+                "choices": [
+                    {
+                        "value": "fix",
+                        "name": "fix: A bug fix. Correlates with PATCH in SemVer",
+                        "key": "x",
+                    },
+                    {
+                        "value": "feat",
+                        "name": "feat: A new feature. Correlates with MINOR in SemVer",
+                        "key": "f",
+                    },
+                    {
+                        "value": "docs",
+                        "name": "docs: Documentation only changes",
+                        "key": "d",
+                    },
+                    {
+                        "value": "style",
+                        "name": (
+                            "style: Changes that do not affect the "
+                            "meaning of the code (white-space, formatting,"
+                            " missing semi-colons, etc)"
+                        ),
+                        "key": "s",
+                    },
+                    {
+                        "value": "refactor",
+                        "name": (
+                            "refactor: A code change that neither fixes "
+                            "a bug nor adds a feature"
+                        ),
+                        "key": "r",
+                    },
+                    {
+                        "value": "perf",
+                        "name": "perf: A code change that improves performance",
+                        "key": "p",
+                    },
+                    {
+                        "value": "test",
+                        "name": ("test: Adding missing or correcting existing tests"),
+                        "key": "t",
+                    },
+                    {
+                        "value": "build",
+                        "name": (
+                            "build: Changes that affect the build system or "
+                            "external dependencies (example scopes: pip, docker, npm)"
+                        ),
+                        "key": "b",
+                    },
+                    {
+                        "value": "ci",
+                        "name": (
+                            "ci: Changes to CI configuration files and "
+                            "scripts (example scopes: GitLabCI)"
+                        ),
+                        "key": "c",
+                    },
+                    {
+                        "value": "chore",
+                        "name": "chore: Changes to the build process or auxiliary tools and libraries such as documentation generation",
+                        "key": "h",
+                    },
+                ],
+            },
+            {
+                "type": "input",
+                "name": "scope",
+                "message": (
+                    "What is the scope of this change? (class or file name): (press [enter] to skip)\n"
+                ),
+                "filter": _parse_scope,
+            },
+            {
+                "type": "input",
+                "name": "subject",
+                "filter": _parse_subject,
+                "message": (
+                    "Write a short and imperative summary of the code changes: (lower case and no period)\n"
+                ),
+            },
+            {
+                "type": "input",
+                "name": "body",
+                "message": (
+                    "Provide additional contextual information about the code changes: (press [enter] to skip)\n"
+                ),
+                "filter": multiple_line_breaker,
+            },
+            {
+                "type": "confirm",
+                "name": "is_breaking_change",
+                "message": "Is this a BREAKING CHANGE? Correlates with MAJOR in SemVer",
+                "default": False,
+            },
+            {
+                "type": "input",
+                "name": "footer",
+                "message": (
+                    "Footer. Information about Breaking Changes and "
+                    "reference issues that this commit closes: (press [enter] to skip)\n"
+                ),
+            },
+        ]
+
+    def message(self, answers: ConventionalCommitsAnswers) -> str:  # type: ignore[override]
+        prefix = answers["prefix"]
+        scope = answers["scope"]
+        subject = answers["subject"]
+        body = answers["body"]
+        footer = answers["footer"]
+        is_breaking_change = answers["is_breaking_change"]
+
+        formatted_scope = f"({scope})" if scope else ""
+        title = f"{prefix}{formatted_scope}"
+        if is_breaking_change:
+            if self.config.settings.get("breaking_change_exclamation_in_title", False):
+                title = f"{title}!"
+            footer = f"BREAKING CHANGE: {footer}"
+
+        formatted_body = f"\n\n{body}" if body else ""
+        formatted_footter = f"\n\n{footer}" if footer else ""
+
+        return f"{title}: {subject}{formatted_body}{formatted_footter}"
+
+
