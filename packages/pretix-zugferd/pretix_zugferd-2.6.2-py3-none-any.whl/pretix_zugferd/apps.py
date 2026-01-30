21748a0b768b31c4f452662e9e031922aa7835b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:26 +0000

    Add package pretix-zugferd/2.6.2/pretix_zugferd-2.6.2-py3-none-any.whl

---
+++ packages/pretix-zugferd/pretix_zugferd-2.6.2-py3-none-any.whl/pretix_zugferd/apps.py
@@ -0,0 +1,29 @@
+from django.apps import AppConfig
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext, gettext_lazy
+
+from . import __version__
+
+
+class PluginApp(AppConfig):
+    name = "pretix_zugferd"
+    verbose_name = "ZUGFeRD invoices for pretix"
+
+    class PretixPluginMeta:
+        name = gettext_lazy("ZUGFeRD invoices")
+        author = "Raphael Michel"
+        visible = True
+        version = __version__
+        category = "FEATURE"
+        compatibility = "pretix>=2025.10.0.dev0"
+
+        @property
+        def description(self):
+            t = gettext(
+                "This plugin provides an invoice renderer that annotates pretix invoices with ZUGFeRD data, "
+                "a structured data format for invoices used in Germany."
+            )
+            return mark_safe(t)
+
+    def ready(self):
+        from . import signals  # NOQA
---
+++ packages/pretix-zugferd/pretix_zugferd-2.6.2-py3-none-any.whl/pretix_zugferd/views.py
@@ -0,0 +1,72 @@
+from django import forms
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.formfields import PhoneNumberField
+from pretix.base.forms import SettingsForm
+from pretix.base.models import Event
+from pretix.control.views.event import EventSettingsFormView, EventSettingsViewMixin
+
+from pretix_zugferd.invoice import ZugferdMixin
+
+
+class ZugferdSettingsForm(SettingsForm):
+    zugferd_seller_contact_name = forms.CharField(
+        label=_("Seller contact name"),
+        help_text=_("Required for a valid XRechnung"),
+        required=False,
+    )
+    zugferd_seller_contact_email = forms.EmailField(
+        label=_("Seller contact email"),
+        help_text=_("Required for a valid XRechnung"),
+        required=False,
+    )
+    zugferd_seller_contact_phone = PhoneNumberField(
+        label=_("Seller contact phone number"),
+        help_text=_("Required for a valid XRechnung"),
+        required=False,
+        empty_value=None,
+    )
+    zugferd_hide_label = forms.BooleanField(
+        label=_("Hide label that advertises the included XRechnung"),
+        required=False,
+    )
+
+
+class SettingsView(EventSettingsViewMixin, EventSettingsFormView):
+    model = Event
+    form_class = ZugferdSettingsForm
+    template_name = "pretix_zugferd/settings.html"
+    permission = "can_change_settings"
+
+    def get_success_url(self) -> str:
+        return reverse(
+            "plugins:pretix_zugferd:settings",
+            kwargs={
+                "organizer": self.request.event.organizer.slug,
+                "event": self.request.event.slug,
+            },
+        )
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(
+            **kwargs,
+            is_zugferd_renderer=isinstance(
+                self.request.event.invoice_renderer, ZugferdMixin
+            ),
+            is_xrechnung_renderer=(
+                getattr(self.request.event.invoice_renderer, "profile", None)
+                == "XRECHNUNG"
+            ),
+            has_leitweg_id=(
+                self.request.event.settings.invoice_address_custom_field.localize("de")
+                and "leitweg"
+                in self.request.event.settings.invoice_address_custom_field.localize(
+                    "de"
+                ).lower()
+            ),
+            tax_rules_used=not self.request.event.tax_rules.exists()
+            or not self.request.event.items.filter(tax_rule__isnull=True).exists(),
+            tax_codes_used=not self.request.event.tax_rules.filter(
+                code__isnull=True
+            ).exists(),
+        )
