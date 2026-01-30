eb23a2f8b7a2e4f3e24c4d82e61e5e8d2f3ca51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:32 +0000

    Add package fastfingertips/0.1.3/fastfingertips-0.1.3-py3-none-any.whl

---
+++ packages/fastfingertips/fastfingertips-0.1.3-py3-none-any.whl/fastfingertips/__init__.py
@@ -0,0 +1,11 @@
+"""
+fastfingertips
+~~~~~~~~~~~~~~
+
+A collection of personal Python utility functions.
+"""
+
+__title__ = "fastfingertips"
+__version__ = "0.1.3"
+__author__ = "FastFingertips"
+__license__ = "MIT"
