1213c3524bef7a6a948f10cb7f9b6c7fa3d33f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:03 +0000

    Add package pheval-exomiser/0.4.6/pheval_exomiser-0.4.6.tar.gz

---
+++ packages/pheval-exomiser/pheval_exomiser-0.4.6.tar.gz/pheval_exomiser-0.4.6/src/pheval_exomiser/post_process/post_process.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from pheval_exomiser.post_process.post_process_results_format import create_standardised_results
+from pheval_exomiser.prepare.tool_specific_configuration_options import ExomiserConfigurations
+
+
+def post_process_result_format(
+    config: ExomiserConfigurations,
+    raw_results_dir: Path,
+    output_dir: Path,
+    phenopacket_dir: Path,
+    variant_analysis: bool,
+    gene_analysis: bool,
+    disease_analysis: bool,
+    exomiser_version: str,
+):
+    """Standardise Exomiser json format to separated gene and variant results."""
+    create_standardised_results(
+        result_dir=raw_results_dir,
+        output_dir=output_dir,
+        phenopacket_dir=phenopacket_dir,
+        sort_order=config.post_process.sort_order,
+        score_name=config.post_process.score_name,
+        gene_analysis=gene_analysis,
+        disease_analysis=disease_analysis,
+        variant_analysis=variant_analysis,
+        exomiser_version=exomiser_version,
+    )
---
+++ packages/pheval-exomiser/pheval_exomiser-0.4.6.tar.gz/pheval_exomiser-0.4.6/src/pheval_exomiser/runner.py
@@ -0,0 +1,76 @@
+"""Exomiser Runner"""
+
+from dataclasses import dataclass
+from pathlib import Path
+
+from pheval.runners.runner import PhEvalRunner
+
+from pheval_exomiser.post_process.post_process import post_process_result_format
+from pheval_exomiser.prepare.tool_specific_configuration_options import ExomiserConfigurations
+from pheval_exomiser.prepare.write_application_properties import ExomiserConfigurationFileWriter
+from pheval_exomiser.run.run import prepare_batch_files, run_exomiser
+
+
+@dataclass
+class ExomiserPhEvalRunner(PhEvalRunner):
+    """_summary_"""
+
+    input_dir: Path
+    testdata_dir: Path
+    tmp_dir: Path
+    output_dir: Path
+    config_file: Path
+    version: str
+
+    def prepare(self):
+        """prepare"""
+        print("preparing")
+        ExomiserConfigurationFileWriter(
+            input_dir=self.input_dir,
+            configurations=ExomiserConfigurations.parse_obj(
+                self.input_dir_config.tool_specific_configuration_options
+            ),
+        ).write_application_properties()
+
+    def run(self):
+        """run"""
+        print("running with exomiser")
+        config = ExomiserConfigurations.parse_obj(
+            self.input_dir_config.tool_specific_configuration_options
+        )
+        prepare_batch_files(
+            input_dir=self.input_dir,
+            config=config,
+            testdata_dir=self.testdata_dir,
+            tool_input_commands_dir=self.tool_input_commands_dir,
+            raw_results_dir=self.raw_results_dir,
+            variant_analysis=self.input_dir_config.variant_analysis,
+            exomiser_version=self.version,
+        )
+        run_exomiser(
+            input_dir=self.input_dir,
+            testdata_dir=self.testdata_dir,
+            config=config,
+            output_dir=self.output_dir,
+            tool_input_commands_dir=self.tool_input_commands_dir,
+            raw_results_dir=self.raw_results_dir,
+            exomiser_version=self.version,
+            variant_analysis=self.input_dir_config.variant_analysis,
+        )
+
+    def post_process(self):
+        """post_process"""
+        print("post processing")
+        config = ExomiserConfigurations.parse_obj(
+            self.input_dir_config.tool_specific_configuration_options
+        )
+        post_process_result_format(
+            config=config,
+            raw_results_dir=self.raw_results_dir,
+            output_dir=self.output_dir,
+            phenopacket_dir=self.testdata_dir.joinpath("phenopackets"),
+            variant_analysis=self.input_dir_config.variant_analysis,
+            gene_analysis=self.input_dir_config.gene_analysis,
+            disease_analysis=self.input_dir_config.disease_analysis,
+            exomiser_version=self.version,
+        )
