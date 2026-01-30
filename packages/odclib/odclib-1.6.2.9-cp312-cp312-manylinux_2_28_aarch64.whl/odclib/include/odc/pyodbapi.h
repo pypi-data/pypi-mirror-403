70faa8c177bedebffffac1e299a3bdbe11672d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:22:38 +0000

    Add package odclib/1.6.2.9/odclib-1.6.2.9-cp312-cp312-manylinux_2_28_aarch64.whl

---
+++ packages/odclib/odclib-1.6.2.9-cp312-cp312-manylinux_2_28_aarch64.whl/odclib/include/odc/pyodbapi.h
@@ -0,0 +1,18 @@
+// Classes user by swig
+
+#include "eckit/eckit.h"
+
+#include "eckit/sql/SQLParser.h"
+#include "odc/DispatchingWriter.h"
+#include "odc/IteratorProxy.h"
+#include "odc/ODBAPISettings.h"
+#include "odc/ODBAPIVersion.h"
+#include "odc/Reader.h"
+#include "odc/Select.h"
+#include "odc/WriterBufferingIterator.h"
+#include "odc/core/Column.h"
+#include "odc/core/MetaData.h"
+#include "odc/csv/TextReader.h"
+#include "odc/csv/TextReaderIterator.h"
+
+using namespace odc;
