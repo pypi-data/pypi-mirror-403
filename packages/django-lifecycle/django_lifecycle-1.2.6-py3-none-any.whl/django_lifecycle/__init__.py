8bb766941d34cea9bf96681e3d73eeebd6c0be22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:24 +0000

    Add package django-lifecycle/1.2.6/django_lifecycle-1.2.6-py3-none-any.whl

---
+++ packages/django-lifecycle/django_lifecycle-1.2.6-py3-none-any.whl/django_lifecycle/__init__.py
@@ -0,0 +1,33 @@
+__version__ = "1.2.6"
+__author__ = "Robert Singer"
+__author_email__ = "robertgsinger@gmail.com"
+
+from .constants import NotSet
+from .decorators import hook
+from .hooks import AFTER_CREATE
+from .hooks import AFTER_DELETE
+from .hooks import AFTER_SAVE
+from .hooks import AFTER_UPDATE
+from .hooks import BEFORE_CREATE
+from .hooks import BEFORE_DELETE
+from .hooks import BEFORE_SAVE
+from .hooks import BEFORE_UPDATE
+from .mixins import LifecycleModelMixin
+from .mixins import bypass_hooks_for
+from .models import LifecycleModel
+
+__all__ = [
+    "hook",
+    "LifecycleModelMixin",
+    "LifecycleModel",
+    "BEFORE_SAVE",
+    "AFTER_SAVE",
+    "BEFORE_CREATE",
+    "AFTER_CREATE",
+    "BEFORE_UPDATE",
+    "AFTER_UPDATE",
+    "BEFORE_DELETE",
+    "AFTER_DELETE",
+    "NotSet",
+    "bypass_hooks_for",
+]
