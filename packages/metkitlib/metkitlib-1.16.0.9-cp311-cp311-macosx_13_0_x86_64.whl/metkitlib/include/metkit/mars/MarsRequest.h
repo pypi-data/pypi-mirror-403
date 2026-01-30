06f4e6a427f3a62cddb683ad0e9df3b4ba7f2129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:22:11 +0000

    Add package metkitlib/1.16.0.9/metkitlib-1.16.0.9-cp311-cp311-macosx_13_0_x86_64.whl

---
+++ packages/metkitlib/metkitlib-1.16.0.9-cp311-cp311-macosx_13_0_x86_64.whl/metkitlib/include/metkit/mars/MarsRequest.h
@@ -0,0 +1,198 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Manuel Fuentes
+/// @author Baudouin Raoult
+/// @author Tiago Quintino
+
+/// @date Sep 96
+
+#ifndef metkit_MarsRequest_H
+#define metkit_MarsRequest_H
+
+#include <optional>
+#include "eckit/value/Value.h"
+#include "metkit/mars/Parameter.h"
+
+namespace eckit {
+class MD5;
+namespace message {
+class Message;
+}
+}  // namespace eckit
+
+struct metkit_marsrequest_t;
+
+namespace metkit::mars {
+
+class Type;
+class MarsRequest;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class MarsRequest {
+public:  // methods
+
+    MarsRequest();
+
+    explicit MarsRequest(const std::string&);
+    explicit MarsRequest(eckit::Stream&, bool lowercase = false);
+
+    MarsRequest(const std::string&, const std::map<std::string, std::string>&);
+    MarsRequest(const std::string&, const eckit::Value&);
+
+    explicit MarsRequest(const eckit::message::Message&);
+
+    ~MarsRequest() = default;
+
+    bool operator<(const MarsRequest& other) const;
+
+    // eckit::Value&        operator[](const std::string&);
+    const std::string& operator[](const std::string&) const;
+
+    operator eckit::Value() const;
+
+    const std::string& verb() const;
+
+    size_t countValues(const std::string&) const;
+    bool has(const std::string&) const;
+
+    bool is(const std::string& param, const std::string& value) const;
+
+    const std::vector<std::string>& values(const std::string&, bool emptyOk = false) const;
+
+    // Returns reference to values or nullopt if not found
+    std::optional<std::reference_wrapper<const std::vector<std::string>>> get(const std::string& keyword) const;
+
+    template <class T>
+    size_t getValues(const std::string& name, std::vector<T>& v, bool emptyOk = false) const;
+
+    void getParams(std::vector<std::string>&) const;
+    std::vector<std::string> params() const;
+
+    std::list<Parameter>& parameters() { return params_; }
+
+    const std::list<Parameter>& parameters() const { return params_; }
+
+    void verb(const std::string&);
+
+    void values(const std::string&, const std::vector<std::string>&);
+
+    template <class T>
+    void setValue(const std::string& name, const T& value);
+
+    void setValue(const std::string& name, const char* value);
+
+    void unsetValues(const std::string&);
+
+    /// Splits a MARS request into multiple requests along the provided key
+    std::vector<MarsRequest> split(const std::string& keys) const;
+
+    /// Splits a MARS request into multiple requests along the indicated keys
+    std::vector<MarsRequest> split(const std::vector<std::string>& keys) const;
+
+    /// Merges one MarsRequest into another
+    /// @todo Improve performance -- uses O(N^2) search / merge in std::list's
+    void merge(const MarsRequest& other);
+
+    /// Create a new MarsRequest from this one with only the given set of keys
+    MarsRequest subset(const std::set<std::string>&) const;
+
+    void json(eckit::JSON&, bool array = false) const;
+
+    void md5(eckit::MD5&) const;
+
+    void dump(std::ostream&, const char* cr = "\n", const char* tab = "\t", bool verb = true) const;
+
+    void setValuesTyped(Type*, const std::vector<std::string>&);
+
+    bool filter(const MarsRequest& filter);
+    bool matches(const MarsRequest& filter) const;
+    bool empty() const;
+
+    size_t count() const;
+
+    MarsRequest extract(const std::string& category) const;
+
+    void erase(const std::string& param);
+
+    std::string asString() const;
+
+public:  // static methods
+
+    static MarsRequest parse(const std::string& s, bool strict = false);
+    static std::vector<MarsRequest> parse(std::istream&, bool strict = false);
+
+    /// Implementation in api/metkit_c.cc
+    static const MarsRequest& fromOpaque(const metkit_marsrequest_t* request);
+
+private:  // members
+
+    std::string verb_;
+    std::list<Parameter> params_;
+
+private:  // methods
+
+    void print(std::ostream&) const;
+    void encode(eckit::Stream&) const;
+
+    std::list<Parameter>::const_iterator find(const std::string&) const;
+    std::list<Parameter>::iterator find(const std::string&);
+
+    // -- Class members
+
+    static eckit::ClassSpec classSpec_;
+    static eckit::Reanimator<MarsRequest> reanimator_;
+
+    friend std::ostream& operator<<(std::ostream& s, const MarsRequest& r) {
+        r.print(s);
+        return s;
+    }
+
+    friend eckit::JSON& operator<<(eckit::JSON& s, const MarsRequest& r) {
+        r.json(s);
+        return s;
+    }
+
+    friend eckit::Stream& operator<<(eckit::Stream& s, const MarsRequest& r) {
+        r.encode(s);
+        return s;
+    }
+};
+
+
+template <class T>
+size_t MarsRequest::getValues(const std::string& name, std::vector<T>& v, bool emptyOk) const {
+    const std::vector<std::string>& s = values(name, emptyOk);
+
+    eckit::Translator<std::string, T> t;
+
+    v.clear();
+
+    for (std::vector<std::string>::const_iterator j = s.begin(); j != s.end(); ++j) {
+        v.push_back(t(*j));
+    }
+
+    return v.size();
+}
+
+
+template <class T>
+void MarsRequest::setValue(const std::string& name, const T& value) {
+    eckit::Translator<T, std::string> t;
+    std::vector<std::string> v(1, t(value));
+    values(name, v);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}  // namespace metkit::mars
+
+#endif
---
+++ packages/metkitlib/metkitlib-1.16.0.9-cp311-cp311-macosx_13_0_x86_64.whl/metkitlib/include/metkit/mars/Type.h
@@ -0,0 +1,265 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @file   Type.h
+/// @author Baudouin Raoult
+/// @author Tiago Quintino
+/// @author Emanuele Danovaro
+/// @date   April 2016
+
+#pragma once
+
+#include <functional>
+#include <iosfwd>
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "eckit/memory/Counted.h"
+#include "eckit/value/Value.h"
+
+#include "metkit/mars/MarsRequest.h"
+
+namespace metkit::mars {
+
+class MarsExpandContext;
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/// @brief abstract class - ContextRule subclasses are used to define a context. A MarsRequest matches a context, if it
+/// matches all its ContextRules
+class ContextRule {
+public:
+
+    ContextRule(const std::string& k) : key_(k) {}
+
+    virtual ~ContextRule() = default;
+
+    const std::string& key() const { return key_; }
+
+    virtual bool matches(MarsRequest req) const = 0;
+
+    friend std::ostream& operator<<(std::ostream& s, const ContextRule& r) {
+        r.print(s);
+        return s;
+    }
+
+protected:
+
+    std::string key_;
+
+private:  // methods
+
+    virtual void print(std::ostream& out) const = 0;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/// @brief A MarsRequest matches an Include ContextRule if at least one of the mars request values matches with the
+/// values associated with the Include rule
+class Include : public ContextRule {
+public:
+
+    Include(const std::string& k, const std::set<std::string>& vv) : ContextRule(k), vals_(vv) {}
+
+    bool matches(MarsRequest req) const override {
+        if (key_ == "_verb") {
+            return (vals_.find(req.verb()) != vals_.end());
+        }
+        if (!req.has(key_)) {
+            return false;
+        }
+        for (const std::string& v : req.values(key_)) {
+            if (vals_.find(v) != vals_.end()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:  // methods
+
+    void print(std::ostream& out) const override { out << "Include[key=" << key_ << ",vals=[" << vals_ << "]]"; }
+
+private:
+
+    std::set<std::string> vals_;
+};
+
+/// @brief A MarsRequest matches an Exclude ContextRule if none of the mars request values matches with the values
+/// associated with the Exclude rule
+class Exclude : public ContextRule {
+public:
+
+    Exclude(const std::string& k, const std::set<std::string>& vv) : ContextRule(k), vals_(vv) {}
+    bool matches(MarsRequest req) const override {
+        if (!req.has(key_)) {
+            return false;
+        }
+        for (const std::string& v : req.values(key_)) {
+            if (vals_.find(v) != vals_.end()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+private:  // methods
+
+    void print(std::ostream& out) const override { out << "Exclude[key=" << key_ << ",vals=[" << vals_ << "]]"; }
+
+private:
+
+    std::set<std::string> vals_;
+};
+
+/// @brief A MarsRequest matches an Undef ContextRule if the specified keyword is not defined in the mars request
+class Undef : public ContextRule {
+public:
+
+    Undef(const std::string& k) : ContextRule(k) {}
+    bool matches(MarsRequest req) const override { return !req.has(key_); }
+
+private:  // methods
+
+    void print(std::ostream& out) const override { out << "Undef[key=" << key_ << "]"; }
+};
+
+/// @brief A MarsRequest matches an Undef ContextRule if the specified keyword is defined in the mars request
+class Def : public ContextRule {
+public:
+
+    Def(const std::string& k) : ContextRule(k) {}
+    bool matches(MarsRequest req) const override { return req.has(key_); }
+
+private:  // methods
+
+    void print(std::ostream& out) const override { out << "Def[key=" << key_ << "]"; }
+};
+
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/// @brief a Context contains a list of ContextRule. A MarsRequest matches a context, if it matches all the ContextRules
+/// associated
+class Context {
+public:
+
+    static std::unique_ptr<Context> parseContext(eckit::Value c);
+
+    /// @note takes ownership of the rule
+    void add(std::unique_ptr<ContextRule> rule);
+
+    size_t maxAxisIndex() const;
+
+    bool matches(MarsRequest req) const;
+
+    friend std::ostream& operator<<(std::ostream& s, const Context& x);
+
+private:  // methods
+
+    void print(std::ostream& out) const;
+
+private:
+
+    std::vector<std::unique_ptr<ContextRule>> rules_;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class ITypeToByList {
+public:
+
+    virtual ~ITypeToByList()                                                                      = default;
+    virtual void expandRanges(std::vector<std::string>& values, const MarsRequest& request) const = 0;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+class Type : public eckit::Counted {
+public:  // methods
+
+    Type(const std::string& name, const eckit::Value& settings);
+
+    ~Type() noexcept override = default;
+
+    virtual bool expand(std::string& value, const MarsRequest& request = {}) const;
+    [[deprecated]] bool expand(const MarsExpandContext& ctx, std::string& value, const MarsRequest& request = {}) const;
+    void expand(std::vector<std::string>& values, const MarsRequest& request = {}) const;
+
+    std::string tidy(const std::string& value, const MarsRequest& request = {}) const;
+
+    virtual void setDefaults(MarsRequest& request);
+    virtual void setInheritance(const std::vector<std::string>& inheritance);
+    virtual void check(const std::vector<std::string>& values) const;
+    virtual void clearDefaults();
+    virtual void reset();
+
+    virtual void pass2(MarsRequest& request);
+    virtual void finalise(MarsRequest& request, bool strict);
+
+    virtual const std::vector<std::string>& flattenValues(const MarsRequest& request);
+    virtual bool flatten() const;
+    virtual bool multiple() const;
+
+    virtual bool filter(const std::vector<std::string>& filter, std::vector<std::string>& values) const;
+    virtual bool filter(const std::string& keyword, const std::vector<std::string>& filter,
+                        std::vector<std::string>& values) const;
+    virtual bool matches(const std::vector<std::string>& filter, const std::vector<std::string>& values) const;
+
+    const std::string& name() const;
+    const std::string& category() const;
+
+    friend std::ostream& operator<<(std::ostream& s, const Type& x);
+
+    virtual size_t count(const std::vector<std::string>& values) const;
+
+protected:  // methods
+
+    virtual bool hasGroups() const { return false; }
+    virtual std::optional<std::reference_wrapper<const std::vector<std::string>>> group(const std::string&) const {
+        NOTIMP;
+    }
+
+    friend class MarsLanguage;
+
+    void defaults(std::shared_ptr<Context> context, const std::vector<std::string>& values);
+    void set(std::shared_ptr<Context> context, const std::vector<std::string>& values);
+    void unset(std::shared_ptr<Context> context);
+
+protected:  // members
+
+    std::string name_;
+    std::string category_;
+
+    bool flatten_;
+    bool multiple_;
+    bool duplicates_;
+
+    std::map<std::shared_ptr<Context>, std::vector<std::string>> defaults_;
+    std::map<std::shared_ptr<Context>, std::vector<std::string>> sets_;
+    std::set<std::shared_ptr<Context>> unsets_;
+
+    std::optional<std::vector<std::string>> inheritance_;
+
+    std::unique_ptr<ITypeToByList> toByList_;
+
+    std::map<std::string, std::function<bool(const std::vector<std::string>&, std::vector<std::string>&)>> filters_;
+
+private:  // methods
+
+    virtual void print(std::ostream& out) const = 0;
+    void patchRequest(MarsRequest& request, const std::vector<std::string>& values);
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+}  // namespace metkit::mars
