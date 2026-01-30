eb0d6384c7407215218b32aa0a53f5e1b4dc2245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:34:27 +0000

    Add package meshconsole/2.2.1/meshconsole-2.2.1-py3-none-any.whl

---
+++ packages/meshconsole/meshconsole-2.2.1-py3-none-any.whl/meshconsole/__init__.py
@@ -0,0 +1,13 @@
+"""
+MeshConsole - A tool for interacting with Meshtastic devices.
+
+Author: M9WAV
+License: MIT
+"""
+
+__version__ = "2.2.1"
+__author__ = "M9WAV"
+
+from meshconsole.core import MeshtasticTool, MeshtasticToolError, PacketSummary
+
+__all__ = ["MeshtasticTool", "MeshtasticToolError", "PacketSummary", "__version__"]
---
+++ packages/meshconsole/meshconsole-2.2.1-py3-none-any.whl/meshconsole/__main__.py
@@ -0,0 +1,8 @@
+"""
+Entry point for running meshconsole as a module: python -m meshconsole
+"""
+
+from meshconsole.core import main
+
+if __name__ == "__main__":
+    main()
