659d4f3c7d731b0b955920fe6976aae3a0b8ab13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:06 +0000

    Add package cl-forge/0.5.1/cl_forge-0.5.1-cp312-cp312-win_amd64.whl

---
+++ packages/cl-forge/cl_forge-0.5.1-cp312-cp312-win_amd64.whl/cl_forge/cmf.py
@@ -0,0 +1,11 @@
+from cl_forge.core.endpoints import Eur, Ipc, Uf, Usd, Utm
+from cl_forge.core.impl.rs_cmf import CmfClient
+
+__all__ = (
+    "CmfClient",
+    "Ipc",
+    "Usd",
+    "Eur",
+    "Uf",
+    "Utm",
+)
