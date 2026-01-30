2d068c6814f346836dc2edd973f62600e25b219e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:52 +0000

    Add package kleister/2.1.1/kleister-2.1.1.tar.gz

---
+++ packages/kleister/kleister-2.1.1.tar.gz/kleister-2.1.1/kleister/api/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+
+# import apis into api package
+from kleister.api.auth_api import AuthApi
+from kleister.api.fabric_api import FabricApi
+from kleister.api.forge_api import ForgeApi
+from kleister.api.group_api import GroupApi
+from kleister.api.minecraft_api import MinecraftApi
+from kleister.api.mod_api import ModApi
+from kleister.api.neoforge_api import NeoforgeApi
+from kleister.api.pack_api import PackApi
+from kleister.api.profile_api import ProfileApi
+from kleister.api.quilt_api import QuiltApi
+from kleister.api.user_api import UserApi
+
