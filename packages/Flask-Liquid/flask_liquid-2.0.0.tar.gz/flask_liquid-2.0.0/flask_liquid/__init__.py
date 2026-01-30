bd67f391a9ab451c38fc42d4e2d59ca5e1157504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:49 +0000

    Add package Flask-Liquid/2.0.0/flask_liquid-2.0.0.tar.gz

---
+++ packages/Flask-Liquid/flask_liquid-2.0.0.tar.gz/flask_liquid-2.0.0/flask_liquid/__init__.py
@@ -0,0 +1,16 @@
+from .flask_liquid import Liquid
+from .flask_liquid import render_template
+from .flask_liquid import render_template_async
+from .flask_liquid import render_template_string
+from .flask_liquid import render_template_string_async
+
+__version__ = "2.0.0"
+
+
+__all__ = (
+    "Liquid",
+    "render_template",
+    "render_template_async",
+    "render_template_string",
+    "render_template_string_async",
+)
