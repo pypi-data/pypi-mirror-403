572209ba4048f64ad78439be98ccd6d1a6766cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:20 +0000

    Add package codac/2.0.0.dev24/codac-2.0.0.dev24-cp313-cp313-win32.whl

---
+++ packages/codac/codac-2.0.0.dev24-cp313-cp313-win32.whl/codac/tests/test_IntFullPivLU.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+#  Codac tests
+# ----------------------------------------------------------------------------
+#  \date       2024
+#  \author     Damien Massé
+#  \copyright  Copyright 2024 Codac Team
+#  \license    GNU Lesser General Public License (LGPL)
+
+
+import unittest
+from codac import *
+
+class TestIntvFullPivLU(unittest.TestCase):
+
+
+# bindings not implemented
+   def test_IntvFullPivLU_1(self):
+      M = Matrix([
+      [ 1, -4, 3, 7 ],
+      [ 2, 1, -4, 6 ],
+      [ 5, 2, 1 , 9 ],
+      [ -1, 0, 3, 2 ]
+      ])
+
+      LUdec = IntvFullPivLU(M)
+      self.assertTrue(LUdec.is_injective()==BoolInterval.TRUE)
+      self.assertTrue(LUdec.is_surjective()==BoolInterval.TRUE)
+      self.assertTrue(LUdec.rank()==Interval(4))
+      self.assertTrue((LUdec.determinant()+602).mag()<=1e-10)
+      self.assertTrue((LUdec.reconstructed_matrix()-M).norm().ub()<=1e-10)
+      I1 = LUdec.solve(IntervalMatrix.eye(4,4));
+      self.assertTrue((I1*M-Matrix.eye(4,4)).norm().ub()<1e-10)
+
+   def test_IntvFullPivLU_2(self):
+      M = Matrix([
+      [ 1, -4, 6, 7 ],
+      [ 2, 1, 3, 6 ],
+      [ 5, 2, 8 , 9 ],
+      [ -1, 0, -2, 2 ]
+      ])
+
+      LUdec = IntvFullPivLU(M)
+      self.assertTrue(LUdec.is_injective()==BoolInterval.UNKNOWN)
+      self.assertTrue(LUdec.is_surjective()==BoolInterval.UNKNOWN)
+      self.assertTrue(LUdec.rank()==Interval([3,4]))
+      self.assertTrue((LUdec.determinant()).mag()<=1e-10)
+      self.assertTrue((LUdec.reconstructed_matrix()-M).norm().ub()<=1e-10)
+      K = LUdec.kernel()
+      self.assertTrue(K.cols()==1)
+      self.assertTrue((M*K).norm().ub()<1e-10)
+      coK = LUdec.cokernel()
+      self.assertTrue(coK.rows()==1)
+      self.assertTrue((coK*M).norm().ub()<1e-10)
+      Im = LUdec.image(M)
+      self.assertTrue(Im.cols()==3)
+      coIm = LUdec.coimage(M)
+      self.assertTrue(coIm.rows()==3)
+
+   def test_IntvFullPivLU_3(self):
+      M = Matrix([
+      [ 1, -4, 6, 7, 6 ],
+      [ 2, 1, 3, 6, -2 ],
+      [ 5, 2, 8 , 9, -1 ]
+      ])
+      LUdec = IntvFullPivLU(M)
+      self.assertTrue(LUdec.is_injective()==BoolInterval.FALSE)
+      self.assertTrue(LUdec.is_surjective()==BoolInterval.TRUE)
+      self.assertTrue(LUdec.rank()==Interval(3))
+      self.assertTrue((LUdec.reconstructed_matrix()-M).norm().ub()<=1e-10)
+      K = LUdec.kernel()
+      self.assertTrue(K.cols()==2)
+      self.assertTrue((M*K).norm().ub()<1e-10)
+      Im = LUdec.image(M)
+      self.assertTrue(Im.cols()==3)
+      I1 = LUdec.solve(IntervalMatrix.eye(3,3));
+      self.assertTrue((M*I1-Matrix.eye(3,3)).norm().ub()<1e-10)
+      A = IntervalMatrix([ [1], [[-20,20]], [2], [[-20,20]], [[-20,20]] ])
+      B = IntervalMatrix([ [2.0], [1.0], [4.0] ])
+      LUdec.solve(B,A)
+      self.assertTrue((M*A-B).norm().ub()<=1e-10)
+
+if __name__ ==  '__main__':
+  unittest.main()
