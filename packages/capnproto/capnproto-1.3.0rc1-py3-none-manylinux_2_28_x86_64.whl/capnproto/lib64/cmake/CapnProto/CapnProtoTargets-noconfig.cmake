e63ecfb86dd302583c36fa11180f7ce7f837dbff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:46 +0000

    Add package capnproto/1.3.0rc1/capnproto-1.3.0rc1-py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/capnproto/capnproto-1.3.0rc1-py3-none-manylinux_2_28_x86_64.whl/capnproto/lib64/cmake/CapnProto/CapnProtoTargets-noconfig.cmake
@@ -0,0 +1,136 @@
+#----------------------------------------------------------------
+# Generated CMake target import file.
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "CapnProto::kj" for configuration ""
+set_property(TARGET CapnProto::kj APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::kj PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libkj.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::kj )
+list(APPEND _cmake_import_check_files_for_CapnProto::kj "${_IMPORT_PREFIX}/lib64/libkj.a" )
+
+# Import target "CapnProto::kj-test" for configuration ""
+set_property(TARGET CapnProto::kj-test APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::kj-test PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libkj-test.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::kj-test )
+list(APPEND _cmake_import_check_files_for_CapnProto::kj-test "${_IMPORT_PREFIX}/lib64/libkj-test.a" )
+
+# Import target "CapnProto::kj-async" for configuration ""
+set_property(TARGET CapnProto::kj-async APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::kj-async PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libkj-async.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::kj-async )
+list(APPEND _cmake_import_check_files_for_CapnProto::kj-async "${_IMPORT_PREFIX}/lib64/libkj-async.a" )
+
+# Import target "CapnProto::kj-http" for configuration ""
+set_property(TARGET CapnProto::kj-http APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::kj-http PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libkj-http.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::kj-http )
+list(APPEND _cmake_import_check_files_for_CapnProto::kj-http "${_IMPORT_PREFIX}/lib64/libkj-http.a" )
+
+# Import target "CapnProto::kj-gzip" for configuration ""
+set_property(TARGET CapnProto::kj-gzip APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::kj-gzip PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libkj-gzip.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::kj-gzip )
+list(APPEND _cmake_import_check_files_for_CapnProto::kj-gzip "${_IMPORT_PREFIX}/lib64/libkj-gzip.a" )
+
+# Import target "CapnProto::capnp" for configuration ""
+set_property(TARGET CapnProto::capnp APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::capnp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libcapnp.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::capnp )
+list(APPEND _cmake_import_check_files_for_CapnProto::capnp "${_IMPORT_PREFIX}/lib64/libcapnp.a" )
+
+# Import target "CapnProto::capnp-rpc" for configuration ""
+set_property(TARGET CapnProto::capnp-rpc APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::capnp-rpc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libcapnp-rpc.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::capnp-rpc )
+list(APPEND _cmake_import_check_files_for_CapnProto::capnp-rpc "${_IMPORT_PREFIX}/lib64/libcapnp-rpc.a" )
+
+# Import target "CapnProto::capnp-json" for configuration ""
+set_property(TARGET CapnProto::capnp-json APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::capnp-json PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libcapnp-json.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::capnp-json )
+list(APPEND _cmake_import_check_files_for_CapnProto::capnp-json "${_IMPORT_PREFIX}/lib64/libcapnp-json.a" )
+
+# Import target "CapnProto::capnp-websocket" for configuration ""
+set_property(TARGET CapnProto::capnp-websocket APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::capnp-websocket PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libcapnp-websocket.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::capnp-websocket )
+list(APPEND _cmake_import_check_files_for_CapnProto::capnp-websocket "${_IMPORT_PREFIX}/lib64/libcapnp-websocket.a" )
+
+# Import target "CapnProto::capnpc" for configuration ""
+set_property(TARGET CapnProto::capnpc APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::capnpc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_NOCONFIG "CXX"
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/lib64/libcapnpc.a"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::capnpc )
+list(APPEND _cmake_import_check_files_for_CapnProto::capnpc "${_IMPORT_PREFIX}/lib64/libcapnpc.a" )
+
+# Import target "CapnProto::capnp_tool" for configuration ""
+set_property(TARGET CapnProto::capnp_tool APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::capnp_tool PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/bin/capnp"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::capnp_tool )
+list(APPEND _cmake_import_check_files_for_CapnProto::capnp_tool "${_IMPORT_PREFIX}/bin/capnp" )
+
+# Import target "CapnProto::capnpc_cpp" for configuration ""
+set_property(TARGET CapnProto::capnpc_cpp APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::capnpc_cpp PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/bin/capnpc-c++"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::capnpc_cpp )
+list(APPEND _cmake_import_check_files_for_CapnProto::capnpc_cpp "${_IMPORT_PREFIX}/bin/capnpc-c++" )
+
+# Import target "CapnProto::capnpc_capnp" for configuration ""
+set_property(TARGET CapnProto::capnpc_capnp APPEND PROPERTY IMPORTED_CONFIGURATIONS NOCONFIG)
+set_target_properties(CapnProto::capnpc_capnp PROPERTIES
+  IMPORTED_LOCATION_NOCONFIG "${_IMPORT_PREFIX}/bin/capnpc-capnp"
+  )
+
+list(APPEND _cmake_import_check_targets CapnProto::capnpc_capnp )
+list(APPEND _cmake_import_check_files_for_CapnProto::capnpc_capnp "${_IMPORT_PREFIX}/bin/capnpc-capnp" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
