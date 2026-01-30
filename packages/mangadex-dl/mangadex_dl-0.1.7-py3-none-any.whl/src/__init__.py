85e3417fe5865ae335b15e5aff5d2a5ec1ea433b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:16:01 +0000

    Add package mangadex-dl/0.1.7/mangadex_dl-0.1.7-py3-none-any.whl

---
+++ packages/mangadex-dl/mangadex_dl-0.1.7-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,9 @@
+"""
+command-line tool to download single or multiple manga chapters from mangadex with ease.
+"""
+
+__version__ = "0.1.7"
+__description__ = "command-line tool to download single or multiple manga chapters from mangadex with ease."
+__author__ = "syvixor"
+__author_email__ = "syvixor@proton.me"
+__license__ = "MIT"
