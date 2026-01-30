c1613b5b3bb0c691f49721081c2b5d129f91ae5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:15:43 +0000

    Add package atlas-compute/0.1.10/atlas_compute-0.1.10.tar.gz

---
+++ packages/atlas-compute/atlas_compute-0.1.10.tar.gz/atlas_compute-0.1.10/setup.py
@@ -0,0 +1,36 @@
+"""Setup script for Atlas SDK."""
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="atlas-compute",
+    version="0.1.10",
+    description="Atlas Compute SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Thomas Tsuma",
+    author_email="tommytsuma7@gmail.com",
+    url="https://github.com/coreoutline/atlas-compute",
+    packages=["atlas_compute", "atlas_compute.repositories"],
+    package_dir={"atlas_compute": "."},
+    install_requires=[
+        "pandas",
+        "numpy",
+        "clickhouse-driver",
+        "matplotlib",
+        "opensearch-py",
+        "minio",
+        "requests",
+        "python-dotenv"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
