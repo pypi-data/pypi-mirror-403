9f2917d9c5173c036cce5025109d7ff4d789e21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:23 +0000

    Add package xulbux/1.9.5/xulbux-1.9.5-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/xulbux/xulbux-1.9.5-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/xulbux/cli/help.py
@@ -0,0 +1,77 @@
+from .. import __version__
+from ..format_codes import FormatCodes
+from ..console import Console
+
+from urllib.error import HTTPError
+from typing import Optional
+import urllib.request as _request
+import json as _json
+
+
+def get_latest_version() -> Optional[str]:
+    with _request.urlopen(URL) as response:
+        if response.status == 200:
+            data = _json.load(response)
+            return data["info"]["version"]
+        else:
+            raise HTTPError(URL, response.status, "Failed to fetch latest version info", response.headers, None)
+
+
+def is_latest_version() -> Optional[bool]:
+    try:
+        if (latest := get_latest_version()) in {"", None}:
+            return None
+        latest_v_parts = tuple(int(part) for part in (latest or "").lower().lstrip("v").split("."))
+        installed_v_parts = tuple(int(part) for part in __version__.lower().lstrip("v").split("."))
+        return latest_v_parts <= installed_v_parts
+    except Exception:
+        return None
+
+
+URL = "https://pypi.org/pypi/xulbux/json"
+IS_LATEST_VERSION = is_latest_version()
+
+CLI_COLORS = {
+    "border": "dim|br:black",
+    "class": "br:cyan",
+    "const": "br:blue",
+    "func": "br:green",
+    "heading": "br:white",
+    "import": "magenta",
+    "lib": "br:magenta",
+    "link": "u|br:blue",
+    "notice": "br:yellow",
+    "punctuator": "br:black",
+    "text": "white",
+}
+CLI_HELP = FormatCodes.to_ansi(
+    rf"""  [_|b|#7075FF]               __  __
+  [b|#7075FF]  _  __ __  __/ / / /_  __  ___  __
+  [b|#7075FF] | |/ // / / / / / __ \/ / / | |/ /
+  [b|#7075FF] > , </ /_/ / /_/ /_/ / /_/ /> , <
+  [b|#7075FF]/_/|_|\____/\__/\____/\____//_/|_|  [*|#000|BG:#8085FF] v[b]{__version__} [*|dim|{CLI_COLORS["notice"]}]({"" if IS_LATEST_VERSION else " (newer available)"})[*]
+
+  [i|#9095FF]A TON OF COOL FUNCTIONS, YOU NEED![*]
+
+  [b|{CLI_COLORS["heading"]}](Usage:)[*]
+  [{CLI_COLORS["border"]}](╭────────────────────────────────────────────────────╮)[*]
+  [{CLI_COLORS["border"]}](│) [i|{CLI_COLORS["punctuator"]}](# LIBRARY CONSTANTS)[*]                                [{CLI_COLORS["border"]}](│)[*]
+  [{CLI_COLORS["border"]}](│) [{CLI_COLORS["import"]}]from [{CLI_COLORS["lib"]}]xulbux[{CLI_COLORS["punctuator"]}].[{CLI_COLORS["lib"]}]base[{CLI_COLORS["punctuator"]}].[{CLI_COLORS["lib"]}]consts [{CLI_COLORS["import"]}]import [{CLI_COLORS["const"]}]COLOR[{CLI_COLORS["punctuator"]}], [{CLI_COLORS["const"]}]CHARS[{CLI_COLORS["punctuator"]}], [{CLI_COLORS["const"]}]ANSI[*]  [{CLI_COLORS["border"]}](│)[*]
+  [{CLI_COLORS["border"]}](│) [i|{CLI_COLORS["punctuator"]}](# Main Classes)[*]                                     [{CLI_COLORS["border"]}](│)[*]
+  [{CLI_COLORS["border"]}](│) [{CLI_COLORS["import"]}]from [{CLI_COLORS["lib"]}]xulbux [{CLI_COLORS["import"]}]import [{CLI_COLORS["class"]}]Code[{CLI_COLORS["punctuator"]}], [{CLI_COLORS["class"]}]Color[{CLI_COLORS["punctuator"]}], [{CLI_COLORS["class"]}]Console[{CLI_COLORS["punctuator"]}], ...[*]       [{CLI_COLORS["border"]}](│)[*]
+  [{CLI_COLORS["border"]}](│) [i|{CLI_COLORS["punctuator"]}](# module specific imports)[*]                          [{CLI_COLORS["border"]}](│)[*]
+  [{CLI_COLORS["border"]}](│) [{CLI_COLORS["import"]}]from [{CLI_COLORS["lib"]}]xulbux[{CLI_COLORS["punctuator"]}].[{CLI_COLORS["lib"]}]color [{CLI_COLORS["import"]}]import [{CLI_COLORS["func"]}]rgba[{CLI_COLORS["punctuator"]}], [{CLI_COLORS["func"]}]hsla[{CLI_COLORS["punctuator"]}], [{CLI_COLORS["func"]}]hexa[*]          [{CLI_COLORS["border"]}](│)
+  [{CLI_COLORS["border"]}](╰────────────────────────────────────────────────────╯)[*]
+  [b|{CLI_COLORS["heading"]}](Documentation:)[*]
+  [{CLI_COLORS["border"]}](╭────────────────────────────────────────────────────╮)[*]
+  [{CLI_COLORS["border"]}](│) [{CLI_COLORS["text"]}]For more information see the GitHub page.          [{CLI_COLORS["border"]}](│)[*]
+  [{CLI_COLORS["border"]}](│) [{CLI_COLORS["link"]}](https://github.com/XulbuX/PythonLibraryXulbuX/wiki) [{CLI_COLORS["border"]}](│)[*]
+  [{CLI_COLORS["border"]}](╰────────────────────────────────────────────────────╯)[*]
+  [_]"""
+)
+
+
+def show_help() -> None:
+    FormatCodes._config_console()
+    print(CLI_HELP)
+    Console.pause_exit(pause=True, prompt="  [dim](Press any key to exit...)\n\n")
---
+++ packages/xulbux/xulbux-1.9.5-cp314-cp314-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/xulbux/regex.py
@@ -0,0 +1,247 @@
+"""
+This module provides the `Regex` class, which includes methods
+to dynamically generate complex regex patterns for common use cases.
+"""
+
+from .base.decorators import mypyc_attr
+
+from typing import Optional
+import regex as _rx
+import re as _re
+
+
+class Regex:
+    """This class provides methods to dynamically generate complex regex patterns for common use cases."""
+
+    @classmethod
+    def quotes(cls) -> str:
+        """Matches pairs of quotes. (strings)\n
+        --------------------------------------------------------------------------------
+        Will create two named groups:
+        - `quote` the quote type (single or double)
+        - `string` everything inside the found quote pair\n
+        ---------------------------------------------------------------------------------
+        Attention: Requires non-standard library `regex`, not standard library `re`!"""
+        return r"""(?P<quote>["'])(?P<string>(?:\\.|(?!\g<quote>).)*?)\g<quote>"""
+
+    @classmethod
+    def brackets(
+        cls,
+        bracket1: str = "(",
+        bracket2: str = ")",
+        is_group: bool = False,
+        strip_spaces: bool = False,
+        ignore_in_strings: bool = True,
+    ) -> str:
+        """Matches everything inside pairs of brackets, including other nested brackets.\n
+        ---------------------------------------------------------------------------------------
+        - `bracket1` -⠀the opening bracket (e.g. `(`, `{`, `[`, …)
+        - `bracket2` -⠀the closing bracket (e.g. `)`, `}`, `]`, …)
+        - `is_group` -⠀whether to create a capturing group for the content inside the brackets
+        - `strip_spaces` -⠀whether to strip spaces from the bracket content or not
+        - `ignore_in_strings` -⠀whether to ignore closing brackets that are inside
+          strings/quotes (e.g. `'…)…'` or `"…)…"`)\n
+        ---------------------------------------------------------------------------------------
+        Attention: Requires non-standard library `regex`, not standard library `re`!"""
+        g = "" if is_group else "?:"
+        b1 = _rx.escape(bracket1) if len(bracket1) == 1 else bracket1
+        b2 = _rx.escape(bracket2) if len(bracket2) == 1 else bracket2
+        s1 = r"\s*" if strip_spaces else ""
+        s2 = "" if strip_spaces else r"\s*"
+
+        if ignore_in_strings:
+            return cls._clean( \
+                rf"""{b1}{s1}({g}{s2}(?:
+                    [^{b1}{b2}"']
+                    |"(?:\\.|[^"\\])*"
+                    |'(?:\\.|[^'\\])*'
+                    |{b1}(?:
+                        [^{b1}{b2}"']
+                        |"(?:\\.|[^"\\])*"
+                        |'(?:\\.|[^'\\])*'
+                        |(?R)
+                    )*{b2}
+                )*{s2}){s1}{b2}"""
+            )
+        else:
+            return cls._clean( \
+                rf"""{b1}{s1}({g}{s2}(?:
+                    [^{b1}{b2}]
+                    |{b1}(?:
+                        [^{b1}{b2}]
+                        |(?R)
+                    )*{b2}
+                )*{s2}){s1}{b2}"""
+            )
+
+    @classmethod
+    def outside_strings(cls, pattern: str = r".*") -> str:
+        """Matches the `pattern` only when it is not found inside a string (`'…'` or `"…"`)."""
+        return rf"""(?<!["'])(?:{pattern})(?!["'])"""
+
+    @classmethod
+    def all_except(cls, disallowed_pattern: str, ignore_pattern: str = "", is_group: bool = False) -> str:
+        """Matches everything up to the `disallowed_pattern`, unless the
+        `disallowed_pattern` is found inside a string/quotes (`'…'` or `"…"`).\n
+        -------------------------------------------------------------------------------------
+        - `disallowed_pattern` -⠀the pattern that is not allowed to be matched
+        - `ignore_pattern` -⠀a pattern that, if found, will make the regex ignore the
+          `disallowed_pattern` (even if it contains the `disallowed_pattern` inside it):<br>
+          For example if `disallowed_pattern` is `>` and `ignore_pattern` is `->`,
+          the `->`-arrows will be allowed, even though they have `>` in them.
+        - `is_group` -⠀whether to create a capturing group for the matched content"""
+        g = "" if is_group else "?:"
+
+        return cls._clean( \
+            rf"""({g}
+                (?:(?!{ignore_pattern}).)*
+                (?:(?!{cls.outside_strings(disallowed_pattern)}).)*
+            )"""
+        )
+
+    @classmethod
+    def func_call(cls, func_name: Optional[str] = None) -> str:
+        """Match a function call, and get back two groups:
+        1. function name
+        2. the function's arguments\n
+        If no `func_name` is given, it will match any function call.\n
+        ---------------------------------------------------------------------------------
+        Attention: Requires non-standard library `regex`, not standard library `re`!"""
+        if func_name in {"", None}:
+            func_name = r"[\w_]+"
+
+        return rf"""(?<=\b)({func_name})\s*{cls.brackets("(", ")", is_group=True)}"""
+
+    @classmethod
+    def rgba_str(cls, fix_sep: Optional[str] = ",", allow_alpha: bool = True) -> str:
+        """Matches an RGBA color inside a string.\n
+        ----------------------------------------------------------------------------------
+        - `fix_sep` -⠀the fixed separator between the RGBA values (e.g. `,`, `;` …)<br>
+          If set to nothing or `None`, any char that is not a letter or number
+          can be used to separate the RGBA values, including just a space.
+        - `allow_alpha` -⠀whether to include the alpha channel in the match\n
+        ----------------------------------------------------------------------------------
+        The RGBA color can be in the formats (for `fix_sep = ','`):
+        - `rgba(r, g, b)`
+        - `rgba(r, g, b, a)` (if `allow_alpha=True`)
+        - `(r, g, b)`
+        - `(r, g, b, a)` (if `allow_alpha=True`)
+        - `r, g, b`
+        - `r, g, b, a` (if `allow_alpha=True`)\n
+        #### Valid ranges:
+        - `r` 0-255 (int: red)
+        - `g` 0-255 (int: green)
+        - `b` 0-255 (int: blue)
+        - `a` 0.0-1.0 (float: opacity)"""
+        fix_sep = _re.escape(fix_sep) if isinstance(fix_sep, str) else r"[^0-9A-Z]"
+
+        rgb_part = rf"""((?:0*(?:25[0-5]|2[0-4][0-9]|1?[0-9]{{1,2}})))
+            (?:\s*{fix_sep}\s*)((?:0*(?:25[0-5]|2[0-4][0-9]|1?[0-9]{{1,2}})))
+            (?:\s*{fix_sep}\s*)((?:0*(?:25[0-5]|2[0-4][0-9]|1?[0-9]{{1,2}})))"""
+
+        if allow_alpha:
+            return cls._clean( \
+                rf"""(?ix)(?:rgb|rgba)?\s*(?:
+                    \(?\s*{rgb_part}
+                        (?:(?:\s*{fix_sep}\s*)((?:0*(?:0?\.[0-9]+|1\.0+|[0-9]+\.[0-9]+|[0-9]+))))?
+                    \s*\)?
+                )"""
+            )
+        else:
+            return cls._clean( \
+                rf"""(?ix)(?:rgb|rgba)?\s*(?:
+                    \(?\s*{rgb_part}\s*\)?
+                )"""
+            )
+
+    @classmethod
+    def hsla_str(cls, fix_sep: Optional[str] = ",", allow_alpha: bool = True) -> str:
+        """Matches a HSLA color inside a string.\n
+        ----------------------------------------------------------------------------------
+        - `fix_sep` -⠀the fixed separator between the HSLA values (e.g. `,`, `;` …)<br>
+          If set to nothing or `None`, any char that is not a letter or number
+          can be used to separate the HSLA values, including just a space.
+        - `allow_alpha` -⠀whether to include the alpha channel in the match\n
+        ----------------------------------------------------------------------------------
+        The HSLA color can be in the formats (for `fix_sep = ','`):
+        - `hsla(h, s, l)`
+        - `hsla(h, s, l, a)` (if `allow_alpha=True`)
+        - `(h, s, l)`
+        - `(h, s, l, a)` (if `allow_alpha=True`)
+        - `h, s, l`
+        - `h, s, l, a` (if `allow_alpha=True`)\n
+        #### Valid ranges:
+        - `h` 0-360 (int: hue)
+        - `s` 0-100 (int: saturation)
+        - `l` 0-100 (int: lightness)
+        - `a` 0.0-1.0 (float: opacity)"""
+        fix_sep = _re.escape(fix_sep) if isinstance(fix_sep, str) else r"[^0-9A-Z]"
+
+        hsl_part = rf"""((?:0*(?:360|3[0-5][0-9]|[12][0-9][0-9]|[1-9]?[0-9])))(?:\s*°)?
+            (?:\s*{fix_sep}\s*)((?:0*(?:100|[1-9][0-9]|[0-9])))(?:\s*%)?
+            (?:\s*{fix_sep}\s*)((?:0*(?:100|[1-9][0-9]|[0-9])))(?:\s*%)?"""
+
+        if allow_alpha:
+            return cls._clean( \
+                rf"""(?ix)(?:hsl|hsla)?\s*(?:
+                    \(?\s*{hsl_part}
+                        (?:(?:\s*{fix_sep}\s*)((?:0*(?:0?\.[0-9]+|1\.0+|[0-9]+\.[0-9]+|[0-9]+))))?
+                    \s*\)?
+                )"""
+            )
+        else:
+            return cls._clean( \
+                rf"""(?ix)(?:hsl|hsla)?\s*(?:
+                    \(?\s*{hsl_part}\s*\)?
+                )"""
+            )
+
+    @classmethod
+    def hexa_str(cls, allow_alpha: bool = True) -> str:
+        """Matches a HEXA color inside a string.\n
+        ----------------------------------------------------------------------
+        - `allow_alpha` -⠀whether to include the alpha channel in the match\n
+        ----------------------------------------------------------------------
+        The HEXA color can be in the formats (prefix `#`, `0x` or no prefix):
+        - `RGB`
+        - `RGBA` (if `allow_alpha=True`)
+        - `RRGGBB`
+        - `RRGGBBAA` (if `allow_alpha=True`)\n
+        #### Valid ranges:
+        every channel from 0-9 and A-F (case insensitive)"""
+        return r"(?i)(?:#|0x)?([0-9A-F]{8}|[0-9A-F]{6}|[0-9A-F]{4}|[0-9A-F]{3})" \
+            if allow_alpha else r"(?i)(?:#|0x)?([0-9A-F]{6}|[0-9A-F]{3})"
+
+    @classmethod
+    def _clean(cls, pattern: str) -> str:
+        """Internal method to make a multiline-string regex pattern into a single-line pattern."""
+        return "".join(line.strip() for line in pattern.splitlines()).strip()
+
+
+@mypyc_attr(native_class=False)
+class LazyRegex:
+    """A class that lazily compiles and caches regex patterns on first access.\n
+    --------------------------------------------------------------------------------
+    - `**patterns` -⠀keyword arguments where the key is the name of the pattern and
+      the value is the regex pattern string to compile\n
+    --------------------------------------------------------------------------------
+    #### Example usage:
+    ```python
+    PATTERNS = LazyRegex(
+        email=r"(?i)[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,}",
+        phone=r"\\+?\\d{1,3}[-.\\s]?\\(?\\d{1,4}\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}",
+    )
+
+    email_pattern = PATTERNS.email  # Compiles and caches the EMAIL pattern
+    phone_pattern = PATTERNS.phone  # Compiles and caches the PHONE pattern
+    ```"""
+
+    def __init__(self, **patterns: str):
+        self._patterns = patterns
+
+    def __getattr__(self, name: str) -> _rx.Pattern:
+        if name in self._patterns:
+            setattr(self, name, compiled := _rx.compile(self._patterns[name]))
+            return compiled
+
+        raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
