abd836f9795cf9135131b2dfc9df1a308ce1701c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:44 +0000

    Add package tsql-yaboiteedoh/0.3.3/tsql_yaboiteedoh-0.3.3-py3-none-any.whl

---
+++ packages/tsql-yaboiteedoh/tsql_yaboiteedoh-0.3.3-py3-none-any.whl/tsql/cli.py
@@ -0,0 +1,11 @@
+import os
+
+from .classes import App
+from .templater import Templater
+
+
+def main():
+    templater = Templater(os.getcwd())
+    app = App(os.getcwd(), templater)
+    app.enter()
+
