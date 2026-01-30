339c21395334a202bbf8fa1b7583785bc075d6c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:34:12 +0000

    Add package add2num/0.1.1/add2num-0.1.1-py3-none-any.whl

---
+++ packages/add2num/add2num-0.1.1-py3-none-any.whl/add2num/__init__.py
@@ -0,0 +1,21 @@
+# server.py
+from mcp.server.fastmcp import FastMCP
+# create a FastMCP instance
+mcp = FastMCP()
+
+@mcp.tool()
+def add2num(a: int, b: int) -> int:
+    """
+    Add two numbers together.
+    """
+    return a + b
+
+@mcp.resource("greeting://{name}")
+def greet(name:str) -> str:
+    """
+    Greet the user.
+    """
+    return f"Hello, {name}!"
+
+def main() -> None:
+    mcp.run(transport="stdio")
