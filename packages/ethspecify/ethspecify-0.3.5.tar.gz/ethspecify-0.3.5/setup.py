2afe61d4f92953869ef4ec22b4f5235ac8859147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:22:06 +0000

    Add package ethspecify/0.3.5/ethspecify-0.3.5.tar.gz

---
+++ packages/ethspecify/ethspecify-0.3.5.tar.gz/ethspecify-0.3.5/setup.py
@@ -0,0 +1,34 @@
+import pathlib
+
+from setuptools import setup, find_packages
+
+
+this_directory = pathlib.Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="ethspecify",
+    version="0.3.5",
+    description="A utility for processing Ethereum specification tags.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Justin Traglia",
+    author_email="jtraglia@pm.me",
+    url="https://github.com/jtraglia/ethspecify",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "ethspecify=ethspecify.cli:main",
+        ],
+    },
+    install_requires=[
+        "requests==2.32.3",
+        "PyYAML>=6.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
