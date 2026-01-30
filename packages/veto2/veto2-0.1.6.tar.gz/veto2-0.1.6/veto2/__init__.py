000a47a7c75f346cdac2e5065f112bee4cea9973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:36 +0000

    Add package veto2/0.1.6/veto2-0.1.6.tar.gz

---
+++ packages/veto2/veto2-0.1.6.tar.gz/veto2-0.1.6/veto2/__init__.py
@@ -0,0 +1,4 @@
+from .decorator import guard, init, ping
+from .exceptions import GovernanceError, PermissionDeniedError
+
+__all__ = ["guard", "init", "ping", "GovernanceError", "PermissionDeniedError"]
