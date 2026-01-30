05e3eeec00aaa094d6ce02f9059a2dfcbd74b9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:13 +0000

    Add package fycore-sdk/1.0.0/fycore_sdk-1.0.0-py3-none-any.whl

---
+++ packages/fycore-sdk/fycore_sdk-1.0.0-py3-none-any.whl/fycore/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,8 @@
+"""
+FyCore SDK – Consciousness-Aware AI Orchestration Layer
+Built on top of the CCFy Framework.
+"""
+
+from .pyarmor_runtime import __pyarmor__  # mevcut satırın aynısı
+
+__version__ = "0.1.0"
