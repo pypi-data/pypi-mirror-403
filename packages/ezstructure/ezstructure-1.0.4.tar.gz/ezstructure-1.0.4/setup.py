12e10d4bfaaa3378eaa685e82dffcb18810927de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:17 +0000

    Add package ezstructure/1.0.4/ezstructure-1.0.4.tar.gz

---
+++ packages/ezstructure/ezstructure-1.0.4.tar.gz/ezstructure-1.0.4/setup.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+import setuptools
+from setuptools.extension import Extension
+
+try:
+    # Build-time dependencies... specified in pyproject.toml but being careful anyway.
+    import Cython.Build
+    import numpy
+except ImportError:
+    print("Please install 'Cython' and 'numpy' before installing this package!")
+    raise
+
+
+with open("README.md", "rt", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+version = {}
+with open("ezstructure/__init__.py", "rt", encoding="UTF-8") as fh:
+    exec(fh.read(), version)
+
+numpy_include = numpy.get_include()
+
+
+setuptools.setup(
+    name="ezstructure",
+    version=version["__version__"],
+    description="EasyStructure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/Jerrythafast/ezstructure/issues",
+        "Source Code": "https://github.com/Jerrythafast/ezstructure",
+    },
+    author="Jerry Hoogenboom",
+    author_email="jerryhoogenboom@outlook.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Cython",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Legal Industry",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"],
+    keywords="bioinformatics forensics structure ancestry",
+    packages=setuptools.find_packages(),
+    ext_modules=[
+        Extension(
+            "ezstructure.vars.utils",
+            sources=["ezstructure/vars/utils.pyx"],
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+            include_dirs=[numpy_include]),
+        Extension(
+            "ezstructure.vars.admixprop",
+            sources=["ezstructure/vars/admixprop.pyx", "ezstructure/vars/C_admixprop.c"],
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+            include_dirs=[numpy_include]),
+        Extension(
+            "ezstructure.vars.allelefreq",
+            sources=["ezstructure/vars/allelefreq.pyx", "ezstructure/vars/C_allelefreq.c"],
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+            extra_compile_args=["-O3"],
+            include_dirs=[numpy_include]),
+        Extension(
+            "ezstructure.vars.marglikehood",
+            sources=["ezstructure/vars/marglikehood.pyx", "ezstructure/vars/C_marglikehood.c"],
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+            include_dirs=[numpy_include]),
+        Extension(
+            "ezstructure.fastStructure",
+            sources=["ezstructure/fastStructure.pyx"],
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+            include_dirs=[numpy_include, "ezstructure/vars/"])],
+    cmdclass={"build_ext": Cython.Build.build_ext},
+    package_data={
+        "ezstructure": ["vars/*.pxd", "vars/*.h"]
+    },
+    install_requires=["numpy>=2,<3", "scipy"],
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            "ezstructure=ezstructure.structure:main",
+            "ezdistruct=ezstructure.distruct:main",
+            "ezstructure_choosek=ezstructure.chooseK:main"
+        ]
+    },
+    zip_safe=False  # Needed for Cython, according to Cython docs.
+)
