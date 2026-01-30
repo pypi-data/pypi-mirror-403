e9fdfd41b989be7f8141e9e1bfa80bfa0227335f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:33:47 +0000

    Add package esiaccel/0.2.3.dev113/esiaccel-0.2.3.dev113-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/esiaccel/esiaccel-0.2.3.dev113-cp310-cp310-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/esiaccel/include/esi/backends/Cosim.h
@@ -0,0 +1,78 @@
+//===- Cosim.h - ESI C++ cosimulation backend -------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This is a specialization of the ESI C++ API (backend) for connection into a
+// simulation of an ESI system. Currently uses gRPC, but that could change.
+// Requires gRPC C++ library.
+//
+// DO NOT EDIT!
+// This file is distributed as part of an ESI package. The source for this file
+// should always be modified within CIRCT (lib/dialect/ESI/runtime/cpp).
+//
+//===----------------------------------------------------------------------===//
+
+// NOLINTNEXTLINE(llvm-header-guard)
+#ifndef ESI_BACKENDS_COSIM_H
+#define ESI_BACKENDS_COSIM_H
+
+#include "esi/Accelerator.h"
+
+#include <memory>
+#include <set>
+
+namespace esi {
+namespace backends {
+namespace cosim {
+
+class CosimEngine;
+class RpcClient;
+
+/// Connect to an ESI simulation.
+class CosimAccelerator : public esi::AcceleratorConnection {
+  friend class CosimEngine;
+
+public:
+  CosimAccelerator(Context &, std::string hostname, uint16_t port);
+  ~CosimAccelerator();
+
+  static std::unique_ptr<AcceleratorConnection>
+  connect(Context &, std::string connectionString);
+
+  // Different ways to retrieve the manifest in Cosimulation.
+  enum ManifestMethod {
+    Cosim, // Use the backdoor cosim interface. Default.
+    MMIO,  // Use MMIO emulation.
+  };
+  // Set the way this connection will retrieve the manifest.
+  void setManifestMethod(ManifestMethod method);
+
+  void createEngine(const std::string &engineTypeName, AppIDPath idPath,
+                    const ServiceImplDetails &details,
+                    const HWClientDetails &clients) override;
+
+protected:
+  virtual Service *createService(Service::Type service, AppIDPath path,
+                                 std::string implName,
+                                 const ServiceImplDetails &details,
+                                 const HWClientDetails &clients) override;
+
+private:
+  std::unique_ptr<RpcClient> rpcClient;
+
+  // We own all channels connected to rpcClient since their lifetime is tied to
+  // rpcClient.
+  std::set<std::unique_ptr<ChannelPort>> channels;
+
+  ManifestMethod manifestMethod = Cosim;
+};
+
+} // namespace cosim
+} // namespace backends
+} // namespace esi
+
+#endif // ESI_BACKENDS_COSIM_H
