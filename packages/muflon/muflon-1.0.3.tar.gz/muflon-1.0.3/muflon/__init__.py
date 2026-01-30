61dcb8001123202903113b8883fedef1dc5a6516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:33 +0000

    Add package muflon/1.0.3/muflon-1.0.3.tar.gz

---
+++ packages/muflon/muflon-1.0.3.tar.gz/muflon-1.0.3/muflon/__init__.py
@@ -0,0 +1,3 @@
+from .io import parse_data_to_matrices, save_results_to_csv
+from .operations import fuzzy_composition_multi, solve_fuzzy_vector
+from .norms import get_norm
---
+++ packages/muflon/muflon-1.0.3.tar.gz/muflon-1.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="muflon",
+    version="1.0.0",
+    description="Matrix Utility for Fuzzy Logic Operations and Norms",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="YJakub Kiryła",
+    author_email="kub-kir@wp.pl",
+    url="https://github.com/Kiryl24/muflon",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.20.0",
+        "pandas>=1.3.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
