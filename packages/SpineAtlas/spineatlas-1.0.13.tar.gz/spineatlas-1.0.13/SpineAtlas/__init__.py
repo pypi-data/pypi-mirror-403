73b305a2f28384b37c78b155734bde6af366045f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:38 +0000

    Add package SpineAtlas/1.0.13/spineatlas-1.0.13.tar.gz

---
+++ packages/SpineAtlas/spineatlas-1.0.13.tar.gz/spineatlas-1.0.13/SpineAtlas/__init__.py
@@ -0,0 +1,1 @@
+from .Spine_Atlas import Anchor, Atlas, AtlasFrame, AtlasTex, SpineAtlas, LineTextReader, rbin, sline, AtlasScale, CutFrame, AtlasImg, ImgPremultiplied, ImgNonPremultiplied, ReadAtlas, ReadAtlasFile, CheckAtlasTextures, getPngSize
---
+++ packages/SpineAtlas/spineatlas-1.0.13.tar.gz/spineatlas-1.0.13/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name="SpineAtlas",
+    version="1.0.13",
+    author='wood',
+    author_email='miraclerinwood@gmail.com',
+    packages=["SpineAtlas"],
+    url='https://github.com/Rin-Wood/SpineAtlas',
+    description="Python spine atlas parsing",
+    long_description=open('README.md', 'rb').read().decode('utf8'),
+	long_description_content_type='text/markdown',
+    license="MIT",
+    keywords="spine, Python, atlas, SpineAtlas",
+    install_requires=["attrs", "Pillow"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+    ],
+    python_requires=">=3.6",
+)
