000d33622d895094b78cae5b4f3f160671761189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:15:33 +0000

    Add package prefpicker/2.18.0/prefpicker-2.18.0-py3-none-any.whl

---
+++ packages/prefpicker/prefpicker-2.18.0-py3-none-any.whl/prefpicker/prefpicker.py
@@ -0,0 +1,265 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""prefpicker module"""
+
+from __future__ import annotations
+
+from datetime import datetime, timezone
+from importlib.metadata import PackageNotFoundError, version
+from json import dumps
+from pathlib import Path
+from random import choice
+from typing import TYPE_CHECKING, Any
+
+from yaml import safe_load
+from yaml.parser import ParserError
+from yaml.scanner import ScannerError
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+__author__ = "Tyson Smith"
+__credits__ = ["Tyson Smith"]
+try:
+    __version__ = version("prefpicker")
+except PackageNotFoundError:  # pragma: no cover
+    # package is not installed
+    __version__ = "unknown"
+
+
+class SourceDataError(Exception):
+    """This is raised when issues are found in the source data."""
+
+
+PrefValue = bool | int | str | None
+PrefVariant = dict[str, list[PrefValue]]
+
+
+class PrefPicker:  # pylint: disable=missing-docstring
+    __slots__ = ("prefs", "variants")
+
+    def __init__(self) -> None:
+        self.prefs: dict[str, dict[str, PrefVariant]] = {}
+        self.variants: set[str] = {"default"}
+
+    def check_combinations(self) -> Generator[tuple[str, int]]:
+        """Count the number of combinations for each variation. Only return
+           variants that have more than one combination.
+
+        Args:
+            None
+
+        Yields:
+            Variant and number of potential combinations.
+        """
+        combos = dict.fromkeys(self.variants, 1)
+        for variants in (x["variants"] for x in self.prefs.values()):
+            for variant in combos:
+                # use 'default' if pref does not have a matching variant entry
+                if variant not in variants:
+                    combos[variant] *= len(variants["default"])
+                else:
+                    combos[variant] *= len(variants[variant])
+        for variant, count in sorted(combos.items()):
+            if count > 1:
+                yield (variant, count)
+
+    def check_duplicates(self) -> Generator[tuple[str, str]]:
+        """Look for variants with values that appear more than once per variant.
+
+        Args:
+            None
+
+        Yields:
+            Pref name and the variant.
+        """
+        for pref, keys in sorted(self.prefs.items()):
+            variants = keys["variants"]
+            for variant in variants:
+                if len(variants[variant]) != len(set(variants[variant])):
+                    yield (pref, variant)
+
+    def check_overwrites(self) -> Generator[tuple[str, str, PrefValue]]:
+        """Look for variants that overwrite the default with the same value.
+
+        Args:
+            None
+
+        Yields:
+            Pref, variant and value.
+        """
+        for pref, keys in sorted(self.prefs.items()):
+            variants = keys["variants"]
+            for variant in variants:
+                if variant == "default":
+                    continue
+                for value in variants[variant]:
+                    if value in variants["default"]:
+                        yield (pref, variant, value)
+
+    def create_prefsjs(self, dest: Path, variant: str = "default") -> None:
+        """Write a `prefs.js` file based on the specified variant. The output file
+           will also include comments containing the variant and a timestamp.
+
+        Args:
+            dest: Path of file to create.
+            variant: Used to pick the values to output.
+
+        Returns:
+            None
+        """
+        with dest.open("w") as prefs_fp:
+            prefs_fp.write(f"// Generated with PrefPicker ({__version__}) @ ")
+            prefs_fp.write(datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S %Z"))
+            prefs_fp.write(f"\n// Variant {variant!r}\n")
+            for pref, keys in sorted(self.prefs.items()):
+                variants = keys["variants"]
+                # choose values
+                if variant not in variants or variant == "default":
+                    options = variants["default"]
+                    default_variant = True
+                else:
+                    options = variants[variant]
+                    default_variant = False
+                value = choice(options)
+                if value is None:
+                    if len(options) > 1:
+                        prefs_fp.write(
+                            f"// '{pref}' skipped, options {dumps(options)}\n"
+                        )
+                    # skipping pref
+                    continue
+                if len(options) > 1:
+                    prefs_fp.write(f"// '{pref}' options {dumps(options)}\n")
+                # sanitize value for writing
+                if isinstance(value, bool):
+                    sanitized = "true" if value else "false"
+                elif isinstance(value, int):
+                    sanitized = str(value)
+                elif isinstance(value, str):
+                    sanitized = repr(value)
+                else:
+                    prefs_fp.write(f"// Failed to sanitized {value!r} ({pref})\n")
+                    raise SourceDataError(
+                        f"Unsupported datatype {type(value).__name__!r}"
+                    )
+                # write to prefs.js file
+                if not default_variant:
+                    prefs_fp.write(f"// {pref!r} defined by variant {variant!r}\n")
+                prefs_fp.write(f'user_pref("{pref}", {sanitized});\n')
+
+    @classmethod
+    def lookup_template(cls, name: str) -> Path | None:
+        """Lookup built-in template Path.
+
+        Args:
+            name: Name of template.
+
+        Returns:
+            Template that matches 'name' or None.
+        """
+        path = Path(__file__).parent / "templates" / name
+        if path.is_file():
+            return path
+        return None
+
+    @classmethod
+    def load_template(cls, input_yml: Path) -> PrefPicker:
+        """Load data from a template YAML file.
+
+        Args:
+            input_yml: Input file.
+
+        Returns:
+            PrefPicker object.
+        """
+        try:
+            raw_prefs = safe_load(input_yml.read_bytes())
+        except (ScannerError, ParserError):
+            raise SourceDataError("invalid YAML") from None
+        cls.verify_data(raw_prefs)
+        picker = cls()
+        picker.variants = set(raw_prefs["variant"] + ["default"])
+        # only add relevant parts
+        for pref, parts in raw_prefs["pref"].items():
+            picker.prefs[pref] = {"variants": parts["variants"]}
+        return picker
+
+    @staticmethod
+    def templates() -> Generator[Path]:
+        """Available YAML template files.
+
+        Args:
+            None
+
+        Yields:
+            Template files.
+        """
+        path = Path(__file__).parent.resolve() / "templates"
+        if path.is_dir():
+            for template in path.iterdir():
+                if template.suffix.lower().endswith(".yml"):
+                    yield template
+
+    @staticmethod
+    def verify_data(raw_data: Any) -> None:
+        """Perform strict sanity checks on raw_data. This exists to help prevent
+           the template file from breaking or becoming unmaintainable.
+
+        Args:
+            raw_data: Data to verify.
+
+        Returns:
+            None
+        """
+        if not isinstance(raw_data, dict):
+            raise SourceDataError("invalid template")
+        # check variant list
+        if "variant" not in raw_data:
+            raise SourceDataError("variant list is missing")
+        if not isinstance(raw_data["variant"], list):
+            raise SourceDataError("variant is not a list")
+        # check variant list entries
+        valid_variants = {"default"}
+        for variant in raw_data["variant"]:
+            if not isinstance(variant, str):
+                raise SourceDataError("variant definition must be a string")
+            valid_variants.add(variant)
+        # check prefs dict
+        if "pref" not in raw_data:
+            raise SourceDataError("pref group is missing")
+        if not isinstance(raw_data["pref"], dict):
+            raise SourceDataError("pref is not a dict")
+        # check entries in prefs dict
+        used_variants = set()
+        for pref, keys in raw_data["pref"].items():
+            if not isinstance(keys, dict):
+                raise SourceDataError(f"{pref!r} entry must contain a dict")
+            variants = keys.get("variants")
+            if not isinstance(variants, dict):
+                raise SourceDataError(f"{pref!r} is missing 'variants' dict")
+            if "default" not in variants:
+                raise SourceDataError(f"{pref!r} is missing 'default' variant")
+            # verify variants
+            for variant in variants:
+                if variant not in valid_variants:
+                    raise SourceDataError(
+                        f"{variant!r} in {pref!r} is not a defined variant"
+                    )
+                if not isinstance(variants[variant], list):
+                    raise SourceDataError(
+                        f"variant {variant!r} in {pref!r} is not a list"
+                    )
+                if not variants[variant]:
+                    raise SourceDataError(f"{variant!r} in {pref!r} is empty")
+                for value in variants[variant]:
+                    if value is not None and not isinstance(value, bool | int | str):
+                        raise SourceDataError(
+                            f"unsupported datatype {type(value).__name__!r} ({pref})"
+                        )
+                used_variants.add(variant)
+        if valid_variants - used_variants:
+            raise SourceDataError(
+                f"Unused variants {' '.join(valid_variants - used_variants)!r}"
+            )
---
+++ packages/prefpicker/prefpicker-2.18.0-py3-none-any.whl/prefpicker/test_prefpicker.py
@@ -0,0 +1,281 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+"""prefpicker.py tests"""
+
+from pytest import mark, raises
+
+from .prefpicker import PrefPicker, SourceDataError
+
+
+def test_prefpicker_01(tmp_path):
+    """test simple PrefPicker"""
+    yml = tmp_path / "test.yml"
+    yml.write_text(
+        """
+        variant: []
+        pref:
+          test.a:
+            variants:
+              default: [1]"""
+    )
+    picker = PrefPicker.load_template(yml)
+    assert len(picker.variants) == 1
+    assert "default" in picker.variants
+    assert len(picker.prefs) == 1
+    assert "test.a" in picker.prefs
+
+
+@mark.parametrize(
+    "data, msg",
+    [
+        # invalid template
+        ([], "invalid template"),
+        # variant list missing
+        ({"pref": {"a.b": {"variants": {"default": [1]}}}}, "variant list is missing"),
+        # variant definition is invalid type
+        ({"variant": [{"bad": 1}]}, "variant definition must be a string"),
+        # variant is invalid type
+        ({"variant": ""}, "variant is not a list"),
+        # pref dict missing
+        ({"variant": []}, "pref group is missing"),
+        # pref is invalid type
+        ({"pref": [], "variant": []}, "pref is not a dict"),
+        # pref entry is invalid
+        (
+            {"pref": {"a.b": None}, "variant": []},
+            r"'a\.b' entry must contain a dict",
+        ),
+        # pref variants is invalid
+        (
+            {"pref": {"a.b": {"variants": None}}, "variant": []},
+            r"'a\.b' is missing 'variants' dict",
+        ),
+        # pref missing default variant
+        (
+            {"variant": [], "pref": {"test.a": {"variants": {}}}},
+            r"'test\.a' is missing 'default' variant",
+        ),
+        # template with undefined variant
+        (
+            {"variant": [], "pref": {"a.b": {"variants": {"default": [1], "x": [2]}}}},
+            r"'x' in 'a\.b' is not a defined variant",
+        ),
+        # template with unused variant
+        (
+            {"variant": ["unused"], "pref": {"a.b": {"variants": {"default": [1]}}}},
+            "Unused variants 'unused'",
+        ),
+        # pref with empty variant list
+        (
+            {"variant": [], "pref": {"a.b": {"variants": {"default": []}}}},
+            r"'default' in 'a\.b' is empty",
+        ),
+        # pref with invalid variant type
+        (
+            {"variant": [], "pref": {"a.b": {"variants": {"default": "x"}}}},
+            r"variant 'default' in 'a\.b' is not a list",
+        ),
+        # pref variant with invalid type
+        (
+            {"variant": [], "pref": {"a.b": {"variants": {"default": [1.11]}}}},
+            r"unsupported datatype 'float' \(a\.b\)",
+        ),
+    ],
+)
+def test_prefpicker_02(data, msg):
+    """test PrefPicker.verify_data()"""
+    with raises(SourceDataError, match=msg):
+        PrefPicker.verify_data(data)
+
+
+def test_prefpicker_03():
+    """test PrefPicker.check_overwrites()"""
+    raw_data = {
+        "variant": ["fail", "safe"],
+        "pref": {
+            "test.a": {"variants": {"default": [1], "fail": [1, 2], "safe": [3]}},
+            "test.b": {"variants": {"default": [9], "safe": [None]}},
+        },
+    }
+    # use verify_data just to sanity check test data
+    PrefPicker.verify_data(raw_data)
+    ppick = PrefPicker()
+    ppick.variants = set(raw_data["variant"] + ["default"])
+    ppick.prefs = raw_data["pref"]
+    results = tuple(ppick.check_overwrites())
+    assert results
+    assert results[0][0] == "test.a"
+    assert results[0][1] == "fail"
+    assert results[0][2] == 1
+
+
+def test_prefpicker_04():
+    """test PrefPicker.check_duplicates()"""
+    raw_data = {
+        "variant": ["fail", "safe"],
+        "pref": {
+            "test.a": {"variants": {"default": [1], "fail": [1, 2, 3, 1], "safe": [3]}},
+            "test.b": {"variants": {"default": [9], "safe": [None]}},
+        },
+    }
+    # use verify_data just to sanity check test data
+    PrefPicker.verify_data(raw_data)
+    ppick = PrefPicker()
+    ppick.variants = set(raw_data["variant"] + ["default"])
+    ppick.prefs = raw_data["pref"]
+    results = tuple(ppick.check_duplicates())
+    assert results
+    assert results[0][0] == "test.a"
+    assert results[0][1] == "fail"
+
+
+def test_prefpicker_05():
+    """test PrefPicker.check_combinations()"""
+    raw_data = {
+        "variant": ["v1", "v2"],
+        "pref": {
+            "test.a": {"variants": {"default": [1], "v1": [1, 2, 3, 4], "v2": [3]}},
+            "test.b": {"variants": {"default": [2], "v1": [1, 2], "v2": [3]}},
+            "test.c": {"variants": {"default": [1, 3], "v2": [None]}},
+        },
+    }
+    # use verify_data just to sanity check test data
+    PrefPicker.verify_data(raw_data)
+    ppick = PrefPicker()
+    ppick.variants = set(raw_data["variant"] + ["default"])
+    ppick.prefs = raw_data["pref"]
+    results = tuple(ppick.check_combinations())
+    assert len(results) == 2
+    assert results[0][0] == "default"
+    assert results[0][1] == 2
+    assert results[1][0] == "v1"
+    assert results[1][1] == 16
+
+
+def test_prefpicker_06(tmp_path):
+    """test simple PrefPicker.create_prefsjs()"""
+    ppick = PrefPicker()
+    prefs = tmp_path / "prefs.js"
+    ppick.create_prefsjs(prefs)
+    assert prefs.is_file()
+    # check only comments were written to the document
+    with prefs.open("r") as in_fp:
+        for line in in_fp:
+            assert line.startswith("//")
+        assert in_fp.tell() > 0
+
+
+def test_prefpicker_07(tmp_path):
+    """test PrefPicker.create_prefsjs() with variants"""
+    raw_data = {
+        "variant": ["test", "skip"],
+        "pref": {
+            "test.a": {"variants": {"default": [0], "test": [1], "skip": [2]}},
+            "test.b": {"variants": {"default": [True]}},
+        },
+    }
+    PrefPicker.verify_data(raw_data)
+    ppick = PrefPicker()
+    ppick.variants = set(raw_data["variant"] + ["default"])
+    ppick.prefs = raw_data["pref"]
+    prefs = tmp_path / "prefs.js"
+    # test with 'default' variant
+    ppick.create_prefsjs(prefs)
+    assert prefs.is_file()
+    prefs_data = prefs.read_text()
+    assert "defined by variant 'default'" not in prefs_data
+    assert 'user_pref("test.a", 0);\n' in prefs_data
+    assert 'user_pref("test.b", true);\n' in prefs_data
+    # test with 'test' variant
+    ppick.create_prefsjs(prefs, variant="test")
+    assert prefs.is_file()
+    prefs_data = prefs.read_text()
+    assert 'user_pref("test.a", 1);\n' in prefs_data
+    assert 'user_pref("test.b", true);\n' in prefs_data
+    assert "// 'test.a' defined by variant 'test'" in prefs_data
+
+
+def test_prefpicker_08(tmp_path):
+    """test PrefPicker.create_prefsjs() with different values"""
+    raw_data = {
+        "variant": [],
+        "pref": {
+            # type int
+            "test.a": {
+                "variants": {
+                    "default": [0, 1],
+                }
+            },
+            # type None (skip)
+            "test.b": {
+                "variants": {
+                    "default": [None],
+                }
+            },
+            # type string
+            "test.c": {
+                "variants": {
+                    "default": ["test string"],
+                }
+            },
+            # type string (with quotes)
+            "test.d": {
+                "variants": {
+                    "default": ["'test' \"string\""],
+                }
+            },
+            # type bool
+            "test.e": {
+                "variants": {
+                    "default": [True, False],
+                }
+            },
+            # add None twice to trigger the 'skip' code path and write out comment
+            "test.f": {
+                "variants": {
+                    "default": [None, None],
+                }
+            },
+            # mixed types
+            "test.g": {
+                "variants": {
+                    "default": ["foo", True, 0],
+                }
+            },
+        },
+    }
+    PrefPicker.verify_data(raw_data)
+    ppick = PrefPicker()
+    ppick.variants = set(raw_data["variant"] + ["default"])
+    ppick.prefs = raw_data["pref"]
+    prefs = tmp_path / "prefs.js"
+    ppick.create_prefsjs(prefs)
+    assert prefs.is_file()
+    prefs_data = prefs.read_text()
+    assert 'user_pref("test.b",' not in prefs_data
+    assert "user_pref(\"test.c\", 'test string');" in prefs_data
+    # test with unsupported value datatype
+    raw_data = {"variant": [], "pref": {"boom.": {"variants": {"default": [1.01]}}}}
+    ppick.variants = set(raw_data["variant"] + ["default"])
+    ppick.prefs = raw_data["pref"]
+    with raises(SourceDataError, match="Unsupported datatype"):
+        ppick.create_prefsjs(prefs)
+
+
+def test_prefpicker_09(tmp_path):
+    """test PrefPicker.load_template() with invalid YAML"""
+    yml = tmp_path / "test.yml"
+    yml.write_text("{-{-{-{-:::")
+    with raises(SourceDataError, match=r"invalid YAML"):
+        PrefPicker.load_template(yml)
+
+
+def test_prefpicker_10():
+    """test PrefPicker.lookup_template()"""
+    # unknown template
+    assert PrefPicker.lookup_template("missing") is None
+    # existing template
+    template = next(PrefPicker.templates(), None)
+    assert template is not None
+    assert PrefPicker.lookup_template(template.name)
