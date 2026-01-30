08d20823a64322e85fd34d7fa27c2e110d4aac41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:15:32 +0000

    Add package datahugger-ng/0.1.1/datahugger_ng-0.1.1-cp310-abi3-musllinux_1_2_i686.whl

---
+++ packages/datahugger-ng/datahugger_ng-0.1.1-cp310-abi3-musllinux_1_2_i686.whl/datahugger/datahugger.pyi
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+import pathlib
+
+from typing import Protocol, TypeVar, AsyncIterator, Iterator
+
+T = TypeVar("T", covariant=True)
+
+class SyncAsyncIterator(Protocol[T]):
+    def __aiter__(self) -> AsyncIterator[T]: ...
+    async def __anext__(self) -> T: ...
+    def __iter__(self) -> Iterator[T]: ...
+    def __next__(self) -> T: ...
+
+class Entry(object):
+    """base entry for file and dir"""
+
+@dataclass
+class DirEntry(Entry):
+    path_craw_rel: pathlib.Path
+    root_url: str
+    api_url: str
+
+@dataclass
+class FileEntry(Entry):
+    path_craw_rel: pathlib.Path
+    download_url: str
+    size: int | None
+    checksum: list[tuple[str, str]]
+
+class Dataset(object):
+    def download_with_validation(self, dst_dir: pathlib.Path, limit: int = 0) -> None:
+        """blocking call, using rust's async runtime"""
+    def crawl(self) -> SyncAsyncIterator[Entry]:
+        """return a stream that can be either sync or async iterator over `Entry`"""
+    def root_url(self) -> str: ...
+
+def resolve(url: str, /) -> Dataset: ...
