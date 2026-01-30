1dd0197ec055acb3ba7f3b7d37ef84891d3d8ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:07:55 +0000

    Add package valanga/0.1.9/valanga-0.1.9.tar.gz

---
+++ packages/valanga/valanga-0.1.9.tar.gz/valanga-0.1.9/src/valanga/evaluations.py
@@ -0,0 +1,59 @@
+"""
+Evaluation-related classes and types.
+"""
+
+from dataclasses import dataclass
+from typing import Protocol
+
+from valanga.evaluator_types import EvaluatorInput
+
+from .game import BranchKey, State
+from .over_event import OverEvent
+from .represention_for_evaluation import ContentRepresentation
+
+
+class EvalItem[StateT: State](Protocol):
+    """
+    The protocol for an evaluation item.
+    An evaluation item is something that has a state and optionally a representation of that state.
+    """
+
+    @property
+    def state(self) -> StateT:
+        """The state associated with this evaluation item."""
+        ...
+
+    @property
+    def state_representation(
+        self,
+    ) -> ContentRepresentation[StateT, EvaluatorInput] | None:
+        """The representation of the state associated with this evaluation item, if available."""
+        ...
+
+
+@dataclass
+class ForcedOutcome:
+    """
+    The class
+    """
+
+    # The forced outcome with optimal play by both sides.
+    outcome: OverEvent
+
+    # the line
+    line: list[BranchKey]
+
+
+@dataclass
+class FloatyStateEvaluation:
+    """
+    The class to defines what is an evaluation of a board.
+    By convention is it always evaluated from the view point of the white side.
+    """
+
+    # The evaluation value for the white side when the outcome is not certain. Typically, a float.
+    # todo can we remove the None option?
+    value_white: float | None
+
+
+StateEvaluation = FloatyStateEvaluation | ForcedOutcome
