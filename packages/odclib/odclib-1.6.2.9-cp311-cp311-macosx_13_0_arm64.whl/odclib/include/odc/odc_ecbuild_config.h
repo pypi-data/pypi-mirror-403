866392f635132552a763b1724c4039e5f62f46b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:22:34 +0000

    Add package odclib/1.6.2.9/odclib-1.6.2.9-cp311-cp311-macosx_13_0_arm64.whl

---
+++ packages/odclib/odclib-1.6.2.9-cp311-cp311-macosx_13_0_arm64.whl/odclib/include/odc/odc_ecbuild_config.h
@@ -0,0 +1,72 @@
+/*
+ * (C) Copyright 2011- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+#ifndef ODC_ecbuild_config_h
+#define ODC_ecbuild_config_h
+
+/* ecbuild info */
+
+#ifndef ECBUILD_VERSION_STR
+#define ECBUILD_VERSION_STR "3.13.0"
+#endif
+#ifndef ECBUILD_VERSION
+#define ECBUILD_VERSION "3.13.0"
+#endif
+#ifndef ECBUILD_MACROS_DIR
+#define ECBUILD_MACROS_DIR  "/opt/actions-runner/work/_work/python-develop-bundle/python-develop-bundle/ecbuild/cmake"
+#endif
+
+/* config info */
+
+#define ODC_OS_NAME          "Darwin-22.5.0"
+#define ODC_OS_BITS          64
+#define ODC_OS_BITS_STR      "64"
+#define ODC_OS_STR           "macosx.64"
+#define ODC_OS_VERSION       "22.5.0"
+#define ODC_SYS_PROCESSOR    "arm64"
+
+#define ODC_BUILD_TIMESTAMP  "20260126165213"
+#define ODC_BUILD_TYPE       "RelWithDebInfo"
+
+#define ODC_C_COMPILER_ID      "AppleClang"
+#define ODC_C_COMPILER_VERSION "14.0.3.14030022"
+
+#define ODC_CXX_COMPILER_ID      "AppleClang"
+#define ODC_CXX_COMPILER_VERSION "14.0.3.14030022"
+
+#define ODC_C_COMPILER       "/Library/Developer/CommandLineTools/usr/bin/cc"
+#define ODC_C_FLAGS          " -pipe -O2 -g -DNDEBUG"
+
+#define ODC_CXX_COMPILER     "/Library/Developer/CommandLineTools/usr/bin/c++"
+#define ODC_CXX_FLAGS        " -pipe -Wall -Wextra -Wno-unused-parameter -Wno-unused-variable -Wno-sign-compare -O2 -g -DNDEBUG"
+
+/* Needed for finding per package config files */
+
+#define ODC_INSTALL_DIR       "/tmp/odc/target/odc"
+#define ODC_INSTALL_BIN_DIR   "/tmp/odc/target/odc/bin"
+#define ODC_INSTALL_LIB_DIR   "/tmp/odc/target/odc/lib"
+#define ODC_INSTALL_DATA_DIR  "/tmp/odc/target/odc/share/odc"
+
+#define ODC_DEVELOPER_SRC_DIR "/opt/actions-runner/work/_work/python-develop-bundle/python-develop-bundle/src/odc"
+#define ODC_DEVELOPER_BIN_DIR "/tmp/odc/build"
+
+/* Fortran support */
+
+#if 0
+
+#define ODC_Fortran_COMPILER_ID      ""
+#define ODC_Fortran_COMPILER_VERSION ""
+
+#define ODC_Fortran_COMPILER ""
+#define ODC_Fortran_FLAGS    ""
+
+#endif
+
+#endif /* ODC_ecbuild_config_h */
