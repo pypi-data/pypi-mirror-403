881715376fd7dc3b1f76ec20925a850698f5c328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:24 +0000

    Add package nepalpy/1.2.0/nepalpy-1.2.0-py3-none-any.whl

---
+++ packages/nepalpy/nepalpy-1.2.0-py3-none-any.whl/nepalpy/__init__.py
@@ -0,0 +1,13 @@
+# nepalpy/__init__.py
+
+from .name.name_generator import (
+    name,
+    name_start_with,
+    name_end_with,
+    last_name,
+    full_name,
+    cast,
+    cast_category,
+)
+
+__version__ = "1.2.0"
