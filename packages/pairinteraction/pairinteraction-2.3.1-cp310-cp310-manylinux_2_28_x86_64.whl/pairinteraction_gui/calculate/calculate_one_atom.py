bbfcfa1a037873f875969d5c338a4b713b0fedf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:35 +0000

    Add package pairinteraction/2.3.1/pairinteraction-2.3.1-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/pairinteraction/pairinteraction-2.3.1-cp310-cp310-manylinux_2_28_x86_64.whl/pairinteraction_gui/calculate/calculate_one_atom.py
@@ -0,0 +1,62 @@
+# SPDX-FileCopyrightText: 2025 PairInteraction Developers
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+import logging
+from typing import TYPE_CHECKING
+
+from attr import dataclass
+
+import pairinteraction as pi_complex
+import pairinteraction.real as pi_real
+from pairinteraction_gui.calculate.calculate_base import Parameters, Results
+from pairinteraction_gui.worker import run_in_other_process
+
+if TYPE_CHECKING:
+    from pairinteraction_gui.page import OneAtomPage  # noqa: F401  # related to ruff extend-generics
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ParametersOneAtom(Parameters["OneAtomPage"]):
+    """Parameters for the single-atom calculation."""
+
+
+@dataclass
+class ResultsOneAtom(Results):
+    """Results for the single-atom calculation."""
+
+
+@run_in_other_process
+def calculate_one_atom(parameters: ParametersOneAtom) -> ResultsOneAtom:
+    """Calculate the energy plot for one atom.
+
+    This means, given a Parameters object, do the PairInteraction calculations and return an ResultsOneAtom object.
+    """
+    return _calculate_one_atom(parameters)
+
+
+def _calculate_one_atom(parameters: ParametersOneAtom) -> ResultsOneAtom:
+    """Make the unwrapped function available for testing."""
+    pi = pi_real if parameters.is_real else pi_complex
+
+    ket = pi.KetAtom(parameters.get_species(), **parameters.get_quantum_numbers())
+    ket_energy = ket.get_energy("GHz")
+    basis = pi.BasisAtom(parameters.get_species(), **parameters.get_quantum_number_restrictions())
+
+    system_list = [
+        pi.SystemAtom(basis)
+        .set_electric_field(parameters.get_efield(step), unit="V/cm")
+        .set_magnetic_field(parameters.get_bfield(step), unit="G")
+        .set_diamagnetism_enabled(parameters.diamagnetism_enabled)
+        for step in range(parameters.steps)
+    ]
+
+    logger.debug("Diagonalizing SystemAtoms...")
+    pi.diagonalize(
+        system_list,
+        **parameters.diagonalize_kwargs,
+        **parameters.get_diagonalize_energy_range_kwargs(ket_energy),
+    )
+    logger.debug("Done diagonalizing SystemAtoms.")
+    return ResultsOneAtom.from_calculate(parameters, system_list, ket, ket_energy)
---
+++ packages/pairinteraction/pairinteraction-2.3.1-cp310-cp310-manylinux_2_28_x86_64.whl/pairinteraction_gui/page/c6_page.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2025 PairInteraction Developers
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+from pairinteraction_gui.page.base_page import SimulationPage
+
+
+class C6Page(SimulationPage):
+    """Page for calculating C6 coefficient."""
+
+    title = "C6"
+    tooltip = "C6"
