7030d8c60cbf0ac16ee53f762e4505876406c6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:36 +0000

    Add package mlp-sdk/0.1.0/mlp_sdk-0.1.0.tar.gz

---
+++ packages/mlp-sdk/mlp_sdk-0.1.0.tar.gz/mlp_sdk-0.1.0/src/mlp_sdk/main.py
@@ -0,0 +1,19 @@
+"""Main entry point for mlp_sdk
+Created by Poneglyph for security research
+"""
+import sys
+
+def main():
+    """Main entry point for the CLI"""
+    print("=" * 60)
+    print("mlp_sdk")
+    print("Created by Poneglyph for security research")
+    print("=" * 60)
+    print()
+    print("This is a security research package.")
+    print("For more information, visit: https://github.com/poneglyph-research")
+    print()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
