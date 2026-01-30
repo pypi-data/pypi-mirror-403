f103d916c1a00304a020f1ad4e8ba8d5e7e5dad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:07:44 +0000

    Add package sportsdataverse/0.0.36.3.3/sportsdataverse-0.0.36.3.3-py3-none-any.whl

---
+++ packages/sportsdataverse/sportsdataverse-0.0.36.3.3-py3-none-any.whl/sportsdataverse/nhl/nhl_loaders.py
@@ -0,0 +1,153 @@
+import pandas as pd
+import json
+from tqdm import tqdm
+from typing import List, Callable, Iterator, Union, Optional
+from sportsdataverse.config import (
+    NHL_BASE_URL,
+    NHL_TEAM_BOX_URL,
+    NHL_TEAM_SCHEDULE_URL,
+    NHL_TEAM_LOGO_URL,
+    NHL_PLAYER_BOX_URL,
+)
+from sportsdataverse.errors import SeasonNotFoundError
+from sportsdataverse.dl_utils import download
+
+
+def load_nhl_pbp(seasons: List[int]) -> pd.DataFrame:
+    """Load NHL play by play data going back to 2011
+
+    Example:
+        `nhl_df = sportsdataverse.nhl.load_nhl_pbp(seasons=range(2011,2021))`
+
+    Args:
+        seasons (list): Used to define different seasons. 2011 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing the play-by-plays available for the requested seasons.
+
+    Raises:
+        ValueError: If `season` is less than 2011.
+    """
+    data = pd.DataFrame()
+    if type(seasons) is int:
+        seasons = [seasons]
+    for i in tqdm(seasons):
+        if int(i) < 2011:
+            raise SeasonNotFoundError("season cannot be less than 2011")
+        i_data = pd.read_parquet(
+            NHL_BASE_URL.format(season=i), engine="auto", columns=None
+        )
+        data = pd.concat([data, i_data], axis=0, ignore_index=True)
+    # Give each row a unique index
+    data.reset_index(drop=True, inplace=True)
+    return data
+
+
+def load_nhl_schedule(seasons: List[int]) -> pd.DataFrame:
+    """Load NHL schedule data
+
+    Example:
+        `nhl_df = sportsdataverse.nhl.load_nhl_schedule(seasons=range(2002,2021))`
+
+    Args:
+        seasons (list): Used to define different seasons. 2002 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing the schedule for the requested seasons.
+
+    Raises:
+        ValueError: If `season` is less than 2002.
+    """
+    data = pd.DataFrame()
+    if type(seasons) is int:
+        seasons = [seasons]
+    for i in tqdm(seasons):
+        if int(i) < 2002:
+            raise SeasonNotFoundError("season cannot be less than 2002")
+        i_data = pd.read_parquet(
+            NHL_TEAM_SCHEDULE_URL.format(season=i), engine="auto", columns=None
+        )
+        data = pd.concat([data, i_data], axis=0, ignore_index=True)
+    # Give each row a unique index
+    data.reset_index(drop=True, inplace=True)
+
+    return data
+
+
+def load_nhl_team_boxscore(seasons: List[int]) -> pd.DataFrame:
+    """Load NHL team boxscore data
+
+    Example:
+        `nhl_df = sportsdataverse.nhl.load_nhl_team_boxscore(seasons=range(2011,2022))`
+
+    Args:
+        seasons (list): Used to define different seasons. 2011 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing the
+        team boxscores available for the requested seasons.
+
+    Raises:
+        ValueError: If `season` is less than 2011.
+    """
+    data = pd.DataFrame()
+    if type(seasons) is int:
+        seasons = [seasons]
+    for i in tqdm(seasons):
+        if int(i) < 2011:
+            raise SeasonNotFoundError("season cannot be less than 2011")
+        i_data = pd.read_parquet(
+            NHL_TEAM_BOX_URL.format(season=i), engine="auto", columns=None
+        )
+        data = pd.concat([data, i_data], axis=0, ignore_index=True)
+    # Give each row a unique index
+    data.reset_index(drop=True, inplace=True)
+
+    return data
+
+
+def load_nhl_player_boxscore(seasons: List[int]) -> pd.DataFrame:
+    """Load NHL player boxscore data
+
+    Example:
+        `nhl_df = sportsdataverse.nhl.load_nhl_player_boxscore(seasons=range(2011,2022))`
+
+    Args:
+        seasons (list): Used to define different seasons. 2011 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing the
+        player boxscores available for the requested seasons.
+
+    Raises:
+        ValueError: If `season` is less than 2011.
+    """
+    data = pd.DataFrame()
+    if type(seasons) is int:
+        seasons = [seasons]
+    for i in tqdm(seasons):
+        if int(i) < 2011:
+            raise SeasonNotFoundError("season cannot be less than 2011")
+        i_data = pd.read_parquet(
+            NHL_PLAYER_BOX_URL.format(season=i), engine="auto", columns=None
+        )
+        data = pd.concat([data, i_data], axis=0, ignore_index=True)
+    # Give each row a unique index
+    data.reset_index(drop=True, inplace=True)
+
+    return data
+
+
+def nhl_teams() -> pd.DataFrame:
+    """Load NHL team ID information and logos
+
+    Example:
+        `nhl_df = sportsdataverse.nhl.nhl_teams()`
+
+    Args:
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing teams available for the requested seasons.
+    """
+    df = pd.read_csv(NHL_TEAM_LOGO_URL, low_memory=False)
+    return df
---
+++ packages/sportsdataverse/sportsdataverse-0.0.36.3.3-py3-none-any.whl/sportsdataverse/nhl/nhl_schedule.py
@@ -0,0 +1,132 @@
+import pandas as pd
+import json
+import datetime
+from sportsdataverse.dl_utils import download, underscore
+
+
+def espn_nhl_schedule(dates=None, season_type=None, limit=500) -> pd.DataFrame:
+    """espn_nhl_schedule - look up the NHL schedule for a given date
+
+    Args:
+        dates (int): Used to define different seasons. 2002 is the earliest available season.
+        season_type (int): season type, 1 for pre-season, 2 for regular season, 3 for post-season, 4 for all-star, 5 for off-season
+        limit (int): number of records to return, default: 500.
+    Returns:
+        pd.DataFrame: Pandas dataframe containing
+        schedule events for the requested season.
+    """
+    if dates is None:
+        dates = ""
+    else:
+        dates = "&dates=" + str(dates)
+    if season_type is None:
+        season_type = ""
+    else:
+        season_type = "&seasontype=" + str(season_type)
+
+    url = "http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard?limit={}{}{}".format(
+        limit, dates, season_type
+    )
+    resp = download(url=url)
+
+    ev = pd.DataFrame()
+    if resp is not None:
+        events_txt = json.loads(resp)
+
+        events = events_txt.get("events")
+        for event in events:
+            event.get("competitions")[0].get("competitors")[0].get("team").pop(
+                "links", None
+            )
+            event.get("competitions")[0].get("competitors")[1].get("team").pop(
+                "links", None
+            )
+            if (
+                event.get("competitions")[0].get("competitors")[0].get("homeAway")
+                == "home"
+            ):
+                event["competitions"][0]["home"] = (
+                    event.get("competitions")[0].get("competitors")[0].get("team")
+                )
+                event["competitions"][0]["away"] = (
+                    event.get("competitions")[0].get("competitors")[1].get("team")
+                )
+            else:
+                event["competitions"][0]["away"] = (
+                    event.get("competitions")[0].get("competitors")[0].get("team")
+                )
+                event["competitions"][0]["home"] = (
+                    event.get("competitions")[0].get("competitors")[1].get("team")
+                )
+
+            del_keys = ["broadcasts", "geoBroadcasts", "headlines", "series"]
+            for k in del_keys:
+                event.get("competitions")[0].pop(k, None)
+            x = pd.json_normalize(event.get("competitions")[0], sep="_")
+            x["game_id"] = x["id"].astype(int)
+            x["season"] = event.get("season").get("year")
+            x["season_type"] = event.get("season").get("type")
+            ev = pd.concat([ev, x], axis=0, ignore_index=True)
+    ev = pd.DataFrame(ev)
+    ev.columns = [underscore(c) for c in ev.columns.tolist()]
+    return ev
+
+
+def espn_nhl_calendar(season=None) -> pd.DataFrame:
+    """espn_nhl_calendar - look up the NHL calendar for a given season
+
+    Args:
+        season (int): Used to define different seasons. 2002 is the earliest available season.
+
+    Returns:
+        pd.DataFrame: Pandas dataframe containing calendar dates for the requested season.
+
+    Raises:
+        ValueError: If `season` is less than 2002.
+    """
+    url = "http://site.api.espn.com/apis/site/v2/sports/hockey/nhl/scoreboard?dates={}".format(
+        season
+    )
+    resp = download(url=url)
+    txt = json.loads(resp)["leagues"][0]["calendar"]
+    datenum = list(map(lambda x: x[:10].replace("-", ""), txt))
+    date = list(map(lambda x: x[:10], txt))
+
+    year = list(map(lambda x: x[:4], txt))
+    month = list(map(lambda x: x[5:7], txt))
+    day = list(map(lambda x: x[8:10], txt))
+
+    data = {
+        "season": season,
+        "datetime": txt,
+        "date": date,
+        "year": year,
+        "month": month,
+        "day": day,
+        "dateURL": datenum,
+    }
+    df = pd.DataFrame(data)
+    df["url"] = (
+        "http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?dates="
+    )
+    df["url"] = df["url"] + df["dateURL"]
+    return df
+
+
+def most_recent_nhl_season():
+    if int(str(datetime.date.today())[5:7]) >= 10:
+        return int(str(datetime.date.today())[0:4]) + 1
+    else:
+        return int(str(datetime.date.today())[0:4])
+
+
+def year_to_season(year):
+    first_year = str(year)[2:4]
+    next_year = int(first_year) + 1
+    if int(next_year) < 10 and int(first_year) >= 0:
+        next_year_formatted = f"0{next_year}"
+    elif int(first_year) == 99:
+        next_year_formatted = "00"
+    else:
+        next_year_formatted = str(next_year)
+    return f"{year}-{next_year_formatted}"
