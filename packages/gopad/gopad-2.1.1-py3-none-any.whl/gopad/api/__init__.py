15e4aeb5ff4e15ff88965ffa7818de129a13b77f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:47 +0000

    Add package gopad/2.1.1/gopad-2.1.1-py3-none-any.whl

---
+++ packages/gopad/gopad-2.1.1-py3-none-any.whl/gopad/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from gopad.api.auth_api import AuthApi
+from gopad.api.group_api import GroupApi
+from gopad.api.profile_api import ProfileApi
+from gopad.api.user_api import UserApi
+
