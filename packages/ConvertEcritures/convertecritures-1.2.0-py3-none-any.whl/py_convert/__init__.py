737d18acf2f8a75afec8c867738f54a5d9ff0338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:26 +0000

    Add package ConvertEcritures/1.2.0/convertecritures-1.2.0-py3-none-any.whl

---
+++ packages/ConvertEcritures/convertecritures-1.2.0-py3-none-any.whl/py_convert/__init__.py
@@ -0,0 +1,3 @@
+from .settings_manager import Settings
+from .error import run_error
+from .cli import main
---
+++ packages/ConvertEcritures/convertecritures-1.2.0-py3-none-any.whl/py_convert/cli.py
@@ -0,0 +1,8 @@
+from py_convert.gui.app import App
+
+def main():
+    app = App()
+    app.run()
+
+if __name__ == "__main__":
+    main()
