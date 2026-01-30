5e69efd059d69e637551c135a0b708952ca0fe5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:17 +0000

    Add package gardener-cicd-cli/1.2739.0/gardener_cicd_cli-1.2739.0-py3-none-any.whl

---
+++ packages/gardener-cicd-cli/gardener_cicd_cli-1.2739.0-py3-none-any.whl/gardener_ci/containerutil.py
@@ -0,0 +1,24 @@
+import ccc.oci
+import ctt.oci_util
+import oci
+
+
+def filter_image(
+    source_ref:str,
+    target_ref:str,
+    remove_files:[str]=[],
+):
+    oci_client = ccc.oci.oci_client()
+
+    ctt.oci_util.filter_image(
+        source_ref=source_ref,
+        target_ref=target_ref,
+        remove_files=remove_files,
+        oci_client=oci_client,
+        mode=oci.ReplicationMode.PREFER_MULTIARCH,
+    )
+
+
+def to_digest_ref(image_ref:str):
+    oci_client = ccc.oci.oci_client()
+    print(oci_client.to_digest_hash(image_reference=image_ref))
