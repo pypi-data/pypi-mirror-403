0682eb4d9e6c365b522156bb5482c714c755e94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:21:58 +0000

    Add package data-repo-client/2.364.0/data_repo_client-2.364.0-py3-none-any.whl

---
+++ packages/data-repo-client/data_repo_client-2.364.0-py3-none-any.whl/data_repo_client/api/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from data_repo_client.api.data_repository_service_api import DataRepositoryServiceApi
+from data_repo_client.api.snapshot_access_request_api import SnapshotAccessRequestApi
+from data_repo_client.api.admin_api import AdminApi
+from data_repo_client.api.configs_api import ConfigsApi
+from data_repo_client.api.datasets_api import DatasetsApi
+from data_repo_client.api.duos_api import DuosApi
+from data_repo_client.api.jobs_api import JobsApi
+from data_repo_client.api.journal_api import JournalApi
+from data_repo_client.api.profiles_api import ProfilesApi
+from data_repo_client.api.register_api import RegisterApi
+from data_repo_client.api.repository_api import RepositoryApi
+from data_repo_client.api.resources_api import ResourcesApi
+from data_repo_client.api.search_api import SearchApi
+from data_repo_client.api.snapshots_api import SnapshotsApi
+from data_repo_client.api.unauthenticated_api import UnauthenticatedApi
+from data_repo_client.api.upgrade_api import UpgradeApi
