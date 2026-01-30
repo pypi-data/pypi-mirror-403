7098e20b3cbf2ef175b5665860854591b23255f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:15 +0000

    Add package kececifractals/0.2.1/kececifractals-0.2.1-py3-none-any.whl

---
+++ packages/kececifractals/kececifractals-0.2.1-py3-none-any.whl/kececifractals/_version.py
@@ -0,0 +1,9 @@
+# _version.py
+
+__version__ = "0.2.1"
+__license__ = "AGPL3.0-or-later"
+__description__ = "Keçeci Fractals: Keçeci-style circle fractal."
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/WhiteSymmetry/kececifractals"
+__docs__ = "https://github.com/WhiteSymmetry/kececifractals"  # Opsiyonel: Dokümantasyon linki
+__dependencies__ = ["python>=3.11"]  # Diğer bağımlılıkları da ekleyebilirsiniz
