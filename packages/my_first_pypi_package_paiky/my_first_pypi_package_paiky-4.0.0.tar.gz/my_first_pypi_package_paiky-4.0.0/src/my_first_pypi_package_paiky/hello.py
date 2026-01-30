bf229f676cf86c47e5398c5caad746320223808e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:09:03 +0000

    Add package my_first_pypi_package_paiky/4.0.0/my_first_pypi_package_paiky-4.0.0.tar.gz

---
+++ packages/my_first_pypi_package_paiky/my_first_pypi_package_paiky-4.0.0.tar.gz/my_first_pypi_package_paiky-4.0.0/src/my_first_pypi_package_paiky/hello.py
@@ -0,0 +1,39 @@
+from typing import TypedDict
+
+
+
+class Person(TypedDict):
+    name: str
+    age: int
+
+
+
+
+
+def say_hello():
+    print("Hello, World!!!")
+
+
+
+
+
+def return_a_string() -> str:
+    return "a"
+
+"ASDAD"
+
+
+
+def return_a_person() -> Person:
+    return {
+        "name": "John",
+        "age": 30
+    }
+
+
+#lolol
+
+"haha"
+
+
+"lol"
