b5b476f702f17911fb786734a2cd30450ddfc773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:09:05 +0000

    Add package flowdacity-queue/0.1.1/flowdacity_queue-0.1.1-py3-none-any.whl

---
+++ packages/flowdacity-queue/flowdacity_queue-0.1.1-py3-none-any.whl/fq/exceptions.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Plivo Team. See LICENSE.txt for details.
+
+
+class FQException(Exception):
+    pass
+
+
+class BadArgumentException(FQException):
+    pass
