10230c616629d56655e4d9886ef4e2948821179e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:34:31 +0000

    Add package make-future/0.2.2/make_future-0.2.2.tar.gz

---
+++ packages/make-future/make_future-0.2.2.tar.gz/make_future-0.2.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='make_future',
+    version='0.2.2',
+    author='Vidal Attias',
+    description='Miniam yet powerful parallelization tool',
+    packages=['make_future'],
+)
