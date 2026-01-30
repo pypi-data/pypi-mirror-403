be8832f80012b08981fb97fd8e6f566d6ff0a440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:30 +0000

    Add package equiflow/0.1.7/equiflow-0.1.7-py3-none-any.whl

---
+++ packages/equiflow/equiflow-0.1.7-py3-none-any.whl/equiflow/__init__.py
@@ -0,0 +1,6 @@
+from .equiflow import TableFlows, TableCharacteristics, TableDrifts, FlowDiagram, EquiFlow, EasyFlow, TablePValues
+
+__author__ = "João Matos <jcmatos@mit.edu>"
+__version__ = "0.1.7"
+
+__all__ = ['TableFlows', 'TableCharacteristics', 'TableDrifts', 'FlowDiagram', 'EquiFlow', 'EasyFlow', 'TablePValues']
