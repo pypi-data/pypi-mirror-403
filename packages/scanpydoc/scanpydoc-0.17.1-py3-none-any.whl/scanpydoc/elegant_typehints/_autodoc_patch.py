d16a211d383b06d0439e6a3d5e4c2422538016f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:33:31 +0000

    Add package scanpydoc/0.17.1/scanpydoc-0.17.1-py3-none-any.whl

---
+++ packages/scanpydoc/scanpydoc-0.17.1-py3-none-any.whl/scanpydoc/elegant_typehints/_autodoc_patch.py
@@ -0,0 +1,70 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from functools import wraps
+
+from sphinx.ext.autodoc import ClassDocumenter
+
+from . import qualname_overrides
+
+
+if TYPE_CHECKING:
+    from sphinx.application import Sphinx
+    from docutils.statemachine import StringList
+
+
+orig = ClassDocumenter.add_directive_header
+
+
+@wraps(orig)
+def add_directive_header(self: ClassDocumenter, sig: str) -> None:
+    orig(self, sig)
+    lines = self.directive.result
+    inferred_role, direc = (
+        ("py:exc", "py:exception")
+        if isinstance(self.object, type) and issubclass(self.object, BaseException)
+        else ("py:class", "py:class")
+    )
+    for (old_role, old_name), (new_role, new_name) in qualname_overrides.items():
+        role = inferred_role if new_role is None else new_role
+        # Currently, autodoc doesn’t link to bases using :exc:
+        lines.replace(
+            f":{old_role or 'py:class'}:`{old_name}`", f":{role}:`{new_name}`"
+        )
+        # But maybe in the future it will
+        lines.replace(f":{role}:`{old_name}`", f":{role}:`{new_name}`")
+        if any("." not in name for name in (old_name, new_name)):
+            continue  # pragma: no cover
+        old_mod, old_cls = old_name.rsplit(".", 1)
+        new_mod, new_cls = new_name.rsplit(".", 1)
+        replace_multi_suffix(
+            lines,
+            (f".. {direc}:: {old_cls}", f"   :module: {old_mod}"),
+            (f".. {direc}:: {new_cls}", f"   :module: {new_mod}"),
+        )
+
+
+def replace_multi_suffix(
+    lines: StringList, old: tuple[str, str], new: tuple[str, str]
+) -> None:
+    if len(old) != len(new) != 2:  # noqa: PLR2004  # pragma: no cover
+        msg = "Only supports replacing 2 lines"
+        raise NotImplementedError(msg)
+    for l, line in enumerate(lines):
+        start = line.find(old[0])
+        if start == -1:
+            continue
+        prefix = line[:start]
+        suffix = line[start + len(old[0]) :]
+        if lines[l + 1].startswith(prefix + old[1]):
+            break
+    else:
+        return
+    lines[l + 0] = prefix + new[0] + suffix
+    lines[l + 1] = prefix + new[1]
+
+
+def setup(app: Sphinx) -> None:
+    # no support for new autodoc
+    if getattr(app.config, "autodoc_use_legacy_class_based", True):
+        ClassDocumenter.add_directive_header = add_directive_header  # type: ignore[method-assign]
---
+++ packages/scanpydoc/scanpydoc-0.17.1-py3-none-any.whl/scanpydoc/elegant_typehints/_formatting.py
@@ -0,0 +1,109 @@
+from __future__ import annotations
+
+from types import GenericAlias
+from typing import TYPE_CHECKING, TypeAliasType, cast, get_args, get_origin
+
+from docutils import nodes
+from sphinx.addnodes import pending_xref
+from sphinx.ext.intersphinx import resolve_reference_detect_inventory
+from sphinx_autodoc_typehints import format_annotation
+
+from scanpydoc import elegant_typehints
+from scanpydoc._types import _GenericAlias
+
+
+if TYPE_CHECKING:
+    from typing import Any
+    from collections.abc import Sequence
+
+    from sphinx.config import Config
+    from sphinx.application import Sphinx
+
+
+def typehints_formatter(
+    annotation: object, config: Config, app: Sphinx | None = None
+) -> str | None:
+    """Generate reStructuredText containing links to the types.
+
+    Can be used as ``typehints_formatter`` for :mod:`sphinx_autodoc_typehints`,
+    to respect the ``qualname_overrides`` option.
+
+    Arguments
+    ---------
+    annotation
+        A type or class used as type annotation.
+    config
+        Sphinx config containing ``sphinx-autodoc-typehints``’s options.
+
+    Returns
+    -------
+    reStructuredText describing the type
+    """
+    if isinstance(annotation, TypeAliasType):
+        if ref := _link_or_expand_alias(annotation, app=app):
+            return ref
+        return format_annotation(annotation.__value__, config)
+
+    if isinstance(annotation, type) and annotation.__module__ == "builtins":
+        return None
+
+    if isinstance(annotation, GenericAlias | _GenericAlias):
+        args = get_args(annotation)
+        annotation = cast("type", get_origin(annotation))
+    else:
+        args = None
+    annotation_cls = annotation if isinstance(annotation, type) else type(annotation)
+    if annotation_cls.__module__ in {"typing", "types"}:
+        return None
+
+    if isinstance(annotation, type):
+        return _fmt_type(annotation, args, config)
+
+    return None  # pragma: no cover
+
+
+def _link_or_expand_alias(
+    annotation: TypeAliasType, *, app: Sphinx | None
+) -> str | None:
+    """Eagerly try to resolve the reference and return it if it does."""
+    if app is None or "sphinx.ext.intersphinx" not in app.extensions:
+        return None
+    role, qualname = "py:type", f"{annotation.__module__}.{annotation.__name__}"
+    if override := elegant_typehints.qualname_overrides.get((role, qualname)):
+        role = override[0] or "py:type"
+        qualname = override[1]
+        return f":{role}:`{qualname}`"
+
+    from . import _last_resolve
+
+    domain, typ = role.split(":", 1)
+    xref = pending_xref(refdomain=domain, reftype=typ, reftarget=qualname)
+    contnode = nodes.TextElement()
+    if _last_resolve(
+        app, app.env, xref, contnode
+    ) or resolve_reference_detect_inventory(app.env, xref, contnode):
+        return f":{role}:`{qualname}`"
+    return None
+
+
+def _fmt_type(cls: type, args: Sequence[Any] | None, config: Config) -> str | None:
+    full_name = f"{cls.__module__}.{cls.__qualname__}"
+    if (
+        override := elegant_typehints.qualname_overrides.get((None, full_name))
+    ) is None:
+        return None
+
+    role, qualname = override
+    if role == "doc":
+        label = full_name if config.typehints_fully_qualified else cls.__qualname__
+        return f":{role}:`{label} <{qualname}>`"
+
+    tilde = "" if config.typehints_fully_qualified else "~"
+    if role is None:
+        role = "py:exc" if issubclass(cls, BaseException) else "py:class"
+    if args is None:
+        formatted_args = ""
+    else:
+        formatted_args = ", ".join(format_annotation(arg, config) for arg in args)
+        formatted_args = rf"\ \[{formatted_args}]"
+    return f":{role}:`{tilde}{qualname}`{formatted_args}"
