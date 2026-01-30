3eacfa9b582c9b489e1482e7de5537ff1e9fd3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:22:00 +0000

    Add package renumerate/2.1.1/renumerate-2.1.1-py3-none-any.whl

---
+++ packages/renumerate/renumerate-2.1.1-py3-none-any.whl/renumerate/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2016 Adam Karpierz
+# SPDX-License-Identifier: Zlib
+
+from .__about__ import * ; del __about__  # type: ignore[name-defined]  # noqa
+
+from ._renumerate import * ; del _renumerate  # type: ignore[name-defined]  # noqa
