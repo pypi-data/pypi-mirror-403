1f5474771641b7095d381574db8573a8285b0cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:22:57 +0000

    Add package multiolib/2.8.0.9/multiolib-2.8.0.9-cp310-cp310-macosx_13_0_arm64.whl

---
+++ packages/multiolib/multiolib-2.8.0.9-cp310-cp310-macosx_13_0_arm64.whl/multiolib/include/multio/multio_config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "multio/multio_ecbuild_config.h"
+
+#include "multio/multio_version.h"
+
+// external packages
+
+/* #undef MULTIO_HAVE_ECKIT */
+/* #undef MULTIO_HAVE_FDB */
+/* #undef MULTIO_HAVE_MIR */
---
+++ packages/multiolib/multiolib-2.8.0.9-cp310-cp310-macosx_13_0_arm64.whl/multiolib/include/multio/transport/TransportRegistry.h
@@ -0,0 +1,42 @@
+/// @author Domokos Sarmany
+
+/// @date Jan 2022
+
+#pragma once
+
+#include "multio/config/ComponentConfiguration.h"
+#include "multio/transport/Transport.h"  // This means circular dependency at the minute
+
+
+namespace eckit {
+class Configuration;
+}
+
+namespace multio::transport {
+
+using config::ComponentConfiguration;
+using message::Message;
+
+
+class TransportRegistry : public eckit::NonCopyable {
+public:
+    static TransportRegistry& instance();
+
+    std::shared_ptr<transport::Transport> get(const ComponentConfiguration& config);
+
+    void openConnections();
+    void closeConnections();
+
+    void abort(const std::string& serverName, std::exception_ptr);
+    void abortAll(std::exception_ptr);
+
+    void synchronize();
+
+private:
+    void add(const std::string& serverName, const ComponentConfiguration& fullConfig);
+
+    std::map<std::string, std::shared_ptr<transport::Transport>> transports_;
+    std::mutex mutex_;
+};
+
+}  // namespace multio::transport
