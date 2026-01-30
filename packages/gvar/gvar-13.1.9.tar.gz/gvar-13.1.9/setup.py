d8ca29b7111721e97f3ad3edf2f677149db9e416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:15:34 +0000

    Add package gvar/13.1.9/gvar-13.1.9.tar.gz

---
+++ packages/gvar/gvar-13.1.9.tar.gz/gvar-13.1.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy
+
+ext_args = dict(
+    # include_dirs=[numpy.get_include()],
+    library_dirs=[],
+    runtime_library_dirs=[],
+    extra_link_args=[]
+    )
+
+ext_modules = [
+    Extension(name="gvar._gvarcore", sources=["src/gvar/_gvarcore.pyx"], **ext_args),
+    Extension(name= "gvar._svec_smat", sources=["src/gvar/_svec_smat.pyx"], **ext_args),
+    Extension(name="gvar._utilities", sources=["src/gvar/_utilities.pyx"], **ext_args),
+    Extension(name="gvar.dataset", sources=["src/gvar/dataset.pyx"], **ext_args),
+    Extension(name="gvar._bufferdict", sources=["src/gvar/_bufferdict.pyx"], **ext_args),
+    ]
+
+setup(ext_modules=cythonize(ext_modules))
