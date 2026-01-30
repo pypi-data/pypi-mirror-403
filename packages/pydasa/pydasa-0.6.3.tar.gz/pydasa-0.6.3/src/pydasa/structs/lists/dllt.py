c65d2ebd37e5cf80ad34c2dd2472bcc1a2b4ea0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:47 +0000

    Add package pydasa/0.6.3/pydasa-0.6.3.tar.gz

---
+++ packages/pydasa/pydasa-0.6.3.tar.gz/pydasa-0.6.3/src/pydasa/structs/lists/dllt.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Module dllt.py
+===========================================
+
+Module to represent the **Doubly Linked List** data structure in *PyDASA*.
+
+This module provides the **DoubleLinkedList** class which implements a doubly linked list with methods for insertion, deletion, and traversal.
+
+classes:
+    **DoubleLinkedList**: Implements a doubly linked list with methods for insertion, deletion, and traversal.
+
+*IMPORTANT:* based on the implementations proposed by the following authors/books:
+
+    #. Algorithms, 4th Edition, Robert Sedgewick and Kevin Wayne.
+    #. Data Structure and Algorithms in Python, M.T. Goodrich, R. Tamassia, M.H. Goldwasser.
+"""
+# TODO complete the DoubleLinkedList implementation
