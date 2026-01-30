299560d86b069bf43ed35cec800fc6b079103be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:15:27 +0000

    Add package maxnewpackage2026/0.0.1/maxnewpackage2026-0.0.1-py3-none-any.whl

---
+++ packages/maxnewpackage2026/maxnewpackage2026-0.0.1-py3-none-any.whl/MaxNewPackage/__init__.py
@@ -0,0 +1,3 @@
+"""
+Docstring for mynewpackage.mynewpackage
+"""
---
+++ packages/maxnewpackage2026/maxnewpackage2026-0.0.1-py3-none-any.whl/MaxNewPackage/player.py
@@ -0,0 +1,18 @@
+"""
+Docstring for mynewpackage.mynewpackage.player
+"""
+
+
+class Player():
+    """
+    Docstring for Player
+    """
+
+    def play(self, cancion):
+        """
+        Docstring for play
+
+        :param self: Description
+        :param cancion: Description
+        """
+        print("reproduciendo: ", cancion)
