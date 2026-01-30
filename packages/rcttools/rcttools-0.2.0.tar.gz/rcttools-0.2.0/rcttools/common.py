db8ebb3ea32cf1543bd345c766a19f98ae1239b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:35:47 +0000

    Add package rcttools/0.2.0/rcttools-0.2.0.tar.gz

---
+++ packages/rcttools/rcttools-0.2.0.tar.gz/rcttools-0.2.0/rcttools/common.py
@@ -0,0 +1,41 @@
+from decimal import Decimal
+from typing import Literal, Tuple
+
+import numpy as np
+
+FRAME_HEIGHT = Literal[40]
+FRAME_WIDTH = Literal[1150]
+FRAME_CHANNELS = Literal[3]
+# Typically 902 frames for 30s clips, but can be shorter
+VIDEO_LENGTH = int
+
+FRAME_TYPE = np.ndarray[
+    Tuple[FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNELS], np.dtype[np.uint8]
+]
+FLOAT_FRAME_TYPE = np.ndarray[
+    Tuple[FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNELS], np.dtype[np.float32]
+]
+VIDEO_TYPE = np.ndarray[
+    Tuple[VIDEO_LENGTH, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNELS], np.dtype[np.uint8]
+]
+FLOAT_VIDEO_TYPE = np.ndarray[
+    Tuple[VIDEO_LENGTH, FRAME_HEIGHT, FRAME_WIDTH, FRAME_CHANNELS], np.dtype[np.float64]
+]
+
+CHAR_WIDTHS = {str(x): 19 for x in range(10)}
+CHAR_WIDTHS["/"] = 14
+CHAR_WIDTHS[" "] = 8
+CHAR_WIDTHS[":"] = 14
+CHAR_WIDTHS["."] = 14
+CHAR_WIDTHS["-"] = 14
+CHAR_WIDTHS[""] = 0
+
+
+def dec_to_dms(gps_decimal: Decimal) -> Tuple[Decimal, Decimal, Decimal]:
+    whole = abs(gps_decimal) // 1
+    fraction = abs(gps_decimal) % 1
+    minutes = (fraction * 60) // 1
+    fraction -= minutes / 60
+    seconds = fraction * 3600
+
+    return whole, minutes, seconds
