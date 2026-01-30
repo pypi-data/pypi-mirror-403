2b06b71cad61c442f7b781d39698b278b10c0833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 04:34:38 +0000

    Add package tkinterweb-tkhtml-extras/1.2.0/tkinterweb_tkhtml_extras-1.2.0-py3-none-win_amd64.whl

---
+++ packages/tkinterweb-tkhtml-extras/tkinterweb_tkhtml_extras-1.2.0-py3-none-win_amd64.whl/tkinterweb_tkhtml_extras/__init__.py
@@ -0,0 +1,18 @@
+"""
+TkinterWeb-Tkhtml-Extras v1.1
+This package provides pre-built binaries of a modified version of the Tkhtml3 widget from https://github.com/Andereoo/TkinterWeb-Tkhtml, 
+which enables the display of styled HTML and CSS code in Tkinter applications.
+
+Copyright (c) 2025 Andrew Clarke
+"""
+
+import os
+
+__title__ = 'TkinterWeb-Tkhtml-Extras'
+__author__ = "Andrew Clarke"
+__copyright__ = "Copyright (c) 2025 Andrew Clarke"
+__license__ = "MIT"
+__version__ = '1.2.0'
+
+
+TKHTML_EXTRAS_ROOT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "tkhtml")
