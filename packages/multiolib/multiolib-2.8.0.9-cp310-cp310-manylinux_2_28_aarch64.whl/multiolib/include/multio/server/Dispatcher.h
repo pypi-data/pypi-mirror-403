59c45b6edf604640746e0004168624db5ad0d930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:23:04 +0000

    Add package multiolib/2.8.0.9/multiolib-2.8.0.9-cp310-cp310-manylinux_2_28_aarch64.whl

---
+++ packages/multiolib/multiolib-2.8.0.9-cp310-cp310-manylinux_2_28_aarch64.whl/multiolib/include/multio/server/Dispatcher.h
@@ -0,0 +1,85 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Domokos Sarmany
+/// @author Simon Smart
+/// @author Tiago Quintino
+
+/// @date Jan 2019
+
+#pragma once
+
+#include <atomic>
+#include <memory>
+
+#include "eckit/container/Queue.h"
+#include "eckit/log/Statistics.h"
+#include "eckit/memory/NonCopyable.h"
+#include "multio/util/FailureHandling.h"
+
+#include "multio/config/ComponentConfiguration.h"
+#include "multio/message/Message.h"
+#include "multio/util/Timing.h"
+
+#include "multio/transport/Transport.h"
+
+namespace eckit {
+class Configuration;
+class Timer;
+}  // namespace eckit
+
+namespace multio {
+
+namespace action {
+class Plan;
+}
+
+namespace server {
+
+struct DispatcherFailureTraits {
+    using OnErrorType = util::OnDispatchError;
+    using FailureOptions = util::DefaultFailureOptions;
+    using FailureState = util::DefaultFailureState;
+    using TagSequence = util::integer_sequence<OnErrorType, OnErrorType::Propagate>;
+    static inline std::optional<OnErrorType> parse(const std::string& str) {
+        return util::parseErrorTag<OnErrorType, TagSequence>(str);
+    }
+    static inline OnErrorType defaultOnErrorTag() { return OnErrorType::Propagate; };
+    static inline std::string configKey() { return std::string("on-dispatch-error"); };
+    static inline FailureOptions parseFailureOptions(const eckit::Configuration& conf) {
+        return util::parseDefaultFailureOptions(conf);
+    };
+    static inline std::string componentName() { return std::string("Dispatcher"); };
+};
+
+
+class Dispatcher : public util::FailureAware<DispatcherFailureTraits>, private eckit::NonCopyable {
+public:
+    Dispatcher(const config::ComponentConfiguration& compConf, eckit::Queue<message::Message>& queue, multio::transport::Transport& transport);
+    ~Dispatcher();
+
+    void dispatch();
+
+    util::FailureHandlerResponse handleFailure(util::OnDispatchError, const util::FailureContext&,
+                                               util::DefaultFailureState&) const override;
+
+private:
+    void handle(message::Message msg) const;
+
+    eckit::Queue<message::Message>& queue_;
+    std::vector<std::unique_ptr<action::Plan>> plans_;
+
+    multio::transport::Transport& transport_;
+
+    util::Timing<> timing_;
+};
+
+}  // namespace server
+}  // namespace multio
---
+++ packages/multiolib/multiolib-2.8.0.9-cp310-cp310-manylinux_2_28_aarch64.whl/multiolib/include/multio/transport/MpiTransport.h
@@ -0,0 +1,95 @@
+/*
+ * (C) Copyright 1996- ECMWF.
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ * In applying this licence, ECMWF does not waive the privileges and immunities
+ * granted to it by virtue of its status as an intergovernmental organisation nor
+ * does it submit to any jurisdiction.
+ */
+
+/// @author Domokos Sarmany
+/// @author Simon Smart
+/// @author Tiago Quintino
+
+/// @date Jan 2019
+
+#pragma once
+
+#include <queue>
+#include <tuple>
+
+#include "eckit/container/Queue.h"
+#include "eckit/io/Buffer.h"
+#include "eckit/log/Statistics.h"
+#include "eckit/mpi/Comm.h"
+#include "eckit/mpi/Group.h"
+#include "eckit/serialisation/ResizableMemoryStream.h"
+
+
+#include "multio/transport/StreamPool.h"
+#include "multio/transport/Transport.h"
+
+namespace multio::transport {
+
+struct ReceivedBuffer {
+    MpiBuffer* buffer;
+    size_t size;
+};
+
+using MpiPeerSetup = std::tuple<MpiPeer, eckit::mpi::Group, eckit::mpi::Group, eckit::mpi::Group>;
+
+class MpiTransport final : public Transport {
+public:
+    MpiTransport(const ComponentConfiguration& compConf);
+    ~MpiTransport();
+
+private:
+    MpiTransport(const ComponentConfiguration& compConf, MpiPeerSetup&& peerSetup);
+
+    void openConnections() override;
+    void closeConnections() override;
+
+    void synchronize() override;
+
+    Message receive() override;
+
+    void abort(std::exception_ptr) override;
+
+    void send(const Message& msg) override;
+
+    void bufferedSend(const Message& msg) override;
+
+    void createPeers() const override;
+
+    void print(std::ostream& os) const override;
+
+    const Peer& localPeer() const override;
+
+    void listen() override;
+
+    PeerList createServerPeers() const override;
+
+    const eckit::mpi::Comm& comm() const;
+
+    eckit::mpi::Status probe();
+    size_t blockingReceive(eckit::mpi::Status& status, MpiBuffer& buffer);
+
+    void encodeMessage(eckit::Stream& strm, const Message& msg);
+
+    size_t getMpiPoolSize(const ComponentConfiguration& compConf);
+    size_t getMpiBufferSize(const ComponentConfiguration& compConf);
+
+    MpiPeer local_;
+    eckit::mpi::Group parentGroup_;
+    eckit::mpi::Group clientGroup_;
+    eckit::mpi::Group serverGroup_;
+
+    StreamPool pool_;
+
+    eckit::Queue<ReceivedBuffer> streamQueue_;
+
+    std::queue<Message> msgPack_;
+};
+
+}  // namespace multio::transport
