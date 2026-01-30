172873bc83a79a70b551cc75a5f7acd05c81b945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:32 +0000

    Add package stockdata77/1.0.0/stockdata77-1.0.0-py3-none-any.whl

---
+++ packages/stockdata77/stockdata77-1.0.0-py3-none-any.whl/stockdata77/__init__.py
@@ -0,0 +1,19 @@
+"""
+	This file is part of stockdata77 Python module.
+
+	Copyright 2022 Dandelion Systems <dandelion.systems@gmail.com>
+
+	stockdata77 is free software; you can redistribute it and/or modify
+	it under the terms of the MIT License.
+
+	stockdata77 is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+	See the MIT License for more details.
+
+	SPDX-License-Identifier: MIT
+"""
+
+from stockdata77.securities import Securities
+
+__version__ = "1.0.0"
