f9e8ce72dd166c8983003dea95738f14e7b1b6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:18 +0000

    Add package outscraper/6.0.1/outscraper-6.0.1.tar.gz

---
+++ packages/outscraper/outscraper-6.0.1.tar.gz/outscraper-6.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='outscraper',
+    version='6.0.1',
+    description='Python bindings for the Outscraper API',
+    long_description=readme(),
+    classifiers = ['Programming Language :: Python',
+                    'License :: OSI Approved :: MIT License',
+                    'Operating System :: OS Independent',
+                    'Intended Audience :: Developers',
+                    'Topic :: Utilities',
+    ],
+    keywords='outscraper webscraper extractor google api maps search json scrape parser reviews google play amazon',
+    url='https://github.com/outscraper/outscraper-python',
+    author='Outscraper',
+    author_email='support@outscraper.com',
+    license='MIT',
+    packages=['outscraper'],
+    install_requires=['requests'],
+    include_package_data=True,
+    zip_safe=False,
+    long_description_content_type='text/x-rst',
+)
