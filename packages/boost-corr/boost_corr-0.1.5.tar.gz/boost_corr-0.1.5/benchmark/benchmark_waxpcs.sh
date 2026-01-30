0a34c2abfd158001bfae561260710e2e3b291601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 06:15:30 +0000

    Add package boost-corr/0.1.5/boost_corr-0.1.5.tar.gz

---
+++ packages/boost-corr/boost_corr-0.1.5.tar.gz/boost_corr-0.1.5/benchmark/benchmark_waxpcs.sh
@@ -0,0 +1,8 @@
+#  gpu_corr_shell \
+boost_corr \
+        -i 0 \
+        -r /clhome/MQICHU/ssd/A050_Cu3Au_1bit_att0/A050_Cu3Au_1bit_att0.h5 \
+        -q /net/s8iddata/export/8-id-e/partitionMapLibrary/2021-2/dufresne20210727_qmap_A050_1.h5 \
+        -v \
+        -o /clhome/MQICHU/ssd/cluster_results
+
