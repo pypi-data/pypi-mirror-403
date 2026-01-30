aafa1956fa44bbb7c050f1cc1d5edf1441f712e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 05:22:06 +0000

    Add package datedict/1.0.11/datedict-1.0.11-py3-none-any.whl

---
+++ packages/datedict/datedict-1.0.11-py3-none-any.whl/datedict/__init__.py
@@ -0,0 +1,7 @@
+from .datedict import DateDict
+from .yeardict import YearDict
+from .timedict import TimeDict
+from .common import ZERO, ONE, NAN, Decimable
+
+__all__ = ["DateDict", "YearDict", "TimeDict", "ZERO", "ONE", "NAN", "Decimable"]
+__version__ = "1.0.11"
