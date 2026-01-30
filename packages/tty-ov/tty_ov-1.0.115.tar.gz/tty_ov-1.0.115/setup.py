e64855d2b9a7bd21401c535a553167b296f83988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 01:36:02 +0000

    Add package tty-ov/1.0.115/tty_ov-1.0.115.tar.gz

---
+++ packages/tty-ov/tty_ov-1.0.115.tar.gz/tty_ov-1.0.115/setup.py
@@ -0,0 +1,32 @@
+"""
+File containing the required information to successfully build a python package
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8", newline="\n") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tty_ov',
+    version='1.0.115',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "colorama==0.4.6",
+        "prettytable==3.17.0",
+        "ask_question==1.2.12",
+        "colourise-output==1.1.6",
+        "prompt-toolkit==3.0.52"
+    ],
+    author="Henry Letellier",
+    author_email="henrysoftwarehouse@protonmail.com",
+    description="A module that emulates a few core functionalities of a tty (see the inner help for a list of functions).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hanra-s-work/tty_ov",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/tty-ov/tty_ov-1.0.115.tar.gz/tty_ov-1.0.115/tty_ov/__init__.py
@@ -0,0 +1,52 @@
+"""
+File in charge of easing the import of the program
+"""
+from .tty_ov import TTY
+from .tty_ov import HLLs
+from .tty_ov import ColouriseOutput as CO
+from .tty_ov import AskQuestion as AQ
+
+
+class ColouriseOutput(CO):
+    """ A rebind of the same library to help you access the ressources """
+    pass
+
+
+class AskQuestion(AQ):
+    """ A rebind of the same library to help you access the ressources """
+    pass
+
+
+class TTY_OV(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class TTY_Ov(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class Tty_Ov(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class TTYOV(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class TtyOv(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class tty_ov(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
+
+
+class tty(TTY):
+    """ A name rebind to for those who whish to follow the library name """
+    pass
