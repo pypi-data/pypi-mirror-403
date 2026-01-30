a1af5cc55bed855c3d4e0a137c0daba6308a63b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:56:08 +0000

    Add package py-youtube-search/0.2.2/py_youtube_search-0.2.2.tar.gz

---
+++ packages/py-youtube-search/py_youtube_search-0.2.2.tar.gz/py_youtube_search-0.2.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="py-youtube-search",
+    version="0.2.2",
+    author="VishvaRam",  
+    install_requires=[
+        "aiohttp>=3.8.0",
+    ],
+    author_email="murthyvishva@gmail.com",  
+    description="A lightweight, regex-based YouTube search library without API keys.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VishvaRam/py-youtube-search",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license="MIT",
+)
