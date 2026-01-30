dd5fbba896806c5c57178047e5c0a081a3298c90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 00:08:20 +0000

    Add package xulbux/1.9.5/xulbux-1.9.5-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/xulbux/xulbux-1.9.5-cp310-cp310-macosx_11_0_arm64.whl/xulbux/__init__.py
@@ -0,0 +1,46 @@
+__package_name__ = "xulbux"
+__version__ = "1.9.5"
+__description__ = "A Python library to simplify common programming tasks."
+__status__ = "Production/Stable"
+
+__url__ = "https://github.com/XulbuX/PythonLibraryXulbuX"
+
+__author__ = "XulbuX"
+__email__ = "xulbux.real@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2024 XulbuX"
+
+__requires_python__ = ">=3.10.0"
+__dependencies__ = [
+    "keyboard>=0.13.5",
+    "prompt_toolkit>=3.0.41",
+    "regex>=2023.10.3",
+]
+
+__all__ = [
+    "Code",
+    "Color",
+    "Console",
+    "Data",
+    "EnvPath",
+    "File",
+    "FileSys",
+    "FormatCodes",
+    "Json",
+    "Regex",
+    "String",
+    "System",
+]
+
+from .code import Code
+from .color import Color
+from .console import Console
+from .data import Data
+from .env_path import EnvPath
+from .file import File
+from .file_sys import FileSys
+from .format_codes import FormatCodes
+from .json import Json
+from .regex import Regex
+from .string import String
+from .system import System
