2585ca6c021b25e78cb7836bc87e5f1ba1b33d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 02:55:35 +0000

    Add package smartpasslib/2.1.1/smartpasslib-2.1.1.tar.gz

---
+++ packages/smartpasslib/smartpasslib-2.1.1.tar.gz/smartpasslib-2.1.1/tests/test_generators/test_smart.py
@@ -0,0 +1,10 @@
+# Copyright (©) 2026, Alexander Suvorov. All rights reserved.
+from smartpasslib.generators.smart import SmartPasswordGenerator
+
+
+class TestSmartPasswordGenerator:
+    def test_generate_with_seed(self):
+        seed = "test_seed"
+        password1 = SmartPasswordGenerator.generate(seed=seed)
+        password2 = SmartPasswordGenerator.generate(seed=seed)
+        assert password1 == password2
---
+++ packages/smartpasslib/smartpasslib-2.1.1.tar.gz/smartpasslib-2.1.1/tests/test_masters/test_master.py
@@ -0,0 +1,38 @@
+# Copyright (©) 2026, Alexander Suvorov. All rights reserved.
+from smartpasslib.masters.smart_password_master import SmartPasswordMaster
+
+
+class TestSmartPasswordMaster:
+    def test_generate_base_password(self):
+        password = SmartPasswordMaster.generate_base_password()
+        assert len(password) == 12
+
+    def test_generate_strong_password(self):
+        password = SmartPasswordMaster.generate_strong_password()
+        assert len(password) == 12
+
+    def test_generate_smart_password(self, test_secret):
+        password = SmartPasswordMaster.generate_smart_password(test_secret)
+        assert len(password) == 12
+
+    def test_generate_public_key(self, test_secret):
+        pub_key = SmartPasswordMaster.generate_public_key(test_secret)
+        assert len(pub_key) == 128
+
+    def test_generate_private_key(self, test_secret):
+        private_key = SmartPasswordMaster.generate_private_key(test_secret)
+        assert len(private_key) == 128
+
+        private_key2 = SmartPasswordMaster.generate_private_key(test_secret)
+        assert private_key == private_key2
+
+        pub_key = SmartPasswordMaster.generate_public_key(test_secret)
+        assert private_key == pub_key
+
+    def test_check_public_key(self, test_secret):
+        pub_key = SmartPasswordMaster.generate_public_key(test_secret)
+        assert SmartPasswordMaster.check_public_key(test_secret, pub_key)
+
+    def test_generate_code(self):
+        password = SmartPasswordMaster.generate_code()
+        assert len(password) == 8
