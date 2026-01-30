a3215c7ae0753871afd278b512aeeed906acab70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-30 03:49:30 +0000

    Add package openSDUC/1.3.34/opensduc-1.3.34.tar.gz

---
+++ packages/openSDUC/opensduc-1.3.34.tar.gz/opensduc-1.3.34/openSDUC/__init__.py
@@ -0,0 +1,18 @@
+"""
+Open Stochastic Daily Unit Commitment of Thermal and ESS Units (openSDUC) - Version 1.3.34 - January 19, 2026
+
+    Args:
+        case:   Name of the folder where the CSV files of the case are found
+        dir:    Main path where the case folder can be found
+        solver: Name of the solver
+
+    Returns:
+        Output results in CSV files that are found in the case folder.
+
+    Examples:
+        >>> import openSDUC as oT
+        >>> oT.routine("16g", "C:\\Users\\UserName\\Documents\\GitHub\\openSDUC", "appsi_highs")
+"""
+__version__ = "1.3.34"
+
+from .openSDUC_main             import main
