929519c03872a1082d7a83c7b5b3a1f92f848991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:08:00 +0000

    Add package crc-pulp-python-client/20260130.1/crc_pulp_python_client-20260130.1.tar.gz

---
+++ packages/crc-pulp-python-client/crc_pulp_python_client-20260130.1.tar.gz/crc_pulp_python_client-20260130.1/pulpcore/client/pulp_python/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_python.api.api_integrity_provenance_api import ApiIntegrityProvenanceApi
+from pulpcore.client.pulp_python.api.api_legacy_api import ApiLegacyApi
+from pulpcore.client.pulp_python.api.api_pypi_api import ApiPypiApi
+from pulpcore.client.pulp_python.api.api_simple_api import ApiSimpleApi
+from pulpcore.client.pulp_python.api.content_packages_api import ContentPackagesApi
+from pulpcore.client.pulp_python.api.content_provenance_api import ContentProvenanceApi
+from pulpcore.client.pulp_python.api.distributions_pypi_api import DistributionsPypiApi
+from pulpcore.client.pulp_python.api.publications_pypi_api import PublicationsPypiApi
+from pulpcore.client.pulp_python.api.pypi_metadata_api import PypiMetadataApi
+from pulpcore.client.pulp_python.api.remotes_python_api import RemotesPythonApi
+from pulpcore.client.pulp_python.api.repositories_python_api import RepositoriesPythonApi
+from pulpcore.client.pulp_python.api.repositories_python_versions_api import RepositoriesPythonVersionsApi
+
