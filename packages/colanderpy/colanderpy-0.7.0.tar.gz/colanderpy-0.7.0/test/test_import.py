1a2a8963e1239e9611c35360b319f06b43fb824c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:32:06 +0000

    Add package colanderpy/0.7.0/colanderpy-0.7.0.tar.gz

---
+++ packages/colanderpy/colanderpy-0.7.0.tar.gz/colanderpy-0.7.0/test/test_import.py
@@ -0,0 +1,30 @@
+import pytest
+
+
+def test_import_colanderpy():
+    """Test that colanderpy can be imported"""
+    import colanderpy
+    assert colanderpy is not None
+
+
+def test_import_sieve():
+    """Test that sieve module can be imported"""
+    from colanderpy import sieve
+    assert hasattr(sieve, 'sieve')
+    assert hasattr(sieve, 'main')
+
+
+def test_import_pi_function():
+    """Test that pi_function module can be imported"""
+    from colanderpy import pi_function
+    assert hasattr(pi_function, 'pi_function')
+    assert hasattr(pi_function, 'main')
+
+
+def test_import_functions_directly():
+    """Test that functions can be imported directly"""
+    from colanderpy.sieve import sieve
+    from colanderpy.pi_function import pi_function
+    
+    assert callable(sieve)
+    assert callable(pi_function)
