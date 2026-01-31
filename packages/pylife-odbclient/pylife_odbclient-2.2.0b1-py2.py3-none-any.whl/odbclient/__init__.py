2c0787841f307df48c7fe32410253b27c1b807ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:45:25 +0000

    Add package pylife-odbclient/2.2.0b1/pylife_odbclient-2.2.0b1-py2.py3-none-any.whl

---
+++ packages/pylife-odbclient/pylife_odbclient-2.2.0b1-py2.py3-none-any.whl/odbclient/__init__.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2019-2021 - for information on the respective copyright owner
+# see the NOTICE file and/or the repository
+# https://github.com/boschresearch/pylife
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = "Johannes Mueller"
+__maintainer__ = __author__
+
+import sys
+
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = "pylife-odbclient"
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
+
+
+from .odbclient import OdbClient
+
+__all__ = ['OdbClient']
