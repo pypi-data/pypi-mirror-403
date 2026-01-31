7a16795748eb63a682b2fec0cdd3071753a6f4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:07:06 +0000

    Add package ypricemagic/5.2.1/ypricemagic-5.2.1-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/ypricemagic/ypricemagic-5.2.1-cp311-cp311-macosx_11_0_arm64.whl/y/prices/stable_swap/saddle.py
@@ -0,0 +1,185 @@
+import logging
+
+from a_sync import a_sync, cgather
+from web3.exceptions import ContractLogicError
+from web3.middleware.validation import is_not_null
+
+from y import ENVIRONMENT_VARIABLES as ENVS
+from y import convert
+from y.classes.common import ERC20
+from y.constants import CONNECTED_TO_MAINNET
+from y.contracts import has_method, has_methods
+from y.datatypes import Address, AddressOrContract, AnyAddressType, Block, UsdPrice, UsdValue
+from y.prices import magic
+from y.utils.multicall import multicall_same_func_same_contract_different_inputs
+
+logger = logging.getLogger(__name__)
+
+_GET_TOKEN_INPUTS = tuple(range(8))
+
+
+@a_sync(default="sync", cache_type="memory", ram_cache_ttl=5 * 60)
+async def is_saddle_lp(token_address: AnyAddressType) -> bool:
+    """
+    Determine if a given token is a Saddle LP token.
+
+    This function checks if the token has specific methods that are characteristic of Saddle LP tokens.
+
+    Args:
+        token_address: The address of the token to check.
+
+    Returns:
+        True if the token is a Saddle LP token, False otherwise.
+
+    Examples:
+        >>> await is_saddle_lp("0x1234567890abcdef1234567890abcdef12345678")
+        False
+
+    See Also:
+        - :func:`get_pool`
+    """
+    pool = await get_pool(token_address, sync=False)
+    return pool is not None and await has_methods(
+        pool,
+        ("getVirtualPrice()(uint)", "getA()(uint)", "getAPrecise()(uint)"),
+        sync=False,
+    )
+
+
+@a_sync(default="sync", ram_cache_ttl=ENVS.CACHE_TTL)
+async def get_pool(token_address: AnyAddressType) -> Address:
+    """
+    Retrieve the pool address for a given Saddle LP token.
+
+    This function checks for specific token addresses on the Ethereum Mainnet and returns hardcoded pool addresses for those tokens.
+    If the token is not one of the known tokens, it attempts to find a pool by checking if the token has a `swap()` method.
+
+    Args:
+        token_address: The address of the token to find the pool for.
+
+    Returns:
+        The address of the pool if found, otherwise None.
+
+    Examples:
+        >>> await get_pool("0xc9da65931ABf0Ed1b74Ce5ad8c041C4220940368")
+        '0xa6018520EAACC06C30fF2e1B3ee2c7c22e64196a'
+
+    See Also:
+        - :func:`is_saddle_lp`
+    """
+    token_address = await convert.to_address_async(token_address)
+    if CONNECTED_TO_MAINNET:
+        # saddle aleth doesn't have swap() function
+        if token_address == "0xc9da65931ABf0Ed1b74Ce5ad8c041C4220940368":
+            return "0xa6018520EAACC06C30fF2e1B3ee2c7c22e64196a"
+        elif token_address == "0xd48cF4D7FB0824CC8bAe055dF3092584d0a1726A":  # saddle d4
+            return "0xC69DDcd4DFeF25D8a793241834d4cc4b3668EAD6"
+        elif token_address == "0xF32E91464ca18fc156aB97a697D6f8ae66Cd21a3":
+            return "0xdf3309771d2BF82cb2B6C56F9f5365C8bD97c4f2"
+    try:
+        pool = await has_method(token_address, "swap()(address)", return_response=True, sync=False)
+    except ContractLogicError:
+        return None
+    else:
+        return pool or None
+
+
+@a_sync(default="sync")
+async def get_price(token_address: AddressOrContract, block: Block | None = None) -> UsdPrice:
+    """
+    Calculate the price of a Saddle LP token in USD.
+
+    This function calculates the price by dividing the total value locked (TVL) by the total supply of the token.
+
+    Args:
+        token_address: The address of the token to get the price for.
+        block: The block number to query. Defaults to the latest block.
+
+    Examples:
+        >>> await get_price("0x1234567890abcdef1234567890abcdef12345678")
+        UsdPrice(1.2345)
+
+    See Also:
+        - :func:`get_tvl`
+    """
+    tvl, total_supply = await cgather(
+        get_tvl(token_address, block, sync=False),
+        ERC20(token_address, asynchronous=True).total_supply_readable(block),
+    )
+    return UsdPrice(tvl / total_supply)
+
+
+@a_sync(default="sync")
+async def get_tvl(
+    token_address: AnyAddressType,
+    block: Block | None = None,
+    skip_cache: bool = ENVS.SKIP_CACHE,
+) -> UsdValue:
+    """
+    Calculate the total value locked (TVL) in a Saddle LP token pool.
+
+    This function retrieves the pool and tokens concurrently, then uses multicall to get the balances.
+    It calculates the TVL by summing the value of each token in the pool.
+
+    Args:
+        token_address: The address of the token to get the TVL for.
+        block: The block number to query. Defaults to the latest block.
+        skip_cache: If True, skip using the cache while fetching price data.
+
+    Examples:
+        >>> await get_tvl("0x1234567890abcdef1234567890abcdef12345678")
+        UsdValue(123456.78)
+
+    See Also:
+        - :func:`get_price`
+    """
+    tokens: list[ERC20]
+    pool, tokens = await cgather(
+        get_pool(token_address, sync=False),
+        get_tokens(token_address, block, sync=False),
+    )
+    balances = await multicall_same_func_same_contract_different_inputs(
+        pool,
+        "getTokenBalance(uint8)(uint)",
+        inputs=range(len(tokens)),
+        sync=False,
+    )
+    scales, prices = await cgather(
+        ERC20.scale.map(tokens).values(pop=True),
+        magic.get_prices(tokens, block, skip_cache=skip_cache, silent=True, sync=False),
+    )
+    return UsdValue(
+        sum(balance / scale * price for balance, scale, price in zip(balances, scales, prices))
+    )
+
+
+@a_sync(default="sync")
+async def get_tokens(token_address: AnyAddressType, block: Block | None = None) -> list[ERC20]:
+    """
+    Retrieve the list of tokens in a Saddle LP token pool.
+
+    This function retrieves the pool and then uses multicall to get the tokens in the pool.
+
+    Args:
+        token_address: The address of the token to get the tokens for.
+        block: The block number to query. Defaults to the latest block.
+
+    Examples:
+        >>> await get_tokens("0x1234567890abcdef1234567890abcdef12345678")
+        [ERC20('0xToken1'), ERC20('0xToken2')]
+
+    See Also:
+        - :func:`get_pool`
+    """
+    pool, tokens = await cgather(
+        get_pool(token_address, sync=False),
+        multicall_same_func_same_contract_different_inputs(
+            pool,
+            "getToken(uint8)(address)",
+            inputs=_GET_TOKEN_INPUTS,
+            block=block,
+            return_None_on_failure=True,
+            sync=False,
+        ),
+    )
+    return list(map(ERC20, filter(is_not_null, tokens)))
---
+++ packages/ypricemagic/ypricemagic-5.2.1-cp311-cp311-macosx_11_0_arm64.whl/y/utils/cache.py
@@ -0,0 +1,128 @@
+import functools
+import os
+from inspect import iscoroutinefunction
+from logging import DEBUG, getLogger
+
+import a_sync
+import eth_retry
+from a_sync._typing import AnyFn, P, T
+from a_sync.a_sync.function import ASyncDecorator
+from brownie import chain
+from joblib import Memory  # type: ignore [import-untyped]
+
+from y import ENVIRONMENT_VARIABLES as ENVS
+
+
+@eth_retry.auto_retry
+def _memory():
+    """
+    Create and return a :class:`Memory` object for caching values for the currently connected blockchain.
+
+    See Also:
+        - :class:`joblib.Memory`
+    """
+    return Memory(f"cache/{chain.id}", verbose=0)
+
+
+memory = _memory()
+
+a_sync_ttl_cache: ASyncDecorator = a_sync.a_sync(ram_cache_ttl=ENVS.CACHE_TTL)
+
+
+# NOTE: we have an optional disk cache that I made for debugging and decided to keep in for my own convenience.
+# TODO: make a real disk cache
+
+try:
+
+    import toolcache
+
+    """User has toolcache, this diskcache decorator will work."""
+
+    logger = getLogger(__name__)
+    cache_base_path = f"./cache/{chain.id}/"
+
+    def optional_async_diskcache(fn: AnyFn[P, T]) -> AnyFn[P, T]:
+        """
+        Decorator to optionally apply disk caching to asynchronous functions.
+
+        If the user has `toolcache` installed, this decorator will apply disk caching
+        to the decorated asynchronous function. If `toolcache` is not installed, the
+        function will be returned as is, without any caching applied.
+
+        Args:
+            fn: The function to be decorated. Must be asynchronous.
+
+        Raises:
+            NotImplementedError: If the function is synchronous.
+
+        Examples:
+            Using the decorator with an asynchronous function when `toolcache` is installed:
+
+            >>> @optional_async_diskcache
+            ... async def fetch_data():
+            ...     return "data"
+
+            This will cache the result of `fetch_data` in the specified cache directory.
+
+            If `toolcache` is not installed, the function will run without caching:
+
+            >>> async def fetch_data():
+            ...     return "data"
+            >>> fetch_data = optional_async_diskcache(fetch_data)
+
+        See Also:
+            - :mod:`toolcache`
+        """
+        if not iscoroutinefunction(fn):
+            raise NotImplementedError(f"{fn} is sync")
+
+        module = fn.__module__.replace(".", "/")
+        cache_path_for_fn = cache_base_path + module + "/" + fn.__name__
+
+        # Ensure the cache dir exists
+        os.makedirs(cache_path_for_fn, exist_ok=True)
+
+        if logger.isEnabledFor(DEBUG):
+
+            @toolcache.cache("disk", cache_dir=cache_path_for_fn)
+            @functools.wraps(fn)
+            async def diskcache_wrap_with_debug_logs(*args, **kwargs) -> T:
+                logger.debug("fetching %s(%s)", fn.__qualname__, ", ".join(map(str, args)))
+                return await fn(*args, **kwargs)
+
+            return diskcache_wrap_with_debug_logs
+
+        else:
+
+            @toolcache.cache("disk", cache_dir=cache_path_for_fn)
+            @functools.wraps(fn)
+            async def diskcache_wrap(*args, **kwargs) -> T:
+                return await fn(*args, **kwargs)
+
+            return diskcache_wrap
+
+except ImportError:
+
+    """User doesn't have toolcache, this decorator will just return the undecorated function."""
+
+    def optional_async_diskcache(fn: AnyFn[P, T]) -> AnyFn[P, T]:
+        """
+        Decorator to optionally apply disk caching to asynchronous functions.
+
+        If `toolcache` is not installed, this decorator will return the function
+        as is, without any caching applied.
+
+        Args:
+            fn: The function to be decorated.
+
+        Examples:
+            Using the decorator with an asynchronous function:
+
+            >>> async def fetch_data():
+            ...     return "data"
+            >>> fetch_data = optional_async_diskcache(fetch_data)
+
+        See Also:
+            - :mod:`toolcache`
+        """
+        return fn
