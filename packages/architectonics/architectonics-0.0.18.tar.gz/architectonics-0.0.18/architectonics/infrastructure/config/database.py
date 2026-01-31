192829ad36ba619986051dbdc9ab6e866a85c790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 06:10:46 +0000

    Add package architectonics/0.0.18/architectonics-0.0.18.tar.gz

---
+++ packages/architectonics/architectonics-0.0.18.tar.gz/architectonics-0.0.18/architectonics/infrastructure/config/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from architectonics.infrastructure.config.database_settings import database_settings
+
+engine = create_async_engine(
+    database_settings.DATABASE_CONNECTION_STRING,
+    future=True,
+    echo=False,
+)
+
+session = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
