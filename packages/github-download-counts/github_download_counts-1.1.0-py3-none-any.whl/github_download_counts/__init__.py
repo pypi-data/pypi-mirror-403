66e7e9a511768a8812910a9b6223bc21f37383d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 17:09:07 +0000

    Add package github-download-counts/1.1.0/github_download_counts-1.1.0-py3-none-any.whl

---
+++ packages/github-download-counts/github_download_counts-1.1.0-py3-none-any.whl/github_download_counts/__init__.py
@@ -0,0 +1,21 @@
+"""a simple Python wrapper around the GitHub REST API to get download counts for release assets and ghcr.io image pulls for organizations' repositories"""
+
+from importlib.metadata import version, PackageNotFoundError
+
+_package_name = __name__
+
+try:
+    __version__ = version(_package_name)
+except PackageNotFoundError:
+    __version__ = None
+
+from .github_download_counts import *  # noqa: F401
+
+__all__ = sorted(
+    [
+        name
+        for name, obj in globals().items()
+        if not name.startswith("_") and getattr(obj, "__module__", _package_name).startswith(_package_name + '.')
+    ],
+    key=str.casefold,
+)
