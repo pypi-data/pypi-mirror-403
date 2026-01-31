3a9239c19150054af143c974d568cc6f205add1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 11:07:29 +0000

    Add package prefect/3.6.15/prefect-3.6.15.tar.gz

---
+++ packages/prefect/prefect-3.6.15.tar.gz/prefect-3.6.15/src/prefect/server/ui-v2/assets/header-BTAWyeXv.js
@@ -0,0 +1,2 @@
+import{j as e}from"./vendor-tanstack-BcZfOOfy.js";import{B as s,g as r,h as t}from"./index-m48E4KcG.js";const c=()=>e.jsx("div",{className:"flex items-center gap-2",children:e.jsx(s,{children:e.jsx(r,{children:e.jsx(t,{className:"text-xl font-semibold",children:"Deployments"})})})});export{c as D};
+//# sourceMappingURL=header-BTAWyeXv.js.map
