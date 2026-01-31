d6d7627c999b3b389097469b35edecadfd5db38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:31:56 +0000

    Add package vlf4ions/1.3.3b1/vlf4ions-1.3.3b1.tar.gz

---
+++ packages/vlf4ions/vlf4ions-1.3.3b1.tar.gz/vlf4ions-1.3.3b1/test/test_analyse_cp.py
@@ -0,0 +1,123 @@
+import vlf4ions.analyse_change_points as acp
+import numpy as np
+
+def test_analyse_bp():
+
+    # Common parameters to all tests
+    detection_threshold = 10
+    amp = 2
+    the_time = np.arange(0, 5, 1)
+    quiet_phase = 1
+    quiet_amp = 1
+    last_time = 2
+    quiet_p1 = 0
+    quiet_breakpoint = 2
+    quiet = 1
+
+    # Keep track of errors
+    errors = []
+
+    # Test 1 - beginning of the flare
+    current_time = the_time[-1]
+    current_p1 = 12
+    phase = np.array([1, 1, 1, 1, 2])
+    quiet_now, prev_timedecay, DP_now, DA = acp.analyse_breakpoint(current_time, current_p1, amp, phase, the_time, quiet, quiet_phase, quiet_amp, last_time, quiet_p1, detection_threshold, quiet_p1, quiet_breakpoint)
+    if quiet_now == 1 or DP_now != 1:
+        errors.append('Problem with breakpoint analysis for flare onset')
+
+    # Prepare for test 2
+    last_p1 = current_p1
+    quiet = quiet_now
+    last_time = 4
+    phase = np.array([1, 1, 1, 1, 2, 2])
+    the_time = np.arange(0, 6, 1)
+
+    # Test 2 - Flare maximum
+    current_p1 = 5
+    current_time = the_time[-1]
+    quiet_now, prev_timedecay, DP_now, DA = acp.analyse_breakpoint(current_time, current_p1, amp, phase, the_time, quiet, quiet_phase, quiet_amp, last_time, last_p1, detection_threshold, quiet_p1, quiet_breakpoint)
+    if quiet_now == 1 or DP_now != 1:
+        errors.append('Problem with breakpoint analysis for flare maximum')
+
+    # Prepare for test 3
+    last_p1 = current_p1
+    quiet = quiet_now
+    last_time = 5
+    phase = np.array([1, 1, 1, 1, 2, 2, 1.9, 1.9])
+    the_time = np.arange(0, 8, 1)
+
+    # Test 3 - Flare decay
+    current_p1 = -1
+    current_time = the_time[-1]
+    quiet_now, prev_timedecay, DP_now, DA = acp.analyse_breakpoint(current_time, current_p1, amp, phase, the_time, quiet, quiet_phase, quiet_amp, last_time, last_p1, detection_threshold, quiet_p1, quiet_breakpoint)
+    if quiet_now == 1:
+        errors.append('Problem with breakpoint analysis for flare decay: declared as quiet')
+    if prev_timedecay != 5.5:
+        errors.append('Problem with breakpoint analysis for flare decay: decay time')
+
+   # Prepare for test 4
+    last_p1 = current_p1
+    quiet = quiet_now
+    last_time = 7
+    phase = np.array([1, 1, 1, 1, 2, 2, 1.9, 1.9, 1.7, 1.7])
+    the_time = np.arange(0, 10, 1)
+
+    # Test 4 - Flare decay update
+    current_p1 = -0.5
+    current_time = the_time[-1]
+    quiet_now, prev_timedecay, DP_now, DA = acp.analyse_breakpoint(current_time, current_p1, amp, phase, the_time, quiet, quiet_phase, quiet_amp, last_time, last_p1, detection_threshold, quiet_p1, quiet_breakpoint)
+    if DP_now != 0.7:
+        errors.append('Problem with flare decay update: wrong DP')
+    if quiet_now == 1:
+        errors.append('Problem with breakpoint analysis for flare decay update: declared as quiet')
+    if prev_timedecay != 8.5:
+        print(prev_timedecay)
+        errors.append('Problem with breakpoint analysis for flare decay update: decay time')
+
+   # Prepare for test 5
+    last_p1 = current_p1
+    quiet = quiet_now
+    last_time = 9
+    phase = np.array([1, 1, 1, 1, 2, 2, 1.9, 1.9, 1.7, 1.7, 1.3, 1.3])
+    the_time = np.arange(0, 12, 1)
+
+    # Test 5 - Back to quiet times
+    current_p1 = 1
+    current_time = the_time[-1]
+    quiet_now, prev_timedecay, DP_now, DA = acp.analyse_breakpoint(current_time, current_p1, amp, phase, the_time, quiet, quiet_phase, quiet_amp, last_time, last_p1, detection_threshold, quiet_p1, quiet_breakpoint)
+    if DP_now != 0:
+        errors.append('Problem with bp analysis - back to quiet: DP is not zero')
+    if quiet_now == 0:
+        errors.append('Problem with bp analysis - does not go back to quiet ')
+
+    # Test 6 - Quiet after quiet
+    last_p1 = current_p1
+    quiet = quiet_now
+    last_time = 9
+    phase = np.array([1, 1, 1, 1, 2, 2, 1.9, 1.9, 1.7, 1.7, 1.3, 1.3, 1])
+    the_time = np.arange(0, 13, 1)
+    current_p1 = 0
+    current_time = the_time[-1]
+    quiet_now, prev_timedecay, DP_now, DA = acp.analyse_breakpoint(current_time, current_p1, amp, phase, the_time, quiet, quiet_phase, quiet_amp, last_time, last_p1, detection_threshold, quiet_p1, quiet_breakpoint)
+    if DP_now != 0:
+        errors.append('Problem with bp analysis - quiet after quiet, DP not zeros')
+    if quiet_now == 0:
+        errors.append('Problem with bp analysis - does not go stay quiet ')
+
+    # Test 7 - Quiet after quiet, with negative slope
+    last_p1 = current_p1
+    quiet = quiet_now
+    last_time = 9
+    phase = np.array([1, 1, 1, 1, 2, 2, 1.9, 1.9, 1.7, 1.7, 1.3, 1.3, 1, 1])
+    the_time = np.arange(0, 14, 1)
+    current_p1 = -1
+    current_time = the_time[-1]
+    quiet_now, prev_timedecay, DP_now, DA = acp.analyse_breakpoint(current_time, current_p1, amp, phase, the_time, quiet, quiet_phase, quiet_amp, last_time, last_p1, detection_threshold, quiet_p1, quiet_breakpoint)
+    if DP_now != 0:
+        errors.append('Problem with bp analysis - quiet after quiet, DP not zero if slope is negative')
+    if quiet_now == 0:
+        errors.append('Problem with bp analysis - does not go stay quiet if new slope is negative')
+
+
+    # assert no error message has been registered, else print messages
+    assert not errors, "errors occured:\n{}".format("\n".join(errors))
---
+++ packages/vlf4ions/vlf4ions-1.3.3b1.tar.gz/vlf4ions-1.3.3b1/test/test_sunrise_sunset.py
@@ -0,0 +1,47 @@
+import vlf4ions.sunrise_sunset as srst
+import vlf4ions as vlf
+import datetime as dt
+from datetime import datetime
+import os
+import pytz
+
+
+def test_sunrise_sunset():
+
+    dirname = os.path.dirname(__file__)
+    la_station = 'NWC'
+    today = dt.datetime(2025, 5, 13, 14, 28, 22, 323128, pytz.UTC)
+    path = os.path.join(dirname, la_station + '_sunrise_sunset.txt')
+
+    sunrise, sunset = srst.get_sunrise_sunset(today, path)
+
+    assert((sunrise == dt.time(3, 48)) and (sunset == dt.time(12, 47)))
+
+def test_check_if_daytime():
+
+    dirname = os.path.dirname(__file__)
+    la_station = 'NWC'
+    path = os.path.join(dirname, la_station + '_sunrise_sunset.txt')
+    today = dt.datetime(2025, 5, 13, 3, 47, 0, 0, pytz.UTC)
+
+    sunrise, sunset = srst.get_sunrise_sunset(today, path)
+
+    flag = 0 # List of flags, 0 if in daytime, 3 in nighttime
+
+    # Times to check
+    today = dt.datetime(2025, 5, 13, 3, 48, 0, 0, pytz.UTC)
+    if not srst.check_if_daytime(today, path):
+        flag += 3
+    today = dt.datetime(2025, 5, 13, 3, 49, 0, 0, pytz.UTC)
+    if not srst.check_if_daytime(today, path):
+        flag += 3
+    today = dt.datetime(2025, 5, 13, 12, 46, 0, 0, pytz.UTC)
+    if not srst.check_if_daytime(today, path):
+        flag += 3
+    today = dt.datetime(2025, 5, 13, 12, 47, 0, 0, pytz.UTC)
+    if not srst.check_if_daytime(today, path):
+        flag += 3
+
+    assert(flag == 6)
+
+# Not sure I need to test the get_sza function, since it only calls the get_altitude function from the pysolar library.
