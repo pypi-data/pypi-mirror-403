e77ce490da42434ebe0902e0f4feb0c694589203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:32:25 +0000

    Add package cell-imaging-utils/1.16.8/cell_imaging_utils-1.16.8-py3-none-any.whl

---
+++ packages/cell-imaging-utils/cell_imaging_utils-1.16.8-py3-none-any.whl/cell_imaging_utils/data_structures/singleton.py
@@ -0,0 +1,13 @@
+"""
+
+All data structures wwe are using.
+can take the form of classes of decorators
+
+"""
+def singleton(class_):
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
