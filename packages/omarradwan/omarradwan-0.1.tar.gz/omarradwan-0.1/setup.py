c8e8b2afbeafb9a0a2c05bd111c689dbfdf10eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 07:12:36 +0000

    Add package omarradwan/0.1/omarradwan-0.1.tar.gz

---
+++ packages/omarradwan/omarradwan-0.1.tar.gz/omarradwan-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name="omarradwan",
+    version="0.1",
+    description="simple library in python for test",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+
+)
