e74ae493b0ac31aa6e53c04a35f387e7b8268a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:45:36 +0000

    Add package MapleX/3.0.0.dev2/maplex-3.0.0.dev2.tar.gz

---
+++ packages/MapleX/maplex-3.0.0.dev2.tar.gz/maplex-3.0.0.dev2/readmes/README_Json.md
@@ -0,0 +1,10 @@
+# MapleJson Class
+
+&nbsp;&nbsp;&nbsp;&nbsp;MapleJson class is a class library to manage the JSON formatted files.
+
+- You can read a JSON file as a `dict` data.
+- You can write the `dict` data into a file as a JSON formatted string
+- You can save the data as an encrypted data string.
+- You can decrypt the encrypted data.
+
+## Class Initialization
---
+++ packages/MapleX/maplex-3.0.0.dev2.tar.gz/maplex-3.0.0.dev2/src/maplex/__init__.py
@@ -0,0 +1,52 @@
+"""
+MapleTree: A Python library for building and managing hierarchical data structures with ease.
+Logger: A simple logging utility for tracking events and debugging.
+"""
+
+from .mapleColors import ConsoleColors
+from .mapleJson import MapleJson
+from .mapleLogger import Logger, getLogger
+from .mapleExceptions import (
+    InvalidMapleFileFormatException,
+    KeyEmptyException,
+    MapleDataNotFoundException,
+    MapleException,
+    MapleEncryptionNotEnabledException,
+    MapleFileEmptyException,
+    MapleFileLockedException,
+    MapleFileNotFoundException,
+    MapleHeaderNotFoundException,
+    MapleSyntaxException,
+    MapleTagNotFoundException,
+    MapleTypeException,
+    NotAMapleFileException
+)
+from .mapleTreeEditor import MapleTree
+from .utils import winHide, winUnHide
+
+__all__ = [
+    'ConsoleColors',
+    'getLogger',
+    'InvalidMapleFileFormatException',
+    'KeyEmptyException',
+    'MapleDataNotFoundException',
+    'MapleEncryptionNotEnabledException',
+    'MapleException',
+    'MapleFileEmptyException',
+    'MapleFileLockedException',
+    'MapleFileNotFoundException',
+    'MapleHeaderNotFoundException',
+    'MapleJson',
+    'MapleSyntaxException',
+    'MapleTagNotFoundException',
+    'MapleTypeException',
+    'NotAMapleFileException',
+    'MapleTree',
+    'Logger',
+    'winHide',
+    'winUnHide'
+]
+
+__version__ = "3.0.0.dev2"
+__author__ = "Ryuji Hazama"
+__license__ = "MIT"
