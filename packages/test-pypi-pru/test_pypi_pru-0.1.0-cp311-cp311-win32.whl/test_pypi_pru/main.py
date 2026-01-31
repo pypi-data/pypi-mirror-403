d3d9a60e51d20942250186fc2a98b597568ccc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:07:21 +0000

    Add package test-pypi-pru/0.1.0/test_pypi_pru-0.1.0-cp311-cp311-win32.whl

---
+++ packages/test-pypi-pru/test_pypi_pru-0.1.0-cp311-cp311-win32.whl/test_pypi_pru/main.py
@@ -0,0 +1,11 @@
+"""Main module for the latuca package."""
+
+import numpy as np
+
+
+def my_function(x: int, y: int) -> int:
+    """
+    This function takes two integers and returns their sum.
+    """
+
+    return np.prod([x, y])
