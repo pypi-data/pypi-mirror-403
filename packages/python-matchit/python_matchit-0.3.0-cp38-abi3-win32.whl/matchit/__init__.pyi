d93b7c92becc50cc2620ffe40924901db395778e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:18:39 +0000

    Add package python-matchit/0.3.0/python_matchit-0.3.0-cp38-abi3-win32.whl

---
+++ packages/python-matchit/python_matchit-0.3.0-cp38-abi3-win32.whl/matchit/__init__.pyi
@@ -0,0 +1,24 @@
+from typing import Mapping, TypeVar, Generic
+
+T = TypeVar("T")
+
+
+class MatchResult(Generic[T]):
+    """Router match result."""
+    value: T
+    params: Mapping[str, str]
+
+class Router(Generic[T]):
+    """Router.
+
+    Holds url patterns for each route and allows to retrieve route matching provided path.
+
+    It is not safe to insert routes after application is started up.
+    """
+
+    def insert(self, url_pattern: str, value: T) -> None:
+        """Add route to the router.
+        """
+        ...
+
+    def at(self, path: str) -> MatchResult[T]: ...
