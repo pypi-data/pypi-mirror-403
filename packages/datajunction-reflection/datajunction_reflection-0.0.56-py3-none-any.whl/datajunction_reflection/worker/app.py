ff0e8806346d3219e2acff6eeb7419faffb0daa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 09:08:52 +0000

    Add package datajunction-reflection/0.0.56/datajunction_reflection-0.0.56-py3-none-any.whl

---
+++ packages/datajunction-reflection/datajunction_reflection-0.0.56-py3-none-any.whl/datajunction_reflection/worker/app.py
@@ -0,0 +1,8 @@
+"""
+Celery app that does the polling of nodes in DJ and then subsequent
+queueing of reflection tasks.
+"""
+
+from datajunction_reflection.worker.utils import get_celery
+
+celery_app = get_celery()
