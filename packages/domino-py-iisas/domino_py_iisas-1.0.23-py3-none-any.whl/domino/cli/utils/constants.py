370dc8a6119613e1e6dddaea7e1190e3bc1ed552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:32:14 +0000

    Add package domino-py-iisas/1.0.23/domino_py_iisas-1.0.23-py3-none-any.whl

---
+++ packages/domino-py-iisas/domino_py_iisas-1.0.23-py3-none-any.whl/domino/cli/utils/constants.py
@@ -0,0 +1,9 @@
+COLOR_PALETTE = {
+    "success": "rgb(113,176,109)",
+    "warning": "rgb(176,169,109)",
+    "error": "rgb(176,109,109)"
+}
+
+DOMINO_HELM_PATH = 'domino-iisas/domino'
+DOMINO_HELM_VERSION = '0.3.1'
+DOMINO_HELM_REPOSITORY = 'https://iisas.github.io/domino/'
