f7d94cea61b9c309da1e11f6ffaf49c569f4a45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 07:12:46 +0000

    Add package aa-campaign/1.0.2/aa_campaign-1.0.2-py3-none-any.whl

---
+++ packages/aa-campaign/aa_campaign-1.0.2-py3-none-any.whl/aacampaign/__init__.py
@@ -0,0 +1,15 @@
+"""Initialize the app"""
+from django.utils.translation import gettext_lazy as _
+
+__version__ = "1.0.2"
+__title__ = "Campaign"
+__title_translated__ = _("AA Campaigns")
+
+__esi_compatibility_date__ = "2025-11-06"
+
+__package_name__ = "aa-campaign"
+__app_name__ = "aa-campaign"
+__app_name_verbose__ = "Alliance Auth Campaigns"
+__app_name_useragent__ = "AA-Campaign"
+
+__github_url__ = f"https://github.com/BroodLK/aa-campaigns"
