bc77d8c4a3c506a89b4b90e3c5ca42b3dcbd48e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 19:10:05 +0000

    Add package robustrankaggregpy/0.1.0/robustrankaggregpy-0.1.0-py3-none-any.whl

---
+++ packages/robustrankaggregpy/robustrankaggregpy-0.1.0-py3-none-any.whl/robustrankaggregpy/__init__.py
@@ -0,0 +1,7 @@
+from importlib.metadata import version
+
+from robustrankaggregpy import aggregate_ranks
+
+__author__ = "Braden Griebel"
+__version__ = version("robustrankaggregpy")
+__all__ = ["aggregate_ranks"]
