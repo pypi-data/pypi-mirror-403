667b0ddd86a4f61b45b1aeadb57f48028790989c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:07:14 +0000

    Add package unigram/0.15.2/unigram-0.15.2.tar.gz

---
+++ packages/unigram/unigram-0.15.2.tar.gz/unigram-0.15.2/src/unigram/grammars/__init__.py
@@ -0,0 +1,58 @@
+from .FOL import FOL_grammar
+from .tinypy import tinypy_grammar
+from .english import simple_english_grammar
+from ..grammar import init_grammar
+import random
+import string
+
+def arith_grammar():
+    g = init_grammar(['py'], name="arithmetics", preprocess_template=lambda s:s)
+    g('start(expr)',        '{0}')
+    g('expr(expr)',       '({0})',            weight=1)
+    g('expr(expr,expr)',  '{0} + {1}', weight=2)
+    g('expr(expr,expr)',  '{0} - {1}', weight=1)
+    g('expr(expr,expr)',  '{0} * {1}')
+    g('expr(expr,expr)',  '{0} / {1}')
+    g('expr(expr)',       '({0})**2',         weight=.25)
+    g('expr(value)',       '{0}',weight= 10)
+    g('value',  'NUM')
+
+    return g
+
+
+def regex_grammar():
+
+    from faker import Faker
+    fake = Faker()
+    wordlist = fake.words(nb=100,unique=True)
+    
+    R = init_grammar(['re'], preprocess_template=lambda x: x)
+
+    R('start(regex)', '{0}')
+    R('regex(regex,regex)', '{0}{1}', weight=2)
+    R('regex(regex)', '({0})', weight=2)
+    R('regex(regex,regex)', '{0}|{1}', weight=2)
+    R('regex(char)', '{0}',weight=1)
+    R('regex(word)', '{0}',weight=3)
+
+    for w in random.sample(wordlist, 8):
+        R('word', w)
+
+    R('regex(regex)?', '{0}?')
+    R('regex(regex)*', '{0}*')
+    R('regex(regex)+', '{0}+')
+    R('regex(rangechar,rangechar)', '[{0}-{1}]')
+    R('regex(predef)', '{0}',weight=3)
+
+    chars = string.ascii_letters + string.digits
+    for c in chars:
+        R('char', c)
+        R('rangechar', c)
+
+    for s in [r'\d', r'\w', r'\s', '.', r'\.']:
+        R('predef', s, weight=1)
+
+    for s in [r'\D', r'\W', r'\S', r'\\', r'\(', r'\)', r'\[', r'\]', r'\t', r'\n']:
+        R('predef', s, weight=0.25)
+
+    return R
