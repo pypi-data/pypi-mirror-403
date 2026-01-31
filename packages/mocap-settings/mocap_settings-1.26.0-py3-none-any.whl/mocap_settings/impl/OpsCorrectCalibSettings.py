f160e4c1a7a27de4ac5082ce23d271cd62968082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 17:09:07 +0000

    Add package mocap-settings/1.26.0/mocap_settings-1.26.0-py3-none-any.whl

---
+++ packages/mocap-settings/mocap_settings-1.26.0-py3-none-any.whl/mocap_settings/impl/OpsCorrectCalibSettings.py
@@ -0,0 +1,18 @@
+import os as _os
+import sys as _sys
+_axeSearchPath = _os.path.dirname(_os.path.abspath(__file__))
+while not _os.path.exists(_os.path.join(_axeSearchPath,
+    'axengine_1769785883_1408080803.py')):
+    _newAxeSearchPath = _os.path.normpath(_os.path.join(_axeSearchPath, '..'))
+    if _newAxeSearchPath == _axeSearchPath or len(_axeSearchPath) == 0:
+        break
+    _axeSearchPath = _newAxeSearchPath
+try:
+    _sys.path.append(_axeSearchPath)
+    import axengine_1769785883_1408080803 as _axengine
+except Exception as e:
+    raise Exception('Failed to load AxEngine!') from e
+finally:
+    _sys.path.pop()
+from axengine_1769785883_1408080803 import WupiError, WupiLicenseError, WupiErrorCode
+_axengine._axe_run(62, globals())
