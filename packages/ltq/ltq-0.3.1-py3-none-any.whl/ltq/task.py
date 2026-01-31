997f67835ac626e607449e732328099f7698cf6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:32:52 +0000

    Add package ltq/0.3.1/ltq-0.3.1-py3-none-any.whl

---
+++ packages/ltq/ltq-0.3.1-py3-none-any.whl/ltq/task.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import Awaitable, Callable, Generic, ParamSpec, TypeVar
+
+from .message import Message
+from .q import Queue
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+class Task(Generic[P, R]):
+    def __init__(
+        self,
+        name: str,
+        fn: Callable[P, Awaitable[R]],
+        queue: Queue,
+        ttl: int | None = None,
+    ) -> None:
+        self.name = name
+        self.fn = fn
+        self.queue = queue
+        self.ttl = ttl
+
+    def message(self, *args: P.args, **kwargs: P.kwargs) -> Message:
+        return Message(
+            args=args,
+            kwargs=kwargs,
+            task=self,
+            task_name=self.name,
+        )
+
+    async def send(self, *args: P.args, **kwargs: P.kwargs) -> str:
+        message = self.message(*args, **kwargs)
+        await self.queue.put([message], ttl=self.ttl)
+        return message.id
+
+    async def __call__(self, *args: P.args, **kwargs: P.kwargs) -> R:
+        return await self.fn(*args, **kwargs)
