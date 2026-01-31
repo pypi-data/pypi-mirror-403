da050bb97c6d7be63f76da39d67b4f5c442dffa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:31:53 +0000

    Add package crc-pulp-file-client/20260129.1/crc_pulp_file_client-20260129.1-py3-none-any.whl

---
+++ packages/crc-pulp-file-client/crc_pulp_file_client-20260129.1-py3-none-any.whl/pulpcore/client/pulp_file/api/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_file.api.acs_file_api import AcsFileApi
+from pulpcore.client.pulp_file.api.content_files_api import ContentFilesApi
+from pulpcore.client.pulp_file.api.distributions_file_api import DistributionsFileApi
+from pulpcore.client.pulp_file.api.publications_file_api import PublicationsFileApi
+from pulpcore.client.pulp_file.api.remotes_file_api import RemotesFileApi
+from pulpcore.client.pulp_file.api.repositories_file_api import RepositoriesFileApi
+from pulpcore.client.pulp_file.api.repositories_file_versions_api import RepositoriesFileVersionsApi
+
