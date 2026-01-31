f1a6014ed1bb929f41db39caa3b7ff019077d97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 20:06:28 +0000

    Add package dagster-gcp-pyspark/0.28.13rc0/dagster_gcp_pyspark-0.28.13rc0.tar.gz

---
+++ packages/dagster-gcp-pyspark/dagster_gcp_pyspark-0.28.13rc0.tar.gz/dagster_gcp_pyspark-0.28.13rc0/setup.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+def get_version() -> str:
+    version: dict[str, str] = {}
+    with open(Path(__file__).parent / "dagster_gcp_pyspark/version.py", encoding="utf8") as fp:
+        exec(fp.read(), version)
+
+    return version["__version__"]
+
+
+ver = get_version()
+# dont pin dev installs to avoid pip dep resolver issues
+pin = "" if ver == "1!0+dev" else f"=={ver}"
+setup(
+    name="dagster-gcp-pyspark",
+    version=get_version(),
+    author="Dagster Labs",
+    author_email="hello@dagsterlabs.com",
+    license="Apache-2.0",
+    description="Package for storing PySpark DataFrames in GCP",
+    url=(
+        "https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/"
+        "dagster-gcp-pyspark"
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=["dagster_gcp_pyspark_tests*"]),
+    include_package_data=True,
+    python_requires=">=3.10,<3.15",
+    install_requires=[
+        "dagster==1.12.13rc0",
+        "dagster-gcp==0.28.13rc0",
+        "pyspark>=4.1; python_version>='3.14'",
+        "pyspark>=3.5,<4; python_version<'3.14'",
+    ],
+    extras_require={"test": ["pandas-gbq"]},
+    zip_safe=False,
+)
