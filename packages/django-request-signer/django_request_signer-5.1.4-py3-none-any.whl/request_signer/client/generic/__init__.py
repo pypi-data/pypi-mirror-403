b6f9cd5c217f4e815bce89fb890467fa96edad60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:06:42 +0000

    Add package django-request-signer/5.1.4/django_request_signer-5.1.4-py3-none-any.whl

---
+++ packages/django-request-signer/django_request_signer-5.1.4-py3-none-any.whl/request_signer/client/generic/__init__.py
@@ -0,0 +1,5 @@
+from generic_request_signer.client import Client  # noqa F401
+from generic_request_signer.exceptions import WebException, HttpMethodNotAllowed  # noqa F401
+from generic_request_signer.request import Request  # noqa F401
+from generic_request_signer.response import Response  # noqa F401
+from generic_request_signer.factory import SignedRequestFactory  # noqa F401
