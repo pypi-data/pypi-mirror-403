1c4226b6e1bbe3524460dd886269a7a1aa5d983c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:07:01 +0000

    Add package scipion-em-membrain/3.1.2/scipion_em_membrain-3.1.2-py3-none-any.whl

---
+++ packages/scipion-em-membrain/scipion_em_membrain-3.1.2-py3-none-any.whl/membrain/protocols/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# Module to declare protocols
+# Find documentation here: https://scipion-em.github.io/docs/docs/developer/creating-a-protocol
+# **************************************************************************
+from .protocol_membrain_seg import ProtMemBrainSeg
+from .protocol_membrain_skeletonize import ProtMemBrainSkeletonize
+
+
