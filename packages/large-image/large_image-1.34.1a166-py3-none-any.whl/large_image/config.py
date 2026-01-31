f03b533e3d163e7edcfee98d1d67fcdfb86226c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:07:36 +0000

    Add package large-image/1.34.1a166/large_image-1.34.1a166-py3-none-any.whl

---
+++ packages/large-image/large_image-1.34.1a166-py3-none-any.whl/large_image/config.py
@@ -0,0 +1,211 @@
+import contextlib
+import functools
+import json
+import logging
+import os
+import pathlib
+import re
+from typing import Any, cast
+
+from . import exceptions
+
+try:
+    import psutil
+    HAS_PSUTIL = True
+except ImportError:
+    HAS_PSUTIL = False
+
+# Default logger
+fallbackLogger = logging.getLogger('large_image')
+fallbackLogger.setLevel(logging.INFO)
+fallbackLogHandler = logging.NullHandler()
+fallbackLogHandler.setLevel(logging.NOTSET)
+fallbackLogger.addHandler(fallbackLogHandler)
+
+
+def _in_notebook() -> bool:
+    """
+    Try to detect if we are in an interactive notebook.
+
+    :returns: True if we think we are in an interactive notebook.
+    """
+    try:
+        return get_ipython() is not None  # type: ignore[name-defined]
+    except NameError:
+        return False
+
+
+ConfigValues = {
+    'logger': fallbackLogger,
+    'logprint': fallbackLogger,
+
+    # Encoding and Projection
+    'default_encoding': 'PNG' if _in_notebook() else 'JPEG',
+    'default_projection': 'EPSG:3857' if _in_notebook() else None,
+
+    # For tiles
+    'cache_backend': None,  # 'python', 'redis' or 'memcached'
+    # 'python' cache can use 1/(val) of the available memory
+    'cache_python_memory_portion': 32,
+    # cache_memcached_url may be a list
+    'cache_memcached_url': '127.0.0.1',
+    'cache_memcached_username': None,
+    'cache_memcached_password': None,
+    'cache_redis_url': '127.0.0.1:6379',
+    'cache_redis_password': None,
+
+    # If set to False, the default will be to not cache tile sources.  This has
+    # substantial performance penalties if sources are used multiple times, so
+    # should only be set in singular dynamic environments such as experimental
+    # notebooks.
+    'cache_sources': not _in_notebook(),
+
+    # Generally, these keys are the form of "cache_<cacheName>_<key>"
+
+    # For tilesources.  These are also limited by available file handles.
+    # 'python' cache can use 1/(val) of the available memory based on a very
+    # rough estimate of the amount of memory used by a tilesource
+    'cache_tilesource_memory_portion': 16,
+    # If >0, this is the maximum number of tilesources that will be cached
+    'cache_tilesource_maximum': 0,
+
+    'max_small_image_size': 4096,
+
+    # Should ICC color correction be applied by default
+    'icc_correction': True,
+
+    # The maximum size of an annotation file that will be ingested into girder
+    # via direct load
+    'max_annotation_input_file_length': 1 * 1024 ** 3 if not HAS_PSUTIL else max(
+        1 * 1024 ** 3, psutil.virtual_memory().total // 16),
+
+    # Any path that matches here will only be opened by a source that matches
+    # extension or mime type.
+    'all_sources_ignored_names': r'(\.mrxs|\.vsi)$',
+}
+
+
+# We use lru_cache over cache because it can be cleared
+@functools.lru_cache(maxsize=1000)
+def getConfig(key: str | None = None,
+              default: str | bool | int | logging.Logger | None = None) -> Any:
+    """
+    Get the config dictionary or a value from the cache config settings.
+
+    :param key: if None, return the config dictionary.  Otherwise, return the
+        value of the key if it is set or the default value if it is not.
+    :param default: a value to return if a key is requested and not set.
+    :returns: either the config dictionary or the value of a key.
+    """
+    if key is None:
+        return ConfigValues
+    envKey = f'LARGE_IMAGE_{key.replace(".", "_").upper()}'
+    if envKey in os.environ:
+        value = os.environ[envKey]
+        if value == '__default__':
+            return default
+        with contextlib.suppress(ValueError):
+            value = json.loads(value)
+        return value
+    return ConfigValues.get(key, default)
+
+
+def getLogger(key: str | None = None,
+              default: logging.Logger | None = None) -> logging.Logger:
+    """
+    Get a logger from the config.  Ensure that it is a valid logger.
+
+    :param key: if None, return the 'logger'.
+    :param default: a value to return if a key is requested and not set.
+    :returns: a logger.
+    """
+    logger = cast(logging.Logger, getConfig(key or 'logger', default))
+    if not isinstance(logger, logging.Logger):
+        logger = fallbackLogger
+    return logger
+
+
+def setConfig(key: str, value: str | bool | int | logging.Logger | None) -> None:
+    """
+    Set a value in the config settings.
+
+    :param key: the key to set.
+    :param value: the value to store in the key.
+    """
+    curConfig = getConfig()
+    if curConfig.get(key) is not value:
+        curConfig[key] = value
+        getConfig.cache_clear()
+
+
+def _ignoreSourceNames(
+        configKey: str, path: str | pathlib.Path, default: str | None = None) -> None:
+    """
+    Given a path, if it is an actual file and there is a setting
+    "source_<configKey>_ignored_names", raise a TileSourceError if the path
+    matches the ignore names setting regex in a case-insensitive search.
+
+    :param configKey: key to use to fetch value from settings.
+    :param path: the file path to check.
+    :param default: a default ignore regex, or None for no default.
+    """
+    ignored_names = getConfig('source_%s_ignored_names' % configKey) or default
+    if not ignored_names or not os.path.isfile(path):
+        return
+    if re.search(ignored_names, os.path.basename(path), flags=re.IGNORECASE):
+        raise exceptions.TileSourceError('File will not be opened by %s reader' % configKey)
+
+
+def cpu_count(logical: bool = True) -> int:
+    """
+    Get the usable CPU count.  If psutil is available, it is used, since it can
+    determine the number of physical CPUS versus logical CPUs.  This returns
+    the smaller of that value from psutil and the number of cpus allowed by the
+    os scheduler, which means that for physical requests (logical=False), the
+    returned value may be more the the number of physical cpus that are usable.
+
+    :param logical: True to get the logical usable CPUs (which include
+        hyperthreading).  False for the physical usable CPUs.
+    :returns: the number of usable CPUs.
+    """
+    count = os.cpu_count() or 2
+    with contextlib.suppress(AttributeError):
+        count = min(count, len(os.sched_getaffinity(0)))
+    if HAS_PSUTIL:
+        count = min(count, psutil.cpu_count(logical) or count)
+    return max(1, count)
+
+
+def total_memory() -> int:
+    """
+    Get the total memory in the system.  If this is in a container, try to
+    determine the memory available to the cgroup.
+
+    :returns: the available memory in bytes, or 8 GB if unknown.
+    """
+    mem = 0
+    if HAS_PSUTIL:
+        mem = psutil.virtual_memory().total
+    try:
+        cgroup = int(open('/sys/fs/cgroup/memory/memory.limit_in_bytes').read().strip())
+        if 1024 ** 3 <= cgroup < 1024 ** 4 and (mem is None or cgroup < mem):
+            mem = cgroup
+    except Exception:
+        pass
+    if mem:
+        return mem
+    return 8 * 1024 ** 3
+
+
+def minimizeCaching(mode=None):
+    """
+    Set python cache sizes to very low values.
+
+    :param mode: None for all caching, 'tile' for the tile cache, 'source' for
+        the source cache.
+    """
+    if not mode or str(mode).lower().startswith('source'):
+        setConfig('cache_tilesource_maximum', 1)
+    if not mode or str(mode).lower().startswith('tile'):
+        setConfig('cache_backend', 'python')
+        setConfig('cache_python_memory_portion', 256)
---
+++ packages/large-image/large_image-1.34.1a166-py3-none-any.whl/large_image/tilesource/tiledict.py
@@ -0,0 +1,284 @@
+from typing import Any, cast
+
+import numpy as np
+import PIL
+import PIL.Image
+import PIL.ImageColor
+import PIL.ImageDraw
+
+from .. import exceptions
+from ..constants import TILE_FORMAT_IMAGE, TILE_FORMAT_NUMPY, TILE_FORMAT_PIL
+from .utilities import ImageBytes, _encodeImage, _imageToNumpy, _imageToPIL
+
+
+class LazyTileDict(dict):
+    """
+    Tiles returned from the tile iterator and dictionaries of information with
+    actual image data in the 'tile' key and the format in the 'format' key.
+    Since some applications need information about the tile but don't need the
+    image data, these two values are lazily computed.  The LazyTileDict can be
+    treated like a regular dictionary, except that when either of those two
+    keys are first accessed, they will cause the image to be loaded and
+    possibly converted to a PIL image and cropped.
+
+    Unless setFormat is called on the tile, tile images may always be returned
+    as PIL images.
+    """
+
+    def __init__(self, tileInfo: dict[str, Any], *args, **kwargs) -> None:
+        """
+        Create a LazyTileDict dictionary where there is enough information to
+        load the tile image.  ang and kwargs are as for the dict() class.
+
+        :param tileInfo: a dictionary of x, y, level, format, encoding, crop,
+            and source, used for fetching the tile image.
+        """
+        self.x = tileInfo['x']
+        self.y = tileInfo['y']
+        self.frame = tileInfo.get('frame')
+        self.level = tileInfo['level']
+        self.format = tileInfo['format']
+        self.encoding = tileInfo['encoding']
+        self.crop = tileInfo['crop']
+        self.source = tileInfo['source']
+        self.resample = tileInfo.get('resample', False)
+        self.requestedScale = tileInfo.get('requestedScale')
+        self.metadata = cast(dict[str, Any], tileInfo.get('metadata'))
+        self.retile = tileInfo.get('retile') and self.metadata
+
+        self.deferredKeys = ('tile', 'format')
+        self.alwaysAllowPIL = True
+        self.imageKwargs: dict[str, Any] = {}
+        self.loaded = False
+        super().__init__(*args, **kwargs)
+        # We set this initially so that they are listed in known keys using the
+        # native dictionary methods
+        self['tile'] = None
+        self['format'] = None
+        self.width = self['width']
+        self.height = self['height']
+
+    def setFormat(
+            self, format: tuple[str, ...], resample: bool = False,
+            imageKwargs: dict[str, Any] | None = None) -> None:
+        """
+        Set a more restrictive output format for a tile, possibly also resizing
+        it via resampling.  If this is not called, the tile may either be
+        returned as one of the specified formats or as a PIL image.
+
+        :param format: a tuple or list of allowed formats.  Formats are members
+            of TILE_FORMAT_*.  This will avoid converting images if they are
+            in the desired output encoding (regardless of subparameters).
+        :param resample: if not False or None, allow resampling.  Once turned
+            on, this cannot be turned off on the tile.
+        :param imageKwargs: additional parameters that should be passed to
+            _encodeImage.
+        """
+        # If any parameters are changed, mark the tile as not loaded, so that
+        # referring to a deferredKey will reload the image.
+        self.alwaysAllowPIL = False
+        if format is not None and format != self.format:
+            self.format = format
+            self.loaded = False
+        if (resample not in (False, None) and not self.resample and
+                self.requestedScale and round(self.requestedScale, 2) != 1.0):
+            self.resample = resample
+            self['scaled'] = 1.0 / self.requestedScale
+            self['tile_x'] = self.get('tile_x', self['x'])
+            self['tile_y'] = self.get('tile_y', self['y'])
+            self['tile_width'] = self.get('tile_width', self.width)
+            self['tile_height'] = self.get('tile_height', self.height)
+            if self.get('magnification', None):
+                self['tile_magnification'] = self.get('tile_magnification', self['magnification'])
+            self['tile_mm_x'] = self.get('mm_x')
+            self['tile_mm_y'] = self.get('mm_y')
+            self['x'] = float(self['tile_x'])
+            self['y'] = float(self['tile_y'])
+            # Add provisional width and height
+            if self.resample not in (False, None) and self.requestedScale:
+                self['width'] = max(1, int(
+                    self['tile_width'] / self.requestedScale))
+                self['height'] = max(1, int(
+                    self['tile_height'] / self.requestedScale))
+                if self.get('tile_magnification', None):
+                    self['magnification'] = self['tile_magnification'] / self.requestedScale
+                if self.get('tile_mm_x', None):
+                    self['mm_x'] = self['tile_mm_x'] * self.requestedScale
+                if self.get('tile_mm_y', None):
+                    self['mm_y'] = self['tile_mm_y'] * self.requestedScale
+            # If we can resample the tile, many parameters may change once the
+            # image is loaded.  Don't include width and height in this list;
+            # the provisional values are sufficient.
+            self.deferredKeys = ('tile', 'format')
+            self.loaded = False
+        if imageKwargs is not None:
+            self.imageKwargs = imageKwargs
+            self.loaded = False
+
+    def _retileTile(self) -> np.ndarray:
+        """
+        Given the tile information, create a numpy array and merge multiple
+        tiles together to form a tile of a different size.
+        """
+        tileWidth = self.metadata['tileWidth']
+        tileHeight = self.metadata['tileHeight']
+        level = self.level
+        frame = self.frame
+        width = self.width
+        height = self.height
+        tx = self['x']
+        ty = self['y']
+
+        retile = None
+        xmin = int(max(0, tx // tileWidth))
+        xmax = int((tx + width - 1) // tileWidth + 1)
+        ymin = int(max(0, ty // tileHeight))
+        ymax = int((ty + height - 1) // tileHeight + 1)
+        for y in range(ymin, ymax):
+            for x in range(xmin, xmax):
+                tileData = self.source.getTile(
+                    x, y, level,
+                    numpyAllowed='always', sparseFallback=True, frame=frame)
+                if not isinstance(tileData, np.ndarray) or len(tileData.shape) != 3:
+                    tileData, _ = _imageToNumpy(tileData)
+                x0 = int(x * tileWidth - tx)
+                y0 = int(y * tileHeight - ty)
+                if x0 < 0:
+                    tileData = tileData[:, -x0:]
+                    x0 = 0
+                if y0 < 0:
+                    tileData = tileData[-y0:, :]
+                    y0 = 0
+                tw = min(tileData.shape[1], width - x0)
+                th = min(tileData.shape[0], height - y0)
+                if retile is None:
+                    retile = np.empty((height, width, tileData.shape[2]), dtype=tileData.dtype)
+                elif tileData.shape[2] < retile.shape[2]:
+                    retile = retile[:, :, :tileData.shape[2]]
+                retile[y0:y0 + th, x0:x0 + tw] = tileData[
+                    :th, :tw, :retile.shape[2]]
+        return cast(np.ndarray, retile)
+
+    def _resample(self, tileData: ImageBytes | PIL.Image.Image | bytes | np.ndarray) -> tuple[
+        ImageBytes | PIL.Image.Image | bytes | np.ndarray, PIL.Image.Image | None,
+    ]:
+        """
+        If we need to resample a tile, use PIL if it is uint8 or we are using
+        a specific resampling mode that is PIL-specific.  Otherwise, use
+        skimage if available.
+
+        :param tileData: the image to scale.
+        :returns: tileData, pilData.  pilData will be None if the results are a
+            numpy array.
+        """
+        pilData = None
+        if self.resample in (False, None) or not self.requestedScale:
+            return tileData, pilData
+        pilResize = True
+        if (isinstance(tileData, np.ndarray) and tileData.dtype != np.uint8 and
+                TILE_FORMAT_NUMPY in self.format and self.resample in {True, 2, 3}):
+            try:
+                import skimage.transform
+                pilResize = False
+            except ImportError:
+                pass
+        if pilResize:
+            pilData = _imageToPIL(tileData)
+
+            self['width'] = max(1, int(
+                pilData.size[0] / self.requestedScale))
+            self['height'] = max(1, int(
+                pilData.size[1] / self.requestedScale))
+            pilData = tileData = pilData.resize(
+                (self['width'], self['height']),
+                resample=getattr(PIL.Image, 'Resampling', PIL.Image).LANCZOS
+                if self.resample is True else self.resample)
+        else:
+            tileData = skimage.transform.resize(
+                cast(np.ndarray, tileData),
+                (self['width'], self['height'],
+                 cast(np.ndarray, tileData).shape[2]),
+                order=3 if self.resample is True else self.resample)
+        return tileData, pilData
+
+    def __getitem__(self, key: str, *args, **kwargs) -> Any:
+        """
+        If this is the first time either the tile or format key is requested,
+        load the tile image data.  Otherwise, just return the internal
+        dictionary result.
+
+        See the base dict class for function details.
+        """
+        if not self.loaded and key in self.deferredKeys:
+            # Flag this immediately to avoid recursion if we refer to the
+            # tile's own values.
+            self.loaded = True
+
+            if not self.retile:
+                tileData = self.source.getTile(
+                    self.x, self.y, self.level,
+                    pilImageAllowed=True,
+                    numpyAllowed='always' if TILE_FORMAT_NUMPY in self.format else True,
+                    sparseFallback=True, frame=self.frame)
+                if self.crop:
+                    tileData, _ = _imageToNumpy(tileData)
+                    tileData = tileData[self.crop[1]:self.crop[3], self.crop[0]:self.crop[2]]
+            else:
+                tileData = self._retileTile()
+
+            pilData = None
+            # resample if needed
+            if self.resample not in (False, None) and self.requestedScale:
+                tileData, pilData = self._resample(tileData)
+
+            tileFormat = (TILE_FORMAT_PIL if isinstance(tileData, PIL.Image.Image)
+                          else (TILE_FORMAT_NUMPY if isinstance(tileData, np.ndarray)
+                                else TILE_FORMAT_IMAGE))
+            tileEncoding = None if tileFormat != TILE_FORMAT_IMAGE else (
+                'JPEG' if tileData[:3] == b'\xff\xd8\xff' else
+                'PNG' if tileData[:4] == b'\x89PNG' else
+                'TIFF' if tileData[:4] == b'II\x2a\x00' else
+                None)
+            # Reformat the image if required
+            if (not self.alwaysAllowPIL or
+                    (TILE_FORMAT_NUMPY in self.format and isinstance(tileData, np.ndarray))):
+                if (tileFormat in self.format and (tileFormat != TILE_FORMAT_IMAGE or (
+                        tileEncoding and
+                        tileEncoding == self.imageKwargs.get('encoding', self.encoding)))):
+                    # already in an acceptable format
+                    pass
+                elif TILE_FORMAT_NUMPY in self.format:
+                    tileData, _ = _imageToNumpy(tileData)
+                    tileFormat = TILE_FORMAT_NUMPY
+                elif TILE_FORMAT_PIL in self.format:
+                    tileData = pilData if pilData is not None else _imageToPIL(tileData)
+                    tileFormat = TILE_FORMAT_PIL
+                elif TILE_FORMAT_IMAGE in self.format:
+                    tileData, _ = _encodeImage(tileData, **self.imageKwargs)
+                    tileFormat = TILE_FORMAT_IMAGE
+                if tileFormat not in self.format:
+                    raise exceptions.TileSourceError(
+                        'Cannot yield tiles in desired format %r' % (
+                            self.format, ))
+            elif (TILE_FORMAT_PIL not in self.format and TILE_FORMAT_NUMPY in self.format and
+                    not isinstance(tileData, PIL.Image.Image)):
+                tileData, _ = _imageToNumpy(tileData)
+                tileFormat = TILE_FORMAT_NUMPY
+            else:
+                tileData = pilData if pilData is not None else _imageToPIL(tileData)
+                tileFormat = TILE_FORMAT_PIL
+
+            self['tile'] = tileData
+            self['format'] = tileFormat
+        return super().__getitem__(key, *args, **kwargs)
+
+    def release(self) -> None:
+        """
+        If the tile has been loaded, unload it.  It can be loaded again.  This
+        is useful if you want to keep tiles available in memory but not their
+        actual tile data.
+        """
+        if self.loaded:
+            self.loaded = False
+            for key in self.deferredKeys:
+                self[key] = None
