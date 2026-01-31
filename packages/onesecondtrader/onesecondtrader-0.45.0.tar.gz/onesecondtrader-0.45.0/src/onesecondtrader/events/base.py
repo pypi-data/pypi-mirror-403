677ef3d88e9176f708ba7b0b64aa06ff6284e498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 19:10:34 +0000

    Add package onesecondtrader/0.45.0/onesecondtrader-0.45.0.tar.gz

---
+++ packages/onesecondtrader/onesecondtrader-0.45.0.tar.gz/onesecondtrader-0.45.0/src/onesecondtrader/events/base.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import dataclasses
+import time
+
+
+@dataclasses.dataclass(kw_only=True, frozen=True, slots=True)
+class EventBase:
+    """
+    Base class for immutable event message objects, using Unix epoch nanoseconds.
+
+    | Field         | Type | Semantics                                                              |
+    |---------------|------|------------------------------------------------------------------------|
+    | ts_event_ns   | int  | Time at which the represented fact occurred, as UTC epoch nanoseconds. |
+    | ts_created_ns | int  | Time at which the event object was created, as UTC epoch nanoseconds.  |
+    """
+
+    ts_event_ns: int
+    ts_created_ns: int = dataclasses.field(default_factory=time.time_ns)
