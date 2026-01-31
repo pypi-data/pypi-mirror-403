41c34e648452daff703b998a6cb03b232ed92b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:07:00 +0000

    Add package testit-adapter-nose/3.11.1/testit_adapter_nose-3.11.1.tar.gz

---
+++ packages/testit-adapter-nose/testit_adapter_nose-3.11.1.tar.gz/testit_adapter_nose-3.11.1/src/testit_adapter_nose/plugin.py
@@ -0,0 +1,43 @@
+from nose2.events import Plugin
+
+from testit_python_commons.services import TmsPluginManager
+
+from .listener import AdapterListener
+
+
+class TmsPlugin(Plugin):
+    configSection = 'testit'
+    commandLineSwitch = (None, 'testit', 'TMS adapter for Nose')
+    __listener = None
+    __tests_for_launch = None
+    __top_level_directory = None
+
+    def __init__(self, *args, **kwargs):
+        super(TmsPlugin, self).__init__(*args, **kwargs)
+
+    def handleDir(self, event):
+        if not self.__top_level_directory:
+            self.__top_level_directory = event.topLevelDirectory
+
+    def startTestRun(self, event):
+        self.__listener = AdapterListener(
+            TmsPluginManager.get_adapter_manager(),
+            TmsPluginManager.get_step_manager(),
+            self.__top_level_directory)
+
+        TmsPluginManager.get_plugin_manager().register(self.__listener)
+
+        self.__listener.start_launch()
+        self.__tests_for_launch = self.__listener.get_tests_for_launch()
+
+    def afterTestRun(self, event):
+        self.__listener.stop_launch()
+
+    def startTest(self, event):
+        self.__listener.start_test(event.test)
+
+    def stopTest(self, event):
+        self.__listener.stop_test()
+
+    def testOutcome(self, event):
+        self.__listener.set_outcome(event)
