7cd1b2931d7cadf648058c38d579d1bff64e084a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:08:04 +0000

    Add package large-image-source-dicom/1.34.1.dev2/large_image_source_dicom-1.34.1.dev2-py3-none-any.whl

---
+++ packages/large-image-source-dicom/large_image_source_dicom-1.34.1.dev2-py3-none-any.whl/large_image_source_dicom/web_client/views/EditAssetstoreWidget.js
@@ -0,0 +1,45 @@
+import EditAssetstoreWidget from '@girder/core/views/widgets/EditAssetstoreWidget';
+import { AssetstoreType } from '@girder/core/constants';
+import { wrap } from '@girder/core/utilities/PluginUtils';
+
+import DWASEditFieldsTemplate from '../templates/dicomwebAssetstoreEditFields.pug';
+
+import authOptions from './AuthOptions';
+
+/**
+ * Adds DICOMweb-specific fields to the edit dialog.
+ */
+wrap(EditAssetstoreWidget, 'render', function (render) {
+    render.call(this);
+
+    if (this.model.get('type') === AssetstoreType.DICOMWEB) {
+        this.$('.g-assetstore-form-fields').append(
+            DWASEditFieldsTemplate({
+                assetstore: this.model,
+                authOptions
+            })
+        );
+    }
+    return this;
+});
+
+EditAssetstoreWidget.prototype.fieldsMap[AssetstoreType.DICOMWEB] = {
+    get: function () {
+        return {
+            url: this.$('#g-edit-dwas-url').val(),
+            qido_prefix: this.$('#g-edit-dwas-qido-prefix').val(),
+            wado_prefix: this.$('#g-edit-dwas-wado-prefix').val(),
+            auth_type: this.$('#g-edit-dwas-auth-type').val(),
+            auth_token: this.$('#g-edit-dwas-auth-token').val()
+        };
+    },
+    set: function () {
+        const dwInfo = this.model.get('dicomweb_meta');
+        this.$('#g-edit-dwas-url').val(dwInfo.url);
+        this.$('#g-edit-dwas-qido-prefix').val(dwInfo.qido_prefix);
+        this.$('#g-edit-dwas-wado-prefix').val(dwInfo.wado_prefix);
+        // HTML can't accept null, so set it to an empty string
+        this.$('#g-edit-dwas-auth-type').val(dwInfo.auth_type || '');
+        this.$('#g-edit-dwas-auth-token').val(dwInfo.auth_token);
+    }
+};
