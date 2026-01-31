33be4fe573f2ff98e4518dac0b8eb47d792cf365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 06:11:24 +0000

    Add package girder-large-image/1.34.0/girder_large_image-1.34.0-py3-none-any.whl

---
+++ packages/girder-large-image/girder_large_image-1.34.0-py3-none-any.whl/girder_large_image/girder_tilesource.py
@@ -0,0 +1,203 @@
+import contextlib
+import inspect
+import os
+import re
+
+from girder.constants import AccessType
+from girder.exceptions import FilePathException, ValidationException
+from girder.models.file import File
+from girder.models.item import Item
+from large_image import config, tilesource
+from large_image.constants import SourcePriority
+from large_image.exceptions import TileSourceAssetstoreError, TileSourceError
+
+AvailableGirderTileSources = {}
+KnownMimeTypes = set()
+KnownExtensions = set()
+KnownMimeTypesWithAdjacentFiles = set()
+KnownExtensionsWithAdjacentFiles = set()
+
+
+class GirderTileSource(tilesource.FileTileSource):
+    girderSource = True
+
+    # If the large image file has one of these extensions or mimetypes, it will
+    # always be treated as if there are possible adjacent files.
+    extensionsWithAdjacentFiles = set()
+    mimeTypesWithAdjacentFiles = set()
+
+    def __init__(self, item, *args, **kwargs):
+        """
+        Initialize the tile class.  See the base class for other available
+        parameters.
+
+        :param item: a Girder item document which contains
+            ['largeImage']['fileId'] identifying the Girder file to be used
+            for the tile source.
+        """
+        super().__init__(item, *args, **kwargs)
+        self.item = item
+
+    @staticmethod
+    def getLRUHash(*args, **kwargs):
+        return '%s,%s,%s,%s,%s,%s,%s,__STYLESTART__,%s,__STYLEEND__' % (
+            args[0]['largeImage']['fileId'],
+            args[0]['updated'],
+            kwargs.get('encoding') or config.getConfig('default_encoding'),
+            kwargs.get('jpegQuality', 95),
+            kwargs.get('jpegSubsampling', 0),
+            kwargs.get('tiffCompression', 'raw'),
+            kwargs.get('edge', False),
+            kwargs.get('style'))
+
+    def getState(self):
+        if hasattr(self, '_classkey'):
+            return self._classkey
+        return '%s,%s,%s,%s,%s,%s,%s,__STYLESTART__,%s,__STYLEEND__' % (
+            self.item['largeImage']['fileId'],
+            self.item['updated'],
+            self.encoding,
+            self.jpegQuality,
+            self.jpegSubsampling,
+            self.tiffCompression,
+            self.edge,
+            self._jsonstyle)
+
+    def mayHaveAdjacentFiles(self, largeImageFile):
+        if not hasattr(self, '_mayHaveAdjacentFiles'):
+            from .models.image_item import ImageItem
+
+            self._mayHaveAdjacentFiles = ImageItem().mayHaveAdjacentFiles(self.item, largeImageFile)
+        return self._mayHaveAdjacentFiles
+
+    def _getLargeImagePath(self):
+        # If self.mayHaveAdjacentFiles is True, we try to use the girder
+        # mount where companion files appear next to each other.
+        largeImageFileId = self.item['largeImage']['fileId']
+        largeImageFile = File().load(largeImageFileId, force=True)
+        try:
+            largeImagePath = None
+            mayHaveAdjacent = self.mayHaveAdjacentFiles(largeImageFile)
+            if (mayHaveAdjacent and hasattr(File(), 'getGirderMountFilePath')):
+                try:
+                    if (largeImageFile.get('imported') and
+                            File().getLocalFilePath(largeImageFile) == largeImageFile['path']):
+                        largeImagePath = largeImageFile['path']
+                except Exception:
+                    pass
+                if not largeImagePath:
+                    with contextlib.suppress(FilePathException):
+                        largeImagePath = File().getGirderMountFilePath(
+                            largeImageFile,
+                            **({'preferFlat': True} if mayHaveAdjacent != 'local' and
+                                'preferFlat' in inspect.signature(
+                                    File.getGirderMountFilePath).parameters else {}))
+            if not largeImagePath:
+                try:
+                    largeImagePath = File().getLocalFilePath(largeImageFile)
+                except AttributeError as e:
+                    raise TileSourceError(
+                        'No local file path for this file: %s' % e.args[0])
+            return largeImagePath
+        except (TileSourceAssetstoreError, FilePathException):
+            raise
+        except (KeyError, ValidationException, TileSourceError) as e:
+            raise TileSourceError(
+                'No large image file in this item: %s' % e.args[0])
+
+
+def loadGirderTileSources():
+    """
+    Load all Girder tilesources from entrypoints and add them to the
+    AvailableGiderTileSources dictionary.
+    """
+    tilesource.loadTileSources('girder_large_image.source', AvailableGirderTileSources)
+    for sourceName in AvailableGirderTileSources:
+        if getattr(AvailableGirderTileSources[sourceName], 'girderSource', False):
+            KnownExtensions.update({
+                key for key in AvailableGirderTileSources[sourceName].extensions
+                if key is not None})
+            KnownExtensionsWithAdjacentFiles.update({
+                key for key in AvailableGirderTileSources[sourceName].extensionsWithAdjacentFiles
+                if key is not None})
+        if getattr(AvailableGirderTileSources[sourceName], 'girderSource', False):
+            KnownMimeTypes.update({
+                key for key in AvailableGirderTileSources[sourceName].mimeTypes
+                if key is not None})
+            KnownMimeTypesWithAdjacentFiles.update({
+                key for key in AvailableGirderTileSources[sourceName].mimeTypesWithAdjacentFiles
+                if key is not None})
+
+
+def getGirderTileSourceName(item, file=None, *args, **kwargs):  # noqa
+    """
+    Get a Girder tilesource name using the known sources.  If tile sources have
+    not yet been loaded, load them.
+
+    :param item: a Girder item.
+    :param file: if specified, the Girder file object to use as the large image
+        file; used here only to check extensions.
+    :returns: The name of a tilesource that can read the Girder item.
+    """
+    if not len(AvailableGirderTileSources):
+        loadGirderTileSources()
+    availableSources = AvailableGirderTileSources
+    if not file:
+        file = File().load(item['largeImage']['fileId'], force=True)
+    mimeType = file['mimeType']
+    try:
+        localPath = File().getLocalFilePath(file)
+    except (FilePathException, AttributeError):
+        localPath = None
+    extensions = [entry.lower().split()[0] for entry in file['exts'] if entry]
+    baseName = os.path.basename(file['name'])
+    properties = {}
+    if localPath:
+        properties['_geospatial_source'] = tilesource.isGeospatial(localPath)
+    ignored_names = config.getConfig('all_sources_ignored_names')
+    ignoreName = (ignored_names and re.search(
+        ignored_names, baseName, flags=re.IGNORECASE))
+    sourceList = []
+    for sourceName in availableSources:
+        if not getattr(availableSources[sourceName], 'girderSource', False):
+            continue
+        sourceExtensions = availableSources[sourceName].extensions
+        priority = sourceExtensions.get(None, SourcePriority.MANUAL)
+        fallback = True
+        if (mimeType and getattr(availableSources[sourceName], 'mimeTypes', None) and
+                mimeType in availableSources[sourceName].mimeTypes):
+            priority = min(priority, availableSources[sourceName].mimeTypes[mimeType])
+            fallback = False
+        for regex in getattr(availableSources[sourceName], 'nameMatches', {}):
+            if re.match(regex, baseName):
+                priority = min(priority, availableSources[sourceName].nameMatches[regex])
+                fallback = False
+        for ext in extensions:
+            if ext in sourceExtensions:
+                priority = min(priority, sourceExtensions[ext])
+                fallback = False
+        if priority >= SourcePriority.MANUAL or (ignoreName and fallback):
+            continue
+        propertiesClash = any(
+            getattr(availableSources[sourceName], k, False) != v
+            for k, v in properties.items())
+        sourceList.append((propertiesClash, fallback, priority, sourceName))
+    for _clash, _fallback, _priority, sourceName in sorted(sourceList):
+        if availableSources[sourceName].canRead(item, *args, **kwargs):
+            return sourceName
+
+
+def getGirderTileSource(item, file=None, *args, **kwargs):
+    """
+    Get a Girder tilesource using the known sources.
+
+    :param item: a Girder item or an item id.
+    :param file: if specified, the Girder file object to use as the large image
+        file; used here only to check extensions.
+    :returns: A girder tilesource for the item.
+    """
+    if not isinstance(item, dict):
+        item = Item().load(item, user=kwargs.get('user'), level=AccessType.READ)
+    sourceName = getGirderTileSourceName(item, file, *args, **kwargs)
+    if sourceName:
+        return AvailableGirderTileSources[sourceName](item, *args, **kwargs)
---
+++ packages/girder-large-image/girder_large_image-1.34.0-py3-none-any.whl/girder_large_image/web_client/views/configView.js
@@ -0,0 +1,309 @@
+import $ from 'jquery';
+import View from '@girder/core/views/View';
+
+import {AccessType} from '@girder/core/constants';
+import events from '@girder/core/events';
+import {restRequest} from '@girder/core/rest';
+import BrowserWidget from '@girder/core/views/widgets/BrowserWidget';
+import PluginConfigBreadcrumbWidget from '@girder/core/views/widgets/PluginConfigBreadcrumbWidget';
+
+import ConfigViewTemplate from '../templates/largeImageConfig.pug';
+import '../stylesheets/largeImageConfig.styl';
+
+/**
+ * Show the default quota settings for users and collections.
+ */
+var ConfigView = View.extend({
+    events: {
+        'submit #g-large-image-form': function (event) {
+            event.preventDefault();
+            this.$('#g-large-image-error-message').empty();
+            this._saveSettings([{
+                key: 'large_image.show_thumbnails',
+                value: this.$('.g-large-image-thumbnails-show').prop('checked')
+            }, {
+                key: 'large_image.show_viewer',
+                value: this.$('.g-large-image-viewer-show').prop('checked')
+            }, {
+                key: 'large_image.default_viewer',
+                value: this.$('.g-large-image-default-viewer').val()
+            }, {
+                key: 'large_image.auto_set',
+                value: this.$('.g-large-image-auto-set-all').prop('checked') ? 'all' : this.$('.g-large-image-auto-set-on').prop('checked')
+            }, {
+                key: 'large_image.max_thumbnail_files',
+                value: +this.$('.g-large-image-max-thumbnail-files').val()
+            }, {
+                key: 'large_image.max_small_image_size',
+                value: +this.$('.g-large-image-max-small-image-size').val()
+            }, {
+                key: 'large_image.show_extra_public',
+                value: this.$('.g-large-image-show-extra-public').val()
+            }, {
+                key: 'large_image.show_extra',
+                value: this.$('.g-large-image-show-extra').val()
+            }, {
+                key: 'large_image.show_extra_admin',
+                value: this.$('.g-large-image-show-extra-admin').val()
+            }, {
+                key: 'large_image.show_item_extra_public',
+                value: this.$('.g-large-image-show-item-extra-public').val()
+            }, {
+                key: 'large_image.show_item_extra',
+                value: this.$('.g-large-image-show-item-extra').val()
+            }, {
+                key: 'large_image.show_item_extra_admin',
+                value: this.$('.g-large-image-show-item-extra-admin').val()
+            }, {
+                key: 'large_image.config_folder',
+                value: (this.$('#g-large-image-config-folder').val() || '').split(' ')[0]
+            }, {
+                key: 'large_image.notification_stream_fallback',
+                value: this.$('.g-large-image-stream-fallback').prop('checked')
+            }, {
+                key: 'large_image.icc_correction',
+                value: this.$('.g-large-image-icc-correction').prop('checked')
+            }, {
+                key: 'large_image.merge_dicom',
+                value: this.$('.g-large-image-merge-dicom-merge').prop('checked')
+            }]);
+        },
+        'click .g-open-browser': '_openBrowser'
+    },
+    initialize: function () {
+        ConfigView.getSettings((settings) => {
+            this.settings = settings;
+            this.render();
+        });
+
+        this._browserWidgetView = new BrowserWidget({
+            parentView: this,
+            titleText: 'Configuration File Location',
+            helpText: 'Browse to a location to select it.',
+            submitText: 'Select Location',
+            validate: function (model) {
+                const isValid = $.Deferred();
+                if (!model || model.get('_modelType') !== 'folder') {
+                    isValid.reject('Please select a folder.');
+                } else {
+                    isValid.resolve();
+                }
+                return isValid.promise();
+            }
+        });
+        this.listenTo(this._browserWidgetView, 'g:saved', function (val) {
+            this.$('#g-large-image-config-folder').val(val.id);
+            restRequest({
+                url: `resource/${val.id}/path`,
+                method: 'GET',
+                data: {type: val.get('_modelType')}
+            }).done((result) => {
+                // Only add the resource path if the value wasn't altered
+                if (this.$('#g-large-image-config-folder').val() === val.id) {
+                    this.$('#g-large-image-config-folder').val(`${val.id} (${result})`);
+                }
+            });
+        });
+    },
+
+    render: function () {
+        this.$el.html(ConfigViewTemplate({
+            settings: this.settings,
+            viewers: ConfigView.viewers
+        }));
+        if (!this.breadcrumb) {
+            this.breadcrumb = new PluginConfigBreadcrumbWidget({
+                pluginName: 'Large image',
+                el: this.$('.g-config-breadcrumb-container'),
+                parentView: this
+            }).render();
+        }
+
+        return this;
+    },
+
+    _saveSettings: function (settings) {
+        /* Now save the settings */
+        return restRequest({
+            type: 'PUT',
+            url: 'system/setting',
+            data: {
+                list: JSON.stringify(settings)
+            },
+            error: null
+        }).done(() => {
+            /* Clear the settings that may have been loaded. */
+            ConfigView.clearSettings();
+            events.trigger('g:alert', {
+                icon: 'ok',
+                text: 'Settings saved.',
+                type: 'success',
+                timeout: 4000
+            });
+        }).fail((resp) => {
+            this.$('#g-large-image-error-message').text(
+                resp.responseJSON.message
+            );
+        });
+    },
+
+    _openBrowser: function () {
+        this._browserWidgetView.setElement($('#g-dialog-container')).render();
+    }
+}, {
+    /* Class methods and objects */
+
+    /* The list of viewers is added as a property to the select widget view so
+     * that it is also available to the settings page. */
+    viewers: [
+        {
+            name: 'geojs',
+            label: 'GeoJS',
+            type: 'geojs'
+        }, {
+            name: 'openseadragon',
+            label: 'OpenSeaDragon',
+            type: 'openseadragon'
+        }, {
+            name: 'openlayers',
+            label: 'OpenLayers',
+            type: 'openlayers'
+        }, {
+            name: 'leaflet',
+            label: 'Leaflet',
+            type: 'leaflet'
+        }, {
+            name: 'slideatlas',
+            label: 'SlideAtlas',
+            type: 'slideatlas'
+        }
+    ],
+
+    /**
+     * Get settings if we haven't yet done so.  Either way, call a callback
+     * when we have settings.
+     *
+     * @param {function} callback a function to call after the settings are
+     *      fetched.  If the settings are already present, this is called
+     *      without any delay.
+     */
+    getSettings: function (callback) {
+        if (!ConfigView.settings && !ConfigView._settingsRequest) {
+            ConfigView._settingsRequest = restRequest({
+                type: 'GET',
+                url: 'large_image/settings'
+            }).done((resp) => {
+                resp.extraInfo = {};
+                resp.extraItemInfo = {};
+                const extraList = [{
+                    access: null,
+                    extraInfo: 'large_image.show_extra_public',
+                    extraItemInfo: 'large_image.show_item_extra_public'
+                }, {
+                    access: AccessType.READ,
+                    extraInfo: 'large_image.show_extra',
+                    extraItemInfo: 'large_image.show_item_extra'
+                }, {
+                    access: AccessType.ADMIN,
+                    extraInfo: 'large_image.show_extra_admin',
+                    extraItemInfo: 'large_image.show_item_extra_admin'
+                }];
+                extraList.forEach((entry) => {
+                    ['extraInfo', 'extraItemInfo'].forEach((key) => {
+                        try {
+                            resp[key][entry.access] = JSON.parse(resp[entry[key]]);
+                        } catch (err) {
+                        }
+                    });
+                });
+                ConfigView.settings = resp;
+                if (callback) {
+                    callback(ConfigView.settings);
+                }
+            });
+        } else if (callback) {
+            ConfigView._settingsRequest.done(() => {
+                callback(ConfigView.settings);
+            });
+        }
+    },
+
+    /**
+     * Get the folder config file for the current user.
+     *
+     * @param {string} folderId the folder to get the config for.
+     * @param {boolean} reload if true, refetch the config file even if it was
+     *      the last one fetched.  Since config files can be changed by the
+     *      user, this should be done based on the UI behavior.
+     * @param {function} callback a function to call after the config file is
+     *      fetched.  If the file is already fetched, this is called without
+     *      any delay.
+     * @returns a promise that resolves to the config file values.
+     */
+    getConfigFile: function (folderId, reload, callback) {
+        if (!folderId) {
+            const result = {};
+            if (callback) {
+                callback(result);
+            }
+            return $.Deferred().resolve({});
+        }
+        if (ConfigView._lastliconfig === folderId && !reload) {
+            if (callback) {
+                callback(ConfigView._liconfig || {});
+            }
+            return $.Deferred().resolve(ConfigView._liconfig);
+        }
+        if (ConfigView._liconfigSettingsRequest) {
+            if (ConfigView._nextliconfig === folderId) {
+                if (callback) {
+                    ConfigView._liconfigSettingsRequest.done((val) => {
+                        callback(val || {});
+                    });
+                }
+                return ConfigView._liconfigSettingsRequest;
+            }
+            ConfigView._liconfigSettingsRequest.cancel();
+        }
+        ConfigView._nextliconfig = folderId;
+        ConfigView._liconfigSettingsRequest = restRequest({
+            url: `folder/${folderId}/yaml_config/.large_image_config.yaml`
+        }).done((val) => {
+            val = val || {};
+            ConfigView._lastliconfig = folderId;
+            ConfigView._liconfigSettingsRequest = null;
+            ConfigView._liconfig = val;
+            if (callback) {
+                callback(ConfigView._liconfig);
+            }
+            return val;
+        }).fail(() => {
+            // fallback matching server values
+            const li = {
+                columns: [
+                    {type: 'record', value: 'name', title: 'Name'},
+                    {type: 'record', value: 'controls', title: 'Controls'},
+                    {type: 'record', value: 'size', title: 'Size'}]
+            };
+            const val = {itemList: li, itemListDialog: li};
+            ConfigView._lastliconfig = folderId;
+            ConfigView._liconfigSettingsRequest = null;
+            ConfigView._liconfig = val;
+            if (callback) {
+                callback(ConfigView._liconfig);
+            }
+            return val;
+        });
+        return ConfigView._liconfigSettingsRequest;
+    },
+
+    /**
+     * Clear the settings so that getSettings will refetch them.
+     */
+    clearSettings: function () {
+        delete ConfigView.settings;
+        delete ConfigView._settingsRequest;
+    }
+});
+
+export default ConfigView;
