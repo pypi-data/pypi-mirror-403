8459de512d1d5a4e5e67809354b366fda84e7467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 07:12:50 +0000

    Add package biznes/1.0.1/biznes-1.0.1-py3-none-any.whl

---
+++ packages/biznes/biznes-1.0.1-py3-none-any.whl/biznes/__init__.py
@@ -0,0 +1,10 @@
+"""
+Biznes - Edukacyjna gra symulująca zakładanie startupu w Polsce
+"""
+
+__version__ = "1.0.0"
+__author__ = "Softreck"
+
+from .shell import BiznesShell, main
+
+__all__ = ["BiznesShell", "main"]
