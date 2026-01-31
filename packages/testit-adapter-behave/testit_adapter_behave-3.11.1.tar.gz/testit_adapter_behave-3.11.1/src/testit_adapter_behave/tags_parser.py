d333b99e9ac9e690147965e3d78a889b6c876b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:07:10 +0000

    Add package testit-adapter-behave/3.11.1/testit_adapter_behave-3.11.1.tar.gz

---
+++ packages/testit-adapter-behave/testit_adapter_behave-3.11.1.tar.gz/testit_adapter_behave-3.11.1/src/testit_adapter_behave/tags_parser.py
@@ -0,0 +1,89 @@
+from .models.label import get_label_model
+from .models.tags import TagType
+from .models.url_link import get_url_to_link_model, get_dict_to_link_model
+
+
+def parse_tags(tags):
+    parsed_tags = {
+        TagType.LINKS: [],
+        TagType.LABELS: [],
+        TagType.WORK_ITEM_IDS: []
+    }
+
+    for tag in tags:
+        if TagType.EXTERNAL_ID in tag:
+            parsed_tags[TagType.EXTERNAL_ID] = parse_space_in_tag(tag[len(TagType.EXTERNAL_ID):])
+
+        elif TagType.DISPLAY_NAME in tag:
+            parsed_tags[TagType.DISPLAY_NAME] = parse_space_in_tag(tag[len(TagType.DISPLAY_NAME):])
+
+        elif TagType.LINKS in tag:
+            parsed_tags[TagType.LINKS].extend(
+                parse_links(
+                    tag[len(TagType.LINKS):]))
+
+        elif TagType.TITLE in tag:
+            parsed_tags[TagType.TITLE] = parse_space_in_tag(tag[len(TagType.TITLE):])
+
+        elif TagType.WORK_ITEM_IDS in tag:
+            parsed_tags[TagType.WORK_ITEM_IDS].extend(
+                parse_massive(
+                    tag[len(TagType.WORK_ITEM_IDS):]))
+
+        elif TagType.DESCRIPTION in tag:
+            parsed_tags[TagType.DESCRIPTION] = parse_space_in_tag(tag[len(TagType.DESCRIPTION):])
+
+        elif TagType.LABELS in tag:
+            parsed_tags[TagType.LABELS].extend(
+                parse_labels(
+                    tag[len(TagType.LABELS):]))
+
+        elif TagType.NAMESPACE in tag:
+            parsed_tags[TagType.NAMESPACE] = parse_space_in_tag(tag[len(TagType.NAMESPACE):])
+
+        elif TagType.CLASSNAME in tag:
+            parsed_tags[TagType.CLASSNAME] = parse_space_in_tag(tag[len(TagType.CLASSNAME):])
+
+    return parsed_tags
+
+
+def parse_massive(tag: str):
+    return tag.split(',')
+
+
+def parse_labels(tag):
+    parsed_labels = []
+
+    for label in parse_massive(tag):
+        parsed_labels.append(get_label_model(label))
+
+    return parsed_labels
+
+
+def parse_links(tag: str):
+    parsed_links = []
+    json_links = parse_json(tag)
+
+    if not json_links:
+        for url in parse_massive(tag):
+            parsed_links.append(get_url_to_link_model(url))
+
+    if isinstance(json_links, tuple):
+        for url in json_links:
+            parsed_links.append(get_url_to_link_model(url))
+
+    if isinstance(json_links, dict):
+        parsed_links.append(get_dict_to_link_model(json_links))
+
+    return parsed_links
+
+
+def parse_json(json_string: str):
+    try:
+        return eval(json_string)
+    except Exception:
+        return
+
+
+def parse_space_in_tag(tag: str) -> str:
+    return tag.replace('\\_', ' ')
