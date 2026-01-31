e0d4473c7aea5bde429294c7af2a3db97736b298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:19:28 +0000

    Add package hwmux-client-python/2.46.0/hwmux_client_python-2.46.0-py3-none-any.whl

---
+++ packages/hwmux-client-python/hwmux_client_python-2.46.0-py3-none-any.whl/hwmux_client/models/__init__.py
@@ -0,0 +1,78 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from hwmux_client.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from hwmux_client.model.auth_token import AuthToken
+from hwmux_client.model.blank_enum import BlankEnum
+from hwmux_client.model.device_group import DeviceGroup
+from hwmux_client.model.device_group_serializer_with_device_pk import DeviceGroupSerializerWithDevicePk
+from hwmux_client.model.device_serializer_public import DeviceSerializerPublic
+from hwmux_client.model.event_enum import EventEnum
+from hwmux_client.model.group_location import GroupLocation
+from hwmux_client.model.label import Label
+from hwmux_client.model.label_serializer_with_permissions import LabelSerializerWithPermissions
+from hwmux_client.model.light_device import LightDevice
+from hwmux_client.model.location import Location
+from hwmux_client.model.location_serializer_write_only import LocationSerializerWriteOnly
+from hwmux_client.model.log import Log
+from hwmux_client.model.log_resource_type import LogResourceType
+from hwmux_client.model.log_status_enum import LogStatusEnum
+from hwmux_client.model.logged_in_user import LoggedInUser
+from hwmux_client.model.nested_device_group import NestedDeviceGroup
+from hwmux_client.model.null_enum import NullEnum
+from hwmux_client.model.object_permissions import ObjectPermissions
+from hwmux_client.model.outbox import Outbox
+from hwmux_client.model.outbox_reservation_status import OutboxReservationStatus
+from hwmux_client.model.outbox_status_enum import OutboxStatusEnum
+from hwmux_client.model.paginated_device_group_list import PaginatedDeviceGroupList
+from hwmux_client.model.paginated_device_serializer_public_list import PaginatedDeviceSerializerPublicList
+from hwmux_client.model.paginated_label_list import PaginatedLabelList
+from hwmux_client.model.paginated_log_list import PaginatedLogList
+from hwmux_client.model.paginated_logged_in_user_list import PaginatedLoggedInUserList
+from hwmux_client.model.paginated_outbox_list import PaginatedOutboxList
+from hwmux_client.model.paginated_part_family_list import PaginatedPartFamilyList
+from hwmux_client.model.paginated_part_list import PaginatedPartList
+from hwmux_client.model.paginated_permission_group_list import PaginatedPermissionGroupList
+from hwmux_client.model.paginated_reservation_session_serializer_read_only_list import PaginatedReservationSessionSerializerReadOnlyList
+from hwmux_client.model.paginated_room_list import PaginatedRoomList
+from hwmux_client.model.paginated_site_list import PaginatedSiteList
+from hwmux_client.model.part import Part
+from hwmux_client.model.part_family import PartFamily
+from hwmux_client.model.patched_device_group_serializer_with_device_pk import PatchedDeviceGroupSerializerWithDevicePk
+from hwmux_client.model.patched_label_serializer_with_permissions import PatchedLabelSerializerWithPermissions
+from hwmux_client.model.patched_logged_in_user import PatchedLoggedInUser
+from hwmux_client.model.patched_object_permissions import PatchedObjectPermissions
+from hwmux_client.model.patched_part import PatchedPart
+from hwmux_client.model.patched_part_family import PatchedPartFamily
+from hwmux_client.model.patched_permission_group import PatchedPermissionGroup
+from hwmux_client.model.patched_resource_permissions import PatchedResourcePermissions
+from hwmux_client.model.patched_room import PatchedRoom
+from hwmux_client.model.patched_site import PatchedSite
+from hwmux_client.model.patched_write_only_device import PatchedWriteOnlyDevice
+from hwmux_client.model.permission_group import PermissionGroup
+from hwmux_client.model.permissions_enum import PermissionsEnum
+from hwmux_client.model.reservation_extension_request import ReservationExtensionRequest
+from hwmux_client.model.reservation_request import ReservationRequest
+from hwmux_client.model.reservation_session_serializer_read_only import ReservationSessionSerializerReadOnly
+from hwmux_client.model.reservation_session_serializer_read_only_owner import ReservationSessionSerializerReadOnlyOwner
+from hwmux_client.model.reservation_session_serializer_read_only_state import ReservationSessionSerializerReadOnlyState
+from hwmux_client.model.reservation_status_enum import ReservationStatusEnum
+from hwmux_client.model.resource_permissions import ResourcePermissions
+from hwmux_client.model.resource_status_request import ResourceStatusRequest
+from hwmux_client.model.resource_type_enum import ResourceTypeEnum
+from hwmux_client.model.room import Room
+from hwmux_client.model.saga_status_enum import SagaStatusEnum
+from hwmux_client.model.site import Site
+from hwmux_client.model.source_enum import SourceEnum
+from hwmux_client.model.state_enum import StateEnum
+from hwmux_client.model.status_enum import StatusEnum
+from hwmux_client.model.token import Token
+from hwmux_client.model.user import User
+from hwmux_client.model.write_only_device import WriteOnlyDevice
