4838e4aaab4947432eba34a2c0f8aa5fe85797ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 13:19:54 +0000

    Add package match-predicting-pub-api/6.12/match_predicting_pub_api-6.12-py3-none-any.whl

---
+++ packages/match-predicting-pub-api/match_predicting_pub_api-6.12-py3-none-any.whl/match_predicting_pub_api/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from match_predicting_pub_api.api.dummy_api import DummyApi
+from match_predicting_pub_api.api.import_api import ImportApi
+from match_predicting_pub_api.api.testing_api import TestingApi
+from match_predicting_pub_api.api.training_api import TrainingApi
+
