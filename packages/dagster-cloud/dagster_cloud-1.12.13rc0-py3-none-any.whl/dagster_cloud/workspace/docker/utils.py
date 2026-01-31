8e16697dbcb1b051aafb3366ad364a8b59a486a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 20:06:30 +0000

    Add package dagster-cloud/1.12.13rc0/dagster_cloud-1.12.13rc0-py3-none-any.whl

---
+++ packages/dagster-cloud/dagster_cloud-1.12.13rc0-py3-none-any.whl/dagster_cloud/workspace/docker/utils.py
@@ -0,0 +1,12 @@
+import re
+
+from dagster_cloud.workspace.user_code_launcher.utils import unique_resource_name
+
+
+def unique_docker_resource_name(deployment_name, location_name):
+    return unique_resource_name(
+        deployment_name,
+        location_name,
+        length_limit=63,  # Max DNS hostname limit
+        sanitize_fn=lambda name: re.sub("[^a-z0-9-]", "", name).strip("-"),
+    )
