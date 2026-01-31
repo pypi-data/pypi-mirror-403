7cdecf6ab09ada6065c0f017d68d85e69bd769b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 06:10:35 +0000

    Add package roifile/2026.1.29/roifile-2026.1.29-py3-none-any.whl

---
+++ packages/roifile/roifile-2026.1.29-py3-none-any.whl/roifile/__init__.py
@@ -0,0 +1,9 @@
+# roifile/__init__.py
+
+from .roifile import *
+from .roifile import __all__, __doc__, __version__
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Roifile version string."""
