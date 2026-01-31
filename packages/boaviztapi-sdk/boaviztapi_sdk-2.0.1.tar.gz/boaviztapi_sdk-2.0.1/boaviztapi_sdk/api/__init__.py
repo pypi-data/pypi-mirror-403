7fb01544f215fb1bbdf88f5282bfb0a90fd23bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:31:50 +0000

    Add package boaviztapi-sdk/2.0.1/boaviztapi_sdk-2.0.1.tar.gz

---
+++ packages/boaviztapi-sdk/boaviztapi_sdk-2.0.1.tar.gz/boaviztapi_sdk-2.0.1/boaviztapi_sdk/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from boaviztapi_sdk.api.cloud_api import CloudApi
+from boaviztapi_sdk.api.component_api import ComponentApi
+from boaviztapi_sdk.api.consumption_profile_api import ConsumptionProfileApi
+from boaviztapi_sdk.api.default_api import DefaultApi
+from boaviztapi_sdk.api.iot_api import IotApi
+from boaviztapi_sdk.api.peripheral_api import PeripheralApi
+from boaviztapi_sdk.api.server_api import ServerApi
+from boaviztapi_sdk.api.terminal_api import TerminalApi
+from boaviztapi_sdk.api.utils_api import UtilsApi
+
