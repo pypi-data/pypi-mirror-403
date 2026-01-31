7219e4c5864daf795055164ba0827c1e5ade2489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 06:10:28 +0000

    Add package large-image-source-tiff/1.33.6.dev43/large_image_source_tiff-1.33.6.dev43.tar.gz

---
+++ packages/large-image-source-tiff/large_image_source_tiff-1.33.6.dev43.tar.gz/large_image_source_tiff-1.33.6.dev43/setup.py
@@ -0,0 +1,57 @@
+import os
+
+from setuptools import find_packages, setup
+
+description = 'A TIFF tilesource for large_image.'
+long_description = description + '\n\nSee the large-image package for more details.'
+
+
+try:
+    from setuptools_scm import get_version
+
+    version = get_version(root='../..')
+    limit_version = f'>={version}' if '+' not in version and not os.getenv('TOX_ENV_NAME') else ''
+except (ImportError, LookupError):
+    limit_version = ''
+
+setup(
+    name='large-image-source-tiff',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    license='Apache-2.0',
+    author='Kitware, Inc.',
+    author_email='kitware@kitware.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        f'large-image{limit_version}',
+        'pylibtiff',
+        'tifftools>=1.7.0',
+    ],
+    extras_require={
+        'all': [
+            'pylibjpeg-openjpeg',
+        ],
+        'girder': f'girder-large-image{limit_version}',
+    },
+    keywords='large_image, tile source',
+    packages=find_packages(exclude=['test', 'test.*']),
+    url='https://github.com/girder/large_image',
+    entry_points={
+        'large_image.source': [
+            'tiff = large_image_source_tiff:TiffFileTileSource',
+        ],
+        'girder_large_image.source': [
+            'tiff = large_image_source_tiff.girder_source:TiffGirderTileSource',
+        ],
+    },
+)
