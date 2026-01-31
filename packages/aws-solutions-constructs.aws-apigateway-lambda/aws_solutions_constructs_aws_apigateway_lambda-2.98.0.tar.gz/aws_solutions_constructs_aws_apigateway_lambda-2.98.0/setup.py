b02749443109166089f2c0dcb4e0840a1854fd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:07:04 +0000

    Add package aws-solutions-constructs.aws-apigateway-lambda/2.98.0/aws_solutions_constructs_aws_apigateway_lambda-2.98.0.tar.gz

---
+++ packages/aws-solutions-constructs.aws-apigateway-lambda/aws_solutions_constructs_aws_apigateway_lambda-2.98.0.tar.gz/aws_solutions_constructs_aws_apigateway_lambda-2.98.0/setup.py
@@ -0,0 +1,64 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "aws-solutions-constructs.aws-apigateway-lambda",
+    "version": "2.98.0",
+    "description": "CDK constructs for defining an interaction between an API Gateway and a Lambda function.",
+    "license": "Apache-2.0",
+    "url": "https://github.com/awslabs/aws-solutions-constructs.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-solutions-constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_solutions_constructs.aws_apigateway_lambda",
+        "aws_solutions_constructs.aws_apigateway_lambda._jsii"
+    ],
+    "package_data": {
+        "aws_solutions_constructs.aws_apigateway_lambda._jsii": [
+            "aws-apigateway-lambda@2.98.0.jsii.tgz"
+        ],
+        "aws_solutions_constructs.aws_apigateway_lambda": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.9",
+    "install_requires": [
+        "aws-cdk-lib>=2.234.0, <3.0.0",
+        "aws-solutions-constructs.core==2.98.0",
+        "constructs>=10.0.0, <11.0.0",
+        "jsii>=1.119.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard>=2.13.3,<4.3.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
