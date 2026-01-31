cbee66db3960a58e96983c0925448f0829bff4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:32:43 +0000

    Add package python-matchit/0.2.0/python_matchit-0.2.0.tar.gz

---
+++ packages/python-matchit/python_matchit-0.2.0.tar.gz/python_matchit-0.2.0/src/lib.rs
@@ -0,0 +1,57 @@
+use pyo3::exceptions::PyLookupError;
+use pyo3::prelude::*;
+use std::collections::HashMap;
+
+#[pyclass(name = "Router")]
+struct Router {
+    inner: matchit::Router<String>,
+}
+
+#[pyclass(name = "MatchResult")]
+struct MatchResult {
+    #[pyo3(get)]
+    route_id: String,
+    #[pyo3(get)]
+    params: std::collections::HashMap<String, String>,
+}
+
+#[pymethods]
+impl Router {
+    #[new]
+    pub fn new() -> Self {
+        Self {
+            inner: matchit::Router::new(),
+        }
+    }
+
+    pub fn insert(&mut self, route: String, value: String) {
+        self.inner
+            .insert(route, value)
+            .expect("Panic! Failed to insert route");
+    }
+
+    pub fn at(&self, path: &str) -> PyResult<MatchResult> {
+        let matched = (&self.inner).at(path);
+        if matched.is_ok() {
+            let unwrapped = matched.unwrap();
+            let mut d = HashMap::new();
+            for (k, v) in unwrapped.params.iter() {
+                d.insert(k.to_string(), v.to_string());
+            }
+            Ok(MatchResult {
+                route_id: unwrapped.value.to_string(),
+                params: d,
+            })
+        } else {
+            Err(PyErr::new::<PyLookupError, _>("No route found"))
+        }
+    }
+}
+
+#[pymodule(name = "matchit", gil_used = false)]
+mod python_matchit {
+    #[pymodule_export]
+    use super::MatchResult;
+    #[pymodule_export]
+    use super::Router;
+}
