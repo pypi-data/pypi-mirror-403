4d25efe7fae76e1902aeb3ee10fd7aba8051a92f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:32:01 +0000

    Add package pkg-publisher/0.1.5/pkg_publisher-0.1.5-py3-none-any.whl

---
+++ packages/pkg-publisher/pkg_publisher-0.1.5-py3-none-any.whl/pkg_publisher/__init__.py
@@ -0,0 +1,9 @@
+"""
+pkg-publisher - Python Package Publisher MCP Service
+"""
+
+from .service import __version__, get_version, setup_logging
+
+__author__ = "pkg-publisher contributors"
+
+__all__ = ["__version__", "get_version", "setup_logging"]
