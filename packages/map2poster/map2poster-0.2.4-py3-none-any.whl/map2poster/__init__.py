d956339dd95a1de877558704abc8224eaa85c8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:18:41 +0000

    Add package map2poster/0.2.4/map2poster-0.2.4-py3-none-any.whl

---
+++ packages/map2poster/map2poster-0.2.4-py3-none-any.whl/map2poster/__init__.py
@@ -0,0 +1,4 @@
+from .core import create_poster as CreatePoster
+from .core import get_available_themes, load_theme, get_coordinates
+
+__all__ = ["CreatePoster", "get_available_themes", "load_theme", "get_coordinates"]
