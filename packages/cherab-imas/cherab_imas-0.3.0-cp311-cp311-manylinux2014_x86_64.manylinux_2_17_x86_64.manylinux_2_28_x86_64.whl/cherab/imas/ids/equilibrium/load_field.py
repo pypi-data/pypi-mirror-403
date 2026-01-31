508055820ff2eb8844411c354f2327c4d66f90dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:07:12 +0000

    Add package cherab-imas/0.3.0/cherab_imas-0.3.0-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/cherab-imas/cherab_imas-0.3.0-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/cherab/imas/ids/equilibrium/load_field.py
@@ -0,0 +1,103 @@
+# Copyright 2023 Euratom
+# Copyright 2023 United Kingdom Atomic Energy Authority
+# Copyright 2023 Centro de Investigaciones Energéticas, Medioambientales y Tecnológicas
+#
+# Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the
+# European Commission - subsequent versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#
+# https://joinup.ec.europa.eu/software/page/eupl5
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the Licence for the specific language governing permissions and limitations
+# under the Licence.
+"""Module for loading magnetic field data from the equilibrium IDS."""
+
+from dataclasses import dataclass
+
+import numpy as np
+
+from imas.ids_defs import EMPTY_INT
+from imas.ids_struct_array import IDSStructArray
+
+RECTANGULAR_GRID = 1
+
+__all__ = ["load_magnetic_field_data"]
+
+
+@dataclass
+class MagneticField2DData:
+    """Dataclass to hold 2D magnetic field data."""
+
+    r: np.ndarray
+    z: np.ndarray
+    b_field_r: np.ndarray
+    b_field_z: np.ndarray
+    b_field_phi: np.ndarray
+
+
+def load_magnetic_field_data(profiles_2d: IDSStructArray) -> MagneticField2DData:
+    """Load 2D profiles of the magnetic field components from equilibrium IDS.
+
+    The magnetic field components are extracted from the ``profiles_2d`` IDS structure,
+    assuming that the profiles are defined on a rectangular grid.
+
+    Parameters
+    ----------
+    profiles_2d
+        The ``profiles_2d`` structure of the equilibrium IDS.
+
+    Returns
+    -------
+    MagneticField2DData
+        Dataclass containing the magnetic field data:
+
+        :r: ``(N,)`` ndarray with R coordinates of rectangular grid.
+        :z: ``(M,)`` ndarray with Z coordinates of rectangular grid.
+        :b_field_r: ``(N, M)`` ndarray with R component of the magnetic field.
+        :b_field_z: ``(N, M)`` ndarray with Z component of the magnetic field.
+        :b_field_phi: ``(N, M)`` ndarray with toroidal component of the magnetic field.
+
+    Raises
+    ------
+    RuntimeError
+        If unable to read the magnetic field due to unsupported grid type or
+        mismatched array shapes.
+    """
+    rectangular_grid = False
+    for prof2d in profiles_2d:
+        if prof2d.grid_type.index == RECTANGULAR_GRID or prof2d.grid_type.index == EMPTY_INT:
+            rectangular_grid = True
+            break
+
+    if not rectangular_grid:
+        raise RuntimeError(
+            "Unable to read magnetic field: "
+            + "rectangular grid for 2D profiles is not found and other grid types are not supported."
+        )
+
+    r = np.asarray_chkfinite(prof2d.grid.dim1)
+    z = np.asarray_chkfinite(prof2d.grid.dim2)
+    shape = (r.size, z.size)
+
+    b_field_r = np.asarray_chkfinite(prof2d.b_field_r)
+    b_field_phi = np.asarray_chkfinite(prof2d.b_field_phi)
+    b_field_z = np.asarray_chkfinite(prof2d.b_field_z)
+
+    if b_field_r.shape != shape or b_field_phi.shape != shape or b_field_z.shape != shape:
+        raise RuntimeError(
+            "Unable to read magnetic field: "
+            + "the shape of the magnetic field components does not match the grid shape."
+        )
+
+    return MagneticField2DData(
+        r=r,
+        z=z,
+        b_field_r=b_field_r,
+        b_field_z=b_field_z,
+        b_field_phi=b_field_phi,
+    )
---
+++ packages/cherab-imas/cherab_imas-0.3.0-cp311-cp311-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/cherab/imas/plasma/equilibrium.py
@@ -0,0 +1,236 @@
+# Copyright 2023 Euratom
+# Copyright 2023 United Kingdom Atomic Energy Authority
+# Copyright 2023 Centro de Investigaciones Energéticas, Medioambientales y Tecnológicas
+#
+# Licensed under the EUPL, Version 1.1 or – as soon they will be approved by the
+# European Commission - subsequent versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#
+# https://joinup.ec.europa.eu/software/page/eupl5
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied.
+#
+# See the Licence for the specific language governing permissions and limitations
+# under the Licence.
+"""Module for loading plasma equilibrium and magnetic field from the equilibrium IDS."""
+
+from typing import Literal, overload
+
+import numpy as np
+from raysect.core.math.function.float import Interpolator1DArray, Interpolator2DArray
+from raysect.core.math.function.vector3d import FloatToVector3DFunction2D
+
+from cherab.imas.ids.equilibrium.load_equilibrium import Equilibrium2DData
+from cherab.tools.equilibrium import EFITEquilibrium
+from imas import DBEntry
+
+from ..ids.common import get_ids_time_slice
+from ..ids.equilibrium import load_equilibrium_data, load_magnetic_field_data
+
+__all__ = ["load_equilibrium", "load_magnetic_field"]
+
+
+@overload
+def load_equilibrium(
+    *args,
+    time: float = 0,
+    occurrence: int = 0,
+    time_threshold: float = np.inf,
+    with_psi_interpolator: Literal[False] = False,
+    **kwargs,
+) -> EFITEquilibrium: ...
+
+
+@overload
+def load_equilibrium(
+    *args,
+    time: float = 0,
+    occurrence: int = 0,
+    time_threshold: float = np.inf,
+    with_psi_interpolator: Literal[True],
+    **kwargs,
+) -> tuple[EFITEquilibrium, Interpolator1DArray | None]: ...
+
+
+def load_equilibrium(
+    *args,
+    time: float = 0,
+    occurrence: int = 0,
+    time_threshold: float = np.inf,
+    with_psi_interpolator: bool = False,
+    **kwargs,
+) -> tuple[EFITEquilibrium, Interpolator1DArray | None] | EFITEquilibrium:
+    """Load plasma equilibrium from the equilibrium IDS and create an `EFITEquilibrium` object.
+
+    Parameters
+    ----------
+    *args
+        Arguments passed to `~imas.db_entry.DBEntry`.
+    time
+        Time for the equilibrium, by default 0.
+    occurrence
+        Occurrence index of the ``equilibrium`` IDS, by default 0.
+    time_threshold
+        Maximum allowed difference between the requested time and the nearest available
+        time, by default `numpy.inf`.
+    with_psi_interpolator
+        If True, returns the ``psi_norm(rho_tor_norm)`` interpolator; otherwise, returns only the
+        equilibrium object.
+    **kwargs
+        Keyword arguments passed to `~imas.db_entry.DBEntry`.
+
+    Returns
+    -------
+    equilibrium : `~cherab.tools.equilibrium.efit.EFITEquilibrium`
+        The plasma equilibrium object.
+    psi_interpolator : `~raysect.core.math.function.float.function1d.interpolate.Interpolator1DArray` | None
+        If ``with_psi_interpolator`` is True and ``rho_tor_norm`` is available, returns the
+        ``psi_norm(rho_tor_norm)`` interpolator.
+        If rho_tor_norm is not available, returns None.
+        Otherwise, returns only the equilibrium object.
+    """
+    with DBEntry(*args, **kwargs) as entry:
+        equilibrium_ids = get_ids_time_slice(
+            entry, "equilibrium", time=time, occurrence=occurrence, time_threshold=time_threshold
+        )
+
+    eq_data = load_equilibrium_data(equilibrium_ids)
+
+    cocos_11to3(eq_data)
+
+    eq_data.psi_norm[0] = min(0, eq_data.psi_norm[0])
+    eq_data.psi_norm[-1] = max(1.0, eq_data.psi_norm[-1])
+
+    f_profile = np.array([eq_data.psi_norm, eq_data.f])
+    q_profile = np.array([eq_data.psi_norm, eq_data.q])
+    equilibrium = EFITEquilibrium(
+        eq_data.r,
+        eq_data.z,
+        eq_data.psi_grid,
+        eq_data.psi_axis,
+        eq_data.psi_lcfs,
+        eq_data.magnetic_axis,
+        eq_data.x_points,
+        eq_data.strike_points,
+        f_profile,
+        q_profile,
+        eq_data.b_vacuum_radius,
+        eq_data.b_vacuum_magnitude,
+        eq_data.lcfs_polygon,
+        None,  # Limiter Polygon
+        eq_data.time,
+    )
+
+    if not with_psi_interpolator:
+        return equilibrium
+
+    if eq_data.rho_tor_norm is None:
+        return equilibrium, None
+
+    psi_interpolator = Interpolator1DArray(
+        eq_data.rho_tor_norm,
+        eq_data.psi_norm,
+        "cubic",
+        "none",
+        0,
+    )
+
+    return equilibrium, psi_interpolator
+
+
+def load_magnetic_field(
+    *args,
+    time: float = 0,
+    occurrence: int = 0,
+    time_threshold: float = np.inf,
+    **kwargs,
+) -> FloatToVector3DFunction2D:
+    """Load the magnetic field from the equilibrium IDS and returns a VectorFunction2D interpolator.
+
+    Parameters
+    ----------
+    *args
+        Arguments passed to `~imas.db_entry.DBEntry`.
+    time
+        Time for the equilibrium, by default 0.
+    occurrence
+        Occurrence index of the ``equilibrium`` IDS, by default 0.
+    time_threshold
+        Maximum allowed difference between the requested time and the nearest available
+        time, by default `numpy.inf`.
+    **kwargs
+        Keyword arguments passed to `~imas.db_entry.DBEntry`.
+
+    Returns
+    -------
+    `~raysect.core.math.function.vector3d.function2d.base.Function2D`
+        The magnetic field interpolator.
+
+    Raises
+    ------
+    RuntimeError
+        If the equilibrium IDS does not have a time slice.
+    """
+    with DBEntry(*args, **kwargs) as entry:
+        equilibrium_ids = get_ids_time_slice(
+            entry, "equilibrium", time=time, occurrence=occurrence, time_threshold=time_threshold
+        )
+
+    if not len(equilibrium_ids.time_slice):
+        raise RuntimeError("Equilibrium IDS does not have a time slice.")
+
+    b_data = load_magnetic_field_data(equilibrium_ids.time_slice[0].profiles_2d)
+
+    extra_range_r = b_data.r[-1] - b_data.r[0]
+    extra_range_z = b_data.z[-1] - b_data.z[0]
+
+    br = Interpolator2DArray(
+        b_data.r,
+        b_data.z,
+        b_data.b_field_r,
+        "cubic",
+        "linear",
+        extra_range_r,
+        extra_range_z,
+    )
+    btor = Interpolator2DArray(
+        b_data.r,
+        b_data.z,
+        b_data.b_field_phi,
+        "cubic",
+        "linear",
+        extra_range_r,
+        extra_range_z,
+    )
+    bz = Interpolator2DArray(
+        b_data.r,
+        b_data.z,
+        b_data.b_field_z,
+        "cubic",
+        "linear",
+        extra_range_r,
+        extra_range_z,
+    )
+    return FloatToVector3DFunction2D(br, btor, bz)
+
+
+def cocos_11to3(eq_data: Equilibrium2DData) -> None:
+    """Convert from COCOS 11 convention used in IMAS to COCOS 3 convention used in EFIT.
+
+    This function modifies the eq_data in place to convert the coordinates
+    and other relevant data from the COCOS 11 convention to the COCOS 3 convention.
+
+    Parameters
+    ----------
+    eq_data
+        `Equilibrium2DData` dataclass instance containing equilibrium data to be converted.
+    """
+    eq_data.psi_grid = -eq_data.psi_grid / (2.0 * np.pi)
+    eq_data.psi_axis = -eq_data.psi_axis / (2.0 * np.pi)
+    eq_data.psi_lcfs = -eq_data.psi_lcfs / (2.0 * np.pi)
+    eq_data.q = -eq_data.q
+    if eq_data.phi is not None:
+        eq_data.phi = -eq_data.phi
