2ca18bcd058a57492005647cb87b2a82ca69630c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:06:39 +0000

    Add package panzi_inotify/1.0.2/panzi_inotify-1.0.2-py3-none-any.whl

---
+++ packages/panzi_inotify/panzi_inotify-1.0.2-py3-none-any.whl/panzi_inotify/__init__.py
@@ -0,0 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from .inotify import *
+from .inotify import __all__
+
+__version__ = '1.0.2'
