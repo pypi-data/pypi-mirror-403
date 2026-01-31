28176dd1061f1c14c317dc46a4b72062c613bc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 08:06:53 +0000

    Add package arprax/0.0.1/arprax-0.0.1.tar.gz

---
+++ packages/arprax/arprax-0.0.1.tar.gz/arprax-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="arprax",
+    version="0.0.1",
+    author="Arprax Lab",
+    author_email="lab@arprax.com",
+    description="The official Arprax namespace package.",
+    packages=[], 
+)
