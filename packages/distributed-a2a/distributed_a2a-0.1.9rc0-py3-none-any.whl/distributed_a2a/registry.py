debc8407ccde7f799bbc1a2fa92586e2e70fa5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 04:32:51 +0000

    Add package distributed-a2a/0.1.9rc0/distributed_a2a-0.1.9rc0-py3-none-any.whl

---
+++ packages/distributed-a2a/distributed_a2a-0.1.9rc0-py3-none-any.whl/distributed_a2a/registry.py
@@ -0,0 +1,55 @@
+import json
+import asyncio
+import boto3
+import requests
+from langchain_core.tools import StructuredTool
+from a2a.types import AgentCard
+
+
+async def registry_heart_beat(name: str, registry_url: str, agent_card: AgentCard, interval_sec: int,
+                              get_expire_at: callable) -> None:
+    registry = AgentRegistryLookup(registry_url=registry_url)
+    while True:
+        try:
+            registry.put_agent_card(name=name, agent_card=agent_card.model_dump(), expire_at=get_expire_at())
+        except Exception as e:
+            print(f"Failed to send heart beat to registry: {e}")
+        await asyncio.sleep(interval_sec)
+
+
+class AgentRegistryLookup:
+    def __init__(self, registry_url: str):
+        self.registry_url = registry_url
+
+    def get_agent_cards(self) -> list[dict]:
+        response = requests.get(url=f"{self.registry_url}/agent-cards", timeout=30)
+        response.raise_for_status()
+        return response.json()
+
+    def put_agent_card(self, name: str, agent_card: dict, expire_at: int) -> None:
+        response = requests.put(
+            url=f"{self.registry_url}/agent-card/{name}",
+            params={"expire_at": str(expire_at)},
+            json=agent_card,
+            timeout=30
+        )
+        response.raise_for_status()
+
+    def as_tool(self) -> StructuredTool:
+        return StructuredTool.from_function(func=lambda: self.get_agent_cards(), name="agent_card_lookup",
+                                            description="Gets all available agent cards")
+
+class DynamoDbRegistryLookup:
+    def __init__(self, agent_card_tabel: str):
+        dynamo = boto3.resource("dynamodb", region_name="eu-central-1")
+        self.table = dynamo.Table(agent_card_tabel)
+
+    def get_agent_cards(self) -> list[dict]:
+
+        items = self.table.scan().get("Items", [])
+        cards: list[dict] = [json.loads(it["card"]) for it in items]
+        return cards
+
+    def as_tool(self) -> StructuredTool:
+        return StructuredTool.from_function(func=lambda: self.get_agent_cards(), name="agent_card_lookup",
+                                            description="Gets all available agent cards")
---
+++ packages/distributed-a2a/distributed_a2a-0.1.9rc0-py3-none-any.whl/distributed_a2a/router.py
@@ -0,0 +1,47 @@
+from typing import Any
+
+from a2a.server.apps import A2ARESTFastAPIApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.types import AgentCard, AgentSkill
+from fastapi import FastAPI
+
+from .executors import RoutingExecutor
+from .model import RouterConfig
+from .registry import DynamoDbRegistryLookup
+from .server import CAPABILITIES, AGENT_CARD_TABLE
+
+
+def load_router(router_dic: dict[str, Any]) -> FastAPI:
+    router_config = RouterConfig.model_validate(router_dic)
+    agent_card = AgentCard(
+        name="Router",
+        description="Agent to redirect to the best matching agent based on the agent card",
+        url=router_config.router.card.url,
+        version="1.0.0",
+        default_input_modes=router_config.router.card.default_input_modes,
+        default_output_modes=router_config.router.card.default_output_modes,
+        skills=[AgentSkill(
+            id='routing',
+            name='Agent routing',
+            description='Identifies the most suitable agent for the given task and returns the agent card',
+            tags=['agent', 'routing']
+        )],
+        preferred_transport=router_config.router.card.preferred_transport_protocol,
+        capabilities=CAPABILITIES
+    )
+
+    executor = RoutingExecutor(
+        router_config=router_config,
+        routing_tool=DynamoDbRegistryLookup(agent_card_tabel=AGENT_CARD_TABLE).as_tool()
+
+    )
+
+    return A2ARESTFastAPIApplication(
+        agent_card=agent_card,
+        http_handler=DefaultRequestHandler(
+            agent_executor=executor,
+            task_store=InMemoryTaskStore()  # TODO replace with dynamodb store
+
+        )).build(title=agent_card.name,
+                 root_path=f"/{router_config.router.card.name}")
