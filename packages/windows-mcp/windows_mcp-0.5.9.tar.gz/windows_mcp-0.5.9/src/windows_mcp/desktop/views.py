a1b0b72e8bb5234875f9805b9d4cbf8c7a8b8c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:06:17 +0000

    Add package windows-mcp/0.5.9/windows_mcp-0.5.9.tar.gz

---
+++ packages/windows-mcp/windows_mcp-0.5.9.tar.gz/windows_mcp-0.5.9/src/windows_mcp/desktop/views.py
@@ -0,0 +1,60 @@
+from windows_mcp.tree.views import TreeState,BoundingBox
+from dataclasses import dataclass
+from tabulate import tabulate
+from typing import Optional
+from PIL.Image import Image
+from enum import Enum
+
+class Browser(Enum):
+    CHROME='Chrome'
+    EDGE='Edge'
+    FIREFOX='Firefox'
+
+class Status(Enum):
+    MAXIMIZED='Maximized'
+    MINIMIZED='Minimized'
+    NORMAL='Normal'
+    HIDDEN='Hidden'
+
+
+@dataclass
+class App:
+    name:str
+    runtime_id:tuple[int]
+    is_browser:bool
+    depth:int
+    status:Status
+    bounding_box:BoundingBox
+    handle: int
+    process_id:int
+    
+    def to_row(self):
+        return [self.name, self.depth, self.status.value, self.bounding_box.width, self.bounding_box.height, self.handle]
+
+@dataclass
+class Size:
+    width:int
+    height:int
+
+    def to_string(self):
+        return f'({self.width},{self.height})'
+
+@dataclass
+class DesktopState:
+    apps:list[App]
+    active_app:Optional[App]
+    screenshot:Optional[Image]=None
+    tree_state:Optional[TreeState]=None
+
+    def active_app_to_string(self):
+        if self.active_app is None:
+            return 'No active app found'
+        headers = ["Name", "Depth", "Status", "Width", "Height", "Handle"]
+        return tabulate([self.active_app.to_row()], headers=headers, tablefmt="simple")
+
+    def apps_to_string(self):
+        if not self.apps:
+            return 'No apps running in background'
+        headers = ["Name", "Depth", "Status", "Width", "Height", "Handle"]
+        rows = [app.to_row() for app in self.apps]
+        return tabulate(rows, headers=headers, tablefmt="simple")
