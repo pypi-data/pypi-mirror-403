4893ce52852751cddd8e4808bd48111b7a590930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 19:10:31 +0000

    Add package torch-l1-snr/0.1.1/torch_l1_snr-0.1.1-py3-none-any.whl

---
+++ packages/torch-l1-snr/torch_l1_snr-0.1.1-py3-none-any.whl/torch_l1_snr/__init__.py
@@ -0,0 +1,17 @@
+from .l1snr import (
+    dbrms,
+    L1SNRLoss,
+    L1SNRDBLoss,
+    STFTL1SNRDBLoss,
+    MultiL1SNRDBLoss,
+)
+
+__all__ = [
+    "dbrms",
+    "L1SNRLoss",
+    "L1SNRDBLoss",
+    "STFTL1SNRDBLoss",
+    "MultiL1SNRDBLoss",
+] 
+
+__version__ = "0.1.1"
