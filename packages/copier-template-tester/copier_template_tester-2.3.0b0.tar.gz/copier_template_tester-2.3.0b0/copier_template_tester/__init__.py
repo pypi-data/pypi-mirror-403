a2b1108934a9e205ac83adb64864c0a72f3ad1a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 17:09:07 +0000

    Add package copier-template-tester/2.3.0b0/copier_template_tester-2.3.0b0.tar.gz

---
+++ packages/copier-template-tester/copier_template_tester-2.3.0b0.tar.gz/copier_template_tester-2.3.0b0/copier_template_tester/__init__.py
@@ -0,0 +1,11 @@
+"""copier_template_tester."""
+
+from ._runtime_type_check_setup import configure_runtime_type_checking_mode
+
+__version__ = '2.3.0b0'
+__pkg_name__ = 'copier_template_tester'
+
+configure_runtime_type_checking_mode()
+
+
+# == Above code must always be first ==
