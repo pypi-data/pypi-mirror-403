5607bb29c3fa5a5a85e9c25eb99cda4bc70ccb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 06:10:07 +0000

    Add package large-image-source-multi/1.34.0/large_image_source_multi-1.34.0.tar.gz

---
+++ packages/large-image-source-multi/large_image_source_multi-1.34.0.tar.gz/large_image_source_multi-1.34.0/setup.py
@@ -0,0 +1,57 @@
+import os
+
+from setuptools import find_packages, setup
+
+description = 'A tilesource for large_image to composite other tile sources'
+long_description = description + '\n\nSee the large-image package for more details.'
+
+
+try:
+    from setuptools_scm import get_version
+
+    version = get_version(root='../..')
+    limit_version = f'>={version}' if '+' not in version and not os.getenv('TOX_ENV_NAME') else ''
+except (ImportError, LookupError):
+    limit_version = ''
+
+setup(
+    name='large-image-source-multi',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    license='Apache-2.0',
+    author='Kitware, Inc.',
+    author_email='kitware@kitware.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        'jsonschema',
+        f'large-image{limit_version}',
+        'pyyaml',
+    ],
+    extras_require={
+        'all': [
+            'scikit-image',
+        ],
+        'girder': f'girder-large-image{limit_version}',
+    },
+    keywords='large_image, tile source',
+    packages=find_packages(exclude=['test', 'test.*']),
+    url='https://github.com/girder/large_image',
+    entry_points={
+        'large_image.source': [
+            'multi = large_image_source_multi:MultiFileTileSource',
+        ],
+        'girder_large_image.source': [
+            'multi = large_image_source_multi.girder_source:MultiGirderTileSource',
+        ],
+    },
+)
