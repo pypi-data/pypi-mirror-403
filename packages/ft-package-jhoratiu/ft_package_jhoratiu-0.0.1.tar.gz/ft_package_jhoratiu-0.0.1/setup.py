25bbfdc7f02deba6eb246659cfbc60bb67171cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:08:03 +0000

    Add package ft-package-jhoratiu/0.0.1/ft_package_jhoratiu-0.0.1.tar.gz

---
+++ packages/ft-package-jhoratiu/ft_package_jhoratiu-0.0.1.tar.gz/ft_package_jhoratiu-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	author="jhoratiu",
+	author_email="jhoratiu@42.fr",
+	url="https://github.com/jhoratiu/ft_package"
+)
+
+# pip install build twine
+# python -m build
+# twine upload dist/*
