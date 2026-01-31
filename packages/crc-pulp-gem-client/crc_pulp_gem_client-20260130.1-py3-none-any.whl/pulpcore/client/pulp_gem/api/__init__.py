d3409d6bc975a5c93751cac73f1158f3240b50da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:08:00 +0000

    Add package crc-pulp-gem-client/20260130.1/crc_pulp_gem_client-20260130.1-py3-none-any.whl

---
+++ packages/crc-pulp-gem-client/crc_pulp_gem_client-20260130.1-py3-none-any.whl/pulpcore/client/pulp_gem/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_gem.api.content_gem_api import ContentGemApi
+from pulpcore.client.pulp_gem.api.distributions_gem_api import DistributionsGemApi
+from pulpcore.client.pulp_gem.api.publications_gem_api import PublicationsGemApi
+from pulpcore.client.pulp_gem.api.remotes_gem_api import RemotesGemApi
+from pulpcore.client.pulp_gem.api.repositories_gem_api import RepositoriesGemApi
+from pulpcore.client.pulp_gem.api.repositories_gem_versions_api import RepositoriesGemVersionsApi
+
