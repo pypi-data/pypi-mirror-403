33c7acfb50ad9e8e87826ee950575e37506e5103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 07:12:05 +0000

    Add package ai-edge-torch/0.7.2/ai_edge_torch-0.7.2-py3-none-any.whl

---
+++ packages/ai-edge-torch/ai_edge_torch-0.7.2-py3-none-any.whl/ai_edge_torch/__init__.py
@@ -0,0 +1,10 @@
+import warnings
+
+
+warnings.warn(
+    "\n\n'ai-edge-torch' is deprecated and has been renamed to 'litert-torch'. "
+    "Please update your dependencies and imports. The 'ai-edge-torch' package "
+    "will not receive further updates.\n",
+    DeprecationWarning,
+    stacklevel=2,
+)
