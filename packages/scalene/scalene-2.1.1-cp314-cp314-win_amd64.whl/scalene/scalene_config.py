97f4570a610b6d2a09bfbe2d9727d185d5bfad68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 17:08:57 +0000

    Add package scalene/2.1.1/scalene-2.1.1-cp314-cp314-win_amd64.whl

---
+++ packages/scalene/scalene-2.1.1-cp314-cp314-win_amd64.whl/scalene/scalene_config.py
@@ -0,0 +1,10 @@
+"""Current version of Scalene; reported by --version."""
+
+scalene_version = "2.1.1"
+scalene_date = "2026.01.30"
+
+# Port to use for Scalene UI
+SCALENE_PORT = 11235
+
+# Must equal src/include/sampleheap.hpp NEWLINE *minus 1*
+NEWLINE_TRIGGER_LENGTH = 98820  # SampleHeap<...>::NEWLINE-1
