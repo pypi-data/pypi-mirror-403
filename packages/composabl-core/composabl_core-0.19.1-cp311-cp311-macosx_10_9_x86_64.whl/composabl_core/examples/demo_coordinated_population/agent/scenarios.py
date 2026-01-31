1865d45139836a29d6be68f8a034b9d13d3dd2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 08:06:40 +0000

    Add package composabl-core/0.19.1/composabl_core-0.19.1-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/composabl-core/composabl_core-0.19.1-cp311-cp311-macosx_10_9_x86_64.whl/composabl_core/examples/demo_coordinated_population/agent/scenarios.py
@@ -0,0 +1,18 @@
+# Copyright (C) Composabl, Inc - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+
+scenarios = [
+    {
+        "counter": {
+            "data": 0,
+            "type": "is_equal",
+        },
+    },
+    {
+        "counter": {"data": -25, "type": "is_equal"},
+    },
+    {
+        "counter": {"data": 25, "type": "is_equal"},
+    },
+]
