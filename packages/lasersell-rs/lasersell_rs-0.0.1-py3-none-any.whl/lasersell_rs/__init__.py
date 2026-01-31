0c6666ea2bc80724f45f593a2cf8e46c2c13711c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:06:01 +0000

    Add package lasersell-rs/0.0.1/lasersell_rs-0.0.1-py3-none-any.whl

---
+++ packages/lasersell-rs/lasersell_rs-0.0.1-py3-none-any.whl/lasersell_rs/__init__.py
@@ -0,0 +1,2 @@
+MESSAGE = "Name reserved for security reasons.\nLaserSell is not distributed via this registry.\nOfficial downloads and documentation:\nhttps://dl.lasersell.io"
+raise RuntimeError(MESSAGE)
