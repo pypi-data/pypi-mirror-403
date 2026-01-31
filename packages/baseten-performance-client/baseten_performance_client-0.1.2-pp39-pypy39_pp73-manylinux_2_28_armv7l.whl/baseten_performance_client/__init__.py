79e26d005fe9ccb39a96a61c8c8dbba987754198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 06:11:26 +0000

    Add package baseten-performance-client/0.1.2/baseten_performance_client-0.1.2-pp39-pypy39_pp73-manylinux_2_28_armv7l.whl

---
+++ packages/baseten-performance-client/baseten_performance_client-0.1.2-pp39-pypy39_pp73-manylinux_2_28_armv7l.whl/baseten_performance_client/__init__.py
@@ -0,0 +1,5 @@
+from .baseten_performance_client import *
+
+__doc__ = baseten_performance_client.__doc__
+if hasattr(baseten_performance_client, "__all__"):
+    __all__ = baseten_performance_client.__all__
