ef81e39b478c9948fa035b39d62acd5e7e467ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:08:55 +0000

    Add package sphinxnotes-project/1.0a37/sphinxnotes_project-1.0a37.tar.gz

---
+++ packages/sphinxnotes-project/sphinxnotes_project-1.0a37.tar.gz/sphinxnotes_project-1.0a37/src/sphinxnotes/project/__init__.py
@@ -0,0 +1,27 @@
+"""
+sphinxnotes.project
+~~~~~~~~~~~~~~~~~~~
+
+Common sphinx extension for sphinxnotes project.
+
+:copyright: Copyright 2025 Shengyu Zhang
+:license: BSD, see LICENSE for details.
+"""
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sphinx.application import Sphinx
+
+from . import meta
+
+
+def setup(app: Sphinx):
+    meta.pre_setup(app)
+
+    from . import anyobjtypes
+
+    anyobjtypes.setup(app)
+
+    return meta.post_setup(app)
