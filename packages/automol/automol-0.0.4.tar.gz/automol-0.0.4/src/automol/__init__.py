27bd5d9965c87c11732569558a22d29f2df6d75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:18:31 +0000

    Add package automol/0.0.4/automol-0.0.4.tar.gz

---
+++ packages/automol/automol-0.0.4.tar.gz/automol-0.0.4/src/automol/__init__.py
@@ -0,0 +1,8 @@
+"""automol."""
+
+__version__ = "0.0.4"
+
+from . import geom, types
+from .geom import Geometry
+
+__all__ = ["geom", "types", "Geometry"]
---
+++ packages/automol/automol-0.0.4.tar.gz/automol-0.0.4/src/automol/types/__init__.py
@@ -0,0 +1,6 @@
+"""Types."""
+
+from .core import FloatArray
+from .pydantic import CoordinatesField, FloatArrayField
+
+__all__ = ["FloatArray", "CoordinatesField", "FloatArrayField"]
