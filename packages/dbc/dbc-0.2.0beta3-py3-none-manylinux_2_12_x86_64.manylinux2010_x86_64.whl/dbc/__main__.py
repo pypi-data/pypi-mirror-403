781526a3104b8bdba2815cb02e6eb96aeffa7cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 09:09:26 +0000

    Add package dbc/0.2.0b3/dbc-0.2.0beta3-py3-none-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/dbc/dbc-0.2.0beta3-py3-none-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/dbc/__main__.py
@@ -0,0 +1,8 @@
+import os, sys
+argv = [os.path.join(os.path.dirname(__file__), "dbc"), *sys.argv[1:]]
+if os.name == 'posix':
+    os.execv(argv[0], argv)
+else:
+    import subprocess; sys.exit(subprocess.call(argv))
+
+def dummy(): """Dummy function for an entrypoint. dbc is executed as a side effect of the import."""
