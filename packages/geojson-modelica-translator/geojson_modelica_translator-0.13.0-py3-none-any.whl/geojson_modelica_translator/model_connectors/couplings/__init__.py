434af705ab20f9882169d189556e2cdd914874c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 11:07:38 +0000

    Add package geojson-modelica-translator/0.13.0/geojson_modelica_translator-0.13.0-py3-none-any.whl

---
+++ packages/geojson-modelica-translator/geojson_modelica_translator-0.13.0-py3-none-any.whl/geojson_modelica_translator/model_connectors/couplings/__init__.py
@@ -0,0 +1,5 @@
+# :copyright (c) URBANopt, Alliance for Energy Innovation, LLC, and other contributors.
+# See also https://github.com/urbanopt/geojson-modelica-translator/blob/develop/LICENSE.md
+
+from .coupling import Coupling  # noqa: F401
+from .graph import CouplingGraph  # noqa: F401
