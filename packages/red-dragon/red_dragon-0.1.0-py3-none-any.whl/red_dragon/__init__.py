4b2d31d3c31291558c78ef224a3512f744b4f15a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 06:10:41 +0000

    Add package red-dragon/0.1.0/red_dragon-0.1.0-py3-none-any.whl

---
+++ packages/red-dragon/red_dragon-0.1.0-py3-none-any.whl/red_dragon/__init__.py
@@ -0,0 +1,9 @@
+"""Red Dragon Package
+
+A Python package that awakens the red dragon!
+"""
+
+__version__ = "0.1.0"
+__author__ = "karzimeg"
+
+print("red-dragon package installed, type 'red-dragon' to use")
