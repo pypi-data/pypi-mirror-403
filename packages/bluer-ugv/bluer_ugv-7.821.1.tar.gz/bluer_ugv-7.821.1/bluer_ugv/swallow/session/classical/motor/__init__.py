24337e5355feb8eb3c68a3a7b5df823bc4ec65c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:07:17 +0000

    Add package bluer-ugv/7.821.1/bluer_ugv-7.821.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-7.821.1.tar.gz/bluer_ugv-7.821.1/bluer_ugv/swallow/session/classical/motor/__init__.py
@@ -0,0 +1,4 @@
+from bluer_ugv.swallow.session.classical.motor.left import ClassicalLeftMotor
+from bluer_ugv.swallow.session.classical.motor.right import ClassicalRightMotor
+from bluer_ugv.swallow.session.classical.motor.rear import ClassicalRearMotors
+from bluer_ugv.swallow.session.classical.motor.steering import ClassicalSteeringMotor
