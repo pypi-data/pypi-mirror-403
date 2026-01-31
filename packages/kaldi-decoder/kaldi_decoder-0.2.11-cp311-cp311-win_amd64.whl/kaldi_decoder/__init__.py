4308f25c94b161c833a3d6361367ea7bc0c2a7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:18:55 +0000

    Add package kaldi-decoder/0.2.11/kaldi_decoder-0.2.11-cp311-cp311-win_amd64.whl

---
+++ packages/kaldi-decoder/kaldi_decoder-0.2.11-cp311-cp311-win_amd64.whl/kaldi_decoder/__init__.py
@@ -0,0 +1,10 @@
+from _kaldi_decoder import (
+    DecodableCtc,
+    DecodableInterface,
+    FasterDecoder,
+    FasterDecoderOptions,
+    LatticeSimpleDecoder,
+    LatticeSimpleDecoderConfig,
+    SimpleDecoder,
+)
+__version__ = '0.2.11'
