6961db65f605885056e4f75336468f8340528625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:08:06 +0000

    Add package moysklad-api/0.5.7a1/moysklad_api-0.5.7a1-py3-none-any.whl

---
+++ packages/moysklad-api/moysklad_api-0.5.7a1-py3-none-any.whl/moysklad_api/types/attribute.py
@@ -0,0 +1,10 @@
+from pydantic import Field
+
+from .entity import Entity
+
+
+class Attribute(Entity):
+    id: str | None = Field(None, alias="id")
+    name: str | None = Field(None, alias="name")
+    type: str | None = Field(None, alias="type")
+    value: str | None = Field(None, alias="value")
