5c95f48228fb76b4e1586fcbb7f7e07720d742cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:07:19 +0000

    Add package djangotenberg/0.1.0/djangotenberg-0.1.0-py3-none-any.whl

---
+++ packages/djangotenberg/djangotenberg-0.1.0-py3-none-any.whl/djangotenberg/retry.py
@@ -0,0 +1,13 @@
+import time
+
+def retry(func, retries=3, delay=0.3):
+    for i in range(retries):
+        try:
+            print(f"Retry {i + 1}")
+            if callable(func):
+                return func()
+            return func
+        except Exception as e:
+            if i == retries - 1:
+                raise e
+            time.sleep(delay)
