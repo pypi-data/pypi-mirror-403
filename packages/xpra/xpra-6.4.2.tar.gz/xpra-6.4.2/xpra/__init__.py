58d14dff18e8443828567ab2314b05fdea02c501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:07:36 +0000

    Add package xpra/6.4.2/xpra-6.4.2.tar.gz

---
+++ packages/xpra/xpra-6.4.2.tar.gz/xpra-6.4.2/xpra/__init__.py
@@ -0,0 +1,8 @@
+# This file is part of Xpra.
+# Copyright (C) 2008 Nathaniel Smith <njs@pobox.com>
+# Copyright (C) 2009 Antoine Martin <antoine@xpra.org>
+# Xpra is released under the terms of the GNU GPL v2, or, at your option, any
+# later version. See the file COPYING for details.
+
+__version_info__ = (6, 4, 2)
+__version__ = "6.4.2"
