a3170e250066b6442f3fae44dfd0423c2975192f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:07:30 +0000

    Add package docc-compiler/0.0.1/docc_compiler-0.0.1-cp314-cp314-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/docc-compiler/docc_compiler-0.0.1-cp314-cp314-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl/include/symengine/symengine_config_cling.h
@@ -0,0 +1,7 @@
+#ifndef SYMENGINE_CONFIG_CLING_HPP
+#define SYMENGINE_CONFIG_CLING_HPP
+
+#pragma cling add_library_path("/tmp/tmpkl_c668p/wheel/platlib/lib64")
+#pragma cling load("libsymengine")
+
+#endif
