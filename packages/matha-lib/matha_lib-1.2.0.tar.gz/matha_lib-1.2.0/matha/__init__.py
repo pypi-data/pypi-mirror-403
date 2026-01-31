a9f01f26d87487908bbe636d6da95cbd06bd5c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:07:57 +0000

    Add package matha-lib/1.2.0/matha_lib-1.2.0.tar.gz

---
+++ packages/matha-lib/matha_lib-1.2.0.tar.gz/matha_lib-1.2.0/matha/__init__.py
@@ -0,0 +1,32 @@
+# src/__init__.py
+# -------------------------
+# @Author : AstroJr0
+# @Date : 16-12-2025
+# @Last-Modified : 30-01-2026
+# -------------------------
+# Import all functions from core modules and expose them
+
+from .algebra import *
+from .calculas import *
+from .geometry import *
+from .linalg import *
+from .numTheory import *
+from .stats import *
+from .trigonometry import *
+from .regression import *
+from .machine_learning import *
+from .vector import *
+from .special_functions import *
+from .graph import *
+from .solvers import *
+from .sort import *
+
+__version__ = "1.2.0"
+
+# Define __all__ for explicit export control (good practice)
+__all__ = [
+    # List functions/classes you guys want to export here, 
+    # but using '*' imports above is often simpler for utility libraries. its efficent :)
+]
+
+
