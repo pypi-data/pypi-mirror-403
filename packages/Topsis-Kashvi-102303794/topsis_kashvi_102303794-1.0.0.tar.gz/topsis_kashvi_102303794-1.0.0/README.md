6f101e8a1c19c343069c741b21db4e44839af461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:06:38 +0000

    Add package Topsis-Kashvi-102303794/1.0.0/topsis_kashvi_102303794-1.0.0.tar.gz

---
+++ packages/Topsis-Kashvi-102303794/topsis_kashvi_102303794-1.0.0.tar.gz/topsis_kashvi_102303794-1.0.0/README.md
@@ -0,0 +1,12 @@
+# TOPSIS Package
+
+This package implements the TOPSIS decision-making method.
+
+## Installation
+pip install Topsis-Kashvi-102303794
+
+## Usage
+```python
+from topsis_kashvi_102303794 import topsis
+
+topsis("input.csv", "1,1,1,2", "+,+,-,+", "output.csv")
---
+++ packages/Topsis-Kashvi-102303794/topsis_kashvi_102303794-1.0.0.tar.gz/topsis_kashvi_102303794-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis-Kashvi-102303794",
+    version="1.0.0",
+    author="Kashvi",
+    author_email="kmittal_be23@thapar.edu",
+    description="A Python package for TOPSIS method",
+    packages=find_packages(),
+    install_requires=["pandas", "numpy"],
+)
