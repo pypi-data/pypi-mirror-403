36840800a75ff56edfacc0f672277527512debe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 07:12:50 +0000

    Add package alibabacloud-rds20140815/15.1.1/alibabacloud_rds20140815-15.1.1-py3-none-any.whl

---
+++ packages/alibabacloud-rds20140815/alibabacloud_rds20140815-15.1.1-py3-none-any.whl/alibabacloud_rds20140815/models/_describe_rccluster_nodes_request.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from __future__ import annotations
+
+from darabonba.model import DaraModel
+
+class DescribeRCClusterNodesRequest(DaraModel):
+    def __init__(
+        self,
+        cluster_id: str = None,
+        node_pool_id: str = None,
+        page_number: int = None,
+        page_size: int = None,
+        region_id: str = None,
+        vpc_id: str = None,
+    ):
+        self.cluster_id = cluster_id
+        self.node_pool_id = node_pool_id
+        # The page number.
+        self.page_number = page_number
+        # The number of entries per page. Valid values: **1 to 100**.
+        # 
+        # Default value: **30**.
+        self.page_size = page_size
+        # The region ID.
+        self.region_id = region_id
+        # The virtual private cloud (VPC) ID.
+        # 
+        # >  This is a reserved parameter.
+        self.vpc_id = vpc_id
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        _map = super().to_map()
+        if _map is not None:
+            result = _map
+        if self.cluster_id is not None:
+            result['ClusterId'] = self.cluster_id
+
+        if self.node_pool_id is not None:
+            result['NodePoolId'] = self.node_pool_id
+
+        if self.page_number is not None:
+            result['PageNumber'] = self.page_number
+
+        if self.page_size is not None:
+            result['PageSize'] = self.page_size
+
+        if self.region_id is not None:
+            result['RegionId'] = self.region_id
+
+        if self.vpc_id is not None:
+            result['VpcId'] = self.vpc_id
+
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('ClusterId') is not None:
+            self.cluster_id = m.get('ClusterId')
+
+        if m.get('NodePoolId') is not None:
+            self.node_pool_id = m.get('NodePoolId')
+
+        if m.get('PageNumber') is not None:
+            self.page_number = m.get('PageNumber')
+
+        if m.get('PageSize') is not None:
+            self.page_size = m.get('PageSize')
+
+        if m.get('RegionId') is not None:
+            self.region_id = m.get('RegionId')
+
+        if m.get('VpcId') is not None:
+            self.vpc_id = m.get('VpcId')
+
+        return self
+
---
+++ packages/alibabacloud-rds20140815/alibabacloud_rds20140815-15.1.1-py3-none-any.whl/alibabacloud_rds20140815/models/_reboot_rcinstance_request.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from __future__ import annotations
+
+from darabonba.model import DaraModel
+
+class RebootRCInstanceRequest(DaraModel):
+    def __init__(
+        self,
+        dry_run: bool = None,
+        force_stop: bool = None,
+        instance_id: str = None,
+        reboot_time: str = None,
+        region_id: str = None,
+    ):
+        # Specifies whether to perform only a dry run, without performing the actual request. Valid values:
+        # 
+        # *   **true**: performs only a dry run. The system checks the request for potential issues, including missing parameter values, incorrect request syntax, service limits, and insufficient inventory errors. If the request passes the dry run, the DryRunOperation error code is returned. Otherwise, an error message is returned.
+        # *   **false**: performs a dry run and performs the actual request. If the request passes the dry run, the instance is restarted.
+        # 
+        # Default value: false
+        self.dry_run = dry_run
+        # Specifies whether to forcefully stop the instance before you restart the instance Valid values:
+        # 
+        # *   **true**: forcefully stops the instance. This operation is equivalent to the typical power-off operation. Cache data that is not written to storage devices on the instance is lost.
+        # *   **false** (default): normally stops the instance.
+        self.force_stop = force_stop
+        # The instance ID.
+        # 
+        # This parameter is required.
+        self.instance_id = instance_id
+        self.reboot_time = reboot_time
+        # The region ID.
+        self.region_id = region_id
+
+    def validate(self):
+        pass
+
+    def to_map(self):
+        result = dict()
+        _map = super().to_map()
+        if _map is not None:
+            result = _map
+        if self.dry_run is not None:
+            result['DryRun'] = self.dry_run
+
+        if self.force_stop is not None:
+            result['ForceStop'] = self.force_stop
+
+        if self.instance_id is not None:
+            result['InstanceId'] = self.instance_id
+
+        if self.reboot_time is not None:
+            result['RebootTime'] = self.reboot_time
+
+        if self.region_id is not None:
+            result['RegionId'] = self.region_id
+
+        return result
+
+    def from_map(self, m: dict = None):
+        m = m or dict()
+        if m.get('DryRun') is not None:
+            self.dry_run = m.get('DryRun')
+
+        if m.get('ForceStop') is not None:
+            self.force_stop = m.get('ForceStop')
+
+        if m.get('InstanceId') is not None:
+            self.instance_id = m.get('InstanceId')
+
+        if m.get('RebootTime') is not None:
+            self.reboot_time = m.get('RebootTime')
+
+        if m.get('RegionId') is not None:
+            self.region_id = m.get('RegionId')
+
+        return self
+
