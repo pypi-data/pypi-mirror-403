e6e496a536032f57392cf6a4f27ebcf1bc5907a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:07:28 +0000

    Add package publicity-idl/0.1.3/publicity_idl-0.1.3.tar.gz

---
+++ packages/publicity-idl/publicity_idl-0.1.3.tar.gz/publicity_idl-0.1.3/README.md
@@ -0,0 +1,12 @@
+# 商讯 / 舆情 IDL
+IDL仓库用于管理 【商讯 / 舆情】 项目的数据模型、枚举、常量等
+
+### 如何发布新的IDL版本？
+请仔细阅读指引文档
+https://kcn67g8bllja.feishu.cn/wiki/XFNswWUbziJ3E0kAw8TcjkVvnte
+
+### GitLab 群组地址
+https://git.xmu.edu.cn/innovation
+
+### 项目技术架构文档
+https://kcn67g8bllja.feishu.cn/wiki/Utr8wiXK9igQwfk2H8OcSIJAnNd
---
+++ packages/publicity-idl/publicity_idl-0.1.3.tar.gz/publicity_idl-0.1.3/publish.bat
@@ -0,0 +1,37 @@
+﻿@echo off
+set PYTHONUTF8=1
+chcp 65001 >nul
+
+:: 1. 清理旧构建产物
+echo 清理旧构建产物...
+if exist dist (
+    rmdir /s /q dist
+)
+mkdir dist 2>nul
+
+:: 2. 自动递增版本号并获取新版本号
+echo 自动递增版本号...
+for /f "delims=" %%v in ('python bump_version.py') do set "NEW_VERSION=%%v"
+echo 版本已更新为: %NEW_VERSION%
+
+:: 3. 构建包
+echo 构建新版本...
+hatch build
+
+:: 4. 处理 Git 提交
+echo 准备提交代码...
+set "COMMIT_MSG=idl更新 - %NEW_VERSION%"
+if not "%~1"=="" (
+    set "COMMIT_MSG=%NEW_VERSION% - %~1"
+)
+
+git add .
+git commit -m "%COMMIT_MSG%"
+git push
+
+:: 5. 上传到 PyPI
+echo 上传到 PyPI...
+twine upload dist/*
+
+echo 发布完成！
+echo 提交信息: %COMMIT_MSG%
