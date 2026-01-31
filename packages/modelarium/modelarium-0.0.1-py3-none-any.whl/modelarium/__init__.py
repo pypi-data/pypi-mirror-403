7b879f83d73e890052e31d1600d1af629a152890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 11:07:47 +0000

    Add package modelarium/0.0.1/modelarium-0.0.1-py3-none-any.whl

---
+++ packages/modelarium/modelarium-0.0.1-py3-none-any.whl/modelarium/__init__.py
@@ -0,0 +1,9 @@
+"""
+Modelarium Python wrapper.
+
+This is a placeholder package reserving the PyPI name.
+The full Python interface to the Modelarium Java library
+will be added in a future release.
+"""
+
+__version__ = "0.0.1"
