a1e501ba3fb550a554b2ecbcabdfce32f499ca55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:07:01 +0000

    Add package pylife/2.2.0rc1/pylife-2.2.0rc1-cp312-cp312-win32.whl

---
+++ packages/pylife/pylife-2.2.0rc1-cp312-cp312-win32.whl/pylife/mesh/__init__.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2019-2023 - for information on the respective copyright owner
+# see the NOTICE file and/or the repository
+# https://github.com/boschresearch/pylife
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = "Johannes Mueller"
+__maintainer__ = __author__
+
+from .meshsignal import PlainMesh, Mesh
+from .gradient import Gradient, Gradient3D
+from .hotspot import HotSpot
+from .meshmapping import Meshmapper
+from .surface import Surface3D
+
+__all__ = [
+    'PlainMesh',
+    'Mesh',
+    'Gradient',
+    'Gradient3D',
+    'HotSpot',
+    'Meshmapper',
+    'Surface3D'
+]
