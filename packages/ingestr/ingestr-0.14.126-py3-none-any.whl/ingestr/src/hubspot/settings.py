417f0cb196803e4d2294eb0d24a6995162b7c0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:07:35 +0000

    Add package ingestr/0.14.126/ingestr-0.14.126-py3-none-any.whl

---
+++ packages/ingestr/ingestr-0.14.126-py3-none-any.whl/ingestr/src/hubspot/settings.py
@@ -0,0 +1,120 @@
+# Copyright 2022-2025 ScaleVector
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Hubspot source settings and constants"""
+
+from dlt.common import pendulum
+
+STARTDATE = pendulum.datetime(year=2000, month=1, day=1)
+
+CRM_CONTACTS_ENDPOINT = (
+    "/crm/v3/objects/contacts?associations=companies,deals,products,tickets,quotes"
+)
+CRM_COMPANIES_ENDPOINT = "/crm/v3/objects/companies?associations=products"
+CRM_DEALS_ENDPOINT = (
+    "/crm/v3/objects/deals?associations=companies,contacts,products,tickets,quotes"
+)
+CRM_PRODUCTS_ENDPOINT = (
+    "/crm/v3/objects/products?associations=companies,contacts,deals,tickets,quotes"
+)
+CRM_TICKETS_ENDPOINT = (
+    "/crm/v3/objects/tickets?associations=companies,contacts,deals,products,quotes"
+)
+CRM_QUOTES_ENDPOINT = (
+    "/crm/v3/objects/quotes?associations=companies,contacts,deals,products,tickets"
+)
+CRM_SCHEMAS_ENDPOINT = "/crm/v3/schemas"
+
+CRM_OBJECT_ENDPOINTS = {
+    "contact": CRM_CONTACTS_ENDPOINT,
+    "company": CRM_COMPANIES_ENDPOINT,
+    "deal": CRM_DEALS_ENDPOINT,
+    "product": CRM_PRODUCTS_ENDPOINT,
+    "ticket": CRM_TICKETS_ENDPOINT,
+    "quote": CRM_QUOTES_ENDPOINT,
+}
+
+WEB_ANALYTICS_EVENTS_ENDPOINT = "/events/v3/events?objectType={objectType}&objectId={objectId}&occurredAfter={occurredAfter}&occurredBefore={occurredBefore}&sort=-occurredAt"
+
+OBJECT_TYPE_SINGULAR = {
+    "companies": "company",
+    "contacts": "contact",
+    "deals": "deal",
+    "tickets": "ticket",
+    "products": "product",
+    "quotes": "quote",
+}
+
+OBJECT_TYPE_PLURAL = {v: k for k, v in OBJECT_TYPE_SINGULAR.items()}
+
+DEFAULT_DEAL_PROPS = [
+    "amount",
+    "closedate",
+    "createdate",
+    "dealname",
+    "dealstage",
+    "hs_lastmodifieddate",
+    "hs_object_id",
+    "pipeline",
+]
+
+DEFAULT_COMPANY_PROPS = [
+    "createdate",
+    "domain",
+    "hs_lastmodifieddate",
+    "hs_object_id",
+    "name",
+]
+
+DEFAULT_CONTACT_PROPS = [
+    "createdate",
+    "email",
+    "firstname",
+    "hs_object_id",
+    "lastmodifieddate",
+    "lastname",
+]
+
+DEFAULT_TICKET_PROPS = [
+    "createdate",
+    "content",
+    "hs_lastmodifieddate",
+    "hs_object_id",
+    "hs_pipeline",
+    "hs_pipeline_stage",
+    "hs_ticket_category",
+    "hs_ticket_priority",
+    "subject",
+]
+
+DEFAULT_PRODUCT_PROPS = [
+    "createdate",
+    "description",
+    "hs_lastmodifieddate",
+    "hs_object_id",
+    "name",
+    "price",
+]
+
+DEFAULT_QUOTE_PROPS = [
+    "hs_createdate",
+    "hs_expiration_date",
+    "hs_lastmodifieddate",
+    "hs_object_id",
+    "hs_public_url_key",
+    "hs_status",
+    "hs_title",
+]
+
+ALL = ("ALL",)
