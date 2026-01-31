006ad58e872c700b47fe889b5ddbaa49b8278a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:45:33 +0000

    Add package hh-applicant-tool/1.5.12/hh_applicant_tool-1.5.12.tar.gz

---
+++ packages/hh-applicant-tool/hh_applicant_tool-1.5.12.tar.gz/hh_applicant_tool-1.5.12/src/hh_applicant_tool/operations/update_resumes.py
@@ -0,0 +1,47 @@
+# Этот модуль можно использовать как образец для других
+from __future__ import annotations
+
+import argparse
+import logging
+from typing import TYPE_CHECKING
+
+from ..api import ApiError, datatypes
+from ..main import BaseNamespace, BaseOperation
+from ..utils.string import shorten
+
+if TYPE_CHECKING:
+    from ..main import HHApplicantTool
+
+
+logger = logging.getLogger(__package__)
+
+
+class Namespace(BaseNamespace):
+    pass
+
+
+class Operation(BaseOperation):
+    """Обновить все резюме"""
+
+    __aliases__ = ["update"]
+
+    def setup_parser(self, parser: argparse.ArgumentParser) -> None:
+        pass
+
+    def run(self, tool: HHApplicantTool) -> None:
+        resumes: list[datatypes.Resume] = tool.get_resumes()
+        # Там вызов API меняет поля
+        tool.storage.resumes.save_batch(resumes)
+
+        for resume in resumes:
+            if not resume.get("can_publish_or_update"):
+                logger.warning(f"Не могу обновить: {resume['alternate_url']}")
+                continue
+            try:
+                r = tool.api_client.post(
+                    f"/resumes/{resume['id']}/publish",
+                )
+                assert {} == r
+                print("✅ Обновлено", shorten(resume["title"]))
+            except ApiError as ex:
+                logger.error(f"Ошибка при обновлении резюме: {ex}")
