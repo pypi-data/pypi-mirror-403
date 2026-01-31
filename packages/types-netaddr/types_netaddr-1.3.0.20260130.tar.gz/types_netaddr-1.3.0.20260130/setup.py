9c0489a876eacb27b44447393eb62889af97ecfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:05:54 +0000

    Add package types-netaddr/1.3.0.20260130/types_netaddr-1.3.0.20260130.tar.gz

---
+++ packages/types-netaddr/types_netaddr-1.3.0.20260130.tar.gz/types_netaddr-1.3.0.20260130/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'netaddr-stubs': ['__init__.pyi', 'cli.pyi', 'compat.pyi', 'contrib/__init__.pyi', 'contrib/subnet_splitter.pyi', 'core.pyi', 'eui/__init__.pyi', 'eui/ieee.pyi', 'fbsocket.pyi', 'ip/__init__.pyi', 'ip/glob.pyi', 'ip/iana.pyi', 'ip/nmap.pyi', 'ip/rfc1924.pyi', 'ip/sets.pyi', 'strategy/__init__.pyi', 'strategy/eui48.pyi', 'strategy/eui64.pyi', 'strategy/ipv4.pyi', 'strategy/ipv6.pyi', 'METADATA.toml', 'py.typed']})
