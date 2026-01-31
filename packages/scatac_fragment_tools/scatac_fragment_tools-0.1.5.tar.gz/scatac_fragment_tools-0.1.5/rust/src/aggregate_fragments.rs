3c827fb11606e42af9139aa46e9942dc11d569c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 17:09:52 +0000

    Add package scatac_fragment_tools/0.1.5/scatac_fragment_tools-0.1.5.tar.gz

---
+++ packages/scatac_fragment_tools/scatac_fragment_tools-0.1.5.tar.gz/scatac_fragment_tools-0.1.5/rust/src/aggregate_fragments.rs
@@ -0,0 +1,208 @@
+use bgzip::BGZFReader;
+use core::fmt;
+use rust_htslib::bgzf::Writer;
+use rust_htslib::tpool::ThreadPool;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::Write;
+
+/// Aggregates multiple fragment files into a single file
+/// This code is just a fancy implementation of the unix command `cat | sort -k1,1 -k2,2n -k3,3n | bgzip`
+/// And might not be super efficient.
+///
+/// It would be better to make an implementation that makes use of the bgzip blocks and the fact that the files are already sorted
+/// If someone wants and knows how to do that, please do!
+
+/// Struct representing a fragment, used for sorting
+///
+/// # Fields
+///
+/// * `chrom` - Chromosome name.
+/// * `start` - Start position.
+/// * `end` - End position.
+/// * `cell_barcode` - Cell barcode.
+/// * `score` - Optional score.
+
+#[derive(PartialEq, Eq)]
+struct Fragment {
+    chrom: String,
+    start: usize,
+    end: usize,
+    cell_barcode: String,
+    score: Option<usize>,
+}
+
+impl Fragment {
+    /// Create a new Fragment from a string.
+    ///
+    /// # Arguments
+    ///
+    /// * `s` - String to parse.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// let fragment = Fragment::new_from_string("chr1\t100\t200\tAACATCGATGGATG-1\t10");
+    /// assert_eq!(fragment.chrom, "chr1");
+    /// assert_eq!(fragment.start, 100);
+    /// assert_eq!(fragment.end, 200);
+    /// assert_eq!(fragment.cell_barcode, "AACATCGATGGATG-1");
+    /// assert_eq!(fragment.score, Some(10));
+    /// ```
+    fn new_from_string(s: &str) -> Fragment {
+        let fields: Vec<&str> = s.split('\t').collect();
+        match fields.len() {
+            4 => Fragment {
+                chrom: fields[0].to_string(),
+                start: fields[1].parse::<usize>().unwrap(),
+                end: fields[2].parse::<usize>().unwrap(),
+                cell_barcode: fields[3].to_string(),
+                score: None,
+            },
+            5 => Fragment {
+                chrom: fields[0].to_string(),
+                start: fields[1].parse::<usize>().unwrap(),
+                end: fields[2].parse::<usize>().unwrap(),
+                cell_barcode: fields[3].to_string(),
+                score: Some(fields[4].parse::<usize>().unwrap()),
+            },
+            _ => Fragment {
+                chrom: fields[0].to_string(),
+                start: fields[1].parse::<usize>().unwrap(),
+                end: fields[2].parse::<usize>().unwrap(),
+                cell_barcode: fields[3].to_string(),
+                score: Some(fields[4].parse::<usize>().unwrap()),
+            },
+        }
+    }
+}
+
+impl Ord for Fragment {
+    fn cmp(&self, other: &Fragment) -> std::cmp::Ordering {
+        let self_chrom = &self.chrom;
+        let other_chrom = &other.chrom;
+
+        let self_start = &self.start;
+        let other_start = &other.start;
+
+        let self_end = &self.end;
+        let other_end = &other.end;
+
+        let self_cell_barcode = &self.cell_barcode;
+        let other_cell_barcode = &other.cell_barcode;
+
+        if self_chrom != other_chrom {
+            self_chrom.cmp(other_chrom)
+        } else if self_start != other_start {
+            return self_start.cmp(other_start);
+        } else if self_end != other_end {
+            return self_end.cmp(other_end);
+        } else {
+            return self_cell_barcode.cmp(other_cell_barcode);
+        }
+    }
+}
+
+impl PartialOrd for Fragment {
+    fn partial_cmp(&self, other: &Fragment) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl fmt::Display for Fragment {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.score {
+            Some(score) => write!(
+                f,
+                "{}\t{}\t{}\t{}\t{}",
+                self.chrom, self.start, self.end, self.cell_barcode, score
+            ),
+            None => write!(
+                f,
+                "{}\t{}\t{}\t{}",
+                self.chrom, self.start, self.end, self.cell_barcode
+            ),
+        }
+    }
+}
+
+/// Aggregates multiple fragment files into a single file.
+///
+/// # Arguments
+/// * `path_to_fragment_files` - Paths to the fragment files.
+/// * `path_to_output_file` - Path to the output file.
+/// * `number_of_threads` - Number of threads to use for writing.
+/// * `verbose` - Whether to print progress messages.
+
+pub fn merge_fragment_files(
+    path_to_fragment_files: &[String],
+    path_to_output_file: &String,
+    number_of_threads: u32,
+    verbose: bool,
+) {
+    // initialize writer
+    let tpool = ThreadPool::new(number_of_threads).unwrap_or_else(|_| {
+        panic!(
+            "Could not create thread pool with {} threads",
+            number_of_threads
+        )
+    });
+    let mut writer = Writer::from_path(path_to_output_file)
+        .unwrap_or_else(|_| panic!("Could not open file {} for writing", path_to_output_file));
+    writer
+        .set_thread_pool(&tpool)
+        .unwrap_or_else(|_| panic!("Could not set thread pool for file {}", path_to_output_file));
+
+    let mut readers: Vec<(String, BGZFReader<File>)> = path_to_fragment_files
+        .iter()
+        .map(|path| {
+            let f = File::open(path).unwrap_or_else(|_| panic!("Could not open file {}", path));
+            let reader = BGZFReader::new(f)
+                .unwrap_or_else(|_| panic!("Could not create BGZF reader for file {}", path));
+            (path.clone(), reader)
+        })
+        .collect();
+
+    // Use a min-heap to keep track of the next fragment from each file
+    // The heap will store (Reverse(Fragment), index_of_file)
+    let mut heap = BinaryHeap::new();
+
+    for i in 0..readers.len() {
+        let (_, reader) = &mut readers[i];
+        // Read the first line from each file
+        let mut line_buffer = String::new();
+        if let Ok(bytes_read) = reader.read_line(&mut line_buffer) {
+            if bytes_read > 0 {
+                let fragment = Fragment::new_from_string(line_buffer.trim());
+                heap.push(Reverse((fragment, i)));
+            }
+        }
+    }
+
+    loop {
+        // Get the smallest fragment from the heap
+        let Reverse((min_fragment, file_idx)) = match heap.pop() {
+            Some(f) => f,
+            None => break, // All files are exhausted
+        };
+
+        // Write the smallest fragment
+        writer
+            .write_all(min_fragment.to_string().as_bytes())
+            .unwrap();
+        writer.write_all(b"\n").unwrap();
+
+        // Read the next fragment from the file that `min_fragment` came from
+        let (_, reader) = &mut readers[file_idx];
+        let mut line_buffer = String::new();
+        if let Ok(bytes_read) = reader.read_line(&mut line_buffer) {
+            if bytes_read > 0 {
+                let next_fragment = Fragment::new_from_string(line_buffer.trim());
+                heap.push(Reverse((next_fragment, file_idx)));
+            }
+        }
+    }
+    writer.flush().unwrap();
+}
---
+++ packages/scatac_fragment_tools/scatac_fragment_tools-0.1.5.tar.gz/scatac_fragment_tools-0.1.5/src/scatac_fragment_tools/cli/main.py
@@ -0,0 +1,330 @@
+from __future__ import annotations
+
+import argparse
+import sys
+from typing import Any, Callable
+
+from rich_argparse import RichHelpFormatter
+
+import scatac_fragment_tools
+from scatac_fragment_tools.cli.commands import (
+    command_fragment_to_bigwigs,
+    command_split_fragments_by_cell_type,
+)
+
+# Constants
+_VERSION = scatac_fragment_tools.__version__
+_REQUIRED_ARGUMENTS_NAME = "Required arguments"
+_OPTIONAL_ARGUMENTS_NAME = "Optional arguments"
+_FORMATTER_CLASS = RichHelpFormatter
+
+
+class SubparserBuilder:
+    """
+    Helper class for building subparsers.
+
+    Automatically adds a required and an optional argument group
+    and prints default values for optional arguments.
+    Also, sets the default formatter class.
+    """
+
+    def __init__(
+        self,
+        name: str,
+        subparsers: argparse._SubParsersAction,
+        func: Callable | None = None,
+        **kwargs,
+    ):
+        self.name = name
+        self.parser: argparse.ArgumentParser = subparsers.add_parser(
+            name=name,
+            formatter_class=_FORMATTER_CLASS,
+            **kwargs,
+        )
+        self.parser.set_defaults(func=func)
+        self.required_arguments = self.parser.add_argument_group(
+            _REQUIRED_ARGUMENTS_NAME
+        )
+        self.optional_arguments = self.parser.add_argument_group(
+            _OPTIONAL_ARGUMENTS_NAME
+        )
+
+    def add_required_argument(self, *args, **kwargs):
+        self.required_arguments.add_argument(
+            *args,
+            required=True,
+            **kwargs,
+        )
+
+    def add_optional_argument(self, *args, default: Any, help: str = "", **kwargs):
+        self.optional_arguments.add_argument(
+            *args,
+            required=False,
+            help=f"{help} Default: {default}",
+            default=default,
+            **kwargs,
+        )
+
+    def get_parser(self) -> dict[str, argparse.ArgumentParser]:
+        return {self.name: self.parser}
+
+
+def add_fragments_to_bigwig_subparser(
+    subparsers: argparse._SubParsersAction,
+) -> dict[str, argparse.ArgumentParser]:
+    parser = SubparserBuilder(
+        name="bigwig",
+        subparsers=subparsers,
+        func=command_fragment_to_bigwigs,
+        description="Calculate genome coverage for fragments and write result to bigWig file.",
+    )
+    parser.add_required_argument(
+        "-c",
+        "--chrom",
+        dest="chrom_sizes_filename",
+        action="store",
+        type=str,
+        help="Filename with chromosome sizes (*.chrom.sizes, *.fa.fai).",
+    )
+    parser.add_required_argument(
+        "-i",
+        "--frag",
+        dest="fragments_filename",
+        action="store",
+        type=str,
+        help="Fragments TSV file for which to calculate genome coverage.",
+    )
+    parser.add_required_argument(
+        "-o",
+        "--bw",
+        dest="bigwig_filename",
+        action="store",
+        type=str,
+        help="BigWig filename to which the genome coverage data will be written.",
+    )
+    parser.add_optional_argument(
+        "-n",
+        "--normalize",
+        dest="normalize",
+        action="store_true",
+        default=False,
+        help="Normalize genome coverage data by dividing by sequencing depth "
+        "(number of fragments) multiplied by 1 million.",
+    )
+    parser.add_optional_argument(
+        "-s",
+        "--scaling",
+        dest="scaling_factor",
+        action="store",
+        type=float,
+        default=1.0,
+        help="Scaling factor for genome coverage data. "
+        "If normalization is enabled, scaling is applied afterwards.",
+    )
+    parser.add_optional_argument(
+        "-x",
+        "--cut-sites",
+        dest="cut_sites",
+        action="store_true",
+        default=False,
+        help="Use 1 bp Tn5 cut sites (start and end of each fragment) instead of whole "
+        "fragment length for coverage calculation.",
+    )
+    parser.add_optional_argument(
+        "-w",
+        "--writer",
+        dest="bigwig_writer",
+        action="store",
+        type=str.lower,
+        choices=["pybigtools", "pybigwig"],
+        default="pybigtools",
+        help="Which bigWig writer implementation to use.",
+    )
+    parser.add_optional_argument(
+        "--chrom-prefix",
+        dest="chrom_prefix",
+        action="store",
+        type=str,
+        default=None,
+        help="Add chromosome prefix to each chromosome name found in the fragments file.",
+    )
+    parser.add_optional_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        default=False,
+        help="Whether to print progress.",
+    )
+    return parser.get_parser()
+
+
+def add_split_fragments_by_cell_type_subparser(
+    subparsers: argparse._SubParsersAction,
+) -> dict[str, argparse.ArgumentParser]:
+    parser = SubparserBuilder(
+        name="split",
+        subparsers=subparsers,
+        func=command_split_fragments_by_cell_type,
+        description="Split fragment files by cell type.",
+    )
+    parser.add_required_argument(
+        "-f",
+        "--sample_fragments",
+        dest="path_to_sample_to_fragment_definition",
+        action="store",
+        type=str,
+        help="Path to a text file mapping sample names to fragment files.",
+    )
+    parser.add_required_argument(
+        "-b",
+        "--cell_type_barcodes",
+        dest="path_to_cell_type_to_cell_barcode_definition",
+        action="store",
+        type=str,
+        help="Path to a text file mapping samples to cell types and cell types to cell barcodes.",
+    )
+    parser.add_required_argument(
+        "-c",
+        "--chrom",
+        dest="chrom_sizes_filename",
+        action="store",
+        type=str,
+        help="Filename with chromosome sizes (*.chrom.sizes, *.fa.fai).",
+    )
+    parser.add_required_argument(
+        "-o",
+        "--output",
+        dest="path_to_output_folder",
+        action="store",
+        type=str,
+        help="Path to output folder.",
+    )
+    parser.add_optional_argument(
+        "-t",
+        "--temp",
+        dest="path_to_temp_folder",
+        action="store",
+        type=str,
+        default="/tmp",
+        help="Path to temporary folder.",
+    )
+    parser.add_optional_argument(
+        "-n",
+        "--n_cpu",
+        dest="n_cpu",
+        action="store",
+        type=int,
+        default=1,
+        help="Number of cores to use.",
+    )
+    parser.add_optional_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        default=False,
+        help="Whether to print progress.",
+    )
+    parser.add_optional_argument(
+        "--clear_temp",
+        dest="clear_temp_folder",
+        action="store_true",
+        default=False,
+        help="Whether to clear the temporary folder.",
+    )
+    parser.add_optional_argument(
+        "-s",
+        "--sep",
+        dest="separator",
+        action="store",
+        type=str,
+        default="\t",
+        help="Separator for text files.",
+    )
+    parser.add_optional_argument(
+        "--sample_column",
+        dest="sample_column_name",
+        action="store",
+        type=str,
+        default="sample",
+        help="Column name for the sample name.",
+    )
+    parser.add_optional_argument(
+        "--fragment_column",
+        dest="path_to_fragment_file_column_name",
+        action="store",
+        type=str,
+        default="path_to_fragment_file",
+        help="Column name for the path to the fragment file.",
+    )
+    parser.add_optional_argument(
+        "--cell_type_column",
+        dest="cell_type_column_name",
+        action="store",
+        type=str,
+        default="cell_type",
+        help="Column name for the cell type.",
+    )
+    parser.add_optional_argument(
+        "--cell_barcode_column",
+        dest="cell_barcode_column_name",
+        action="store",
+        type=str,
+        default="cell_barcode",
+        help="Column name for the cell barcode.",
+    )
+    parser.add_optional_argument(
+        "--add_sample_id",
+        dest="add_sample_id",
+        action="store_true",
+        default=False,
+        help="Prefix sample id to cell barcode in pseudobulk fragment file."
+    )
+    return parser.get_parser()
+
+
+_PARSERS_CREATOR_FUNCS = [
+    add_fragments_to_bigwig_subparser,
+    add_split_fragments_by_cell_type_subparser,
+]
+
+
+def main() -> int:
+    main_parser = argparse.ArgumentParser(
+        description=f"scATAC-fragment-tools (v{_VERSION}): Tools for processing scATAC-seq fragments.",
+        formatter_class=RichHelpFormatter,
+    )
+    subparsers = main_parser.add_subparsers(
+        title="subcommands",
+    )
+    _commands = {}
+    for parser_creator_func in _PARSERS_CREATOR_FUNCS:
+        _commands.update(parser_creator_func(subparsers))
+    # When program is called without subcommand, print main help message.
+    if len(sys.argv) == 1:
+        main_parser.print_help()
+    # When program is called with a subcommand, but no other arguments, print
+    # subcommand's help message. When the subcommand is not recognized, print
+    # main help message, unless the subcommand is "--help", in which case print.
+    elif len(sys.argv) == 2:
+        called_command = sys.argv[1]
+        if called_command in _commands:
+            _commands[called_command].print_help()
+        elif called_command == "--help":
+            main_parser.print_help()
+        else:
+            print(
+                f"\033[1;31mUnrecognized command:\033[0m \033[1m{called_command}\033[0m"
+            )
+            main_parser.print_help()
+    # When program is called with a subcommand and other arguments, parse arguments.
+    else:
+        args = main_parser.parse_args()
+        if hasattr(args, "func"):
+            args.func(args)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
