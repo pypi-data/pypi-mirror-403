f837aa19b83f2d02e4ea9249705641edc12508f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:18:40 +0000

    Add package grafana-foundation-sdk/1769699932!10.2.0/grafana_foundation_sdk-1769699932!10.2.0-py3-none-any.whl

---
+++ packages/grafana-foundation-sdk/grafana_foundation_sdk-1769699932!10.2.0-py3-none-any.whl/grafana_foundation_sdk/builders/folder.py
@@ -0,0 +1,51 @@
+# Code generated - EDITING IS FUTILE. DO NOT EDIT.
+
+import typing
+from ..cog import builder as cogbuilder
+from ..models import folder
+
+
+class Folder(cogbuilder.Builder[folder.Folder]):    
+    """
+    TODO:
+    common metadata will soon support setting the parent folder in the metadata
+    """
+    
+    _internal: folder.Folder
+
+    def __init__(self) -> None:
+        self._internal = folder.Folder()
+
+    def build(self) -> folder.Folder:
+        """
+        Builds the object.
+        """
+        return self._internal    
+    
+    def uid(self, uid: str) -> typing.Self:    
+        """
+        Unique folder id. (will be k8s name)
+        """
+            
+        self._internal.uid = uid
+    
+        return self
+    
+    def title(self, title: str) -> typing.Self:    
+        """
+        Folder title
+        """
+            
+        self._internal.title = title
+    
+        return self
+    
+    def description(self, description: str) -> typing.Self:    
+        """
+        Description of the folder.
+        """
+            
+        self._internal.description = description
+    
+        return self
+    
