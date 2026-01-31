8943766540356d1786fd0403445e43256335e347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 07:12:10 +0000

    Add package async-tiff/0.5.0b1/async_tiff-0.5.0b1-cp310-cp310-win_amd64.whl

---
+++ packages/async-tiff/async_tiff-0.5.0b1-cp310-cp310-win_amd64.whl/async_tiff/_async_tiff.pyi
@@ -0,0 +1,17 @@
+from ._array import Array
+from ._decoder import DecoderRegistry
+from ._geo import GeoKeyDirectory
+from ._ifd import ImageFileDirectory
+from ._thread_pool import ThreadPool
+from ._tiff import TIFF
+from ._tile import Tile
+
+__all__ = [
+    "Array",
+    "DecoderRegistry",
+    "GeoKeyDirectory",
+    "ImageFileDirectory",
+    "ThreadPool",
+    "TIFF",
+    "Tile",
+]
