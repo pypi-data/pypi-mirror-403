fb09eb68aed3b3b06a6ddf78909aba7b7c625f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 11:07:02 +0000

    Add package oxarchive/0.5.1/oxarchive-0.5.1-py3-none-any.whl

---
+++ packages/oxarchive/oxarchive-0.5.1-py3-none-any.whl/oxarchive/resources/__init__.py
@@ -0,0 +1,18 @@
+"""Resource modules."""
+
+from .orderbook import OrderBookResource
+from .trades import TradesResource
+from .instruments import InstrumentsResource, LighterInstrumentsResource
+from .funding import FundingResource
+from .openinterest import OpenInterestResource
+from .candles import CandlesResource
+
+__all__ = [
+    "OrderBookResource",
+    "TradesResource",
+    "InstrumentsResource",
+    "LighterInstrumentsResource",
+    "FundingResource",
+    "OpenInterestResource",
+    "CandlesResource",
+]
