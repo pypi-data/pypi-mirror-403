5ff27372a6bc3a107c9e705b9bbbdbae027bb99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:07:06 +0000

    Add package ypricemagic/5.2.1/ypricemagic-5.2.1-cp313-cp313-musllinux_1_2_x86_64.whl

---
+++ packages/ypricemagic/ypricemagic-5.2.1-cp313-cp313-musllinux_1_2_x86_64.whl/y/prices/dex/balancer/_abc.py
@@ -0,0 +1,155 @@
+import abc
+from typing import Generic, TypeVar
+
+import a_sync
+
+import y.ENVIRONMENT_VARIABLES as ENVS
+from y import contracts
+from y._decorators import stuck_coro_debugger
+from y.classes._abc import LiquidityPool
+from y.datatypes import AddressOrContract, AnyAddressType, Block, Pool, UsdPrice
+
+
+class BalancerPool(LiquidityPool):
+    """
+    Represents a liquidity pool specific to the Balancer protocol.
+
+    This class is a subclass of :class:`~y.classes._abc.LiquidityPool` and is intended to represent
+    a Balancer pool. However, it currently does not implement any specific functionality or methods
+    related to the Balancer protocol.
+
+    Examples:
+        >>> class MyBalancerPool(BalancerPool):
+        ...     pass
+        >>> pool = MyBalancerPool("0xAddress")
+        >>> print(pool)
+        <MyBalancerPool object at 0x...>
+
+    See Also:
+        - :class:`~y.classes._abc.LiquidityPool`
+    """
+
+
+_B = TypeVar("_B", bound=BalancerPool)
+
+
+class BalancerABC(a_sync.ASyncGenericBase, Generic[_B]):
+    """
+    Abstract base class for interacting with Balancer pools.
+
+    This class provides asynchronous methods to check if a token is a Balancer pool and to get the price
+    of a pool. It uses the :mod:`a_sync` library for asynchronous operations.
+
+    Examples:
+        >>> class MyBalancer(BalancerABC):
+        ...     _pool_type = MyBalancerPool
+        ...     _check_methods = ("method1", "method2")
+        ...     async def get_token_price(self, token_address, block=None, skip_cache=False):
+        ...         return UsdPrice(100.0)
+        >>> balancer = MyBalancer()
+        >>> is_pool = await balancer.is_pool("0xTokenAddress")
+        >>> print(is_pool)
+        True
+
+    See Also:
+        - :class:`~y.classes._abc.LiquidityPool`
+    """
+
+    def __repr__(self) -> str:
+        return f"<{type(self).__name__} object at {hex(id(self))}>"
+
+    @a_sync.a_sync(ram_cache_ttl=5 * 60)
+    @stuck_coro_debugger
+    async def is_pool(self, token_address: AnyAddressType) -> bool:
+        """
+        Check if the given token address is a Balancer pool.
+
+        Args:
+            token_address: The address of the token to check.
+
+        Returns:
+            True if the token is a Balancer pool, False otherwise.
+
+        Examples:
+            >>> is_pool = await balancer.is_pool("0xTokenAddress")
+            >>> print(is_pool)
+            True
+        """
+        return await contracts.has_methods(token_address, self._check_methods, sync=False)
+
+    @stuck_coro_debugger
+    async def get_pool_price(
+        self,
+        pool_address: AnyAddressType,
+        block: Block | None = None,
+        skip_cache: bool = ENVS.SKIP_CACHE,
+        ignore_pools: tuple[Pool, ...] = (),
+    ) -> UsdPrice:
+        """
+        Get the price of a Balancer pool.
+
+        Args:
+            pool_address: The address of the pool.
+            block: The block number at which to get the price. If None, uses the latest block.
+            skip_cache: If True, bypasses ypricemagic's local caching mechanisms and forces a fresh calculation.
+
+        Returns:
+            The price of the pool as a :class:`~y.datatypes.UsdPrice` object.
+
+        Examples:
+            >>> price = await balancer.get_pool_price("0xPoolAddress")
+            >>> print(price)
+            100.0
+        """
+        return await self._pool_type(pool_address, asynchronous=True).get_pool_price(
+            block=block, skip_cache=skip_cache
+        )
+
+    @property
+    @abc.abstractmethod
+    def _pool_type(self) -> type[_B]:
+        """
+        The type of Balancer pool.
+
+        This property should be overridden by subclasses to specify the type of Balancer pool.
+
+        Examples:
+            >>> class MyBalancer(BalancerABC):
+            ...     _pool_type = MyBalancerPool
+        """
+
+    @property
+    @abc.abstractmethod
+    def _check_methods(self) -> tuple[str]:
+        """
+        The methods to check for identifying a Balancer pool.
+
+        This property should be overridden by subclasses to specify the methods used to identify a Balancer pool.
+
+        Examples:
+            >>> class MyBalancer(BalancerABC):
+            ...     _check_methods = ("method1", "method2")
+        """
+
+    @abc.abstractmethod
+    async def get_token_price(
+        self,
+        token_address: AddressOrContract,
+        block: Block | None = None,
+        skip_cache: bool = ENVS.SKIP_CACHE,
+    ) -> UsdPrice | None:
+        """
+        Get the price of a token in a Balancer pool.
+
+        This method should be implemented by subclasses to provide the logic for retrieving the token price.
+
+        Args:
+            token_address: The address of the token.
+            block: The block number at which to get the price. If None, uses the latest block.
+            skip_cache: If True, bypasses ypricemagic's local caching mechanisms and forces a fresh calculation.
+
+        Examples:
+            >>> price = await balancer.get_token_price("0xTokenAddress")
+            >>> print(price)
+            100.0
+        """
---
+++ packages/ypricemagic/ypricemagic-5.2.1-cp313-cp313-musllinux_1_2_x86_64.whl/y/prices/dex/uniswap/v1.py
@@ -0,0 +1,189 @@
+from contextlib import suppress
+from logging import DEBUG, getLogger
+from typing import Any
+
+import a_sync
+from a_sync import cgather
+from brownie import ZERO_ADDRESS, chain
+from dank_mids.exceptions import Revert
+
+from y import ENVIRONMENT_VARIABLES as ENVS
+from y._decorators import stuck_coro_debugger
+from y.classes.common import ERC20
+from y.constants import usdc
+from y.contracts import Contract, contract_creation_block_async
+from y.datatypes import Address, Block, Pool, UsdPrice
+from y.exceptions import ContractNotVerified, UnsupportedNetwork, continue_if_call_reverted
+from y.networks import Network
+from y.utils.raw_calls import _decimals
+
+logger = getLogger(__name__)
+
+
+class UniswapV1(a_sync.ASyncGenericBase):
+    factory = "0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95"
+
+    def __init__(self, *, asynchronous: bool = False) -> None:
+        """
+        Initialize the UniswapV1 class.
+
+        Args:
+            asynchronous: If True, the class will operate in asynchronous mode.
+
+        Raises:
+            UnsupportedNetwork: If the current network is not Ethereum Mainnet.
+
+        Examples:
+            >>> uniswap_v1 = UniswapV1()
+            >>> uniswap_v1.factory
+            '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
+        """
+        if chain.id != Network.Mainnet:
+            raise UnsupportedNetwork(f"UniswapV1 does not suppport chainid {chain.id}")
+        super().__init__()
+        self.asynchronous = asynchronous
+
+    @stuck_coro_debugger
+    @a_sync.a_sync(ram_cache_maxsize=None)
+    async def get_exchange(self, token_address: Address) -> Contract | None:
+        """
+        Get the exchange contract for a given token address.
+
+        Args:
+            token_address: The address of the token for which to find the exchange.
+
+        Returns:
+            The exchange contract if it exists, otherwise None.
+
+        Examples:
+            >>> uniswap_v1 = UniswapV1()
+            >>> exchange = await uniswap_v1.get_exchange("0xTokenAddress")
+            >>> print(exchange)
+            <Contract '0xExchangeAddress'>
+
+        See Also:
+            - :class:`~y.contracts.Contract`
+        """
+        factory = await Contract.coroutine(self.factory)
+        try:
+            exchange = await factory.getExchange.coroutine(token_address)
+        except Revert:
+            return None
+        if exchange == ZERO_ADDRESS:
+            return None
+        with suppress(ContractNotVerified):
+            return await Contract.coroutine(exchange)
+
+    @stuck_coro_debugger
+    async def get_price(
+        self,
+        token_address: Address,
+        block: Block | None,
+        ignore_pools: tuple[Pool, ...] = (),  # unused
+        skip_cache: bool = ENVS.SKIP_CACHE,  # unused
+    ) -> UsdPrice | None:
+        """
+        Get the price of a token in USD.
+
+        Args:
+            token_address: The address of the token to get the price for.
+            block: The block number at which to get the price.
+            ignore_pools: Unused parameter.
+            skip_cache: Unused parameter.
+
+        Returns:
+            The price of the token in USD, or None if the price cannot be determined.
+
+        Examples:
+            >>> uniswap_v1 = UniswapV1()
+            >>> price = await uniswap_v1.get_price("0xTokenAddress", 12345678)
+            >>> print(price)
+            1.23
+
+        See Also:
+            - :class:`~y.datatypes.UsdPrice`
+        """
+        exchange, usdc_exchange, decimals = await cgather(
+            self.get_exchange(token_address, sync=False),
+            self.get_exchange(usdc, sync=False),
+            _decimals(token_address, block, sync=False),
+        )
+        if exchange is None:
+            return None
+
+        try:
+            eth_bought = await exchange.getTokenToEthInputPrice.coroutine(
+                10**decimals, block_identifier=block
+            )
+            usdc_bought = (
+                await usdc_exchange.getEthToTokenInputPrice.coroutine(
+                    eth_bought, block_identifier=block
+                )
+                / 1e6
+            )
+            fees = 0.997**2
+            return UsdPrice(usdc_bought / fees)
+        except ValueError as e:
+            if "invalid jump destination" in str(e):
+                return None
+            continue_if_call_reverted(e)
+
+    @stuck_coro_debugger
+    @a_sync.a_sync(ram_cache_maxsize=100_000, ram_cache_ttl=60 * 60)
+    async def check_liquidity(
+        self, token_address: Address, block: Block, ignore_pools: tuple[Pool, ...] = ()
+    ) -> int:
+        """
+        Check the liquidity of a token in its exchange.
+
+        Args:
+            token_address: The address of the token to check liquidity for.
+            block: The block number at which to check liquidity.
+            ignore_pools: A tuple of pools to ignore when checking liquidity.
+
+        Returns:
+            The liquidity of the token in its exchange.
+
+        Examples:
+            >>> uniswap_v1 = UniswapV1()
+            >>> liquidity = await uniswap_v1.check_liquidity("0xTokenAddress", 12345678)
+            >>> print(liquidity)
+            1000000
+
+        See Also:
+            - :class:`~y.classes.common.ERC20`
+        """
+        if debug_logs_enabled := logger.isEnabledFor(DEBUG):
+            log_debug(
+                "checking Uniswap v1 liquidity for %s %s at %s ignoring %s",
+                await ERC20(token_address, asynchronous=True).symbol,
+                token_address,
+                block,
+                ignore_pools,
+            )
+        exchange = await self.get_exchange(token_address, sync=False)
+        if exchange is None or exchange in ignore_pools:
+            if debug_logs_enabled:
+                log_debug("no Uniswap v1 exchange found for %s", token_address)
+            return 0
+        if block < await contract_creation_block_async(exchange):
+            if debug_logs_enabled:
+                log_debug("block %s prior to %s deploy block", block, exchange)
+            return 0
+        liquidity = await ERC20(token_address, asynchronous=True).balance_of(exchange, block)
+        if debug_logs_enabled:
+            log_debug(
+                "Uniswap v1 liquidity for %s %s at %s is %s",
+                await ERC20(token_address, asynchronous=True).symbol,
+                token_address,
+                block,
+                liquidity,
+            )
+        return liquidity
+
+
+def log_debug(msg: str, *args: Any) -> None:
+    __logger_log(DEBUG, msg, args)
+
+
+__logger_log = logger._log
