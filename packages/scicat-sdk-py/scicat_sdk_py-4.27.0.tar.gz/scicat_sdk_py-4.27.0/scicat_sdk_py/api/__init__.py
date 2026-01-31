76051b0fc79a34286e51eb1988eb4b611bf58258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:45:29 +0000

    Add package scicat-sdk-py/4.27.0/scicat_sdk_py-4.27.0.tar.gz

---
+++ packages/scicat-sdk-py/scicat_sdk_py-4.27.0.tar.gz/scicat_sdk_py-4.27.0/scicat_sdk_py/api/__init__.py
@@ -0,0 +1,29 @@
+# flake8: noqa
+
+# import apis into api package
+from scicat_sdk_py.api.health_api import HealthApi
+from scicat_sdk_py.api.admin_api import AdminApi
+from scicat_sdk_py.api.attachments_v4_api import AttachmentsV4Api
+from scicat_sdk_py.api.auth_api import AuthApi
+from scicat_sdk_py.api.datablocks_api import DatablocksApi
+from scicat_sdk_py.api.datasets_api import DatasetsApi
+from scicat_sdk_py.api.datasets_public_v4_api import DatasetsPublicV4Api
+from scicat_sdk_py.api.datasets_v4_api import DatasetsV4Api
+from scicat_sdk_py.api.elastic_search_api import ElasticSearchApi
+from scicat_sdk_py.api.history_api import HistoryApi
+from scicat_sdk_py.api.instruments_api import InstrumentsApi
+from scicat_sdk_py.api.jobs_api import JobsApi
+from scicat_sdk_py.api.jobs_v4_api import JobsV4Api
+from scicat_sdk_py.api.logbooks_api import LogbooksApi
+from scicat_sdk_py.api.origdatablocks_api import OrigdatablocksApi
+from scicat_sdk_py.api.origdatablocks_public_v4_api import OrigdatablocksPublicV4Api
+from scicat_sdk_py.api.origdatablocks_v4_api import OrigdatablocksV4Api
+from scicat_sdk_py.api.policies_api import PoliciesApi
+from scicat_sdk_py.api.proposals_api import ProposalsApi
+from scicat_sdk_py.api.published_data_api import PublishedDataApi
+from scicat_sdk_py.api.published_data_v4_api import PublishedDataV4Api
+from scicat_sdk_py.api.runtime_configurations_api import RuntimeConfigurationsApi
+from scicat_sdk_py.api.samples_api import SamplesApi
+from scicat_sdk_py.api.user_identities_api import UserIdentitiesApi
+from scicat_sdk_py.api.users_api import UsersApi
+
