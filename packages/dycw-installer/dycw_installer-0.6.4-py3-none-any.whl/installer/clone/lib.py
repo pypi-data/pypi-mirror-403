162105e8a92ea65a250551072d87c06e233af7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 09:09:46 +0000

    Add package dycw-installer/0.6.4/dycw_installer-0.6.4-py3-none-any.whl

---
+++ packages/dycw-installer/dycw_installer-0.6.4-py3-none-any.whl/installer/clone/lib.py
@@ -0,0 +1,135 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import utilities.subprocess
+from utilities.constants import HOME, PWD
+from utilities.core import always_iterable, log_info, write_text
+from utilities.subprocess import HOST_KEY_ALGORITHMS, cp, ssh_keyscan
+
+from installer.clone.constants import GIT_CLONE_HOST
+from installer.configs.lib import setup_ssh_config
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    from utilities.types import LoggerLike, PathLike, Retry
+
+
+def git_clone(
+    key: PathLike,
+    owner: str,
+    repo: str,
+    /,
+    *,
+    logger: LoggerLike | None = None,
+    home: PathLike = HOME,
+    host: str = GIT_CLONE_HOST,
+    retry: Retry | None = None,
+    port: int | None = None,
+    dest: PathLike = PWD,
+    branch: str | None = None,
+) -> None:
+    log_info(logger, "Cloning repository...")
+    _git_clone_prepare(key, logger=logger, home=home, host=host, retry=retry, port=port)
+    stem = Path(key).stem
+    utilities.subprocess.git_clone(f"git@{stem}:{owner}/{repo}", dest, branch=branch)
+
+
+def _git_clone_prepare(
+    key: PathLike,
+    /,
+    *,
+    logger: LoggerLike | None = None,
+    home: PathLike = HOME,
+    host: str = GIT_CLONE_HOST,
+    retry: Retry | None = None,
+    port: int | None = None,
+) -> None:
+    key = Path(key)
+    setup_ssh_config(logger=logger, home=home)
+    _set_up_deploy_key(key, logger=logger, home=home)
+    _set_up_known_hosts(logger=logger, host=host, home=home, retry=retry, port=port)
+    _set_up_ssh_conf(key, logger=logger, home=home, host=host, port=port)
+
+
+def _set_up_deploy_key(
+    path: PathLike, /, *, logger: LoggerLike | None = None, home: PathLike = HOME
+) -> None:
+    log_info(logger, "Setting up deploy key...")
+    dest = _get_path_deploy_key(path, home=home)
+    cp(path, dest, perms="u=rw,g=,o=")
+
+
+def _get_path_deploy_key(path: PathLike, /, *, home: PathLike = HOME) -> Path:
+    stem = Path(path).stem
+    return Path(home, ".ssh/deploy-keys", stem)
+
+
+def _set_up_known_hosts(
+    *,
+    logger: LoggerLike | None = None,
+    host: str = GIT_CLONE_HOST,
+    home: PathLike = HOME,
+    retry: Retry | None = None,
+    port: int | None = None,
+) -> None:
+    log_info(logger, "Setting up known hosts...")
+    ssh_keyscan(host, path=Path(home, ".ssh/known_hosts"), retry=retry, port=port)
+
+
+def _set_up_ssh_conf(
+    path: PathLike,
+    /,
+    *,
+    logger: LoggerLike | None = None,
+    home: PathLike = HOME,
+    host: str = GIT_CLONE_HOST,
+    port: int | None = None,
+) -> None:
+    log_info(logger, "Setting up ...")
+    config = _get_path_conf(path, home=home)
+    text = "\n".join(_yield_conf_lines(path, home=home, host=host, port=port))
+    write_text(config, text, overwrite=True)
+
+
+def _get_path_conf(path: PathLike, /, *, home: PathLike = HOME) -> Path:
+    stem = Path(path).stem
+    return Path(home, f".ssh/config.d/{stem}.conf")
+
+
+def _yield_conf_lines(
+    path: PathLike,
+    /,
+    *,
+    home: PathLike = HOME,
+    host: str = GIT_CLONE_HOST,
+    port: int | None = None,
+) -> Iterator[str]:
+    stem = Path(path).stem
+    yield f"Host {stem}"
+    for line in _yield_conf_lines_core(path, home=home, host=host, port=port):
+        yield f"    {line}"
+
+
+def _yield_conf_lines_core(
+    path: PathLike,
+    /,
+    *,
+    home: PathLike = HOME,
+    host: str = GIT_CLONE_HOST,
+    port: int | None = None,
+) -> Iterator[str]:
+    yield "User git"
+    yield f"HostName {host}"
+    if port is not None:
+        yield f"Port {port}"
+    yield f"IdentityFile {_get_path_deploy_key(path, home=home)}"
+    yield "IdentitiesOnly yes"
+    yield "BatchMode yes"
+    yield f"HostKeyAlgorithms {','.join(always_iterable(HOST_KEY_ALGORITHMS))}"
+    yield "StrictHostKeyChecking yes"
+
+
+__all__ = ["git_clone"]
