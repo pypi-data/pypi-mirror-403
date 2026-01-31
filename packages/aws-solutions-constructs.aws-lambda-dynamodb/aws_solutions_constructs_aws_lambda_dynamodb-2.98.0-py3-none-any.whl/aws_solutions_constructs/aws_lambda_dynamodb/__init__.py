37e22a2c2bf4294c79bd01c745759231f1c28012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:07:02 +0000

    Add package aws-solutions-constructs.aws-lambda-dynamodb/2.98.0/aws_solutions_constructs_aws_lambda_dynamodb-2.98.0-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-lambda-dynamodb/aws_solutions_constructs_aws_lambda_dynamodb-2.98.0-py3-none-any.whl/aws_solutions_constructs/aws_lambda_dynamodb/__init__.py
@@ -0,0 +1,332 @@
+r'''
+Documentation for this pattern can be found [here](https://github.com/awslabs/aws-solutions-constructs/blob/main/source/patterns/%40aws-solutions-constructs/aws-lambda-dynamodb/README.adoc)
+'''
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import typeguard
+from importlib.metadata import version as _metadata_package_version
+TYPEGUARD_MAJOR_VERSION = int(_metadata_package_version('typeguard').split('.')[0])
+
+def check_type(argname: str, value: object, expected_type: typing.Any) -> typing.Any:
+    if TYPEGUARD_MAJOR_VERSION <= 2:
+        return typeguard.check_type(argname=argname, value=value, expected_type=expected_type) # type:ignore
+    else:
+        if isinstance(value, jsii._reference_map.InterfaceDynamicProxy): # pyright: ignore [reportAttributeAccessIssue]
+           pass
+        else:
+            if TYPEGUARD_MAJOR_VERSION == 3:
+                typeguard.config.collection_check_strategy = typeguard.CollectionCheckStrategy.ALL_ITEMS # type:ignore
+                typeguard.check_type(value=value, expected_type=expected_type) # type:ignore
+            else:
+                typeguard.check_type(value=value, expected_type=expected_type, collection_check_strategy=typeguard.CollectionCheckStrategy.ALL_ITEMS) # type:ignore
+
+from ._jsii import *
+
+import aws_cdk.aws_dynamodb as _aws_cdk_aws_dynamodb_ceddda9d
+import aws_cdk.aws_ec2 as _aws_cdk_aws_ec2_ceddda9d
+import aws_cdk.aws_lambda as _aws_cdk_aws_lambda_ceddda9d
+import constructs as _constructs_77d1e7e8
+
+
+class LambdaToDynamoDB(
+    _constructs_77d1e7e8.Construct,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@aws-solutions-constructs/aws-lambda-dynamodb.LambdaToDynamoDB",
+):
+    def __init__(
+        self,
+        scope: _constructs_77d1e7e8.Construct,
+        id: builtins.str,
+        *,
+        deploy_vpc: typing.Optional[builtins.bool] = None,
+        dynamo_table_props: typing.Optional[typing.Union[_aws_cdk_aws_dynamodb_ceddda9d.TableProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        existing_lambda_obj: typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function] = None,
+        existing_table_obj: typing.Optional[_aws_cdk_aws_dynamodb_ceddda9d.Table] = None,
+        existing_vpc: typing.Optional[_aws_cdk_aws_ec2_ceddda9d.IVpc] = None,
+        lambda_function_props: typing.Optional[typing.Union[_aws_cdk_aws_lambda_ceddda9d.FunctionProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        table_environment_variable_name: typing.Optional[builtins.str] = None,
+        table_permissions: typing.Optional[builtins.str] = None,
+        vpc_props: typing.Optional[typing.Union[_aws_cdk_aws_ec2_ceddda9d.VpcProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    ) -> None:
+        '''
+        :param scope: - represents the scope for all the resources.
+        :param id: - this is a a scope-unique id.
+        :param deploy_vpc: Whether to deploy a new VPC. Default: - false
+        :param dynamo_table_props: Optional user provided props to override the default props for the DynamoDB Table. Providing both this and ``existingTableInterface`` causes an error. Default: - Partition key ID: string
+        :param existing_lambda_obj: Optional - instance of an existing Lambda Function object, providing both this and ``lambdaFunctionProps`` will cause an error. Default: - None
+        :param existing_table_obj: Existing instance of DynamoDB table object, providing both this and ``dynamoTableProps`` will cause an error. Default: - None
+        :param existing_vpc: An existing VPC for the construct to use (construct will NOT create a new VPC in this case).
+        :param lambda_function_props: Optional - user provided props to override the default props for the Lambda function. Providing both this and ``existingLambdaObj`` causes an error. Default: - Default props are used
+        :param table_environment_variable_name: Optional Name for the Lambda function environment variable set to the name of the DynamoDB table. Default: - DDB_TABLE_NAME
+        :param table_permissions: Optional table permissions to grant to the Lambda function. One of the following may be specified: "All", "Read", "ReadWrite", "Write". Default: - Read/write access is given to the Lambda function if no value is specified.
+        :param vpc_props: Properties to override default properties if deployVpc is true.
+
+        :access: public
+        :since: 0.8.0
+        :summary: Constructs a new instance of the LambdaToDynamoDB class.
+        '''
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__092571f3790131920e423b6b48f6ada271812c0ab4c4b76127eec81553b7a3b0)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        props = LambdaToDynamoDBProps(
+            deploy_vpc=deploy_vpc,
+            dynamo_table_props=dynamo_table_props,
+            existing_lambda_obj=existing_lambda_obj,
+            existing_table_obj=existing_table_obj,
+            existing_vpc=existing_vpc,
+            lambda_function_props=lambda_function_props,
+            table_environment_variable_name=table_environment_variable_name,
+            table_permissions=table_permissions,
+            vpc_props=vpc_props,
+        )
+
+        jsii.create(self.__class__, self, [scope, id, props])
+
+    @builtins.property
+    @jsii.member(jsii_name="dynamoTable")
+    def dynamo_table(self) -> _aws_cdk_aws_dynamodb_ceddda9d.Table:
+        return typing.cast(_aws_cdk_aws_dynamodb_ceddda9d.Table, jsii.get(self, "dynamoTable"))
+
+    @builtins.property
+    @jsii.member(jsii_name="lambdaFunction")
+    def lambda_function(self) -> _aws_cdk_aws_lambda_ceddda9d.Function:
+        return typing.cast(_aws_cdk_aws_lambda_ceddda9d.Function, jsii.get(self, "lambdaFunction"))
+
+    @builtins.property
+    @jsii.member(jsii_name="vpc")
+    def vpc(self) -> typing.Optional[_aws_cdk_aws_ec2_ceddda9d.IVpc]:
+        return typing.cast(typing.Optional[_aws_cdk_aws_ec2_ceddda9d.IVpc], jsii.get(self, "vpc"))
+
+
+@jsii.data_type(
+    jsii_type="@aws-solutions-constructs/aws-lambda-dynamodb.LambdaToDynamoDBProps",
+    jsii_struct_bases=[],
+    name_mapping={
+        "deploy_vpc": "deployVpc",
+        "dynamo_table_props": "dynamoTableProps",
+        "existing_lambda_obj": "existingLambdaObj",
+        "existing_table_obj": "existingTableObj",
+        "existing_vpc": "existingVpc",
+        "lambda_function_props": "lambdaFunctionProps",
+        "table_environment_variable_name": "tableEnvironmentVariableName",
+        "table_permissions": "tablePermissions",
+        "vpc_props": "vpcProps",
+    },
+)
+class LambdaToDynamoDBProps:
+    def __init__(
+        self,
+        *,
+        deploy_vpc: typing.Optional[builtins.bool] = None,
+        dynamo_table_props: typing.Optional[typing.Union[_aws_cdk_aws_dynamodb_ceddda9d.TableProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        existing_lambda_obj: typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function] = None,
+        existing_table_obj: typing.Optional[_aws_cdk_aws_dynamodb_ceddda9d.Table] = None,
+        existing_vpc: typing.Optional[_aws_cdk_aws_ec2_ceddda9d.IVpc] = None,
+        lambda_function_props: typing.Optional[typing.Union[_aws_cdk_aws_lambda_ceddda9d.FunctionProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        table_environment_variable_name: typing.Optional[builtins.str] = None,
+        table_permissions: typing.Optional[builtins.str] = None,
+        vpc_props: typing.Optional[typing.Union[_aws_cdk_aws_ec2_ceddda9d.VpcProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    ) -> None:
+        '''
+        :param deploy_vpc: Whether to deploy a new VPC. Default: - false
+        :param dynamo_table_props: Optional user provided props to override the default props for the DynamoDB Table. Providing both this and ``existingTableInterface`` causes an error. Default: - Partition key ID: string
+        :param existing_lambda_obj: Optional - instance of an existing Lambda Function object, providing both this and ``lambdaFunctionProps`` will cause an error. Default: - None
+        :param existing_table_obj: Existing instance of DynamoDB table object, providing both this and ``dynamoTableProps`` will cause an error. Default: - None
+        :param existing_vpc: An existing VPC for the construct to use (construct will NOT create a new VPC in this case).
+        :param lambda_function_props: Optional - user provided props to override the default props for the Lambda function. Providing both this and ``existingLambdaObj`` causes an error. Default: - Default props are used
+        :param table_environment_variable_name: Optional Name for the Lambda function environment variable set to the name of the DynamoDB table. Default: - DDB_TABLE_NAME
+        :param table_permissions: Optional table permissions to grant to the Lambda function. One of the following may be specified: "All", "Read", "ReadWrite", "Write". Default: - Read/write access is given to the Lambda function if no value is specified.
+        :param vpc_props: Properties to override default properties if deployVpc is true.
+
+        :summary: The properties for the LambdaToDynamoDB Construct
+        '''
+        if isinstance(dynamo_table_props, dict):
+            dynamo_table_props = _aws_cdk_aws_dynamodb_ceddda9d.TableProps(**dynamo_table_props)
+        if isinstance(lambda_function_props, dict):
+            lambda_function_props = _aws_cdk_aws_lambda_ceddda9d.FunctionProps(**lambda_function_props)
+        if isinstance(vpc_props, dict):
+            vpc_props = _aws_cdk_aws_ec2_ceddda9d.VpcProps(**vpc_props)
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__a2c30a6a57824df9f62e5321acb93af2c3fb40432124ad8139a8da216f3d87fc)
+            check_type(argname="argument deploy_vpc", value=deploy_vpc, expected_type=type_hints["deploy_vpc"])
+            check_type(argname="argument dynamo_table_props", value=dynamo_table_props, expected_type=type_hints["dynamo_table_props"])
+            check_type(argname="argument existing_lambda_obj", value=existing_lambda_obj, expected_type=type_hints["existing_lambda_obj"])
+            check_type(argname="argument existing_table_obj", value=existing_table_obj, expected_type=type_hints["existing_table_obj"])
+            check_type(argname="argument existing_vpc", value=existing_vpc, expected_type=type_hints["existing_vpc"])
+            check_type(argname="argument lambda_function_props", value=lambda_function_props, expected_type=type_hints["lambda_function_props"])
+            check_type(argname="argument table_environment_variable_name", value=table_environment_variable_name, expected_type=type_hints["table_environment_variable_name"])
+            check_type(argname="argument table_permissions", value=table_permissions, expected_type=type_hints["table_permissions"])
+            check_type(argname="argument vpc_props", value=vpc_props, expected_type=type_hints["vpc_props"])
+        self._values: typing.Dict[builtins.str, typing.Any] = {}
+        if deploy_vpc is not None:
+            self._values["deploy_vpc"] = deploy_vpc
+        if dynamo_table_props is not None:
+            self._values["dynamo_table_props"] = dynamo_table_props
+        if existing_lambda_obj is not None:
+            self._values["existing_lambda_obj"] = existing_lambda_obj
+        if existing_table_obj is not None:
+            self._values["existing_table_obj"] = existing_table_obj
+        if existing_vpc is not None:
+            self._values["existing_vpc"] = existing_vpc
+        if lambda_function_props is not None:
+            self._values["lambda_function_props"] = lambda_function_props
+        if table_environment_variable_name is not None:
+            self._values["table_environment_variable_name"] = table_environment_variable_name
+        if table_permissions is not None:
+            self._values["table_permissions"] = table_permissions
+        if vpc_props is not None:
+            self._values["vpc_props"] = vpc_props
+
+    @builtins.property
+    def deploy_vpc(self) -> typing.Optional[builtins.bool]:
+        '''Whether to deploy a new VPC.
+
+        :default: - false
+        '''
+        result = self._values.get("deploy_vpc")
+        return typing.cast(typing.Optional[builtins.bool], result)
+
+    @builtins.property
+    def dynamo_table_props(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_dynamodb_ceddda9d.TableProps]:
+        '''Optional user provided props to override the default props for the DynamoDB Table.
+
+        Providing both this and
+        ``existingTableInterface`` causes an error.
+
+        :default: - Partition key ID: string
+        '''
+        result = self._values.get("dynamo_table_props")
+        return typing.cast(typing.Optional[_aws_cdk_aws_dynamodb_ceddda9d.TableProps], result)
+
+    @builtins.property
+    def existing_lambda_obj(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function]:
+        '''Optional - instance of an existing Lambda Function object, providing both this and ``lambdaFunctionProps`` will cause an error.
+
+        :default: - None
+        '''
+        result = self._values.get("existing_lambda_obj")
+        return typing.cast(typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function], result)
+
+    @builtins.property
+    def existing_table_obj(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_dynamodb_ceddda9d.Table]:
+        '''Existing instance of DynamoDB table object, providing both this and ``dynamoTableProps`` will cause an error.
+
+        :default: - None
+        '''
+        result = self._values.get("existing_table_obj")
+        return typing.cast(typing.Optional[_aws_cdk_aws_dynamodb_ceddda9d.Table], result)
+
+    @builtins.property
+    def existing_vpc(self) -> typing.Optional[_aws_cdk_aws_ec2_ceddda9d.IVpc]:
+        '''An existing VPC for the construct to use (construct will NOT create a new VPC in this case).'''
+        result = self._values.get("existing_vpc")
+        return typing.cast(typing.Optional[_aws_cdk_aws_ec2_ceddda9d.IVpc], result)
+
+    @builtins.property
+    def lambda_function_props(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_lambda_ceddda9d.FunctionProps]:
+        '''Optional - user provided props to override the default props for the Lambda function.
+
+        Providing both this and ``existingLambdaObj``
+        causes an error.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("lambda_function_props")
+        return typing.cast(typing.Optional[_aws_cdk_aws_lambda_ceddda9d.FunctionProps], result)
+
+    @builtins.property
+    def table_environment_variable_name(self) -> typing.Optional[builtins.str]:
+        '''Optional Name for the Lambda function environment variable set to the name of the DynamoDB table.
+
+        :default: - DDB_TABLE_NAME
+        '''
+        result = self._values.get("table_environment_variable_name")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def table_permissions(self) -> typing.Optional[builtins.str]:
+        '''Optional table permissions to grant to the Lambda function.
+
+        One of the following may be specified: "All", "Read", "ReadWrite", "Write".
+
+        :default: - Read/write access is given to the Lambda function if no value is specified.
+        '''
+        result = self._values.get("table_permissions")
+        return typing.cast(typing.Optional[builtins.str], result)
+
+    @builtins.property
+    def vpc_props(self) -> typing.Optional[_aws_cdk_aws_ec2_ceddda9d.VpcProps]:
+        '''Properties to override default properties if deployVpc is true.'''
+        result = self._values.get("vpc_props")
+        return typing.cast(typing.Optional[_aws_cdk_aws_ec2_ceddda9d.VpcProps], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "LambdaToDynamoDBProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "LambdaToDynamoDB",
+    "LambdaToDynamoDBProps",
+]
+
+publication.publish()
+
+def _typecheckingstub__092571f3790131920e423b6b48f6ada271812c0ab4c4b76127eec81553b7a3b0(
+    scope: _constructs_77d1e7e8.Construct,
+    id: builtins.str,
+    *,
+    deploy_vpc: typing.Optional[builtins.bool] = None,
+    dynamo_table_props: typing.Optional[typing.Union[_aws_cdk_aws_dynamodb_ceddda9d.TableProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    existing_lambda_obj: typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function] = None,
+    existing_table_obj: typing.Optional[_aws_cdk_aws_dynamodb_ceddda9d.Table] = None,
+    existing_vpc: typing.Optional[_aws_cdk_aws_ec2_ceddda9d.IVpc] = None,
+    lambda_function_props: typing.Optional[typing.Union[_aws_cdk_aws_lambda_ceddda9d.FunctionProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    table_environment_variable_name: typing.Optional[builtins.str] = None,
+    table_permissions: typing.Optional[builtins.str] = None,
+    vpc_props: typing.Optional[typing.Union[_aws_cdk_aws_ec2_ceddda9d.VpcProps, typing.Dict[builtins.str, typing.Any]]] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__a2c30a6a57824df9f62e5321acb93af2c3fb40432124ad8139a8da216f3d87fc(
+    *,
+    deploy_vpc: typing.Optional[builtins.bool] = None,
+    dynamo_table_props: typing.Optional[typing.Union[_aws_cdk_aws_dynamodb_ceddda9d.TableProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    existing_lambda_obj: typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function] = None,
+    existing_table_obj: typing.Optional[_aws_cdk_aws_dynamodb_ceddda9d.Table] = None,
+    existing_vpc: typing.Optional[_aws_cdk_aws_ec2_ceddda9d.IVpc] = None,
+    lambda_function_props: typing.Optional[typing.Union[_aws_cdk_aws_lambda_ceddda9d.FunctionProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    table_environment_variable_name: typing.Optional[builtins.str] = None,
+    table_permissions: typing.Optional[builtins.str] = None,
+    vpc_props: typing.Optional[typing.Union[_aws_cdk_aws_ec2_ceddda9d.VpcProps, typing.Dict[builtins.str, typing.Any]]] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
