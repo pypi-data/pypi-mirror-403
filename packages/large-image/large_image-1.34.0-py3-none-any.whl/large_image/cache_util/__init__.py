60e5244001453b05dc29040ba11becf816c12be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 06:11:24 +0000

    Add package large-image/1.34.0/large_image-1.34.0-py3-none-any.whl

---
+++ packages/large-image/large_image-1.34.0-py3-none-any.whl/large_image/cache_util/__init__.py
@@ -0,0 +1,110 @@
+#############################################################################
+#  Copyright Kitware Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#############################################################################
+
+import atexit
+from collections.abc import Callable
+from typing import Any
+
+from .cache import (CacheProperties, LruCacheMetaclass, getTileCache,
+                    isTileCacheSetup, methodcache, strhash)
+from .cachefactory import CacheFactory, pickAvailableCache
+
+MemCache: Any
+RedisCache: Any
+try:
+    from .memcache import MemCache
+except ImportError:
+    MemCache = None
+try:
+    from .rediscache import RedisCache
+except ImportError:
+    RedisCache = None
+
+_cacheClearFuncs: list[Callable] = []
+
+
+@atexit.register
+def cachesClearExceptTile(*args, **kwargs) -> None:
+    """
+    Clear the tilesource caches and the load model cache.  Note that this does
+    not clear memcached (which could be done with tileCache._client.flush_all,
+    but that can affect programs other than this one).
+    """
+    for name in LruCacheMetaclass.namedCaches:
+        with LruCacheMetaclass.namedCaches[name][1]:
+            LruCacheMetaclass.namedCaches[name][0].clear()
+    for func in _cacheClearFuncs:
+        func()
+
+
+def cachesClear(*args, **kwargs) -> None:
+    """
+    Clear the tilesource caches, the load model cache, and the tile cache.
+    """
+    cachesClearExceptTile()
+    if isTileCacheSetup():
+        tileCache, tileLock = getTileCache()
+        try:
+            if tileLock:
+                with tileLock:
+                    tileCache.clear()
+            else:
+                tileCache.clear()
+        except Exception:
+            pass
+
+
+def cachesInfo(*args, **kwargs) -> dict[str, dict[str, int]]:
+    """
+    Report on each cache.
+
+    :returns: a dictionary with the cache names as the keys and values that
+        include 'maxsize' and 'used', if known.
+    """
+    info = {}
+    for name in LruCacheMetaclass.namedCaches:
+        with LruCacheMetaclass.namedCaches[name][1]:
+            cache = LruCacheMetaclass.namedCaches[name][0]
+            info[name] = {
+                'maxsize': cache.maxsize,
+                'used': cache.currsize,
+            }
+    if isTileCacheSetup():
+        tileCache, tileLock = getTileCache()
+        try:
+            if tileLock:
+                with tileLock:
+                    info['tileCache'] = {
+                        'maxsize': tileCache.maxsize,
+                        'used': tileCache.currsize,
+                        'items': getattr(tileCache, 'curritems' if hasattr(
+                            tileCache, 'curritems') else 'currsize', None),
+                    }
+            else:
+                info['tileCache'] = {
+                    'maxsize': tileCache.maxsize,
+                    'used': tileCache.currsize,
+                    'items': getattr(tileCache, 'curritems' if hasattr(
+                        tileCache, 'curritems') else 'currsize', None),
+                }
+        except Exception:
+            pass
+    return info
+
+
+__all__ = ('CacheFactory', 'getTileCache', 'isTileCacheSetup', 'MemCache', 'RedisCache',
+           'strhash', 'LruCacheMetaclass', 'pickAvailableCache', 'methodcache',
+           'CacheProperties')
---
+++ packages/large-image/large_image-1.34.0-py3-none-any.whl/large_image/exceptions.py
@@ -0,0 +1,66 @@
+import contextlib
+import errno
+from typing import Any
+
+
+class TileGeneralError(Exception):
+    pass
+
+
+class TileSourceError(TileGeneralError):
+    pass
+
+
+class TileSourceAssetstoreError(TileSourceError):
+    pass
+
+
+class TileSourceRangeError(TileSourceError):
+    pass
+
+
+class TileSourceXYZRangeError(TileSourceRangeError):
+    pass
+
+
+class TileSourceInefficientError(TileSourceError):
+    pass
+
+
+class TileSourceMalformedError(TileSourceError):
+    pass
+
+
+class TileSourceFileNotFoundError(TileSourceError, FileNotFoundError):
+    def __init__(self, *args) -> None:
+        super().__init__(*args)
+        self.errno = errno.ENOENT
+
+
+class TileCacheError(TileGeneralError):
+    pass
+
+
+class TileCacheConfigurationError(TileCacheError):
+    pass
+
+
+def _improveJsonschemaValidationError(exp):
+    try:
+        error_freq: dict[str, Any] = {}
+        for err in exp.context:
+            key = err.schema_path[0]
+            error_freq.setdefault(key, [])
+            error_freq[key].append(err)
+        min_error = min(error_freq.values(), key=lambda k: (len(k), k[0].schema_path))[0]
+        for key in dir(min_error):
+            if not key.startswith('_'):
+                with contextlib.suppress(Exception):
+                    setattr(exp, key, getattr(min_error, key))
+    except Exception:
+        pass
+
+
+TileGeneralException = TileGeneralError
+TileSourceException = TileSourceError
+TileSourceAssetstoreException = TileSourceAssetstoreError
