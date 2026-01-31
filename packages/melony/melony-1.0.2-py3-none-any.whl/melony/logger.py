2d2aefeb3b148b10d1d9dc2a301ec9f3be0e88fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:20:00 +0000

    Add package melony/1.0.2/melony-1.0.2-py3-none-any.whl

---
+++ packages/melony/melony-1.0.2-py3-none-any.whl/melony/logger.py
@@ -0,0 +1,42 @@
+import traceback
+
+from datetime import datetime
+from logging import getLogger
+from typing import Final
+
+
+_logger = getLogger(__name__)
+_MELONY_LOG_PREFIX: Final[str] = "[Melony]"
+_DEBUG = True  # Toggle this for debugging via print python function  TODO: to env
+_WRAP_LINE_WIDTH: Final[int] = 60
+
+
+def log_info(message: str, consumer_id: int | None = None) -> None:
+    if _DEBUG:
+        if consumer_id is None:
+            print(f"{_MELONY_LOG_PREFIX}[INFO][{datetime.now()}]: {message}")  # noqa: WPS421, WPS226
+        else:
+            print(  # noqa: WPS421
+                f"{_MELONY_LOG_PREFIX}[INFO][consumer-{consumer_id}][{datetime.now()}]: "  # noqa: WPS226
+                f"{message}"
+            )
+    else:
+        if consumer_id is None:
+            _logger.info(f"{_MELONY_LOG_PREFIX}[INFO]: {message}")
+        else:
+            _logger.info(
+                f"{_MELONY_LOG_PREFIX}[INFO][consumer-{consumer_id}]: {message}"
+            )
+
+
+
+def log_error(message: str, exc: BaseException | None = None) -> None:
+    if _DEBUG:
+        print(f"{_MELONY_LOG_PREFIX}[ERROR][{datetime.now()}]: {message}")  # noqa: WPS421
+        if exc:
+            formatted_traceback = traceback.format_exception(exc)
+            for line in formatted_traceback:
+                print(line)  # noqa: WPS421
+            print("-" * _WRAP_LINE_WIDTH)  # noqa: WPS421
+    else:
+        _logger.error(message, exc_info=True)
