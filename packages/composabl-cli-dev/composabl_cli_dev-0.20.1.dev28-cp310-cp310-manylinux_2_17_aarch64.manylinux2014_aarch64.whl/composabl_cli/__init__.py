7415b4ded0a4d0dc6357c04f2801da613e9d1635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 20:06:36 +0000

    Add package composabl-cli-dev/0.20.1.dev28/composabl_cli_dev-0.20.1.dev28-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/composabl-cli-dev/composabl_cli_dev-0.20.1.dev28-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/composabl_cli/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) Composabl, Inc - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+
+# We want to expose these classes to the user at the base of core
+# ex: `from composabl.core import Agent``
+
+# Continue with the imports
+from .cli import *
