72602eebc56556d501492ca8df1dc9989d1b4a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:18:49 +0000

    Add package quercle-crewai/0.1.0/quercle_crewai-0.1.0-py3-none-any.whl

---
+++ packages/quercle-crewai/quercle_crewai-0.1.0-py3-none-any.whl/quercle_crewai/__init__.py
@@ -0,0 +1,15 @@
+"""Quercle tools for CrewAI."""
+
+from quercle_crewai.tools import (
+    QuercleFetchInput,
+    QuercleFetchTool,
+    QuercleSearchInput,
+    QuercleSearchTool,
+)
+
+__all__ = [
+    "QuercleFetchInput",
+    "QuercleFetchTool",
+    "QuercleSearchInput",
+    "QuercleSearchTool",
+]
