c6518bae61d526467ff9b957303b9f75e3992ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 13:19:55 +0000

    Add package secretting/1.0.1/secretting-1.0.1.tar.gz

---
+++ packages/secretting/secretting-1.0.1.tar.gz/secretting-1.0.1/secretting/__init__.py
@@ -0,0 +1,30 @@
+from secrets import choice as _choice, randint as _randint
+from string import ascii_letters as letters, digits, punctuation
+from hashlib import sha512 as s512, sha256 as s256
+
+chars: tuple = (*letters, *digits, *punctuation)
+def salt(length: int = 9) -> str:
+    return ''.join(_choice(chars) for _ in range(length))
+def sha256(text: str) -> str:
+    return s256(text.encode()).hexdigest()
+def sha512(text: str) -> str:
+    return s512(text.encode()).hexdigest()
+def secure256(text: str, customSalt: str = None) -> tuple:
+    gSalt: str = customSalt or salt()
+    return sha256(text + gSalt), gSalt
+def secure512(text: str, customSalt: str = None) -> tuple:
+    gSalt: str = customSalt or salt()
+    return sha512(text + gSalt), gSalt
+def multi256(text: str, times: int = 3) -> str:
+    return sha256(text * times)
+def multi512(text: str, times: int = 3) -> str:
+    return sha512(text * times)
+def hashByKey(text: str, key: str) -> str:
+	result = []
+	for i, c in enumerate(text):
+		result.append(str(ord(c) ^ ord(key[i % len(key)])))
+	return ''.join(result)
+def randint(min: float, max: float):
+    return _choice([_randint(min, max) for _ in range(20)])
+def choice(seq: list):
+    return _choice([_choice(seq) for _ in range(20)])
