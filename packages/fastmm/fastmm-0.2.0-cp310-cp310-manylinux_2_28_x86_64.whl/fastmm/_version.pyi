184f5a2975f6cc6b26303ca2326063e6c65cb7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 07:12:23 +0000

    Add package fastmm/0.2.0/fastmm-0.2.0-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/fastmm/fastmm-0.2.0-cp310-cp310-manylinux_2_28_x86_64.whl/fastmm/_version.pyi
@@ -0,0 +1,10 @@
+from __future__ import annotations
+from builtins import object as COMMIT_ID
+from builtins import object as VERSION_TUPLE
+__all__: list = ['__version__', '__version_tuple__', 'version', 'version_tuple', '__commit_id__', 'commit_id']
+__commit_id__: str = 'g5e209532e'
+__version__: str = '0.1.3.dev23'
+__version_tuple__: tuple = (0, 1, 3, 'dev23')
+commit_id: str = 'g5e209532e'
+version: str = '0.1.3.dev23'
+version_tuple: tuple = (0, 1, 3, 'dev23')
