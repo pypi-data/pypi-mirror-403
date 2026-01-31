681a32832f81edeaabe090407bab3d0fa702e1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 11:07:39 +0000

    Add package sentinel-sdk/0.1.1/sentinel_sdk-0.1.1.tar.gz

---
+++ packages/sentinel-sdk/sentinel_sdk-0.1.1.tar.gz/sentinel_sdk-0.1.1/src/sentinel_sdk/modules/node.py
@@ -0,0 +1,241 @@
+import ssl
+import threading
+import urllib.parse
+import urllib.request
+import http.client
+from typing import Any
+import json
+import uuid
+import base64
+import hashlib
+import ecdsa
+import grpc
+import sentinel_protobuf.sentinel.node.v3.node_pb2 as node_pb2
+import sentinel_protobuf.sentinel.node.v3.querier_pb2 as sentinel_node_v3_querier_pb2
+import sentinel_protobuf.sentinel.node.v3.querier_pb2_grpc as sentinel_node_v3_querier_pb2_grpc
+import sentinel_protobuf.sentinel.node.v2.msg_pb2 as msg_pb2
+import sentinel_protobuf.sentinel.node.v3.msg_pb2 as msg_pb2_3
+from sentinel_protobuf.sentinel.types.v1.price_pb2 import Price
+
+from sentinel_sdk.querier.querier import Querier
+from sentinel_sdk.transactor.transactor import Transactor
+from sentinel_sdk.types import PageRequest, TxParams, NodeType
+
+from .wireguard import WgKey
+
+
+class NodeModule(Querier, Transactor):
+    def __init__(self, channel: grpc.Channel, node_timeout: int, account, client):
+        self.node_timeout = node_timeout
+        self.__stub = sentinel_node_v3_querier_pb2_grpc.QueryServiceStub(channel)
+
+        # Disable SSL verification
+        self.__ssl_ctx = ssl.create_default_context()
+        self.__ssl_ctx.check_hostname = False
+        self.__ssl_ctx.verify_mode = ssl.CERT_NONE
+
+        self._account = account
+        self._client = client
+
+        self.__nodes_status_cache = {}
+        
+    def QueryParams(self) -> Any:
+        return self.__stub.QueryParams(sentinel_node_v3_querier_pb2.QueryParamsRequest()).params
+    
+    def QueryNode(self, address: str) -> Any:
+        r = self.__stub.QueryNode(
+            sentinel_node_v3_querier_pb2.QueryNodeRequest(address=address)
+        )
+        return r.node
+
+    def QueryNodes(self, status: int, pagination: PageRequest = None) -> list:
+        return self.QueryAll(
+            query=self.__stub.QueryNodes,
+            request=sentinel_node_v3_querier_pb2.QueryNodesRequest,
+            attribute="nodes",
+            status=status.value,
+            pagination=pagination,
+        )
+
+    def QueryNumOfNodesWithStatus(self, status: int) -> int:
+        r = self.__stub.QueryNodes(
+            sentinel_node_v3_querier_pb2.QueryNodesRequest(status=status.value)
+        )
+        return r.pagination.total
+
+    def QueryNodeStatus(self, node: node_pb2.Node, is_in_thread: bool = False) -> str:
+        node_endpoint = "https://" + node.remote_addrs[0]
+        gb_price = ""
+        hr_price = ""
+        if node.gigabyte_prices:
+            for prices in node.gigabyte_prices:
+                gb_price = gb_price + prices.quote_value + prices.denom + ','
+        else:
+            gb_price = "0udvpn"
+        if node.hourly_prices:
+            for prices in node.hourly_prices:
+                hr_price = hr_price + prices.quote_value + prices.denom + ','
+        else:
+            hr_price = "0udvpn"
+        
+        gb_price = gb_price.rstrip(',')
+        hr_price = hr_price.rstrip(',')
+        print(f"GB Prices: {gb_price}\n Hourly Prices: {hr_price}")
+        print(f"Node Endpoint: {node_endpoint}")
+        try:
+            contents = urllib.request.urlopen(
+                f"{node_endpoint}",
+                context=self.__ssl_ctx,
+                timeout=self.node_timeout,
+            ).read()
+            contents = contents.decode("utf-8")
+        except urllib.error.URLError as E:
+            print(str(E))
+            contents = '{"success":false,"urllib-error":"URLError encountered"}'
+        except TimeoutError:
+            contents = '{"success":false,"urllib-error":"Data reading timed out"}'
+        except http.client.RemoteDisconnected:
+            contents = '{"success":false,"http-error":"Remote endpoint closed connection"}'
+        except:
+            contents = '{"success":false,"error":"Unrecognizable error encountered"}'
+        contents = json.loads(contents)
+        contents['gigabyte_prices'] = gb_price
+        contents['hourly_prices']   = hr_price
+        contents = json.dumps(contents)
+        
+        if is_in_thread:
+            self.__nodes_status_cache[node.address] = contents
+        else:
+            return contents
+
+    def QueryNodesStatus(self, nodes: list[node_pb2.Node], n_threads: int = 8) -> dict:
+        chunks = list(self.__split_into_chunks(nodes, n_threads))
+        cur_threads = []
+        for c in chunks:
+            t = threading.Thread(target=self.__QueryNodesChunk, args=(c,))
+            cur_threads.append(t)
+            t.start()
+        for t in cur_threads:
+            t.join()
+
+        result = self.__nodes_status_cache
+        self.__nodes_status_cache = {}
+
+        return result
+
+    def QueryNodesForPlan(
+        self, plan_id: int, status: int, pagination: PageRequest = None
+    ) -> list:
+        return self.QueryAll(
+            query=self.__stub.QueryNodesForPlan,
+            request=sentinel_node_v3_querier_pb2.QueryNodesForPlanRequest,
+            attribute="nodes",
+            status=status.value,
+            id=plan_id,
+            pagination=pagination,
+        )
+
+    def __QueryNodesChunk(self, chunk: list[node_pb2.Node]):
+        for node in chunk:
+            self.QueryNodeStatus(node, True)
+
+    def __split_into_chunks(self, items: list, size: int):
+        for i in range(0, len(items), size):
+            plus = i + size
+            yield items[i:plus]
+            # https://github.com/PyCQA/pycodestyle/issues/373
+
+    def RegisterNode(self, gigabyte_prices: int, hourly_prices: int, remote_url: str, tx_params: TxParams = TxParams()):
+        msg = msg_pb2.MsgRegisterRequest(
+            frm = self._account.address,
+            gigabyte_prices=gigabyte_prices,
+            hourly_prices=hourly_prices,
+            remote_url=remote_url,
+        )
+        return self.transaction([msg], tx_params)
+
+    '''
+    def SubscribeToNode(self, node_address: str, gigabytes: int = 0, hours: int = 0, denom: str = "udvpn", tx_params: TxParams = TxParams()):
+        msg = msg_pb2.MsgSubscribeRequest(
+            frm = self._account.address,
+            denom = denom,
+            gigabytes = gigabytes,
+            hours = hours,
+            node_address = node_address,
+        )
+        return self.transaction([msg], tx_params)
+    '''
+    
+    def SubscribeToNode(self, node_address: str, price: Price, gigabytes: int = 0, hours: int = 0, tx_params: TxParams = TxParams()):
+        msg = msg_pb2_3.MsgStartSessionRequest(
+            frm = self._account.address,
+            gigabytes = gigabytes,
+            hours = hours,
+            node_address = node_address,
+            max_price = price,
+        )
+        return self.transaction([msg], tx_params)
+    def UpdateNodeDetails(self, gigabyte_prices: int, hourly_prices: int, remote_url: str, tx_params: TxParams = TxParams()):
+        msg = msg_pb2.MsgUpdateDetailsRequest(
+            frm = self._account.address,
+            gigabyte_prices = gigabyte_prices,
+            hourly_prices = hourly_prices,
+            remote_url = remote_url,
+        )
+        return self.transaction([msg], tx_params)
+
+    def UpdateNodeStatus(self, status: int, tx_params: TxParams = TxParams()):
+        msg = msg_pb2.MsgUpdateStatusRequest(
+            frm = self._account.address,
+            status = status.value,
+        )
+        return self.transaction([msg], tx_params)
+
+    def __post_session(self, url: str, payload: dict) -> str:
+        request = urllib.request.Request(url)
+        request.add_header("Content-Type", "application/json; charset=utf-8")
+        json_data_bytes = json.dumps(payload).encode("utf-8")  # needs to be bytes
+        request.add_header("Content-Length", len(json_data_bytes))
+        # TODO: status code != 200 was not handled
+        with urllib.request.urlopen(request, json_data_bytes, timeout=self.node_timeout, context=self.__ssl_ctx) as f:
+            return f.read().decode("utf-8")
+
+    # TODO: find another 'fancy' name
+    def PostSession(
+        self, session_id: int, remote_url: str, node_type: NodeType = NodeType.WIREGUARD
+    ):
+        if node_type == NodeType.WIREGUARD:
+            # [from golang] wgPrivateKey, err = wireguardtypes.NewPrivateKey()
+            # [from golang] key = wgPrivateKey.Public().String()
+            wgkey = WgKey()
+            # The private key should be used by the wireguard client
+            key = wgkey.pubkey
+        else:  # NodeType.V2RAY
+            # os.urandom(16)
+            # [from golang] uid, err = uuid.GenerateRandomBytes(16)
+            # [from golang] key = base64.StdEncoding.EncodeToString(append([]byte{0x01}, uid...))
+            key = base64.b64encode(uuid.uuid4().bytes).decode("utf-8")
+
+        # append([]byte{0x01}, uid) is required)
+        # The key of wireguard is 32 bytes length, for v2ray only 16?
+
+        # self._account inherited from Transactor class.
+        # self._account.private_key and self._account.address
+
+        sk = ecdsa.SigningKey.from_string(
+            self._account.private_key, curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256
+        )
+
+        session_id = int(session_id)
+        # Uint64ToBigEndian
+        bige_session = session_id.to_bytes(8, "big")
+        signature = sk.sign(bige_session)
+
+        payload = {
+            "key": key,
+            "signature": base64.b64encode(signature).decode("utf-8"),
+        }
+        return self.__post_session(
+            f"{remote_url}/accounts/{self._account.address}/sessions/{session_id}",
+            payload,
+        )
---
+++ packages/sentinel-sdk/sentinel_sdk-0.1.1.tar.gz/sentinel_sdk-0.1.1/src/sentinel_sdk/transactor/transactor.py
@@ -0,0 +1,89 @@
+import grpc
+import ssl
+import time
+from typing import Any
+from sentinel_protobuf.cosmos.base.v1beta1.coin_pb2 import Coin
+from sentinel_sdk.types import NodeType, TxParams
+from mospy import Transaction
+
+class Transactor:
+    def __init__(
+        self,
+        account,
+        client
+    ):
+        self._account = account
+        self._client = client
+
+    # TODO: subscribe_to_gigabytes, subscribe_to_hours, start_request and all method should have fee denom and amount as args?
+    # We need to find a good way to this. **The method could return only the MsgRequest and type_url**
+    # Who wants to submit the tx, can call transaction (we should also implement multi-add_raw_msg), one tx with multiple msg
+
+    def PrepareOrTransactMsg(   self,
+                                msg: Any,
+                                transact: bool = False,
+                                tx_params : TxParams = TxParams(),
+                                **kwargs):
+        msg_args = {
+            k: kwargs[k] for k in kwargs if k in ["address", "node_address", "id", "gigabytes", "hours", "rating", "denom"]
+        }
+
+        msg_args['frm'] = self.__account.address
+        prepared_msg = msg(**msg_args)
+
+        return self.transaction([prepared_msg], tx_params) if transact else prepared_msg
+
+
+    def transaction(
+        self,
+        messages: list,
+        tx_params: TxParams = TxParams(),
+        next_sequence: bool = False
+    ) -> dict:
+        if self._account is None or self._client is None:
+            raise ValueError("Transactor was not initialized due missing secret, unable to transact")
+
+        tx = Transaction(
+            account=self._account,
+            fee=Coin(denom=tx_params.denom, amount=f"{tx_params.fee_amount}"),
+            gas=tx_params.gas,
+            protobuf="sentinel",
+            chain_id="sentinelhub-2",
+        )
+
+        if not isinstance(messages, list):
+            messages = [messages]
+
+        for message in messages:
+            tx.add_raw_msg(message, type_url=f"/{message.DESCRIPTOR.full_name}")
+
+        # Required before each tx of we get account sequence mismatch, expected 945, got 944: incorrect account sequence
+        #self._client.load_account_data(account=self._account)
+        
+        if next_sequence:
+            self._account.increase_sequence()
+
+        # inplace, auto-update gas with update=True
+        # auto calculate the gas only if was not already passed as args:
+        if tx_params.gas == 0:
+            self._client.estimate_gas(
+                transaction=tx, update=True, multiplier=tx_params.gas_multiplier
+            )
+
+        tx_response = self._client.broadcast_transaction(transaction=tx)
+        # tx_response = {"hash": hash, "code": code, "log": log}
+        return tx_response
+
+    def wait_for_tx(self, tx_hash: str, timeout: float = 120, poll_period: float = 10):
+        if self._account is None or self._client is None:
+            raise ValueError("Transactor was not initialized due missing secret, unable to wait transaction")
+
+        return self._client.wait_for_tx(
+            tx_hash=tx_hash,
+            timeout=timeout,
+            poll_period=poll_period
+        )
+
+    # back-compatibility
+    def wait_transaction(self, tx_hash: str, timeout: float = 120, poll_period: float = 10):
+        return self.wait_for_tx(tx_hash=tx_hash, timeout=timeout, poll_period=poll_period)
