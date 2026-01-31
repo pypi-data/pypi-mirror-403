37d619f8a060cd39f0d182b3b531f149ad003f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 08:07:10 +0000

    Add package eggp/1.0.11/eggp-1.0.11-cp312-cp312-manylinux_2_28_aarch64.whl

---
+++ packages/eggp/eggp-1.0.11-cp312-cp312-manylinux_2_28_aarch64.whl/eggp/_binding.py
@@ -0,0 +1,22 @@
+def __bootstrap__():
+   global __bootstrap__, __file__, __loader__
+   import sys, os, importlib.resources as irs, importlib.util
+#rtld   import dl
+   with irs.files(__name__).joinpath(
+     '_binding.cpython-312-aarch64-linux-gnu.so') as __file__:
+      del __bootstrap__
+      if '__loader__' in globals():
+          del __loader__
+#rtld      old_flags = sys.getdlopenflags()
+      old_dir = os.getcwd()
+      try:
+        os.chdir(os.path.dirname(__file__))
+#rtld        sys.setdlopenflags(dl.RTLD_NOW)
+        spec = importlib.util.spec_from_file_location(
+                   __name__, __file__)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+      finally:
+#rtld        sys.setdlopenflags(old_flags)
+        os.chdir(old_dir)
+__bootstrap__()
