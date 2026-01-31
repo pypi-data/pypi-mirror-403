81f3da77de7ce43b5073be77394ddef3aa27b431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 13:20:29 +0000

    Add package bvi-aom/0.3/bvi_aom-0.3.tar.gz

---
+++ packages/bvi-aom/bvi_aom-0.3.tar.gz/bvi_aom-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='bvi_aom',
+    version='0.3',
+    packages=find_packages(),
+    install_requires = [
+        'wget'
+    ],
+    entry_points={
+        "console_scripts": [
+            "bvi-aom = bvi_aom:main"
+        ]
+    },
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
