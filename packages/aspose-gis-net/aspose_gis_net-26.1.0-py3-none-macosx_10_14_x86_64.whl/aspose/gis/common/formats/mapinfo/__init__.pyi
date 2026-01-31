b82b0c864f450361501dfe666011f4faaa838507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:06:54 +0000

    Add package aspose-gis-net/26.1.0/aspose_gis_net-26.1.0-py3-none-macosx_10_14_x86_64.whl

---
+++ packages/aspose-gis-net/aspose_gis_net-26.1.0-py3-none-macosx_10_14_x86_64.whl/aspose/gis/common/formats/mapinfo/__init__.pyi
@@ -0,0 +1,390 @@
+from typing import List, Optional, Dict, Iterable, Any, overload
+import io
+import collections.abc
+from collections.abc import Sequence
+from datetime import datetime
+from aspose.pyreflection import Type
+import aspose.pycore
+import aspose.pydrawing
+from uuid import UUID
+import aspose.gis
+import aspose.gis.common
+import aspose.gis.common.formats
+import aspose.gis.common.formats.dbase
+import aspose.gis.common.formats.gdbtable
+import aspose.gis.common.formats.mapinfo
+import aspose.gis.common.formats.mapinfo.graphicalobjects
+import aspose.gis.common.formats.mapinfo.interchangeformat
+import aspose.gis.common.formats.mapinfo.styling
+import aspose.gis.common.formats.mapinfo.tabformat
+import aspose.gis.common.formats.mapinfo.tabformat.map
+import aspose.gis.common.formats.wkb
+import aspose.gis.common.formats.wkt
+import aspose.gis.common.formats.xml
+import aspose.gis.common.io
+import aspose.gis.epsg
+import aspose.gis.formats
+import aspose.gis.formats.bmpw
+import aspose.gis.formats.csv
+import aspose.gis.formats.database
+import aspose.gis.formats.database.dataediting
+import aspose.gis.formats.database.fromdefinition
+import aspose.gis.formats.esriascii
+import aspose.gis.formats.esrijson
+import aspose.gis.formats.filegdb
+import aspose.gis.formats.gdal
+import aspose.gis.formats.geojson
+import aspose.gis.formats.geojsonseq
+import aspose.gis.formats.geopackage
+import aspose.gis.formats.geotiff
+import aspose.gis.formats.gml
+import aspose.gis.formats.gpx
+import aspose.gis.formats.infile
+import aspose.gis.formats.inmemory
+import aspose.gis.formats.jpegw
+import aspose.gis.formats.kml
+import aspose.gis.formats.kml.specificfields
+import aspose.gis.formats.kml.styles
+import aspose.gis.formats.mapinfointerchange
+import aspose.gis.formats.mapinfotab
+import aspose.gis.formats.osmxml
+import aspose.gis.formats.pngw
+import aspose.gis.formats.postgis
+import aspose.gis.formats.shapefile
+import aspose.gis.formats.sqlserver
+import aspose.gis.formats.tiffw
+import aspose.gis.formats.topojson
+import aspose.gis.formats.worldfile
+import aspose.gis.formats.xyztile
+import aspose.gis.geometries
+import aspose.gis.geotools
+import aspose.gis.geotools.extensions
+import aspose.gis.geotools.layersmap
+import aspose.gis.geotools.mapbuilder
+import aspose.gis.geotools.wayanalyzer
+import aspose.gis.imagemetadata
+import aspose.gis.indexing
+import aspose.gis.indexing.bplustree
+import aspose.gis.indexing.qixtree
+import aspose.gis.indexing.rtree
+import aspose.gis.labeling
+import aspose.gis.labeling.line
+import aspose.gis.painting
+import aspose.gis.projections
+import aspose.gis.raster
+import aspose.gis.raster.web
+import aspose.gis.relationship
+import aspose.gis.relationship.joins
+import aspose.gis.rendering
+import aspose.gis.rendering.colorizers
+import aspose.gis.rendering.formats
+import aspose.gis.rendering.formats.bmp
+import aspose.gis.rendering.formats.jpeg
+import aspose.gis.rendering.formats.png
+import aspose.gis.rendering.formats.svg
+import aspose.gis.rendering.labelings
+import aspose.gis.rendering.sld
+import aspose.gis.rendering.symbolizers
+import aspose.gis.spatialreferencing
+import aspose.gis.topology
+import aspose.gis.topology.algorithms
+import aspose.gis.topology.buffer
+import aspose.gis.topology.graph
+
+class Column:
+    
+    def __init__(self) -> None:
+        raise NotImplementedError()
+    
+    @staticmethod
+    def get_type(type_name : str) -> aspose.gis.common.formats.mapinfo.ColumnType:
+        raise NotImplementedError()
+    
+    @property
+    def name(self) -> str:
+        raise NotImplementedError()
+    
+    @name.setter
+    def name(self, value : str) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def column_type(self) -> aspose.gis.common.formats.mapinfo.ColumnType:
+        raise NotImplementedError()
+    
+    @column_type.setter
+    def column_type(self, value : aspose.gis.common.formats.mapinfo.ColumnType) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def width(self) -> int:
+        raise NotImplementedError()
+    
+    @width.setter
+    def width(self, value : int) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def precision(self) -> int:
+        raise NotImplementedError()
+    
+    @precision.setter
+    def precision(self, value : int) -> None:
+        raise NotImplementedError()
+    
+
+class CoordinateSystem:
+    
+    def __init__(self) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def projection_id(self) -> int:
+        raise NotImplementedError()
+    
+    @projection_id.setter
+    def projection_id(self, value : int) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def projection_parameters(self) -> List[float]:
+        raise NotImplementedError()
+    
+    @property
+    def unit_id(self) -> aspose.gis.common.formats.mapinfo.UnitId:
+        raise NotImplementedError()
+    
+    @unit_id.setter
+    def unit_id(self, value : aspose.gis.common.formats.mapinfo.UnitId) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def datum(self) -> aspose.gis.common.formats.mapinfo.Datum:
+        raise NotImplementedError()
+    
+
+class Datum:
+    
+    def __init__(self) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def id(self) -> int:
+        raise NotImplementedError()
+    
+    @id.setter
+    def id(self, value : int) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def has_datum_parameters(self) -> bool:
+        raise NotImplementedError()
+    
+    @has_datum_parameters.setter
+    def has_datum_parameters(self, value : bool) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def ellipsoid_id(self) -> int:
+        raise NotImplementedError()
+    
+    @ellipsoid_id.setter
+    def ellipsoid_id(self, value : int) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def shift_x(self) -> float:
+        raise NotImplementedError()
+    
+    @shift_x.setter
+    def shift_x(self, value : float) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def shift_y(self) -> float:
+        raise NotImplementedError()
+    
+    @shift_y.setter
+    def shift_y(self, value : float) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def shift_z(self) -> float:
+        raise NotImplementedError()
+    
+    @shift_z.setter
+    def shift_z(self, value : float) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def rotation_x(self) -> float:
+        raise NotImplementedError()
+    
+    @rotation_x.setter
+    def rotation_x(self, value : float) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def rotation_y(self) -> float:
+        raise NotImplementedError()
+    
+    @rotation_y.setter
+    def rotation_y(self, value : float) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def rotation_z(self) -> float:
+        raise NotImplementedError()
+    
+    @rotation_z.setter
+    def rotation_z(self, value : float) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def scale(self) -> float:
+        raise NotImplementedError()
+    
+    @scale.setter
+    def scale(self, value : float) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def prime_meridian(self) -> float:
+        raise NotImplementedError()
+    
+    @prime_meridian.setter
+    def prime_meridian(self, value : float) -> None:
+        raise NotImplementedError()
+    
+
+class IDataRow:
+    
+    def is_null(self, field_index : int) -> bool:
+        raise NotImplementedError()
+    
+    def read_character(self, field_index : int) -> str:
+        raise NotImplementedError()
+    
+    def read_integer(self, field_index : int) -> int:
+        raise NotImplementedError()
+    
+    def read_small_integer(self, field_index : int) -> int:
+        raise NotImplementedError()
+    
+    def read_large_integer(self, field_index : int) -> int:
+        raise NotImplementedError()
+    
+    def read_decimal(self, field_index : int) -> float:
+        raise NotImplementedError()
+    
+    def read_float(self, field_index : int) -> float:
+        raise NotImplementedError()
+    
+    def read_date_time(self, field_index : int) -> datetime:
+        raise NotImplementedError()
+    
+    def read_date(self, field_index : int) -> datetime:
+        raise NotImplementedError()
+    
+    def read_time(self, field_index : int) -> datetime:
+        raise NotImplementedError()
+    
+    def read_logical(self, field_index : int) -> bool:
+        raise NotImplementedError()
+    
+    @property
+    def row_number(self) -> int:
+        raise NotImplementedError()
+    
+    @property
+    def fields_count(self) -> int:
+        raise NotImplementedError()
+    
+
+class IDataRowsFile:
+    
+
+class IGraphcialObjectsFile:
+    
+    @property
+    def common_graphical_objects_type(self) -> Optional[aspose.gis.common.formats.mapinfo.graphicalobjects.GraphicalObjectType]:
+        raise NotImplementedError()
+    
+
+class MapInfoException:
+    
+    @overload
+    def __init__(self) -> None:
+        raise NotImplementedError()
+    
+    @overload
+    def __init__(self, message : str) -> None:
+        raise NotImplementedError()
+    
+
+class MapInfoSizes:
+    
+    def __init__(self) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def HEADER_SIZE(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def OBJECTS_HEADER(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def COORDINATE_HEADER(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def UNCOMPRESSED_COORDINATE(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def COMPRESSED_COORDINATE(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def SEGMENT_HEADER(self) -> int:
+        raise NotImplementedError()
+
+    @property
+    def SEGMENT_HEADER450(self) -> int:
+        raise NotImplementedError()
+
+
+class ColumnType:
+    
+    CHAR : ColumnType
+    INTEGER : ColumnType
+    SMALL_INTEGER : ColumnType
+    LARGE_INTEGER : ColumnType
+    DECIMAL : ColumnType
+    FLOAT : ColumnType
+    DATE : ColumnType
+    TIME : ColumnType
+    DATE_TIME : ColumnType
+    LOGICAL : ColumnType
+
+class UnitId:
+    
+    UNKNOWN : UnitId
+    MILE : UnitId
+    KILOMETER : UnitId
+    INCH : UnitId
+    INTERNATIONAL_FOOT : UnitId
+    YARD : UnitId
+    MILLIMETER : UnitId
+    CENTIMETER : UnitId
+    METER : UnitId
+    US_SURVEY_FOOT : UnitId
+    NAUTICAL_MILE : UnitId
+    EMPTY : UnitId
+    LINK : UnitId
+    CHAIN : UnitId
+    ROD : UnitId
+    DEGREE : UnitId
+
---
+++ packages/aspose-gis-net/aspose_gis_net-26.1.0-py3-none-macosx_10_14_x86_64.whl/aspose/gis/common/formats/wkt/__init__.pyi
@@ -0,0 +1,448 @@
+from typing import List, Optional, Dict, Iterable, Any, overload
+import io
+import collections.abc
+from collections.abc import Sequence
+from datetime import datetime
+from aspose.pyreflection import Type
+import aspose.pycore
+import aspose.pydrawing
+from uuid import UUID
+import aspose.gis
+import aspose.gis.common
+import aspose.gis.common.formats
+import aspose.gis.common.formats.dbase
+import aspose.gis.common.formats.gdbtable
+import aspose.gis.common.formats.mapinfo
+import aspose.gis.common.formats.mapinfo.graphicalobjects
+import aspose.gis.common.formats.mapinfo.interchangeformat
+import aspose.gis.common.formats.mapinfo.styling
+import aspose.gis.common.formats.mapinfo.tabformat
+import aspose.gis.common.formats.mapinfo.tabformat.map
+import aspose.gis.common.formats.wkb
+import aspose.gis.common.formats.wkt
+import aspose.gis.common.formats.xml
+import aspose.gis.common.io
+import aspose.gis.epsg
+import aspose.gis.formats
+import aspose.gis.formats.bmpw
+import aspose.gis.formats.csv
+import aspose.gis.formats.database
+import aspose.gis.formats.database.dataediting
+import aspose.gis.formats.database.fromdefinition
+import aspose.gis.formats.esriascii
+import aspose.gis.formats.esrijson
+import aspose.gis.formats.filegdb
+import aspose.gis.formats.gdal
+import aspose.gis.formats.geojson
+import aspose.gis.formats.geojsonseq
+import aspose.gis.formats.geopackage
+import aspose.gis.formats.geotiff
+import aspose.gis.formats.gml
+import aspose.gis.formats.gpx
+import aspose.gis.formats.infile
+import aspose.gis.formats.inmemory
+import aspose.gis.formats.jpegw
+import aspose.gis.formats.kml
+import aspose.gis.formats.kml.specificfields
+import aspose.gis.formats.kml.styles
+import aspose.gis.formats.mapinfointerchange
+import aspose.gis.formats.mapinfotab
+import aspose.gis.formats.osmxml
+import aspose.gis.formats.pngw
+import aspose.gis.formats.postgis
+import aspose.gis.formats.shapefile
+import aspose.gis.formats.sqlserver
+import aspose.gis.formats.tiffw
+import aspose.gis.formats.topojson
+import aspose.gis.formats.worldfile
+import aspose.gis.formats.xyztile
+import aspose.gis.geometries
+import aspose.gis.geotools
+import aspose.gis.geotools.extensions
+import aspose.gis.geotools.layersmap
+import aspose.gis.geotools.mapbuilder
+import aspose.gis.geotools.wayanalyzer
+import aspose.gis.imagemetadata
+import aspose.gis.indexing
+import aspose.gis.indexing.bplustree
+import aspose.gis.indexing.qixtree
+import aspose.gis.indexing.rtree
+import aspose.gis.labeling
+import aspose.gis.labeling.line
+import aspose.gis.painting
+import aspose.gis.projections
+import aspose.gis.raster
+import aspose.gis.raster.web
+import aspose.gis.relationship
+import aspose.gis.relationship.joins
+import aspose.gis.rendering
+import aspose.gis.rendering.colorizers
+import aspose.gis.rendering.formats
+import aspose.gis.rendering.formats.bmp
+import aspose.gis.rendering.formats.jpeg
+import aspose.gis.rendering.formats.png
+import aspose.gis.rendering.formats.svg
+import aspose.gis.rendering.labelings
+import aspose.gis.rendering.sld
+import aspose.gis.rendering.symbolizers
+import aspose.gis.spatialreferencing
+import aspose.gis.topology
+import aspose.gis.topology.algorithms
+import aspose.gis.topology.buffer
+import aspose.gis.topology.graph
+
+class WktCompositeValue(WktValue):
+    
+    @overload
+    def equals(self, obj : Any) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, composite : aspose.gis.common.formats.wkt.WktCompositeValue) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, other : aspose.gis.common.formats.wkt.WktValue) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def get_composite_value(self, regular_name : str, other_name : str) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    @overload
+    def get_composite_value(self, other_names : List[str]) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    @overload
+    def get_composite_value(self, index : int) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    @overload
+    def get_composite_value(self, index : int, expected_name : str) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    @overload
+    def try_get_composite_value(self, value_name : str, value : List[aspose.gis.common.formats.wkt.WktCompositeValue]) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def try_get_composite_value(self, index : int, output : List[aspose.gis.common.formats.wkt.WktCompositeValue]) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def try_get_composite_value(self, index : int, expected_name : str, output : List[aspose.gis.common.formats.wkt.WktCompositeValue]) -> bool:
+        raise NotImplementedError()
+    
+    @staticmethod
+    def create(wkt : str) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    def get_hash_code(self) -> int:
+        raise NotImplementedError()
+    
+    def to_string(self) -> str:
+        raise NotImplementedError()
+    
+    def get_string(self, index : int) -> str:
+        raise NotImplementedError()
+    
+    def get_double(self, index : int) -> float:
+        raise NotImplementedError()
+    
+    def get_integer(self, index : int) -> int:
+        raise NotImplementedError()
+    
+    def get_or_default_integer(self, index : int, default_value : int) -> int:
+        raise NotImplementedError()
+    
+    def get_keyword(self, index : int) -> str:
+        raise NotImplementedError()
+    
+    def try_get_double(self, index : int, output : List[Double]) -> bool:
+        raise NotImplementedError()
+    
+    def get_composite_value_index(self, value_name : str) -> int:
+        raise NotImplementedError()
+    
+    @property
+    def parent(self) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    @property
+    def name(self) -> str:
+        raise NotImplementedError()
+    
+    @property
+    def count(self) -> int:
+        raise NotImplementedError()
+    
+    def __getitem__(self, key : int) -> aspose.gis.common.formats.wkt.WktValue:
+        raise NotImplementedError()
+    
+
+class WktDateTime(WktValue):
+    
+    def __init__(self, value : str) -> None:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, obj : Any) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, other : aspose.gis.common.formats.wkt.WktValue) -> bool:
+        raise NotImplementedError()
+    
+    @staticmethod
+    def create(wkt : str) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    def get_hash_code(self) -> int:
+        raise NotImplementedError()
+    
+    def to_string(self) -> str:
+        raise NotImplementedError()
+    
+    @property
+    def parent(self) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    @property
+    def value(self) -> str:
+        raise NotImplementedError()
+    
+
+class WktKeyword(WktValue):
+    
+    def __init__(self, value : str) -> None:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, obj : Any) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, other : aspose.gis.common.formats.wkt.WktValue) -> bool:
+        raise NotImplementedError()
+    
+    @staticmethod
+    def create(wkt : str) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    def get_hash_code(self) -> int:
+        raise NotImplementedError()
+    
+    def to_string(self) -> str:
+        raise NotImplementedError()
+    
+    @property
+    def parent(self) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    @property
+    def value(self) -> str:
+        raise NotImplementedError()
+    
+
+class WktNumber(WktValue):
+    
+    def __init__(self, value : float) -> None:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, obj : Any) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, other : aspose.gis.common.formats.wkt.WktValue) -> bool:
+        raise NotImplementedError()
+    
+    @staticmethod
+    def create(wkt : str) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    def get_hash_code(self) -> int:
+        raise NotImplementedError()
+    
+    def to_string(self) -> str:
+        raise NotImplementedError()
+    
+    @property
+    def parent(self) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    @property
+    def value(self) -> float:
+        raise NotImplementedError()
+    
+
+class WktString(WktValue):
+    
+    def __init__(self, value : str) -> None:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, obj : Any) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, other : aspose.gis.common.formats.wkt.WktValue) -> bool:
+        raise NotImplementedError()
+    
+    @staticmethod
+    def create(wkt : str) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    def get_hash_code(self) -> int:
+        raise NotImplementedError()
+    
+    def to_string(self) -> str:
+        raise NotImplementedError()
+    
+    @property
+    def parent(self) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    @property
+    def value(self) -> str:
+        raise NotImplementedError()
+    
+
+class WktToken:
+    
+    @overload
+    def __init__(self, type : aspose.gis.common.formats.wkt.WktTokenType, text : str, position : int) -> None:
+        raise NotImplementedError()
+    
+    @overload
+    def __init__(self) -> None:
+        raise NotImplementedError()
+    
+    def is_keyword_with_text(self, text : str) -> bool:
+        raise NotImplementedError()
+    
+    def equals(self, other : aspose.gis.common.formats.wkt.WktToken) -> bool:
+        raise NotImplementedError()
+    
+    @property
+    def type(self) -> aspose.gis.common.formats.wkt.WktTokenType:
+        raise NotImplementedError()
+    
+    @property
+    def text(self) -> str:
+        raise NotImplementedError()
+    
+    @property
+    def position(self) -> int:
+        raise NotImplementedError()
+    
+
+class WktTokenizer:
+    
+    def __init__(self, text : str) -> None:
+        raise NotImplementedError()
+    
+    def extract_next_token(self) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def current_token(self) -> aspose.gis.common.formats.wkt.WktToken:
+        raise NotImplementedError()
+    
+
+class WktValue:
+    
+    @overload
+    def equals(self, other : aspose.gis.common.formats.wkt.WktValue) -> bool:
+        raise NotImplementedError()
+    
+    @overload
+    def equals(self, o : Any) -> bool:
+        raise NotImplementedError()
+    
+    @staticmethod
+    def create(wkt : str) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+    def get_hash_code(self) -> int:
+        raise NotImplementedError()
+    
+    def to_string(self) -> str:
+        raise NotImplementedError()
+    
+    @property
+    def parent(self) -> aspose.gis.common.formats.wkt.WktCompositeValue:
+        raise NotImplementedError()
+    
+
+class WktWriter:
+    
+    @overload
+    def write_number(self, number : float) -> None:
+        raise NotImplementedError()
+    
+    @overload
+    def write_number(self, number : int) -> None:
+        raise NotImplementedError()
+    
+    def write_composite_start(self, composite_value_name : str) -> None:
+        raise NotImplementedError()
+    
+    def write_composite_end(self) -> None:
+        raise NotImplementedError()
+    
+    def write_keyword(self, keyword : str) -> None:
+        raise NotImplementedError()
+    
+    def write_string(self, string : str) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def options(self) -> aspose.gis.common.formats.wkt.WktWriterOptions:
+        raise NotImplementedError()
+    
+
+class WktWriterOptions:
+    
+    def __init__(self) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def use_square_brackets(self) -> bool:
+        raise NotImplementedError()
+    
+    @use_square_brackets.setter
+    def use_square_brackets(self, value : bool) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def force_uppercase_keywords(self) -> bool:
+        raise NotImplementedError()
+    
+    @force_uppercase_keywords.setter
+    def force_uppercase_keywords(self, value : bool) -> None:
+        raise NotImplementedError()
+    
+    @property
+    def dispose_writer(self) -> bool:
+        raise NotImplementedError()
+    
+    @dispose_writer.setter
+    def dispose_writer(self, value : bool) -> None:
+        raise NotImplementedError()
+    
+
+class WktTokenType:
+    
+    INVALID : WktTokenType
+    KEYWORD : WktTokenType
+    NUMBER : WktTokenType
+    STRING : WktTokenType
+    DATE_TIME : WktTokenType
+    COMMA : WktTokenType
+    OPEN : WktTokenType
+    CLOSE : WktTokenType
+    SEMI_COLON : WktTokenType
+    EQUALS : WktTokenType
+    EOF : WktTokenType
+
