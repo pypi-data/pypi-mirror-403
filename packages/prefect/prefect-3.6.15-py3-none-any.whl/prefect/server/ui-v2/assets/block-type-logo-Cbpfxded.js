d673e0c5e181fdce5d9cc553b51c97ef73cb08dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 11:07:28 +0000

    Add package prefect/3.6.15/prefect-3.6.15-py3-none-any.whl

---
+++ packages/prefect/prefect-3.6.15-py3-none-any.whl/prefect/server/ui-v2/assets/block-type-logo-Cbpfxded.js
@@ -0,0 +1,2 @@
+import{j as a}from"./vendor-tanstack-BcZfOOfy.js";import{cB as e}from"./index-m48E4KcG.js";const c=({alt:o,logoUrl:s,size:r="sm"})=>a.jsx("img",{alt:o,src:s??void 0,className:i({size:r})}),i=e("border-4 bg-gray-200 rounded",{variants:{size:{sm:"size-8",lg:"size-14"}}});export{c as B};
+//# sourceMappingURL=block-type-logo-Cbpfxded.js.map
