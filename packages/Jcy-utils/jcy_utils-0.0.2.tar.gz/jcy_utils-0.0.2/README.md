69b5c0b56ef94633764a35dc24b63a65653d8a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:06:16 +0000

    Add package Jcy-utils/0.0.2/jcy_utils-0.0.2.tar.gz

---
+++ packages/Jcy-utils/jcy_utils-0.0.2.tar.gz/jcy_utils-0.0.2/README.md
@@ -0,0 +1,8 @@
+# Jcy_utils
+
+A comprehensive Python utility library for image processing, file management, and metric calculations.
+
+## 📦 Installation
+
+```bash
+pip install Jcy_utils
---
+++ packages/Jcy-utils/jcy_utils-0.0.2.tar.gz/jcy_utils-0.0.2/src/Jcy_utils/__init__.py
@@ -0,0 +1,17 @@
+from .core import (
+    split_image_into_four,
+    add_text_to_image,
+    pad_to_square,
+    concat_images_with_padding,
+    concat_images_list_with_padding,
+    concat_images_list_with_padding_rowmax,
+    copy_pyfile,
+    calculate_pr,
+    calculate_pr2,
+    remove_folder,
+    get_name_list,
+    wrap_dir,
+    retry
+)
+
+__version__ = "0.0.1"
