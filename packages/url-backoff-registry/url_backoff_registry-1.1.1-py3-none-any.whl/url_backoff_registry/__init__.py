6ad19b17315751f1dc8ca93874134e7dc356097d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:08:34 +0000

    Add package url-backoff-registry/1.1.1/url_backoff_registry-1.1.1-py3-none-any.whl

---
+++ packages/url-backoff-registry/url_backoff_registry-1.1.1-py3-none-any.whl/url_backoff_registry/__init__.py
@@ -0,0 +1,5 @@
+"""URL backoff registry helpers."""
+
+from url_backoff_registry.registry import BackoffError, BackoffRegistry, BackoffRule, BackoffStats
+
+__all__ = ["BackoffError", "BackoffRegistry", "BackoffRule", "BackoffStats"]
