cc9f6af6ba627eafb1f74e0a47cf3061d1e238f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:07:16 +0000

    Add package aws-solutions-constructs.aws-apigateway-lambda/2.98.0/aws_solutions_constructs_aws_apigateway_lambda-2.98.0-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-apigateway-lambda/aws_solutions_constructs_aws_apigateway_lambda-2.98.0-py3-none-any.whl/aws_solutions_constructs/aws_apigateway_lambda/__init__.py
@@ -0,0 +1,263 @@
+r'''
+Documentation for this pattern can be found [here](https://github.com/awslabs/aws-solutions-constructs/blob/main/source/patterns/%40aws-solutions-constructs/aws-apigateway-lambda/README.adoc)
+'''
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import typeguard
+from importlib.metadata import version as _metadata_package_version
+TYPEGUARD_MAJOR_VERSION = int(_metadata_package_version('typeguard').split('.')[0])
+
+def check_type(argname: str, value: object, expected_type: typing.Any) -> typing.Any:
+    if TYPEGUARD_MAJOR_VERSION <= 2:
+        return typeguard.check_type(argname=argname, value=value, expected_type=expected_type) # type:ignore
+    else:
+        if isinstance(value, jsii._reference_map.InterfaceDynamicProxy): # pyright: ignore [reportAttributeAccessIssue]
+           pass
+        else:
+            if TYPEGUARD_MAJOR_VERSION == 3:
+                typeguard.config.collection_check_strategy = typeguard.CollectionCheckStrategy.ALL_ITEMS # type:ignore
+                typeguard.check_type(value=value, expected_type=expected_type) # type:ignore
+            else:
+                typeguard.check_type(value=value, expected_type=expected_type, collection_check_strategy=typeguard.CollectionCheckStrategy.ALL_ITEMS) # type:ignore
+
+from ._jsii import *
+
+import aws_cdk.aws_apigateway as _aws_cdk_aws_apigateway_ceddda9d
+import aws_cdk.aws_iam as _aws_cdk_aws_iam_ceddda9d
+import aws_cdk.aws_lambda as _aws_cdk_aws_lambda_ceddda9d
+import aws_cdk.aws_logs as _aws_cdk_aws_logs_ceddda9d
+import constructs as _constructs_77d1e7e8
+
+
+class ApiGatewayToLambda(
+    _constructs_77d1e7e8.Construct,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@aws-solutions-constructs/aws-apigateway-lambda.ApiGatewayToLambda",
+):
+    '''
+    :summary: The ApiGatewayToLambda class.
+    '''
+
+    def __init__(
+        self,
+        scope: _constructs_77d1e7e8.Construct,
+        id: builtins.str,
+        *,
+        api_gateway_props: typing.Any = None,
+        create_usage_plan: typing.Optional[builtins.bool] = None,
+        existing_lambda_obj: typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function] = None,
+        lambda_function_props: typing.Optional[typing.Union[_aws_cdk_aws_lambda_ceddda9d.FunctionProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        log_group_props: typing.Optional[typing.Union[_aws_cdk_aws_logs_ceddda9d.LogGroupProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    ) -> None:
+        '''
+        :param scope: - represents the scope for all the resources.
+        :param id: - this is a a scope-unique id.
+        :param api_gateway_props: Optional - user-provided props to override the default props for the API Gateway API. Default: - Default props are used.
+        :param create_usage_plan: Whether to create a Usage Plan attached to the API. Must be true if apiGatewayProps.defaultMethodOptions.apiKeyRequired is true Default: - true (to match legacy behavior)
+        :param existing_lambda_obj: Existing instance of Lambda Function object, providing both this and ``lambdaFunctionProps`` will cause an error. Default: - None
+        :param lambda_function_props: Optional - user provided props to override the default props for the Lambda function. Providing both this and ``existingLambdaObj`` causes an error. Default: - Default props are used.
+        :param log_group_props: User provided props to override the default props for the CloudWatchLogs LogGroup. Default: - Default props are used
+
+        :access: public
+        :since: 0.8.0
+        :summary: Constructs a new instance of the ApiGatewayToLambda class.
+        '''
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__8cee3d89f313102b389deed0cd200d7ab2323cfe0ad8769e94f7167b7806bfaa)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        props = ApiGatewayToLambdaProps(
+            api_gateway_props=api_gateway_props,
+            create_usage_plan=create_usage_plan,
+            existing_lambda_obj=existing_lambda_obj,
+            lambda_function_props=lambda_function_props,
+            log_group_props=log_group_props,
+        )
+
+        jsii.create(self.__class__, self, [scope, id, props])
+
+    @builtins.property
+    @jsii.member(jsii_name="apiGateway")
+    def api_gateway(self) -> _aws_cdk_aws_apigateway_ceddda9d.RestApi:
+        return typing.cast(_aws_cdk_aws_apigateway_ceddda9d.RestApi, jsii.get(self, "apiGateway"))
+
+    @builtins.property
+    @jsii.member(jsii_name="apiGatewayLogGroup")
+    def api_gateway_log_group(self) -> _aws_cdk_aws_logs_ceddda9d.LogGroup:
+        return typing.cast(_aws_cdk_aws_logs_ceddda9d.LogGroup, jsii.get(self, "apiGatewayLogGroup"))
+
+    @builtins.property
+    @jsii.member(jsii_name="lambdaFunction")
+    def lambda_function(self) -> _aws_cdk_aws_lambda_ceddda9d.Function:
+        return typing.cast(_aws_cdk_aws_lambda_ceddda9d.Function, jsii.get(self, "lambdaFunction"))
+
+    @builtins.property
+    @jsii.member(jsii_name="apiGatewayCloudWatchRole")
+    def api_gateway_cloud_watch_role(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_iam_ceddda9d.Role]:
+        return typing.cast(typing.Optional[_aws_cdk_aws_iam_ceddda9d.Role], jsii.get(self, "apiGatewayCloudWatchRole"))
+
+
+@jsii.data_type(
+    jsii_type="@aws-solutions-constructs/aws-apigateway-lambda.ApiGatewayToLambdaProps",
+    jsii_struct_bases=[],
+    name_mapping={
+        "api_gateway_props": "apiGatewayProps",
+        "create_usage_plan": "createUsagePlan",
+        "existing_lambda_obj": "existingLambdaObj",
+        "lambda_function_props": "lambdaFunctionProps",
+        "log_group_props": "logGroupProps",
+    },
+)
+class ApiGatewayToLambdaProps:
+    def __init__(
+        self,
+        *,
+        api_gateway_props: typing.Any = None,
+        create_usage_plan: typing.Optional[builtins.bool] = None,
+        existing_lambda_obj: typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function] = None,
+        lambda_function_props: typing.Optional[typing.Union[_aws_cdk_aws_lambda_ceddda9d.FunctionProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        log_group_props: typing.Optional[typing.Union[_aws_cdk_aws_logs_ceddda9d.LogGroupProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    ) -> None:
+        '''The properties for the ApiGatewayToLambda class.
+
+        :param api_gateway_props: Optional - user-provided props to override the default props for the API Gateway API. Default: - Default props are used.
+        :param create_usage_plan: Whether to create a Usage Plan attached to the API. Must be true if apiGatewayProps.defaultMethodOptions.apiKeyRequired is true Default: - true (to match legacy behavior)
+        :param existing_lambda_obj: Existing instance of Lambda Function object, providing both this and ``lambdaFunctionProps`` will cause an error. Default: - None
+        :param lambda_function_props: Optional - user provided props to override the default props for the Lambda function. Providing both this and ``existingLambdaObj`` causes an error. Default: - Default props are used.
+        :param log_group_props: User provided props to override the default props for the CloudWatchLogs LogGroup. Default: - Default props are used
+        '''
+        if isinstance(lambda_function_props, dict):
+            lambda_function_props = _aws_cdk_aws_lambda_ceddda9d.FunctionProps(**lambda_function_props)
+        if isinstance(log_group_props, dict):
+            log_group_props = _aws_cdk_aws_logs_ceddda9d.LogGroupProps(**log_group_props)
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__9be9eacf3e580c106bf97e65962dc01354d2e8ce46fb5733931b1f0ec7f5a9ef)
+            check_type(argname="argument api_gateway_props", value=api_gateway_props, expected_type=type_hints["api_gateway_props"])
+            check_type(argname="argument create_usage_plan", value=create_usage_plan, expected_type=type_hints["create_usage_plan"])
+            check_type(argname="argument existing_lambda_obj", value=existing_lambda_obj, expected_type=type_hints["existing_lambda_obj"])
+            check_type(argname="argument lambda_function_props", value=lambda_function_props, expected_type=type_hints["lambda_function_props"])
+            check_type(argname="argument log_group_props", value=log_group_props, expected_type=type_hints["log_group_props"])
+        self._values: typing.Dict[builtins.str, typing.Any] = {}
+        if api_gateway_props is not None:
+            self._values["api_gateway_props"] = api_gateway_props
+        if create_usage_plan is not None:
+            self._values["create_usage_plan"] = create_usage_plan
+        if existing_lambda_obj is not None:
+            self._values["existing_lambda_obj"] = existing_lambda_obj
+        if lambda_function_props is not None:
+            self._values["lambda_function_props"] = lambda_function_props
+        if log_group_props is not None:
+            self._values["log_group_props"] = log_group_props
+
+    @builtins.property
+    def api_gateway_props(self) -> typing.Any:
+        '''Optional - user-provided props to override the default props for the API Gateway API.
+
+        :default: - Default props are used.
+        '''
+        result = self._values.get("api_gateway_props")
+        return typing.cast(typing.Any, result)
+
+    @builtins.property
+    def create_usage_plan(self) -> typing.Optional[builtins.bool]:
+        '''Whether to create a Usage Plan attached to the API.
+
+        Must be true if
+        apiGatewayProps.defaultMethodOptions.apiKeyRequired is true
+
+        :default: - true (to match legacy behavior)
+        '''
+        result = self._values.get("create_usage_plan")
+        return typing.cast(typing.Optional[builtins.bool], result)
+
+    @builtins.property
+    def existing_lambda_obj(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function]:
+        '''Existing instance of Lambda Function object, providing both this and ``lambdaFunctionProps`` will cause an error.
+
+        :default: - None
+        '''
+        result = self._values.get("existing_lambda_obj")
+        return typing.cast(typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function], result)
+
+    @builtins.property
+    def lambda_function_props(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_lambda_ceddda9d.FunctionProps]:
+        '''Optional - user provided props to override the default props for the Lambda function.
+
+        Providing both this and ``existingLambdaObj``
+        causes an error.
+
+        :default: - Default props are used.
+        '''
+        result = self._values.get("lambda_function_props")
+        return typing.cast(typing.Optional[_aws_cdk_aws_lambda_ceddda9d.FunctionProps], result)
+
+    @builtins.property
+    def log_group_props(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_logs_ceddda9d.LogGroupProps]:
+        '''User provided props to override the default props for the CloudWatchLogs LogGroup.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("log_group_props")
+        return typing.cast(typing.Optional[_aws_cdk_aws_logs_ceddda9d.LogGroupProps], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "ApiGatewayToLambdaProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "ApiGatewayToLambda",
+    "ApiGatewayToLambdaProps",
+]
+
+publication.publish()
+
+def _typecheckingstub__8cee3d89f313102b389deed0cd200d7ab2323cfe0ad8769e94f7167b7806bfaa(
+    scope: _constructs_77d1e7e8.Construct,
+    id: builtins.str,
+    *,
+    api_gateway_props: typing.Any = None,
+    create_usage_plan: typing.Optional[builtins.bool] = None,
+    existing_lambda_obj: typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function] = None,
+    lambda_function_props: typing.Optional[typing.Union[_aws_cdk_aws_lambda_ceddda9d.FunctionProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    log_group_props: typing.Optional[typing.Union[_aws_cdk_aws_logs_ceddda9d.LogGroupProps, typing.Dict[builtins.str, typing.Any]]] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__9be9eacf3e580c106bf97e65962dc01354d2e8ce46fb5733931b1f0ec7f5a9ef(
+    *,
+    api_gateway_props: typing.Any = None,
+    create_usage_plan: typing.Optional[builtins.bool] = None,
+    existing_lambda_obj: typing.Optional[_aws_cdk_aws_lambda_ceddda9d.Function] = None,
+    lambda_function_props: typing.Optional[typing.Union[_aws_cdk_aws_lambda_ceddda9d.FunctionProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    log_group_props: typing.Optional[typing.Union[_aws_cdk_aws_logs_ceddda9d.LogGroupProps, typing.Dict[builtins.str, typing.Any]]] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
