0234e8878ce27055e4ff0c23504c901aef430981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 19:10:15 +0000

    Add package async-tiff/0.5.0b2/async_tiff-0.5.0b2-pp311-pypy311_pp73-musllinux_1_2_i686.whl

---
+++ packages/async-tiff/async_tiff-0.5.0b2-pp311-pypy311_pp73-musllinux_1_2_i686.whl/async_tiff/__init__.py
@@ -0,0 +1,31 @@
+from . import enums, store
+from ._async_tiff import (
+    TIFF,
+    Array,
+    Colormap,
+    DecoderRegistry,
+    GeoKeyDirectory,
+    ImageFileDirectory,
+    ThreadPool,
+    Tile,
+    ___version,  # noqa: F403 # pyright:ignore[reportAttributeAccessIssue]
+)
+from ._decoder_runtime import Decoder
+from ._input import ObspecInput
+
+__version__: str = ___version()
+
+__all__ = [
+    "enums",
+    "store",
+    "Array",
+    "Colormap",
+    "Decoder",
+    "DecoderRegistry",
+    "GeoKeyDirectory",
+    "ImageFileDirectory",
+    "ThreadPool",
+    "TIFF",
+    "ObspecInput",
+    "Tile",
+]
