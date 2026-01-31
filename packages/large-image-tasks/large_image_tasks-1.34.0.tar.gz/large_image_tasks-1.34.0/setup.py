ffe4a649f7914e50f244b40e10d1f1a62d0e4803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 06:11:20 +0000

    Add package large-image-tasks/1.34.0/large_image_tasks-1.34.0.tar.gz

---
+++ packages/large-image-tasks/large_image_tasks-1.34.0.tar.gz/large_image_tasks-1.34.0/setup.py
@@ -0,0 +1,62 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open('README.rst') as fh:
+    readme = fh.read()
+
+description = 'Girder Worker tasks for Large Image.'
+long_description = readme
+
+
+try:
+    from setuptools_scm import get_version
+
+    version = get_version(root='../..')
+    limit_version = f'>={version}' if '+' not in version and not os.getenv('TOX_ENV_NAME') else ''
+except (ImportError, LookupError):
+    limit_version = ''
+
+setup(
+    name='large-image-tasks',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    license='Apache-2.0',
+    author='Kitware Inc',
+    author_email='kitware@kitware.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        # Packages required by both producer and consumer side installations
+        'girder-worker-utils>=0.8.5',
+    ],
+    extras_require={
+        'girder': [
+            # Dependencies required on the producer (Girder) side.
+            f'large-image-converter{limit_version}',
+            'girder-worker[girder]>=0.6.0',
+        ],
+        'worker': [
+            # Dependencies required on the consumer (Girder Worker) side.
+            f'large-image-converter[sources]{limit_version}',
+            'girder-worker[worker]>=0.6.0',
+        ],
+    },
+    entry_points={
+        'girder_worker_plugins': [
+            'large_image_tasks = large_image_tasks:LargeImageTasks',
+        ],
+    },
+    packages=find_packages(),
+)
