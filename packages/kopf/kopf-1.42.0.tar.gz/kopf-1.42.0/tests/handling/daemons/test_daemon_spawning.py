94651e417b09de8c8b7313380068266bc78f6354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 03:45:27 +0000

    Add package kopf/1.42.0/kopf-1.42.0.tar.gz

---
+++ packages/kopf/kopf-1.42.0.tar.gz/kopf-1.42.0/tests/handling/daemons/test_daemon_spawning.py
@@ -0,0 +1,57 @@
+import asyncio
+import logging
+
+import kopf
+
+
+async def test_daemon_is_spawned_at_least_once(
+        resource, dummy, caplog, assert_logs, k8s_mocked, simulate_cycle, looptime):
+    caplog.set_level(logging.DEBUG)
+    executed = asyncio.Event()
+
+    @kopf.daemon(*resource, id='fn')
+    async def fn(**kwargs):
+        dummy.mock(**kwargs)
+        executed.set()
+
+    await simulate_cycle({})
+    await executed.wait()
+
+    assert looptime == 0
+    assert dummy.mock.call_count == 1  # not restarted
+
+
+async def test_daemon_initial_delay_obeyed(
+        resource, dummy, caplog, assert_logs, k8s_mocked, simulate_cycle, looptime):
+    caplog.set_level(logging.DEBUG)
+    executed = asyncio.Event()
+
+    @kopf.daemon(*resource, id='fn', initial_delay=5.0)
+    async def fn(**kwargs):
+        dummy.mock(**kwargs)
+        executed.set()
+
+    await simulate_cycle({})
+    await executed.wait()
+
+    assert looptime == 5.0
+
+
+async def test_daemon_initial_delay_callable_obeyed(
+        resource, dummy, caplog, assert_logs, k8s_mocked, simulate_cycle, looptime):
+    caplog.set_level(logging.DEBUG)
+    executed = asyncio.Event()
+
+    def get_delay(body, **_):
+        return body.get('spec', {}).get('delay', 0.0)
+
+    @kopf.daemon(*resource, id='fn', initial_delay=get_delay)
+    async def fn(**kwargs):
+        dummy.mock(**kwargs)
+        executed.set()
+
+    await simulate_cycle({'spec': {'delay': 7.0}})
+    await executed.wait()
+
+    assert looptime == 7.0
+    assert dummy.mock.call_count == 1
---
+++ packages/kopf/kopf-1.42.0.tar.gz/kopf-1.42.0/tests/handling/daemons/test_timer_intervals.py
@@ -0,0 +1,45 @@
+import asyncio
+import logging
+
+import kopf
+
+
+async def test_timer_regular_interval(
+        resource, dummy, caplog, assert_logs, k8s_mocked, simulate_cycle, looptime):
+    caplog.set_level(logging.DEBUG)
+    trigger = asyncio.Condition()
+
+    @kopf.timer(*resource, id='fn', interval=1.0, sharp=False)
+    async def fn(**kwargs):
+        dummy.mock(**kwargs)
+        async with trigger:
+            trigger.notify_all()
+        await asyncio.sleep(0.3)  # simulate a slow operation
+
+    await simulate_cycle({})
+    async with trigger:
+        await trigger.wait()
+        await trigger.wait()
+
+    assert dummy.mock.call_count == 2
+    assert looptime == 1.3
+
+
+async def test_timer_sharp_interval(
+        resource, dummy, caplog, assert_logs, k8s_mocked, simulate_cycle, looptime):
+    caplog.set_level(logging.DEBUG)
+    trigger = asyncio.Condition()
+
+    @kopf.timer(*resource, id='fn', interval=1.0, sharp=True)
+    async def fn(**kwargs):
+        dummy.mock(**kwargs)
+        async with trigger:
+            trigger.notify_all()
+        await asyncio.sleep(0.3)  # simulate a slow operation
+
+    await simulate_cycle({})
+    async with trigger:
+        await trigger.wait_for(lambda: dummy.mock.call_count >= 2)
+
+    assert dummy.mock.call_count == 2
+    assert looptime == 1  # not 1.3!
