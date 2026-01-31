c552012191ab758a421572ce5959e7d2651a64b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:18:29 +0000

    Add package image-processor-lite/1.0.3/image_processor_lite-1.0.3.tar.gz

---
+++ packages/image-processor-lite/image_processor_lite-1.0.3.tar.gz/image_processor_lite-1.0.3/imgprocessor/config/__init__.py
@@ -0,0 +1,5 @@
+"""Configuration module for ImageProcessor package."""
+
+from .module_config import ImageProcessorConfig, ModuleConfig
+
+__all__ = ['ImageProcessorConfig', 'ModuleConfig']
---
+++ packages/image-processor-lite/image_processor_lite-1.0.3.tar.gz/image_processor_lite-1.0.3/imgprocessor/text_detection/__init__.py
@@ -0,0 +1,5 @@
+"""Text detection module for ImageProcessor package."""
+
+from .detector import TextDetector, TextRegion
+
+__all__ = ['TextDetector', 'TextRegion']
