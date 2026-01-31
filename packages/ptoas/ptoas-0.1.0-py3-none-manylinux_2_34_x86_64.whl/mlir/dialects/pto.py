69f3439158c51d136272a32b2c46015ea6f103c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:08:11 +0000

    Add package ptoas/0.1.0/ptoas-0.1.0-py3-none-manylinux_2_34_x86_64.whl

---
+++ packages/ptoas/ptoas-0.1.0-py3-none-manylinux_2_34_x86_64.whl/mlir/dialects/pto.py
@@ -0,0 +1,43 @@
+#  Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+#  See https://llvm.org/LICENSE.txt for license information.
+#  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+from ._pto_ops_gen import *
+
+from .._mlir_libs._pto import (
+    register_dialect,
+    PtrType,
+    TensorViewType,
+    TileViewType,
+    TileType,
+    TileBufType,
+    AddressSpace,
+    AddressSpaceAttr,
+    get_gm_type,
+    TileBufConfigAttr,
+    BLayout,
+    BLayoutAttr,
+    SLayout,
+    SLayoutAttr,
+    PadValue,
+    PadValueAttr,
+    RoundMode,
+    RoundModeAttr
+)
+
+__all__ = [
+    # Dialect utilities
+    "register_dialect",
+
+    # Types
+    "PtrType",
+    "TensorViewType",
+    "TileViewType",
+    "TileType",
+    "TileBufType",
+    "AddressSpace", "AddressSpaceAttr",
+    "BLayout","BLayoutAttr",
+    "SLayout","SLayoutAttr",
+    "PadValue","PadValueAttr",
+    "RoundMode", "RoundModeAttr",
+    "get_gm_type", "TileBufConfigAttr"
+]
