a2e51d4917c012ec6f0d497c817ee28c952fafea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 18:07:14 +0000

    Add package coinsignal/1.6.5/coinsignal-1.6.5-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl

---
+++ packages/coinsignal/coinsignal-1.6.5-cp312-cp312-manylinux2014_x86_64.manylinux_2_17_x86_64.whl/coinsignal/__init__.py
@@ -0,0 +1,12 @@
+from . import _ext
+from . import data
+from . import feature
+from . import label
+from . import model
+from . import parallel
+from . import sampler
+from . import tools
+
+__version__ = '1.6.5'
+
+__all__ = ['data', 'feature', 'label', 'model', 'parallel', 'sampler', 'tools']
