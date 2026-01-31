3cd0a5097f4a6f3f19fdb4f584e51acccb7b8e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:07:05 +0000

    Add package aws-solutions-constructs.aws-kinesis-streams-gluejob/2.98.0/aws_solutions_constructs_aws_kinesis_streams_gluejob-2.98.0.tar.gz

---
+++ packages/aws-solutions-constructs.aws-kinesis-streams-gluejob/aws_solutions_constructs_aws_kinesis_streams_gluejob-2.98.0.tar.gz/aws_solutions_constructs_aws_kinesis_streams_gluejob-2.98.0/setup.py
@@ -0,0 +1,64 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "aws-solutions-constructs.aws-kinesis-streams-gluejob",
+    "version": "2.98.0",
+    "description": "CDK Constructs for streaming data from AWS Kinesis Data Stream for Glue ETL custom Job processing",
+    "license": "Apache-2.0",
+    "url": "https://github.com/awslabs/aws-solutions-constructs.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-solutions-constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_solutions_constructs.aws_kinesis_streams_gluejob",
+        "aws_solutions_constructs.aws_kinesis_streams_gluejob._jsii"
+    ],
+    "package_data": {
+        "aws_solutions_constructs.aws_kinesis_streams_gluejob._jsii": [
+            "aws-kinesisstreams-gluejob@2.98.0.jsii.tgz"
+        ],
+        "aws_solutions_constructs.aws_kinesis_streams_gluejob": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.9",
+    "install_requires": [
+        "aws-cdk-lib>=2.234.0, <3.0.0",
+        "aws-solutions-constructs.core==2.98.0",
+        "constructs>=10.0.0, <11.0.0",
+        "jsii>=1.119.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard>=2.13.3,<4.3.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
