d969e5123504f7b9b6c47cf6191fba9d5961e57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:07:15 +0000

    Add package stardag/0.1.0/stardag-0.1.0.tar.gz

---
+++ packages/stardag/stardag-0.1.0.tar.gz/stardag-0.1.0/tests/test_integration/test_prefect/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+try:
+    from prefect.testing.utilities import prefect_test_harness
+except ImportError:
+    # If the user doesn't have prefect installed, we'll provide a dummy harness
+    from contextlib import contextmanager
+
+    @contextmanager
+    def prefect_test_harness():
+        yield
+
+
+@pytest.fixture(autouse=True, scope="session")
+def prefect_test_fixture():
+    with prefect_test_harness():
+        yield
