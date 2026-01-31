d3ef0ca51620c6927550e2ab7f9d6013ed8ed8fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:09:04 +0000

    Add package distributed-a2a/0.1.9rc8/distributed_a2a-0.1.9rc8.tar.gz

---
+++ packages/distributed-a2a/distributed_a2a-0.1.9rc8.tar.gz/distributed_a2a-0.1.9rc8/distributed_a2a/router.py
@@ -0,0 +1,60 @@
+from contextlib import asynccontextmanager
+from typing import Any, AsyncGenerator
+
+from a2a.server.apps import A2ARESTFastAPIApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.types import AgentCard, AgentSkill
+from fastapi import FastAPI
+
+from .executors import RoutingExecutor
+from .model import RouterConfig
+from .registry import AgentRegistryLookup, registry_heart_beat
+from .server import CAPABILITIES, HEART_BEAT_INTERVAL_SEC, get_expire_at
+
+
+def load_router(router_dic: dict[str, Any]) -> FastAPI:
+    router_config: RouterConfig = RouterConfig.model_validate(router_dic)
+    agent_card = AgentCard(
+        name="Router",
+        description="Agent to redirect to the best matching agent based on the agent card",
+        url=router_config.router.card.url,
+        version="1.0.0",
+        default_input_modes=router_config.router.card.default_input_modes,
+        default_output_modes=router_config.router.card.default_output_modes,
+        skills=[AgentSkill(
+            id='routing',
+            name='Agent routing',
+            description='Identifies the most suitable agent for the given task and returns the agent card',
+            tags=['agent', 'routing']
+        )],
+        preferred_transport=router_config.router.card.preferred_transport_protocol,
+        capabilities=CAPABILITIES
+    )
+
+    executor = RoutingExecutor(
+        router_config=router_config,
+        routing_tool=AgentRegistryLookup(router_config.router.registry.agent.url).as_tool()
+    )
+
+    @asynccontextmanager
+    async def lifespan(_: FastAPI) -> AsyncGenerator[None, Any]:
+        import asyncio
+        asyncio.create_task(registry_heart_beat(
+            name=router_config.router.card.name,
+            registry_url=router_config.router.registry.agent.url,
+            agent_card=agent_card,
+            interval_sec=HEART_BEAT_INTERVAL_SEC,
+            get_expire_at=get_expire_at
+        ))
+        yield
+
+    return A2ARESTFastAPIApplication(
+        agent_card=agent_card,
+        http_handler=DefaultRequestHandler(
+            agent_executor=executor,
+            task_store=InMemoryTaskStore()  # TODO replace with dynamodb store
+
+        )).build(title=agent_card.name,
+                 lifespan=lifespan,
+                 root_path=f"/{router_config.router.card.name}")
