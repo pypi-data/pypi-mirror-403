468f2dd5c301fe36da349518588e2c5f8ab6e718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 14:07:29 +0000

    Add package django-session-timeout-2025/0.1.1/django_session_timeout_2025-0.1.1.tar.gz

---
+++ packages/django-session-timeout-2025/django_session_timeout_2025-0.1.1.tar.gz/django_session_timeout_2025-0.1.1/setup.py
@@ -0,0 +1,62 @@
+import re
+
+from setuptools import find_packages, setup
+
+docs_require = ["sphinx>=1.8.4"]
+
+tests_require = [
+    "coverage[toml]==5.0.3",
+    "freezegun==0.3.15",
+    "pytest==5.3.5",
+    "pytest-django==3.8.0",
+    "pytest-cov==2.8.1",\
+    # Linting
+    "isort[pyproject]==4.3.21",
+    "flake8==3.7.9",
+    "flake8-blind-except==0.1.1",
+    "flake8-debugger==3.1.0",
+]
+
+with open("README.md") as fh:
+    long_description = re.sub(
+        "<!-- start-no-pypi -->.*<!-- end-no-pypi -->\n",
+        "",
+        fh.read(),
+        flags=re.M | re.S,
+    )
+
+setup(
+    name="django-session-timeout-2025",
+    version="0.1.1",
+    description="Middleware to expire sessions after specific amount of time",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nunombispo/django-session-timeout-2025",
+    author="Nuno Bispo",
+    author_email="nunombispo@gmail.com",
+    install_requires=["Django>=3.0", "six>=1.12"],
+    tests_require=tests_require,
+    extras_require={"docs": docs_require, "test": tests_require},
+    use_scm_version=True,
+    entry_points={},
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    zip_safe=False,
+)
---
+++ packages/django-session-timeout-2025/django_session_timeout_2025-0.1.1.tar.gz/django_session_timeout_2025-0.1.1/tests/test_middleware.py
@@ -0,0 +1,146 @@
+import pytest
+from django.contrib.sessions.middleware import SessionMiddleware
+from freezegun import freeze_time
+
+from django_session_timeout.middleware import (
+    SESSION_TIMEOUT_KEY,
+    SessionTimeoutMiddleware,
+)
+
+
+@pytest.fixture(scope="function")
+def r(rf):
+    req = rf.get("/")
+    middleware = SessionMiddleware()
+    middleware.process_request(req)
+    req.session["example_key"] = "1"
+
+    req.session.save()
+    yield req
+
+
+def test_session_new(r):
+    middleware = SessionTimeoutMiddleware()
+    assert middleware.process_request(r) is None
+    assert r.session[SESSION_TIMEOUT_KEY]
+
+
+def test_session_new_empty_session(r):
+    r.session.flush()
+
+    middleware = SessionTimeoutMiddleware()
+    assert middleware.process_request(r) is None
+    assert SESSION_TIMEOUT_KEY not in r.session
+
+
+def test_session_expire(r, settings):
+    settings.SESSION_EXPIRE_SECONDS = 3600
+    middleware = SessionTimeoutMiddleware()
+
+    with freeze_time("2017-08-31 21:46:00"):
+        assert middleware.process_request(r) is None
+
+    with freeze_time("2017-08-31 22:45:00"):
+        assert middleware.process_request(r) is None
+
+    with freeze_time("2017-08-31 22:46:01"):
+        response = middleware.process_request(r)
+        assert SESSION_TIMEOUT_KEY not in r.session
+        assert response["location"] == "/accounts/login/?next=/"
+
+
+def test_session_expire_custom_redirect(r, settings):
+    settings.SESSION_EXPIRE_SECONDS = 3600
+    settings.SESSION_TIMEOUT_REDIRECT = "/foobar/"
+    middleware = SessionTimeoutMiddleware()
+
+    with freeze_time("2017-08-31 21:46:00"):
+        assert middleware.process_request(r) is None
+
+    with freeze_time("2017-08-31 22:46:01"):
+        response = middleware.process_request(r)
+        assert response["location"] == "/foobar/"
+
+
+def test_session_expire_no_expire_setting(r, settings):
+    settings.SESSION_COOKIE_AGE = 3600
+    middleware = SessionTimeoutMiddleware()
+
+    with freeze_time("2017-08-31 21:46:00"):
+        assert middleware.process_request(r) is None
+
+    with freeze_time("2017-08-31 22:45:00"):
+        assert middleware.process_request(r) is None
+
+    with freeze_time("2017-08-31 22:46:01"):
+        response = middleware.process_request(r)
+        assert SESSION_TIMEOUT_KEY not in r.session
+        assert response["location"] == "/accounts/login/?next=/"
+
+
+def test_session_expire_last_activity(r, settings):
+    settings.SESSION_COOKIE_AGE = 3600
+    settings.SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+    middleware = SessionTimeoutMiddleware()
+
+    with freeze_time("2017-08-31 20:46:00"):
+        assert middleware.process_request(r) is None
+
+    with freeze_time("2017-08-31 21:45:00"):
+        assert middleware.process_request(r) is None
+
+    with freeze_time("2017-08-31 21:46:01"):
+        assert middleware.process_request(r) is None
+
+    with freeze_time("2017-08-31 23:46:02"):
+        response = middleware.process_request(r)
+        assert SESSION_TIMEOUT_KEY not in r.session
+        assert response["location"] == "/accounts/login/?next=/"
+
+
+def test_session_expire_last_activity_grace_(r, settings):
+    settings.SESSION_COOKIE_AGE = 3600
+    settings.SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+    settings.SESSION_EXPIRE_AFTER_LAST_ACTIVITY_GRACE_PERIOD = 90
+    middleware = SessionTimeoutMiddleware()
+
+    value = None
+
+    with freeze_time("2017-08-31 20:46:00"):
+        assert middleware.process_request(r) is None
+        value = r.session[SESSION_TIMEOUT_KEY]
+
+    with freeze_time("2017-08-31 20:47:00"):
+        assert middleware.process_request(r) is None
+        assert r.session[SESSION_TIMEOUT_KEY] is value
+
+    with freeze_time("2017-08-31 20:47:31"):
+        assert middleware.process_request(r) is None
+        assert r.session[SESSION_TIMEOUT_KEY] is not value
+
+    with freeze_time("2017-08-31 21:47:32"):
+        response = middleware.process_request(r)
+        assert SESSION_TIMEOUT_KEY not in r.session
+        assert response["location"] == "/accounts/login/?next=/"
+
+
+def test_session_expire_last_activity_grace_not_update(r, settings):
+    settings.SESSION_COOKIE_AGE = 3600
+    settings.SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+    settings.SESSION_EXPIRE_AFTER_LAST_ACTIVITY_GRACE_PERIOD = 90
+    middleware = SessionTimeoutMiddleware()
+
+    value = None
+
+    with freeze_time("2017-08-31 20:46:00"):
+        assert middleware.process_request(r) is None
+        value = r.session[SESSION_TIMEOUT_KEY]
+
+    with freeze_time("2017-08-31 20:47:00"):
+        assert middleware.process_request(r) is None
+        assert r.session[SESSION_TIMEOUT_KEY] is value
+
+    with freeze_time("2017-08-31 21:46:01"):
+        response = middleware.process_request(r)
+        assert SESSION_TIMEOUT_KEY not in r.session
+        assert response["location"] == "/accounts/login/?next=/"
