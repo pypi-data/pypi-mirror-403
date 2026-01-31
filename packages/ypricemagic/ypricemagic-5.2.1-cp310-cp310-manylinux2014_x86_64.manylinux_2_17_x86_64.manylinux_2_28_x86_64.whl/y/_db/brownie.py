23cbffd3bc7a2ac040c91fed0063ba724e1358d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:06:43 +0000

    Add package ypricemagic/5.2.1/ypricemagic-5.2.1-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/ypricemagic/ypricemagic-5.2.1-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/y/_db/brownie.py
@@ -0,0 +1,186 @@
+import hashlib
+import json
+from asyncio import Lock
+from collections.abc import Callable, Container
+from functools import lru_cache
+from pathlib import Path
+from sqlite3 import OperationalError
+from typing import Any, Final, Literal, cast, final
+
+import aiosqlite
+from a_sync import SmartProcessingQueue
+from aiosqlite.context import Result
+from brownie._config import CONFIG, _get_data_folder
+from brownie.exceptions import BrownieEnvironmentError
+from brownie.network.contract import _resolve_address  # type: ignore [attr-defined]
+
+SourceKey = Literal[
+    "address",
+    "alias",
+    "paths",
+    "abi",
+    "ast",
+    "bytecode",
+    "compiler",
+    "contractName",
+    "deployedBytecode",
+    "deployedSourceMap",
+    "language",
+    "natspec",
+    "opcodes",
+    "pcMap",
+    "sourceMap",
+    "type",
+]
+
+# TODO: replace these with the typed dicts in brownie >=1.22
+BuildJson = dict[str, Any]
+Sources = dict[SourceKey, Any]
+
+
+SOURCE_KEYS: Final = (
+    "address",
+    "alias",
+    "paths",
+    "abi",
+    "ast",
+    "bytecode",
+    "compiler",
+    "contractName",
+    "deployedBytecode",
+    "deployedSourceMap",
+    "language",
+    "natspec",
+    "opcodes",
+    "pcMap",
+    "sourceMap",
+    "type",
+)
+
+DISCARD_SOURCE_KEYS: Final = (
+    "ast",
+    "bytecode",
+    "coverageMap",
+    "deployedBytecode",
+    "deployedSourceMap",
+    "natspec",
+    "opcodes",
+    "pcMap",
+)
+"""
+To keep our Contract objects smaller, we do not load all contract source data. Many are not relevant for our data analysis type use case.
+
+These keys will not be included in y.Contract object build data. If you need them, consider just using a dank_mids.Contract for your use case.
+"""
+
+
+# C constants
+
+sha1: Final = hashlib.sha1
+
+dumps: Final = json.dumps
+loads: Final = json.loads
+
+sqlite_lock: Final = Lock()
+
+
+@final
+class AsyncCursor:
+    def __init__(self, filename: Path) -> None:
+        self._filename: Final = filename
+        self._db: aiosqlite.Connection | None = None
+        self._connected: bool = False
+        self._execute: Callable[..., Result[aiosqlite.Cursor]] | None = None
+
+    async def connect(self) -> None:
+        """Establish an async connection to the SQLite database"""
+        db = self._db  # must assign before checking to avoid a TypeError below
+        if db is not None:
+            raise RuntimeError("already connected")
+        async with sqlite_lock:
+            if self._db is not None:
+                return
+            self._db = await aiosqlite.connect(self._filename, isolation_level=None)
+            self._execute = self._db.execute
+
+    async def insert(self, table: str, *values: Any) -> None:
+        raise NotImplementedError
+        if self._db is None:
+            await self.connect()
+
+        # Convert any dictionaries/lists to JSON strings before inserting
+        values = [dumps(val) if isinstance(val, (dict, list)) else val for val in values]
+
+        # Prepare the parameter placeholders
+        placeholders = ",".join("?" * len(values))
+        query = f"INSERT OR REPLACE INTO {table} VALUES ({placeholders})"
+
+        # Execute the query and commit the changes
+        async with self._execute(query, values):
+            await self._db.commit()
+
+    async def fetchone(self, cmd: str, *args: Any) -> tuple[Any, ...] | None:
+        if self._db is None:
+            await self.connect()
+        async with sqlite_lock:
+            execute = cast(Callable[..., Result[aiosqlite.Cursor]], self._execute)
+            async with execute(cmd, args) as cursor:
+                row = await cursor.fetchone()
+                if row is None:
+                    return None
+                # Convert any JSON-serialized columns back to their original data structures
+                return tuple(loads(i) if str(i).startswith(("[", "{")) else i for i in row)
+
+
+cur: Final = AsyncCursor(_get_data_folder().joinpath("deployments.db"))
+fetchone: Final = SmartProcessingQueue(cur.fetchone, num_workers=32)
+
+
+@lru_cache(maxsize=None)
+def _get_select_statement() -> str:
+    try:
+        return f"SELECT * FROM chain{CONFIG.active_network['chainid']}"
+    except KeyError:
+        raise BrownieEnvironmentError("Functionality not available in local environment") from None
+
+
+async def _get_deployment(
+    address: str | None = None,
+    alias: str | None = None,
+    skip_source_keys: Container[SourceKey] = cast(Container[SourceKey], DISCARD_SOURCE_KEYS),
+) -> tuple[BuildJson | None, Sources | None]:
+    if address and alias:
+        raise ValueError("Passed both params address and alias, should be only one!")
+    if address:
+        address = _resolve_address(address)
+        where_clause = f" WHERE address='{address}'"
+    elif alias:
+        where_clause = f" WHERE alias='{alias}'"
+
+    try:
+        row = await fetchone(_get_select_statement() + where_clause)
+    except OperationalError:
+        row = None
+    if not row:
+        return None, None
+
+    build_json = dict(zip(SOURCE_KEYS, row))
+    path_map: dict = build_json.pop("paths")  # type: ignore [type-arg]
+
+    sources = {
+        source_key: await __fetch_source_for_hash(val)
+        for val, source_key in path_map.values()
+        if source_key not in skip_source_keys
+    }
+
+    build_json["allSourcePaths"] = {k: v[1] for k, v in path_map.items()}
+    pc_map: dict | None = build_json.get("pcMap")  # type: ignore [type-arg]
+    if pc_map is not None:
+        build_json["pcMap"] = {int(key): pc_map[key] for key in pc_map}
+
+    return build_json, sources
+
+
+async def __fetch_source_for_hash(hashval: str) -> Any:
+    row = await fetchone("SELECT source FROM sources WHERE hash=?", hashval)
+    return cast(tuple[Any, ...], row)[0]
---
+++ packages/ypricemagic/ypricemagic-5.2.1-cp310-cp310-manylinux2014_x86_64.manylinux_2_17_x86_64.manylinux_2_28_x86_64.whl/y/prices/tokenized_fund/piedao.py
@@ -0,0 +1,194 @@
+import logging
+from decimal import Decimal
+
+import a_sync
+from a_sync import cgather
+from brownie import ZERO_ADDRESS
+from multicall import Call
+
+from y import ENVIRONMENT_VARIABLES as ENVS
+from y.classes.common import ERC20
+from y.contracts import has_method
+from y.datatypes import Address, AnyAddressType, Block, UsdPrice, UsdValue
+from y.exceptions import call_reverted
+from y.utils.raw_calls import raw_call
+
+logger = logging.getLogger(__name__)
+
+
+@a_sync.a_sync(default="sync", cache_type="memory")
+async def is_pie(token: AnyAddressType) -> bool:
+    """
+    Check if a given token is a PieDAO token.
+
+    Args:
+        token: The address of the token to check.
+
+    Returns:
+        True if the token is a PieDAO token, False otherwise.
+
+    Example:
+        >>> is_pie("0x1234567890abcdef1234567890abcdef12345678")
+        True
+    """
+    return await has_method(token, "getCap()(uint)", sync=False)
+
+
+@a_sync.a_sync(default="sync")
+async def get_price(
+    pie: AnyAddressType,
+    block: Block | None = None,
+    skip_cache: bool = ENVS.SKIP_CACHE,
+) -> UsdPrice | None:
+    """
+    Get the price of a PieDAO token in USD.
+
+    Args:
+        pie: The address of the PieDAO token.
+        block: The block number to query the price at. Defaults to the latest block.
+        skip_cache: Whether to skip the cache when fetching the price. Defaults to the value of ENVS.SKIP_CACHE.
+
+    Example:
+        >>> get_price("0x1234567890abcdef1234567890abcdef12345678")
+        123.45
+
+    See Also:
+        - :func:`get_tvl`
+        - :class:`ERC20`
+    """
+    tvl, total_supply = await cgather(
+        get_tvl(pie, block, skip_cache=skip_cache),
+        ERC20(pie, asynchronous=True).total_supply_readable(block),
+    )
+    if tvl is None:
+        return None
+    return UsdPrice(tvl / total_supply)
+
+
+async def get_tokens(pie_address: Address, block: Block | None = None) -> list[ERC20] | None:
+    """
+    Get the list of tokens in a PieDAO token.
+
+    Args:
+        pie_address: The address of the PieDAO token.
+        block: The block number to query the tokens at. Defaults to the latest block.
+
+    Returns:
+        A list of :class:`ERC20` objects representing the tokens in the PieDAO token.
+
+    Example:
+        >>> get_tokens("0x1234567890abcdef1234567890abcdef12345678")
+        [ERC20('0xTokenAddress1'), ERC20('0xTokenAddress2')]
+
+    Note:
+        This function retrieves token addresses using a multicall and then creates :class:`ERC20` instances from those addresses.
+    """
+    tokens = await Call(pie_address, "getTokens()(address[])", block_id=block)
+    return None if tokens is None else list(map(ERC20, tokens))
+
+
+async def get_bpool(pie_address: Address, block: Block | None = None) -> Address:
+    """
+    Get the Balancer pool address for a PieDAO token.
+
+    Args:
+        pie_address: The address of the PieDAO token.
+        block: The block number to query the pool at. Defaults to the latest block.
+
+    Returns:
+        The address of the Balancer pool, or the PieDAO address if no pool is found.
+
+    Example:
+        >>> get_bpool("0x1234567890abcdef1234567890abcdef12345678")
+        '0xBpoolAddress'
+    """
+    try:
+        bpool = await raw_call(pie_address, "getBPool()", output="address", block=block, sync=False)
+        return bpool if bpool != ZERO_ADDRESS else pie_address
+    except Exception as e:
+        if not call_reverted(e):
+            raise
+        return pie_address
+
+
+async def get_tvl(
+    pie_address: Address,
+    block: Block | None = None,
+    skip_cache: bool = ENVS.SKIP_CACHE,
+) -> UsdValue | None:
+    """
+    Get the total value locked (TVL) in a PieDAO token in USD.
+
+    Args:
+        pie_address: The address of the PieDAO token.
+        block: The block number to query the TVL at. Defaults to the latest block.
+        skip_cache: Whether to skip the cache when fetching the TVL. Defaults to the value of ENVS.SKIP_CACHE.
+
+    Example:
+        >>> get_tvl("0x1234567890abcdef1234567890abcdef12345678")
+        1000000.00
+
+    See Also:
+        - :func:`get_value`
+        - :func:`get_bpool`
+        - :func:`get_tokens`
+    """
+    tokens: list[ERC20]
+    pool, tokens = await cgather(get_bpool(pie_address, block), get_tokens(pie_address, block))
+    if tokens is None:
+        return None
+    return await a_sync.map(get_value, tokens, bpool=pool, block=block, skip_cache=skip_cache).sum(
+        pop=True, sync=False
+    )
+
+
+async def get_balance(bpool: Address, token: ERC20, block: Block | None = None) -> Decimal:
+    """
+    Get the balance of a token in a Balancer pool.
+
+    Args:
+        bpool: The address of the Balancer pool.
+        token: The :class:`ERC20` token to query.
+        block: The block number to query the balance at. Defaults to the latest block.
+
+    Example:
+        >>> get_balance("0xBpoolAddress", ERC20("0xTokenAddress"))
+        1000.0
+    """
+    balance, scale = await cgather(
+        Call(token.address, ("balanceOf(address)(uint)", bpool), block_id=block),
+        token.__scale__,
+    )
+    return Decimal(balance) / scale
+
+
+async def get_value(
+    bpool: Address,
+    token: ERC20,
+    block: Block | None = None,
+    skip_cache: bool = ENVS.SKIP_CACHE,
+) -> UsdValue:
+    """
+    Get the USD value of a token in a Balancer pool.
+
+    Args:
+        bpool: The address of the Balancer pool.
+        token: The :class:`ERC20` token to query.
+        block: The block number to query the value at. Defaults to the latest block.
+        skip_cache: Whether to skip the cache when fetching the value. Defaults to the value of ENVS.SKIP_CACHE.
+
+    Example:
+        >>> get_value("0xBpoolAddress", ERC20("0xTokenAddress"))
+        500.0
+
+    Note:
+        This function calculates the value by multiplying the token balance in the pool by its price.
+
+    See Also:
+        - :func:`get_balance`
+    """
+    balance, price = await cgather(
+        get_balance(bpool, token, block),
+        token.price(block, skip_cache=skip_cache, sync=False),
+    )
+    return UsdValue(balance * Decimal(price))
