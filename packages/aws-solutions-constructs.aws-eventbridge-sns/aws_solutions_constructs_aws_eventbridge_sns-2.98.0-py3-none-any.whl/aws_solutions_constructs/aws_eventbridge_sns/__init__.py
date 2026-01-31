9a20448b9f913870b6552aa1f3139eadd1597301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 12:07:16 +0000

    Add package aws-solutions-constructs.aws-eventbridge-sns/2.98.0/aws_solutions_constructs_aws_eventbridge_sns-2.98.0-py3-none-any.whl

---
+++ packages/aws-solutions-constructs.aws-eventbridge-sns/aws_solutions_constructs_aws_eventbridge_sns-2.98.0-py3-none-any.whl/aws_solutions_constructs/aws_eventbridge_sns/__init__.py
@@ -0,0 +1,325 @@
+r'''
+Documentation for this pattern can be found [here](https://github.com/awslabs/aws-solutions-constructs/blob/main/source/patterns/%40aws-solutions-constructs/aws-eventbridge-sns/README.adoc)
+'''
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import typeguard
+from importlib.metadata import version as _metadata_package_version
+TYPEGUARD_MAJOR_VERSION = int(_metadata_package_version('typeguard').split('.')[0])
+
+def check_type(argname: str, value: object, expected_type: typing.Any) -> typing.Any:
+    if TYPEGUARD_MAJOR_VERSION <= 2:
+        return typeguard.check_type(argname=argname, value=value, expected_type=expected_type) # type:ignore
+    else:
+        if isinstance(value, jsii._reference_map.InterfaceDynamicProxy): # pyright: ignore [reportAttributeAccessIssue]
+           pass
+        else:
+            if TYPEGUARD_MAJOR_VERSION == 3:
+                typeguard.config.collection_check_strategy = typeguard.CollectionCheckStrategy.ALL_ITEMS # type:ignore
+                typeguard.check_type(value=value, expected_type=expected_type) # type:ignore
+            else:
+                typeguard.check_type(value=value, expected_type=expected_type, collection_check_strategy=typeguard.CollectionCheckStrategy.ALL_ITEMS) # type:ignore
+
+from ._jsii import *
+
+import aws_cdk.aws_events as _aws_cdk_aws_events_ceddda9d
+import aws_cdk.aws_kms as _aws_cdk_aws_kms_ceddda9d
+import aws_cdk.aws_sns as _aws_cdk_aws_sns_ceddda9d
+import constructs as _constructs_77d1e7e8
+
+
+class EventbridgeToSns(
+    _constructs_77d1e7e8.Construct,
+    metaclass=jsii.JSIIMeta,
+    jsii_type="@aws-solutions-constructs/aws-eventbridge-sns.EventbridgeToSns",
+):
+    def __init__(
+        self,
+        scope: _constructs_77d1e7e8.Construct,
+        id: builtins.str,
+        *,
+        event_rule_props: typing.Union[_aws_cdk_aws_events_ceddda9d.RuleProps, typing.Dict[builtins.str, typing.Any]],
+        enable_encryption_with_customer_managed_key: typing.Optional[builtins.bool] = None,
+        encryption_key: typing.Optional[_aws_cdk_aws_kms_ceddda9d.Key] = None,
+        encryption_key_props: typing.Optional[typing.Union[_aws_cdk_aws_kms_ceddda9d.KeyProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        event_bus_props: typing.Optional[typing.Union[_aws_cdk_aws_events_ceddda9d.EventBusProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        existing_event_bus_interface: typing.Optional[_aws_cdk_aws_events_ceddda9d.IEventBus] = None,
+        existing_topic_obj: typing.Optional[_aws_cdk_aws_sns_ceddda9d.Topic] = None,
+        topic_props: typing.Optional[typing.Union[_aws_cdk_aws_sns_ceddda9d.TopicProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    ) -> None:
+        '''
+        :param scope: - represents the scope for all the resources.
+        :param id: - this is a a scope-unique id.
+        :param event_rule_props: User provided eventRuleProps to override the defaults. Default: - None
+        :param enable_encryption_with_customer_managed_key: If no key is provided, this flag determines whether the topic is encrypted with a new CMK or an AWS managed key. This flag is ignored if any of the following are defined: topicProps.masterKey, encryptionKey or encryptionKeyProps. Default: - True if topicProps.masterKey, encryptionKey, and encryptionKeyProps are all undefined.
+        :param encryption_key: An optional, imported encryption key to encrypt the SNS topic with. Default: - None.
+        :param encryption_key_props: Optional user provided properties to override the default properties for the KMS encryption key used to encrypt the SNS topic with. Default: - None
+        :param event_bus_props: Optional - user provided properties to override the default properties when creating a custom EventBus. Setting this value to ``{}`` will create a custom EventBus using all default properties. If neither this nor ``existingEventBusInterface`` is provided the construct will use the default EventBus. Providing both this and ``existingEventBusInterface`` causes an error. Default: - None
+        :param existing_event_bus_interface: Optional - user provided custom EventBus for this construct to use. Providing both this and ``eventBusProps`` causes an error. Default: - None
+        :param existing_topic_obj: Existing instance of SNS Topic object, providing both this and topicProps will cause an error.. Default: - Default props are used
+        :param topic_props: User provided props to override the default props for the SNS Topic. Default: - Default props are used
+
+        :access: public
+        :summary: Constructs a new instance of the EventbridgeToSns class.
+        '''
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__ae837bca1b6fb3f1872dc439ed6193b4a79c7eccc2534070ee97df52fe9ad88d)
+            check_type(argname="argument scope", value=scope, expected_type=type_hints["scope"])
+            check_type(argname="argument id", value=id, expected_type=type_hints["id"])
+        props = EventbridgeToSnsProps(
+            event_rule_props=event_rule_props,
+            enable_encryption_with_customer_managed_key=enable_encryption_with_customer_managed_key,
+            encryption_key=encryption_key,
+            encryption_key_props=encryption_key_props,
+            event_bus_props=event_bus_props,
+            existing_event_bus_interface=existing_event_bus_interface,
+            existing_topic_obj=existing_topic_obj,
+            topic_props=topic_props,
+        )
+
+        jsii.create(self.__class__, self, [scope, id, props])
+
+    @builtins.property
+    @jsii.member(jsii_name="eventsRule")
+    def events_rule(self) -> _aws_cdk_aws_events_ceddda9d.Rule:
+        return typing.cast(_aws_cdk_aws_events_ceddda9d.Rule, jsii.get(self, "eventsRule"))
+
+    @builtins.property
+    @jsii.member(jsii_name="snsTopic")
+    def sns_topic(self) -> _aws_cdk_aws_sns_ceddda9d.Topic:
+        return typing.cast(_aws_cdk_aws_sns_ceddda9d.Topic, jsii.get(self, "snsTopic"))
+
+    @builtins.property
+    @jsii.member(jsii_name="encryptionKey")
+    def encryption_key(self) -> typing.Optional[_aws_cdk_aws_kms_ceddda9d.Key]:
+        return typing.cast(typing.Optional[_aws_cdk_aws_kms_ceddda9d.Key], jsii.get(self, "encryptionKey"))
+
+    @builtins.property
+    @jsii.member(jsii_name="eventBus")
+    def event_bus(self) -> typing.Optional[_aws_cdk_aws_events_ceddda9d.IEventBus]:
+        return typing.cast(typing.Optional[_aws_cdk_aws_events_ceddda9d.IEventBus], jsii.get(self, "eventBus"))
+
+
+@jsii.data_type(
+    jsii_type="@aws-solutions-constructs/aws-eventbridge-sns.EventbridgeToSnsProps",
+    jsii_struct_bases=[],
+    name_mapping={
+        "event_rule_props": "eventRuleProps",
+        "enable_encryption_with_customer_managed_key": "enableEncryptionWithCustomerManagedKey",
+        "encryption_key": "encryptionKey",
+        "encryption_key_props": "encryptionKeyProps",
+        "event_bus_props": "eventBusProps",
+        "existing_event_bus_interface": "existingEventBusInterface",
+        "existing_topic_obj": "existingTopicObj",
+        "topic_props": "topicProps",
+    },
+)
+class EventbridgeToSnsProps:
+    def __init__(
+        self,
+        *,
+        event_rule_props: typing.Union[_aws_cdk_aws_events_ceddda9d.RuleProps, typing.Dict[builtins.str, typing.Any]],
+        enable_encryption_with_customer_managed_key: typing.Optional[builtins.bool] = None,
+        encryption_key: typing.Optional[_aws_cdk_aws_kms_ceddda9d.Key] = None,
+        encryption_key_props: typing.Optional[typing.Union[_aws_cdk_aws_kms_ceddda9d.KeyProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        event_bus_props: typing.Optional[typing.Union[_aws_cdk_aws_events_ceddda9d.EventBusProps, typing.Dict[builtins.str, typing.Any]]] = None,
+        existing_event_bus_interface: typing.Optional[_aws_cdk_aws_events_ceddda9d.IEventBus] = None,
+        existing_topic_obj: typing.Optional[_aws_cdk_aws_sns_ceddda9d.Topic] = None,
+        topic_props: typing.Optional[typing.Union[_aws_cdk_aws_sns_ceddda9d.TopicProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    ) -> None:
+        '''
+        :param event_rule_props: User provided eventRuleProps to override the defaults. Default: - None
+        :param enable_encryption_with_customer_managed_key: If no key is provided, this flag determines whether the topic is encrypted with a new CMK or an AWS managed key. This flag is ignored if any of the following are defined: topicProps.masterKey, encryptionKey or encryptionKeyProps. Default: - True if topicProps.masterKey, encryptionKey, and encryptionKeyProps are all undefined.
+        :param encryption_key: An optional, imported encryption key to encrypt the SNS topic with. Default: - None.
+        :param encryption_key_props: Optional user provided properties to override the default properties for the KMS encryption key used to encrypt the SNS topic with. Default: - None
+        :param event_bus_props: Optional - user provided properties to override the default properties when creating a custom EventBus. Setting this value to ``{}`` will create a custom EventBus using all default properties. If neither this nor ``existingEventBusInterface`` is provided the construct will use the default EventBus. Providing both this and ``existingEventBusInterface`` causes an error. Default: - None
+        :param existing_event_bus_interface: Optional - user provided custom EventBus for this construct to use. Providing both this and ``eventBusProps`` causes an error. Default: - None
+        :param existing_topic_obj: Existing instance of SNS Topic object, providing both this and topicProps will cause an error.. Default: - Default props are used
+        :param topic_props: User provided props to override the default props for the SNS Topic. Default: - Default props are used
+        '''
+        if isinstance(event_rule_props, dict):
+            event_rule_props = _aws_cdk_aws_events_ceddda9d.RuleProps(**event_rule_props)
+        if isinstance(encryption_key_props, dict):
+            encryption_key_props = _aws_cdk_aws_kms_ceddda9d.KeyProps(**encryption_key_props)
+        if isinstance(event_bus_props, dict):
+            event_bus_props = _aws_cdk_aws_events_ceddda9d.EventBusProps(**event_bus_props)
+        if isinstance(topic_props, dict):
+            topic_props = _aws_cdk_aws_sns_ceddda9d.TopicProps(**topic_props)
+        if __debug__:
+            type_hints = typing.get_type_hints(_typecheckingstub__cd3a4fbe328ce3e78ad0dd66198b27fe5366d91c3b7fb76b43b7f099e751d219)
+            check_type(argname="argument event_rule_props", value=event_rule_props, expected_type=type_hints["event_rule_props"])
+            check_type(argname="argument enable_encryption_with_customer_managed_key", value=enable_encryption_with_customer_managed_key, expected_type=type_hints["enable_encryption_with_customer_managed_key"])
+            check_type(argname="argument encryption_key", value=encryption_key, expected_type=type_hints["encryption_key"])
+            check_type(argname="argument encryption_key_props", value=encryption_key_props, expected_type=type_hints["encryption_key_props"])
+            check_type(argname="argument event_bus_props", value=event_bus_props, expected_type=type_hints["event_bus_props"])
+            check_type(argname="argument existing_event_bus_interface", value=existing_event_bus_interface, expected_type=type_hints["existing_event_bus_interface"])
+            check_type(argname="argument existing_topic_obj", value=existing_topic_obj, expected_type=type_hints["existing_topic_obj"])
+            check_type(argname="argument topic_props", value=topic_props, expected_type=type_hints["topic_props"])
+        self._values: typing.Dict[builtins.str, typing.Any] = {
+            "event_rule_props": event_rule_props,
+        }
+        if enable_encryption_with_customer_managed_key is not None:
+            self._values["enable_encryption_with_customer_managed_key"] = enable_encryption_with_customer_managed_key
+        if encryption_key is not None:
+            self._values["encryption_key"] = encryption_key
+        if encryption_key_props is not None:
+            self._values["encryption_key_props"] = encryption_key_props
+        if event_bus_props is not None:
+            self._values["event_bus_props"] = event_bus_props
+        if existing_event_bus_interface is not None:
+            self._values["existing_event_bus_interface"] = existing_event_bus_interface
+        if existing_topic_obj is not None:
+            self._values["existing_topic_obj"] = existing_topic_obj
+        if topic_props is not None:
+            self._values["topic_props"] = topic_props
+
+    @builtins.property
+    def event_rule_props(self) -> _aws_cdk_aws_events_ceddda9d.RuleProps:
+        '''User provided eventRuleProps to override the defaults.
+
+        :default: - None
+        '''
+        result = self._values.get("event_rule_props")
+        assert result is not None, "Required property 'event_rule_props' is missing"
+        return typing.cast(_aws_cdk_aws_events_ceddda9d.RuleProps, result)
+
+    @builtins.property
+    def enable_encryption_with_customer_managed_key(
+        self,
+    ) -> typing.Optional[builtins.bool]:
+        '''If no key is provided, this flag determines whether the topic is encrypted with a new CMK or an AWS managed key.
+
+        This flag is ignored if any of the following are defined: topicProps.masterKey, encryptionKey or encryptionKeyProps.
+
+        :default: - True if topicProps.masterKey, encryptionKey, and encryptionKeyProps are all undefined.
+        '''
+        result = self._values.get("enable_encryption_with_customer_managed_key")
+        return typing.cast(typing.Optional[builtins.bool], result)
+
+    @builtins.property
+    def encryption_key(self) -> typing.Optional[_aws_cdk_aws_kms_ceddda9d.Key]:
+        '''An optional, imported encryption key to encrypt the SNS topic with.
+
+        :default: - None.
+        '''
+        result = self._values.get("encryption_key")
+        return typing.cast(typing.Optional[_aws_cdk_aws_kms_ceddda9d.Key], result)
+
+    @builtins.property
+    def encryption_key_props(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_kms_ceddda9d.KeyProps]:
+        '''Optional user provided properties to override the default properties for the KMS encryption key used to  encrypt the SNS topic with.
+
+        :default: - None
+        '''
+        result = self._values.get("encryption_key_props")
+        return typing.cast(typing.Optional[_aws_cdk_aws_kms_ceddda9d.KeyProps], result)
+
+    @builtins.property
+    def event_bus_props(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_events_ceddda9d.EventBusProps]:
+        '''Optional - user provided properties to override the default properties when creating a custom EventBus.
+
+        Setting
+        this value to ``{}`` will create a custom EventBus using all default properties. If neither this nor
+        ``existingEventBusInterface`` is provided the construct will use the default EventBus. Providing both this and
+        ``existingEventBusInterface`` causes an error.
+
+        :default: - None
+        '''
+        result = self._values.get("event_bus_props")
+        return typing.cast(typing.Optional[_aws_cdk_aws_events_ceddda9d.EventBusProps], result)
+
+    @builtins.property
+    def existing_event_bus_interface(
+        self,
+    ) -> typing.Optional[_aws_cdk_aws_events_ceddda9d.IEventBus]:
+        '''Optional - user provided custom EventBus for this construct to use.
+
+        Providing both this and ``eventBusProps``
+        causes an error.
+
+        :default: - None
+        '''
+        result = self._values.get("existing_event_bus_interface")
+        return typing.cast(typing.Optional[_aws_cdk_aws_events_ceddda9d.IEventBus], result)
+
+    @builtins.property
+    def existing_topic_obj(self) -> typing.Optional[_aws_cdk_aws_sns_ceddda9d.Topic]:
+        '''Existing instance of SNS Topic object, providing both this and topicProps will cause an error..
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("existing_topic_obj")
+        return typing.cast(typing.Optional[_aws_cdk_aws_sns_ceddda9d.Topic], result)
+
+    @builtins.property
+    def topic_props(self) -> typing.Optional[_aws_cdk_aws_sns_ceddda9d.TopicProps]:
+        '''User provided props to override the default props for the SNS Topic.
+
+        :default: - Default props are used
+        '''
+        result = self._values.get("topic_props")
+        return typing.cast(typing.Optional[_aws_cdk_aws_sns_ceddda9d.TopicProps], result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "EventbridgeToSnsProps(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "EventbridgeToSns",
+    "EventbridgeToSnsProps",
+]
+
+publication.publish()
+
+def _typecheckingstub__ae837bca1b6fb3f1872dc439ed6193b4a79c7eccc2534070ee97df52fe9ad88d(
+    scope: _constructs_77d1e7e8.Construct,
+    id: builtins.str,
+    *,
+    event_rule_props: typing.Union[_aws_cdk_aws_events_ceddda9d.RuleProps, typing.Dict[builtins.str, typing.Any]],
+    enable_encryption_with_customer_managed_key: typing.Optional[builtins.bool] = None,
+    encryption_key: typing.Optional[_aws_cdk_aws_kms_ceddda9d.Key] = None,
+    encryption_key_props: typing.Optional[typing.Union[_aws_cdk_aws_kms_ceddda9d.KeyProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    event_bus_props: typing.Optional[typing.Union[_aws_cdk_aws_events_ceddda9d.EventBusProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    existing_event_bus_interface: typing.Optional[_aws_cdk_aws_events_ceddda9d.IEventBus] = None,
+    existing_topic_obj: typing.Optional[_aws_cdk_aws_sns_ceddda9d.Topic] = None,
+    topic_props: typing.Optional[typing.Union[_aws_cdk_aws_sns_ceddda9d.TopicProps, typing.Dict[builtins.str, typing.Any]]] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
+
+def _typecheckingstub__cd3a4fbe328ce3e78ad0dd66198b27fe5366d91c3b7fb76b43b7f099e751d219(
+    *,
+    event_rule_props: typing.Union[_aws_cdk_aws_events_ceddda9d.RuleProps, typing.Dict[builtins.str, typing.Any]],
+    enable_encryption_with_customer_managed_key: typing.Optional[builtins.bool] = None,
+    encryption_key: typing.Optional[_aws_cdk_aws_kms_ceddda9d.Key] = None,
+    encryption_key_props: typing.Optional[typing.Union[_aws_cdk_aws_kms_ceddda9d.KeyProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    event_bus_props: typing.Optional[typing.Union[_aws_cdk_aws_events_ceddda9d.EventBusProps, typing.Dict[builtins.str, typing.Any]]] = None,
+    existing_event_bus_interface: typing.Optional[_aws_cdk_aws_events_ceddda9d.IEventBus] = None,
+    existing_topic_obj: typing.Optional[_aws_cdk_aws_sns_ceddda9d.Topic] = None,
+    topic_props: typing.Optional[typing.Union[_aws_cdk_aws_sns_ceddda9d.TopicProps, typing.Dict[builtins.str, typing.Any]]] = None,
+) -> None:
+    """Type checking stubs"""
+    pass
