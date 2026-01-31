e26c0d28156afd83359e56a9e589d78717136ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 05:18:45 +0000

    Add package pyneedle-spec/0.1.1/pyneedle_spec-0.1.1.tar.gz

---
+++ packages/pyneedle-spec/pyneedle_spec-0.1.1.tar.gz/pyneedle_spec-0.1.1/src/needle/spec/__init__.py
@@ -0,0 +1,18 @@
+# Namespace package support
+__path__ = __import__("pkgutil").extend_path(__path__, __name__)
+
+from .protocols import (
+    SemanticPointerProtocol,
+    PointerSetProtocol,
+    OperatorProtocol,
+    RendererProtocol,
+)
+from .events import GenericEventIR
+
+__all__ = [
+    "SemanticPointerProtocol",
+    "PointerSetProtocol",
+    "OperatorProtocol",
+    "RendererProtocol",
+    "GenericEventIR",
+]
