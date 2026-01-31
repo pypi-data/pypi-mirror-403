8173311bb92f70b34c465cc245cf12cbf490ba38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:08:18 +0000

    Add package quart-db/0.12.0/quart_db-0.12.0-py3-none-any.whl

---
+++ packages/quart-db/quart_db-0.12.0-py3-none-any.whl/quart_db/backends/aiosqlite.py
@@ -0,0 +1,270 @@
+import asyncio
+import json
+from collections.abc import AsyncGenerator
+from sqlite3 import PARSE_DECLTYPES, ProgrammingError
+from types import TracebackType
+from typing import Any
+from urllib.parse import urlsplit
+from uuid import uuid4
+
+import aiosqlite
+
+from ..interfaces import (
+    BackendABC,
+    ConnectionABC,
+    MultipleRowsError,
+    RecordType,
+    TransactionABC,
+    TypeConverters,
+    UndefinedParameterError,
+    ValueType,
+)
+
+try:
+    from typing import LiteralString
+except ImportError:
+    from typing_extensions import LiteralString
+
+DEFAULT_TYPE_CONVERTERS: TypeConverters = {
+    "": {
+        "json": (json.dumps, json.loads, dict),
+    },
+}
+
+
+class Transaction(TransactionABC):
+    def __init__(self, connection: "Connection", *, force_rollback: bool = False) -> None:
+        self._connection = connection
+        self._force_rollback = force_rollback
+        self._savepoints: list[str] = []
+
+    async def __aenter__(self) -> "Transaction":
+        await self.start()
+        return self
+
+    async def __aexit__(self, exc_type: type, exc_value: BaseException, tb: TracebackType) -> None:
+        if self._force_rollback or exc_type is not None:
+            await self.rollback()
+        else:
+            await self.commit()
+
+    async def start(self) -> None:
+        if self._connection._connection.in_transaction:
+            savepoint_name = f"QUART_DB_SAVEPOINT_{uuid4().hex}"
+            await self._connection._connection.execute(f"SAVEPOINT {savepoint_name}")
+            self._savepoints.append(savepoint_name)
+        else:
+            async with self._connection._lock:
+                await self._connection._connection.execute("BEGIN")
+
+    async def commit(self) -> None:
+        if len(self._savepoints):
+            savepoint_name = self._savepoints.pop()
+            await self._connection._connection.execute(f"RELEASE SAVEPOINT {savepoint_name}")
+        else:
+            async with self._connection._lock:
+                await self._connection._connection.execute("COMMIT")
+
+    async def rollback(self) -> None:
+        if len(self._savepoints):
+            savepoint_name = self._savepoints.pop()
+            await self._connection._connection.execute(f"ROLLBACK TO SAVEPOINT {savepoint_name}")
+        else:
+            async with self._connection._lock:
+                await self._connection._connection.execute("ROLLBACK")
+
+
+class Connection(ConnectionABC):
+    supports_advisory_lock = False
+    supports_for_update = False
+
+    def __init__(self, connection: aiosqlite.Connection) -> None:
+        self._connection = connection
+        self._lock = asyncio.Lock()
+
+    async def execute(self, query: LiteralString, values: ValueType | None = None) -> None:
+        try:
+            async with self._lock:
+                await self._connection.execute(query, values)
+        except ProgrammingError as error:
+            raise UndefinedParameterError(str(error))
+
+    async def execute_many(self, query: LiteralString, values: list[ValueType]) -> None:
+        if not values:
+            return
+
+        try:
+            async with self._lock:
+                await self._connection.executemany(query, values)
+        except ProgrammingError as error:
+            raise UndefinedParameterError(str(error))
+
+    async def fetch_all(
+        self,
+        query: LiteralString,
+        values: ValueType | None = None,
+    ) -> list[RecordType]:
+        try:
+            async with self._lock:
+                async with self._connection.execute(query, values) as cursor:
+                    rows = await cursor.fetchall()
+        except ProgrammingError as error:
+            raise UndefinedParameterError(str(error))
+        else:
+            return [{key: row[key] for key in row.keys()} for row in rows]
+
+    async def fetch_first(
+        self,
+        query: LiteralString,
+        values: ValueType | None = None,
+    ) -> RecordType | None:
+        try:
+            async with self._lock:
+                async with self._connection.execute(query, values) as cursor:
+                    row = await cursor.fetchone()
+        except ProgrammingError as error:
+            raise UndefinedParameterError(str(error))
+        else:
+            if row is not None:
+                return {key: row[key] for key in row.keys()}
+            return None
+
+    async def fetch_sole(
+        self,
+        query: LiteralString,
+        values: ValueType | None = None,
+    ) -> RecordType | None:
+        try:
+            async with self._lock:
+                async with self._connection.execute(query, values) as cursor:
+                    rows = await cursor.fetchmany(2)
+        except ProgrammingError as error:
+            raise UndefinedParameterError(str(error))
+        else:
+            if len(rows) > 1:
+                raise MultipleRowsError()
+            elif len(rows) == 1:
+                return {key: rows[0][key] for key in rows[0].keys()}
+            else:
+                return None
+
+    async def fetch_val(
+        self,
+        query: LiteralString,
+        values: ValueType | None = None,
+    ) -> Any | None:
+        try:
+            async with self._lock:
+                async with self._connection.execute(query, values) as cursor:
+                    result = await cursor.fetchone()
+        except ProgrammingError as error:
+            raise UndefinedParameterError(str(error))
+        else:
+            if result is not None:
+                return result[0]
+            return None
+
+    async def iterate(
+        self,
+        query: LiteralString,
+        values: ValueType | None = None,
+    ) -> AsyncGenerator[RecordType, None]:
+        try:
+            async with self._lock:
+                async with self._connection.execute(query, values) as cursor:
+                    async for row in cursor:
+                        yield {key: row[key] for key in row.keys()}
+        except ProgrammingError as error:
+            raise UndefinedParameterError(str(error))
+
+    def transaction(self, *, force_rollback: bool = False) -> "Transaction":
+        return Transaction(self, force_rollback=force_rollback)
+
+
+class Backend(BackendABC):
+    def __init__(self, url: str, options: dict[str, Any], type_converters: TypeConverters) -> None:
+        _, _, path, *_ = urlsplit(url)
+        self._path = path[1:]
+        self._options = options
+        self._connections: set[aiosqlite.Connection] = set()
+        for _, converters in {**DEFAULT_TYPE_CONVERTERS, **type_converters}.items():
+            for typename, (encoder, decoder, pytype) in converters.items():
+                aiosqlite.register_adapter(pytype, encoder)
+                aiosqlite.register_converter(typename, decoder)
+
+    async def connect(self) -> None:
+        pass
+
+    async def disconnect(self, timeout: int | None = None) -> None:
+        tasks = [asyncio.ensure_future(connection.close()) for connection in self._connections]
+        await asyncio.wait_for(asyncio.gather(*tasks), timeout)
+
+    async def acquire(self) -> Connection:
+        connection = aiosqlite.connect(
+            database=self._path,
+            isolation_level=None,
+            detect_types=PARSE_DECLTYPES,
+            **self._options,
+        )
+        await connection.__aenter__()
+        connection.row_factory = aiosqlite.Row
+        self._connections.add(connection)
+        return Connection(connection)
+
+    async def release(self, connection: Connection) -> None:  # type: ignore[override]
+        await connection._connection.__aexit__(None, None, None)
+        self._connections.remove(connection._connection)
+
+    async def _acquire_migration_connection(self) -> Connection:
+        connection = aiosqlite.connect(
+            database=self._path,
+            isolation_level=None,
+        )
+        await connection.__aenter__()
+        connection.row_factory = aiosqlite.Row
+        return Connection(connection)
+
+    async def _release_migration_connection(self, connection: Connection) -> None:  # type: ignore[override]  # noqa: E501
+        await connection._connection.__aexit__(None, None, None)
+
+
+class TestingBackend(BackendABC):
+    def __init__(self, url: str, options: dict[str, Any], type_converters: TypeConverters) -> None:
+        _, _, path, *_ = urlsplit(url)
+        self._path = path[1:]
+        self._options = options
+        for _, converters in {**DEFAULT_TYPE_CONVERTERS, **type_converters}.items():
+            for typename, (encoder, decoder, pytype) in converters.items():
+                aiosqlite.register_adapter(pytype, encoder)
+                aiosqlite.register_converter(typename, decoder)
+
+    async def connect(self) -> None:
+        connection = aiosqlite.connect(
+            database=self._path,
+            isolation_level=None,
+            detect_types=PARSE_DECLTYPES,
+            **self._options,
+        )
+        await connection.__aenter__()
+        self._connection = Connection(connection)
+
+    async def disconnect(self, timeout: int | None = None) -> None:
+        await asyncio.wait_for(self._connection._connection.close(), timeout)
+
+    async def acquire(self) -> Connection:
+        return self._connection
+
+    async def release(self, connection: Connection) -> None:  # type: ignore[override]
+        pass
+
+    async def _acquire_migration_connection(self) -> Connection:
+        connection = aiosqlite.connect(
+            database=self._path,
+            isolation_level=None,
+        )
+        await connection.__aenter__()
+        connection.row_factory = aiosqlite.Row
+        return Connection(connection)
+
+    async def _release_migration_connection(self, connection: Connection) -> None:  # type: ignore[override]  # noqa: E501
+        await connection._connection.__aexit__(None, None, None)
---
+++ packages/quart-db/quart_db-0.12.0-py3-none-any.whl/quart_db/extension.py
@@ -0,0 +1,288 @@
+import asyncio
+from collections import defaultdict
+from collections.abc import AsyncIterator, Callable
+from contextlib import asynccontextmanager
+from pathlib import Path
+from typing import Any
+from urllib.parse import urlsplit, urlunsplit
+
+import click
+from quart import g, Quart
+from quart.cli import pass_script_info, ScriptInfo
+
+from ._migration import ensure_state_table, execute_data_loader, execute_migrations
+from .interfaces import BackendABC, ConnectionABC, TypeConverters
+
+
+class MigrationTimeoutError(Exception):
+    pass
+
+
+class QuartDB:
+    """A QuartDB database instance from which connections can be acquired.
+
+    This can be used to initialise Quart-Schema documentation a given
+    app, either directly,
+
+    .. code-block:: python
+
+        app = Quart(__name__)
+        quart_db = QuartDB(app)
+
+    or via the factory pattern,
+
+    .. code-block:: python
+
+        quart_db = QuartDB()
+
+        def create_app():
+            app = Quart(__name__)
+            quart_db.init_app(app)
+            return app
+
+    It can then be used to establish connections to the database,
+
+    .. code-block:: python
+
+        async with quart_db.connection() as connection:
+            await connection.execute("SELECT 1")
+
+    Arguments:
+        app: The app to associate this instance with, can be None if
+             using the factory pattern.
+        url: The URL to use to connect to the database, can be None
+             and QUART_DB_DATABASE_URL used instead.
+        migrations_folder: Location of migrations relative to the
+             app's root path, defaults to "migrations".
+        data_path: Location of any initial data relative to the apps'
+             root path. Can be None.
+        auto_request_connection: If True (the default) a connection
+             is acquired and placed on g for each request.
+        backend_options: Options to pass directly to the backend
+             engines. Will depend on the backend used.
+        test_connection_options: Options to pass directly to the
+             connection used for testing. Will depend on the
+             backend used.
+        state_table_name: The name of the table used to store the
+             migration status.
+    """
+
+    def __init__(
+        self,
+        app: Quart | None = None,
+        *,
+        url: str | None = None,
+        migrations_folder: str | None = "migrations",
+        data_path: str | None = None,
+        auto_request_connection: bool = True,
+        backend_options: dict[str, Any] | None = None,
+        test_connection_options: dict[str, Any] | None = None,
+        migration_timeout: float | None = None,
+        state_table_name: str | None = None,
+    ) -> None:
+        self._close_timeout = 5  # Seconds
+        self._url = url
+        self._backend_options = backend_options
+        if self._backend_options is None:
+            self._backend_options = {}
+        self._test_connection_options = test_connection_options
+        if self._test_connection_options is None:
+            self._test_connection_options = {}
+        self._backend: BackendABC | None = None
+        self._type_converters: TypeConverters = defaultdict(dict)
+        self._migrations_folder = migrations_folder
+        self._migration_timeout = migration_timeout
+        self._data_path = data_path
+        self._auto_request_connection = auto_request_connection
+        self._state_table_name = state_table_name
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app: Quart) -> None:
+        app.extensions.setdefault("QUART_DB", []).append(self)
+
+        if self._url is None:
+            self._url = app.config["QUART_DB_DATABASE_URL"]
+        if self._migrations_folder is None:
+            self._migrations_folder = app.config.get("QUART_DB_MIGRATIONS_FOLDER")
+        if self._data_path is None:
+            self._data_path = app.config.get("QUART_DB_DATA_PATH")
+        if self._migration_timeout is None:
+            self._migration_timeout = app.config.get("QUART_DB_MIGRATION_TIMEOUT", 60)
+        if self._state_table_name is None:
+            self._state_table_name = app.config.get("QUART_DB_STATE_TABLE_NAME", "schema_migration")
+        self._root_path = Path(app.root_path)
+        self._testing = app.testing and app.config.get("QUART_DB_TESTING", None)
+
+        if app.config["PROPAGATE_EXCEPTIONS"] is None:
+            # Ensure exceptions aren't propagated so as to ensure
+            # connections are released.
+            app.config["PROPAGATE_EXCEPTIONS"] = False
+
+        app.before_serving(self.before_serving)
+        app.after_serving(self.after_serving)
+
+        if app.config.get("QUART_DB_AUTO_REQUEST_CONNECTION", self._auto_request_connection):
+            app.before_request(self.before_request)
+            app.teardown_request(self.teardown_request)
+
+        app.cli.add_command(_migrate_command)
+        app.cli.add_command(_schema_command)
+
+        self._app = app
+
+    async def before_serving(self) -> None:
+        self._backend = self._create_backend()
+
+        if self._migrations_folder is not None or self._data_path is not None:
+            try:
+                await asyncio.wait_for(self.migrate(), timeout=self._migration_timeout)
+            except asyncio.TimeoutError:
+                raise MigrationTimeoutError()
+        await self._backend.connect()
+
+    async def after_serving(self) -> None:
+        await self._backend.disconnect(self._close_timeout)
+
+    async def before_request(self) -> None:
+        g.connection = await self.acquire()
+
+    async def teardown_request(self, _exception: BaseException | None) -> None:
+        if getattr(g, "connection", None) is not None:
+            await self.release(g.connection)
+        g.connection = None
+
+    async def migrate(self) -> None:
+        await ensure_state_table(self._backend, self._state_table_name)
+
+        if self._migrations_folder is not None:
+            migrations_folder = self._root_path / self._migrations_folder
+            await execute_migrations(self._backend, migrations_folder, self._state_table_name)
+
+        if self._data_path is not None:
+            data_path = self._root_path / self._data_path
+            await execute_data_loader(self._backend, data_path, self._state_table_name)
+
+    @asynccontextmanager
+    async def connection(self) -> AsyncIterator[ConnectionABC]:
+        """Acquire a connection to the database.
+
+        This should be used in an async with block as so,
+
+        .. code-block:: python
+
+            async with quart_db.connection() as connection:
+                await connection.execute("SELECT 1")
+
+        """
+        conn = await self.acquire()
+        try:
+            yield conn
+        finally:
+            await self.release(conn)
+
+    async def acquire(self) -> ConnectionABC:
+        """Acquire a connection to the database.
+
+        Don't forget to release it after usage,
+
+        .. code-block::: python
+
+            connection = await quart_db.acquire()
+            await connection.execute("SELECT 1")
+            await quart_db.release(connection)
+        """
+        return await self._backend.acquire()
+
+    async def release(self, connection: ConnectionABC) -> None:
+        """Release a connection to the database.
+
+        This should be used with :meth:`acquire`,
+
+        .. code-block::: python
+
+            connection = await quart_db.acquire()
+            await connection.execute("SELECT 1")
+            await quart_db.release(connection)
+        """
+        await self._backend.release(connection)
+
+    def set_converter(
+        self,
+        typename: str,
+        encoder: Callable,
+        decoder: Callable,
+        *,
+        pytype: type | None = None,
+        schema: str = "public",
+    ) -> None:
+        """Set the type converter
+
+        This allows postgres and python types to be converted between
+        one another.
+
+        Arguments:
+            typename: The postgres name for the type.
+            encoder: A callable that takes the Python type and encodes it
+                into data postgres understands.
+            decoder: A callable that takes the postgres data and decodes
+                it into a Python type.
+            pytype: Optional Python type, required for SQLite.
+            schema: Optional Postgres schema, defaults to "public".
+        """
+        self._type_converters[schema][typename] = (encoder, decoder, pytype)
+
+    def _create_backend(self) -> BackendABC:
+        scheme, *parts = urlsplit(self._url)
+        if scheme.startswith(("postgresql", "postgres")):
+            if scheme.endswith("psycopg"):
+                from .backends.psycopg import Backend, TestingBackend
+            else:
+                from .backends.asyncpg import Backend, TestingBackend  # type: ignore
+            scheme = "postgresql"
+            url = urlunsplit((scheme, *parts))
+        elif scheme == "sqlite":
+            from .backends.aiosqlite import Backend, TestingBackend  # type: ignore
+
+            url = self._url
+        else:
+            raise ValueError(f"{scheme} is not a supported backend")
+
+        if self._testing:
+            return TestingBackend(url, self._test_connection_options, self._type_converters)
+        else:
+            return Backend(url, self._backend_options, self._type_converters)
+
+
+@click.command("db-schema")
+@click.option(
+    "--output",
+    "-o",
+    default="quart_db_schema.png",
+    type=click.Path(),
+    help="Output the schema diagram to a file given by a path.",
+)
+@pass_script_info
+def _schema_command(info: ScriptInfo, output: str | None) -> None:
+    app = info.load_app()
+
+    try:
+        from eralchemy2 import render_er  # type: ignore
+    except ImportError:
+        click.echo("Quart-DB needs to be installed with the erdiagram extra")
+    else:
+        render_er(app.config["QUART_DB_DATABASE_URL"], output, exclude_tables=["schema_migration"])
+
+
+@click.command("db-migrate")
+@pass_script_info
+def _migrate_command(info: ScriptInfo) -> None:
+    app = info.load_app()
+
+    async def _inner() -> None:
+        for extension in app.extensions["QUART_DB"]:
+            extension._backend = extension._create_backend()
+            await extension.migrate(force_foreground=True)
+
+    asyncio.run(_inner())
+    click.echo("Quart-DB migration complete")
