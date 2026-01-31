876bf19b3f9363717915d9cf5a3febc20dbd021c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 16:08:17 +0000

    Add package fastMONAI/0.5.4/fastmonai-0.5.4-py3-none-any.whl

---
+++ packages/fastMONAI/fastmonai-0.5.4-py3-none-any.whl/fastMONAI/vision_all.py
@@ -0,0 +1,13 @@
+#imports all of the functions and classes from the fastMONAI library
+from .vision_core import *
+from .vision_data import *
+from .vision_augmentation import *
+from .vision_loss import *
+from .vision_metrics import *
+from .vision_inference import *
+from .vision_patch import *
+from .utils import *
+from .external_data import *
+from .dataset_info import *
+
+from fastai.vision.all import *
