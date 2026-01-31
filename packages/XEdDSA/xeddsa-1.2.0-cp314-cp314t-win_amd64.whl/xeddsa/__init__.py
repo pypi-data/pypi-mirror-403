23a5bf360cd7e772a5cc5f9afd998298b7e568b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 08:06:17 +0000

    Add package XEdDSA/1.2.0/xeddsa-1.2.0-cp314-cp314t-win_amd64.whl

---
+++ packages/XEdDSA/xeddsa-1.2.0-cp314-cp314t-win_amd64.whl/xeddsa/__init__.py
@@ -0,0 +1,29 @@
+from .version import __version__ as __version__
+
+from .bindings import (
+    Priv as Priv, PRIV_SIZE as PRIV_SIZE,
+    Seed as Seed, SEED_SIZE as SEED_SIZE,
+    Curve25519Pub as Curve25519Pub, CURVE_25519_PUB_SIZE as CURVE_25519_PUB_SIZE,
+    Ed25519Pub as Ed25519Pub, ED_25519_PUB_SIZE as ED_25519_PUB_SIZE,
+    Ed25519Signature as Ed25519Signature, ED_25519_SIGNATURE_SIZE as ED_25519_SIGNATURE_SIZE,
+    Nonce as Nonce, NONCE_SIZE as NONCE_SIZE,
+    SharedSecret as SharedSecret, SHARED_SECRET_SIZE as SHARED_SECRET_SIZE,
+
+    XEdDSAException as XEdDSAException,
+
+    ed25519_priv_sign as ed25519_priv_sign,
+    ed25519_seed_sign as ed25519_seed_sign,
+    ed25519_verify as ed25519_verify,
+
+    curve25519_pub_to_ed25519_pub as curve25519_pub_to_ed25519_pub,
+    ed25519_pub_to_curve25519_pub as ed25519_pub_to_curve25519_pub,
+
+    priv_to_curve25519_pub as priv_to_curve25519_pub,
+    priv_to_ed25519_pub as priv_to_ed25519_pub,
+    seed_to_ed25519_pub as seed_to_ed25519_pub,
+
+    priv_force_sign as priv_force_sign,
+    seed_to_priv as seed_to_priv,
+
+    x25519 as x25519
+)
