116a777d785fe3d64f983360eb21b32598cdcbd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:07:57 +0000

    Add package invenio-github/5.0.0/invenio_github-5.0.0-py2.py3-none-any.whl

---
+++ packages/invenio-github/invenio_github-5.0.0-py2.py3-none-any.whl/invenio_github/utils.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2023 CERN.
+# Copyright (C) 2026 Graz University of Technology.
+#
+# Invenio is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# Invenio is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Invenio; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Various utility functions."""
+
+from datetime import datetime, timezone
+
+import dateutil.parser
+import six
+from werkzeug.utils import import_string
+
+
+def utcnow():
+    """UTC timestamp (with timezone)."""
+    return datetime.now(tz=timezone.utc)
+
+
+def iso_utcnow():
+    """UTC ISO8601 formatted timestamp."""
+    return utcnow().isoformat()
+
+
+def parse_timestamp(x):
+    """Parse ISO8601 formatted timestamp."""
+    dt = dateutil.parser.parse(x)
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=timezone.utc)
+    return dt
+
+
+def obj_or_import_string(value, default=None):
+    """Import string or return object.
+
+    :params value: Import path or class object to instantiate.
+    :params default: Default object to return if the import fails.
+    :returns: The imported object.
+    """
+    if isinstance(value, six.string_types):
+        return import_string(value)
+    elif value:
+        return value
+    return default
