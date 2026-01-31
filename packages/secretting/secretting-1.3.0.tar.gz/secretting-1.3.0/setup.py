8116062bc6a41b9fe136b1bef056317e25d5a429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 13:20:00 +0000

    Add package secretting/1.3.0/secretting-1.3.0.tar.gz

---
+++ packages/secretting/secretting-1.3.0.tar.gz/secretting-1.3.0/setup.py
@@ -0,0 +1,36 @@
+from io import open
+from setuptools import setup, find_packages
+
+name = 'secretting'
+version = '1.3.0'
+
+
+setup(
+    name=name,
+    version=version,
+    description='More secrets and randoms!',
+    long_description='''# More secrets and randoms!
+# Installation:
+```bash
+pip install secretting
+```
+## If wont work:
+```bash
+pip3 install secretting
+```
+## Or:
+```bash
+pip install --upgrade secretting
+```
+
+# Example:
+```python
+salt() # Returns random salt
+sha256("a") # Output: ca978...
+secure256("a") # Output: ("generated hash", "generated salt")
+# And more nice tools!
+```''',
+    long_description_content_type='text/markdown',
+    install_requires=[],
+    packages=find_packages(),
+)
