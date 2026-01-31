db483f64c652e1a0f6f726af416e547cfc96e683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2026-01-31 15:07:55 +0000

    Add package pulser-pasqal/0.20.8/pulser_pasqal-0.20.8-py3-none-any.whl

---
+++ packages/pulser-pasqal/pulser_pasqal-0.20.8-py3-none-any.whl/pulser_pasqal/__init__.py
@@ -0,0 +1,39 @@
+# Copyright 2022 Pulser Development Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Classes for interfacing with Pasqal backends."""
+
+from pasqal_cloud import BaseConfig, EmulatorType, Endpoints  # noqa: F401
+
+from pulser_pasqal._version import __version__ as __version__
+
+from pulser_pasqal.backends import (
+    EmuFreeBackend,
+    EmuFreeBackendV2,
+    EmuMPSBackend,
+    EmuTNBackend,
+    EmuSVBackend,
+)
+
+from pulser_pasqal.ovh import OVHConnection
+from pulser_pasqal.pasqal_cloud import PasqalCloud
+
+__all__ = [
+    "EmuFreeBackend",
+    "EmuFreeBackendV2",
+    "EmuTNBackend",
+    "EmuSVBackend",
+    "PasqalCloud",
+    "EmuMPSBackend",
+    "OVHConnection",
+]
